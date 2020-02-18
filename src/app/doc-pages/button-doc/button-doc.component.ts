4b7cf9eadc8f8509b63a5918240a0b61048b59ff
Fride Skarseth <fs@computas.com>
2020-02-18 09:37:12 +0100

    Fixed routing

---
+++ src/app/doc-pages/button-doc/button-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-doc',
+  templateUrl: './button-doc.component.html',
+  styleUrls: ['./button-doc.component.scss']
+})
+export class ButtonDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/doc-pages/button-doc/button-doc.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonDocComponent } from './button-doc.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ButtonDocComponent]
+})
+export class ButtonDocModule { }
