fc57694d74f20a98e8f3f88cb6c5fc3e21c3e414
Fride Skarseth <fs@computas.com>
2020-02-18 08:43:42 +0100

    Made shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarModule } from './shell/navbar/navbar.module';
+import { HeaderModule } from './shell/header/header.module';
+import { MainModule } from './shell/main/main.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HeaderModule,
+    NavbarModule,
+    MainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shell/main/main.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './main.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MainComponent],
+  exports: [MainComponent]
+})
+export class MainModule { }
