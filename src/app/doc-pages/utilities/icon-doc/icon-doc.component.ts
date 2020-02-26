7a370cf744521afdfcc90e844e7b60493188aa71
Michael Pande <michaelpande@gmail.com>
2020-02-26 12:05:47 +0100

    Added basic support for custom icons

--- src/app/doc-pages/utilities/icon-doc/icon-doc.component.ts
+++ src/app/doc-pages/utilities/icon-doc/icon-doc.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-icon-doc',
-  templateUrl: './icon-doc.component.html', 
+  templateUrl: './icon-doc.component.html',
 })
 export class IconDocComponent implements OnInit {
 
@@ -10,13 +10,19 @@ export class IconDocComponent implements OnInit {
   componentStatus = '';
   componentClasses = ['.e-icon'];
 
-  example = `<i class="e-icon-clock"></i>
-<i class="e-icon-clock e-icon-xl"></i>
+  example = `<i class="e-icon-clock e-icon-xl"></i>
 <i class="e-icon-clock e-icon-lg"></i>
 <i class="e-icon-clock e-icon-md"></i>
 <i class="e-icon-clock e-icon-sm"></i>
-<i class="e-icon-clock e-icon-xs"></i>`
+<i class="e-icon-clock e-icon-xs"></i>`;
 
+example2 = `<i class="e-icon-custom e-icon-xl">
+  <img src="assets/icons/example-custom-icon.svg"></img>
+</i>
+
+<i class="e-icon-custom">
+  <img src="assets/icons/example-custom-icon.svg"></img>
+</i>`;
   constructor() { }
 
   ngOnInit() {
