a63d2d5f19e7c1a3efba26b6327167a2c543691d
Fride Skarseth <fs@computas.com>
2020-02-28 10:12:54 +0100

    Fixed typography

--- src/app/doc-pages/utilities/typography-doc/typography-doc.component.ts
+++ src/app/doc-pages/utilities/typography-doc/typography-doc.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypographyDocComponent implements OnInit {
 
+  componentClasses = ['e-title', 'e-text', 'e-label'];
+
+  example1 = `<h1 class="e-title-large">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h1>
+<h2 class="e-title-medium">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h2>
+<h3 class="e-title-small">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h3>`;
+  example2 = `<p class="e-text-lead">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-text-quote">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-text-caps">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-text-body">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-text-description">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-text-info">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>`;
+  example3 = `<p class="e-label-input">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+<p class="e-label-option">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>`;
+
   constructor() { }
 
   ngOnInit() {
