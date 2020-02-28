8d4ad331d4c8f5ace28fdc13d194a9a681677552
Fride Skarseth <fs@computas.com>
2020-02-28 16:21:48 +0100

    Added header to accessibility

--- src/app/doc-pages/accessibility/accessibility/accessibility.module.ts
+++ src/app/doc-pages/accessibility/accessibility/accessibility.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccessibilityComponent } from './accessibility.component';
+import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    ComponentHeaderModule
   ],
   declarations: [AccessibilityComponent]
 })
