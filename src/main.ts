0efd76950ab99698c344eb605649a327889393e1
frideska <frideskarz28@gmail.com>
2020-03-01 21:24:00 +0100

    Added polyfills + ponyfill(css-variables) for IE11 support

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import cssVars from 'css-vars-ponyfill';
+
+cssVars({
+  include: 'style',
+  onlyLegacy: true,
+  watch: true,
+  onComplete(cssText, styleNode, cssVariables) {
+    console.log(cssText);
+  },
+});
+
 if (environment.production) {
   enableProdMode();
 }
