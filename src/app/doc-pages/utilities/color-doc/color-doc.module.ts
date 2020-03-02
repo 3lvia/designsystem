e27b6dbc1d4ba0ebe5ec234da91be78ecbfceb5e
Fride Skarseth <fs@computas.com>
2020-03-02 11:40:01 +0100

    Added guidelines for colors

--- src/app/doc-pages/utilities/color-doc/color-doc.module.ts
+++ src/app/doc-pages/utilities/color-doc/color-doc.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorDocComponent } from './color-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
+import { DoDontModule } from 'src/app/shared/do-dont/do-dont.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ComponentHeaderModule
+    ComponentHeaderModule,
+    DoDontModule
   ],
   declarations: [ColorDocComponent]
 })
