567ccb9a1803fa70376c295f10a8f6a2dce2f338
Martin Espeland <m.espeland89@gmail.com>
2020-03-05 17:46:25 +0100

    fixes and cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule,
     BrowserAnimationsModule,
