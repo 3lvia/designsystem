31ef8abd8580727cf82b7de734bd3755d84b11d3
Fride Skarseth <fs@computas.com>
2020-03-06 10:12:46 +0100

    moved tabs into header

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -3,7 +3,6 @@ import { HeaderModule } from './header/header.module';
 import { HomeModule } from './home/home.module';
 import { MainModule } from './main/main.module';
 import { NavbarModule } from './navbar/navbar.module';
-import { HeaderTabsModule } from './header-tabs/header-tabs.module';
 import { NavbarAccessibilityModule } from './navbar-accessibility/navbar-accessibility.module';
 import { NavbarComponentsModule } from './navbar-components/navbar-components.module';
 import { NavbarUtilitiesModule } from './navbar-utilities/navbar-utilities.module';
@@ -11,7 +10,6 @@ import { NavbarUtilitiesModule } from './navbar-utilities/navbar-utilities.modul
 @NgModule({
   imports: [
     HeaderModule,
-    HeaderTabsModule,
     HomeModule,
     MainModule,
     NavbarModule,
