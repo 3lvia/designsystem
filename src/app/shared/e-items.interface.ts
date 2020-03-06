6707d1f2d039c703a3dad118a1d0e6162d8c288d
Martin Espeland <m.espeland89@gmail.com>
2020-03-06 12:15:54 +0100

    Update e-items.interface.ts

--- src/app/shared/e-items.interface.ts
+++ src/app/shared/e-items.interface.ts
@@ -2,7 +2,7 @@ export interface EItems {
     title: string;
     description: string;
     absolutePath?: string;
-    actionText?: string;
+    actionText: string;
     docUrl?: string;
     externalUrl?: string;
     status?: string;
