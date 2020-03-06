9901a13c8440f714f0d6338aa3bba74ea25bb4c0
Martin Espeland <m.espeland89@gmail.com>
2020-03-06 12:37:37 +0100

    SlideIn animation for navbars

--- src/app/shell/navbar/navbar.component.ts
+++ src/app/shell/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { EItems } from 'src/app/shared/e-items.interface';
+import { slideIn } from 'src/app/shared/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [ slideIn ]
 })
 export class NavbarComponent implements OnInit {
 
