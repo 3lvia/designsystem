747876cce766e3b63b7fbd0bcddeec9ada6b949e
Martin Espeland <m.espeland89@gmail.com>
2020-03-10 16:16:41 +0100

    Update overview-accessibility.spec.ts

--- cypress/integration/navigation/overview-pages/overview-accessibility.spec.ts
+++ cypress/integration/navigation/overview-pages/overview-accessibility.spec.ts
@@ -1,30 +1,31 @@
-describe('Elvis accessibility overview page navigation Test', () => {
+// TODO: Implement when section is ready
+// describe('Elvis accessibility overview page navigation Test', () => {
 
-    beforeEach(() => {
-        cy.visit('http://localhost:4200/accessibility');
-    });
+//     beforeEach(() => {
+//         cy.visit('http://localhost:4200/accessibility');
+//     });
 
-    it('Navigates to Accessibility doc page when clicking card link', () => {
-        cy.get('.e-card_title').contains('Accessibility')
-            .parent('.e-card_header_text')
-            .parent('.e-card_header')
-            .siblings('.e-card_actions').children().contains('View requirements').click();
-        cy.url().should('eq', 'http://localhost:4200/accessibility/accessibility-doc');
-    });
+//     it('Navigates to Accessibility doc page when clicking card link', () => {
+//         cy.get('.e-card_title').contains('Accessibility')
+//             .parent('.e-card_header_text')
+//             .parent('.e-card_header')
+//             .siblings('.e-card_actions').children().contains('View requirements').click();
+//         cy.url().should('eq', 'http://localhost:4200/accessibility/accessibility-doc');
+//     });
 
-    it('Navigates to Components doc page when clicking card link', () => {
-        cy.get('.e-card_title').contains('Alt-texts')
-            .parent('.e-card_header_text')
-            .parent('.e-card_header')
-            .siblings('.e-card_actions').children().contains('View requirements').click();
-        cy.url().should('eq', 'http://localhost:4200/accessibility/alttext-doc');
-    });
+//     it('Navigates to Components doc page when clicking card link', () => {
+//         cy.get('.e-card_title').contains('Alt-texts')
+//             .parent('.e-card_header_text')
+//             .parent('.e-card_header')
+//             .siblings('.e-card_actions').children().contains('View requirements').click();
+//         cy.url().should('eq', 'http://localhost:4200/accessibility/alttext-doc');
+//     });
 
-    it('Navigates to Components doc page when clicking card link', () => {
-        cy.get('.e-card_title').contains('UU Tools')
-            .parent('.e-card_header_text')
-            .parent('.e-card_header')
-            .siblings('.e-card_actions').children().contains('View tools').click();
-        cy.url().should('eq', 'http://localhost:4200/accessibility/uutools-doc');
-    });
-});
+//     it('Navigates to Components doc page when clicking card link', () => {
+//         cy.get('.e-card_title').contains('UU Tools')
+//             .parent('.e-card_header_text')
+//             .parent('.e-card_header')
+//             .siblings('.e-card_actions').children().contains('View tools').click();
+//         cy.url().should('eq', 'http://localhost:4200/accessibility/uutools-doc');
+//     });
+// });
