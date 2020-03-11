1aab1788df9ffbe0ecf65a20b097686eb4ac2524
Fride Skarseth <fs@computas.com>
2020-03-11 12:53:13 +0100

    Added doc for typo

--- src/app/doc-pages/utilities/typography-doc/typography-doc.component.ts
+++ src/app/doc-pages/utilities/typography-doc/typography-doc.component.ts
@@ -19,14 +19,16 @@ export class TypographyDocComponent implements OnInit {
   example1 = `<h1 class="e-title-large">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h1>
 <h2 class="e-title-medium">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h2>
 <h3 class="e-title-small">Du åt ca fire wienerpølser og tok taxi hjem fra byen</h3>`;
-  example2 = `<p class="e-text-lead">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
-<p class="e-text-quote">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
-<p class="e-text-caps">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
+  example2 = `<p class="e-text-lead">En tørr sommer i tillegg til en kald vinter, gjør at mange vil få høyere strømregning.</p>
+<p class="e-text-quote">En hjemmeladestasjon for elbilen er smart! Ladingen går raskere og den blir tryggere.</p>
 <p class="e-text-body">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
-<p class="e-text-description">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
-<p class="e-text-info">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>`;
-  example3 = `<p class="e-label-input">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>
-<p class="e-label-option">Du åt ca fire wienerpølser og tok taxi hjem fra byen</p>`;
+<p class="e-text-img">Jordfeil er en samlebetegnelse på situasjoner der du har feil 
+eller isolasjonssvikt i det elektriske anlegget eller tilknyttet utstyr. </p>
+<p class="e-text-description">Ved å legge til e-post på din bruker</p>
+<p class="e-text-caps">AV PETTER JENSEN</p>
+<p class="e-text-info">Tekst i en tabell</p>`;
+  example3 = `<p class="e-label-input">Label title</p>
+<p class="e-label-option">Placeholder text</p>`;
 
   constructor() { }
 
