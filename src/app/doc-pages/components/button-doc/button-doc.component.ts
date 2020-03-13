e8a238f6f4a84718c7d36ad968668582e5c154f8
Martin Espeland <m.espeland89@gmail.com>
2020-03-13 11:06:58 +0100

    Update button-doc.component.ts

--- src/app/doc-pages/components/button-doc/button-doc.component.ts
+++ src/app/doc-pages/components/button-doc/button-doc.component.ts
@@ -12,44 +12,43 @@ export class ButtonDocComponent {
   componentClasses = ['e-btn', 'e-btn btn-secondary', 'e-btn has-icon', 'e-btn btn-loading'];
   public clicked = false;
 
-  example1 = `<!-- Style tags are only for examples -->
-  <button class="e-btn" style="margin: 10px">
-    Standard
-  </button>
-<button class="e-btn" disabled style="margin: 10px">
+  example1 = `
+<button class="e-btn e-m-2" >
+  Standard
+</button>
+<button class="e-btn e-m-2" disabled >
   Disabled
 </button>
 `;
 
-  example2 = `<!-- Style tags are only for examples -->
-  <button class="e-btn btn-secondary" style="margin: 10px">
-    Standard
-  </button>
-<button class="e-btn btn-secondary" disabled style="margin: 10px">
+  example2 = `
+<button class="e-btn btn-secondary e-m-2" >
+  Standard
+</button>
+<button class="e-btn btn-secondary e-m-2" disabled >
   Disabled
 </button>
 `;
 
   example3 = `<div>
-  <!-- Style tags are only for examples -->
-  <button class="e-btn has-icon" style="margin: 10px">
-    <span class="e-icon-user-white-bg"></span>Icon button
-  </button>
-  <button class="e-btn has-icon" disabled style="margin: 10px">
-    <span class="e-icon-user-white-bg"></span>Icon button
-  </button>
-  <br>
-  <button class="e-btn btn-secondary has-icon" style="margin: 10px">
-    <span class="e-icon-search"></span>Icon button
-  </button>
-  <button class="e-btn btn-secondary has-icon" disabled style="margin: 10px">
-    <span class="e-icon-search-white"></span>Icon button
-  </button>
+<button class="e-btn has-icon e-m-2" >
+  <span class="e-icon-user-white-bg"></span>Icon button
+</button>
+<button class="e-btn has-icon e-m-2" disabled >
+  <span class="e-icon-user-white-bg"></span>Icon button
+</button>
+<br>
+<button class="e-btn btn-secondary has-icon e-m-2" >
+  <span class="e-icon-search"></span>Icon button
+</button>
+<button class="e-btn btn-secondary has-icon e-m-2" disabled >
+  <span class="e-icon-search-white"></span>Icon button
+</button>
 </div>
 `;
 
   example4 = `
-  <button class="e-btn btn-loading">
+  <button class="e-btn btn-loading e-m-2">
     <span></span>
     <span></span>
     <span></span>
