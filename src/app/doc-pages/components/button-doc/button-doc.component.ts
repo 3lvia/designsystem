29226acd0adae898b5d6990fd5232ef0ea1494a7
Martin Espeland <m.espeland89@gmail.com>
2020-03-13 09:46:08 +0100

    Updated names for button classes

--- src/app/doc-pages/components/button-doc/button-doc.component.ts
+++ src/app/doc-pages/components/button-doc/button-doc.component.ts
@@ -9,46 +9,46 @@ import { getComponent } from 'src/app/shared/e-items';
 export class ButtonDocComponent {
 
   componentStatus = getComponent('button-doc').status;
-  componentClasses = ['e-button', 'e-button is-secondary', 'e-button has-icon', 'e-button loading'];
+  componentClasses = ['e-btn', 'e-btn btn-secondary', 'e-btn has-icon', 'e-btn btn-loading'];
   public clicked = false;
 
   example1 = `<!-- Style tags are only for examples -->
-  <button class="e-button" style="margin: 10px">
+  <button class="e-btn" style="margin: 10px">
     Standard
   </button>
-<button class="e-button" disabled style="margin: 10px">
+<button class="e-btn" disabled style="margin: 10px">
   Disabled
 </button>
 `;
 
   example2 = `<!-- Style tags are only for examples -->
-  <button class="e-button is-secondary" style="margin: 10px">
+  <button class="e-btn btn-secondary" style="margin: 10px">
     Standard
   </button>
-<button class="e-button is-secondary" disabled style="margin: 10px">
+<button class="e-btn btn-secondary" disabled style="margin: 10px">
   Disabled
 </button>
 `;
 
   example3 = `<div>
   <!-- Style tags are only for examples -->
-  <button class="e-button has-icon" style="margin: 10px">
+  <button class="e-btn has-icon" style="margin: 10px">
     <span class="e-icon-user-white-bg"></span>Icon button
   </button>
-  <button class="e-button has-icon" disabled style="margin: 10px">
+  <button class="e-btn has-icon" disabled style="margin: 10px">
     <span class="e-icon-user-white-bg"></span>Icon button
   </button>
-  <button class="e-button is-secondary has-icon" style="margin: 10px">
+  <button class="e-btn btn-secondary has-icon" style="margin: 10px">
     <span class="e-icon-search"></span>Icon button
   </button>
-  <button class="e-button is-secondary has-icon" disabled style="margin: 10px">
+  <button class="e-btn btn-secondary has-icon" disabled style="margin: 10px">
     <span class="e-icon-search"></span>Icon button
   </button>
 </div>
 `;
 
   example4 = `
-  <button class="e-button loading">
+  <button class="e-btn btn-loading">
     <span></span>
     <span></span>
     <span></span>
