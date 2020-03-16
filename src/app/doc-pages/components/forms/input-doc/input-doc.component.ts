b8663be2ce83256fab594a0f3f33e727b8d35a49
Fride Skarseth <fs@computas.com>
2020-03-16 15:21:07 +0100

    Combined input fields

--- src/app/doc-pages/components/forms/input-doc/input-doc.component.ts
+++ src/app/doc-pages/components/forms/input-doc/input-doc.component.ts
@@ -17,14 +17,14 @@ export class InputDocComponent implements OnInit {
   example1 = `<div class="e-form_field">
   <label class="e-form_field_label" for="normal">Normal input field </label>
   <div class="e-form_input">
-    <input id="normal" placeholder="Placeholder text">
+    <input id="normal" type="text" placeholder="Placeholder text">
     <i class="e-icon-email e-icon-xs"></i>
   </div>
 </div>
 <div class="e-form_field">
   <label class="e-form_field_label" for="disabled">Disabled input field </label>
   <div class="e-form_input is-disabled">
-    <input id="disabled" placeholder="Placeholder text" disabled>
+    <input id="disabled" type="text" placeholder="Placeholder text" disabled>
     <i class="e-icon-email e-icon-xs"></i>
   </div>
 </div>
@@ -50,9 +50,39 @@ export class InputDocComponent implements OnInit {
 `;
 
   example3 = `<div class="e-form_field">
-  <label class="e-form_field_label is-optional" for="title">Textarea</label>
+  <label class="e-form_field_label is-optional" for="textarea">Textarea</label>
   <div class="e-form_input">
-    <textarea placeholder="Placeholder text"></textarea>
+    <textarea id="textarea" placeholder="Placeholder text" type="text"></textarea>
+  </div>
+</div>
+`;
+
+  example4 = `<div class="e-form_field">
+  <label class="e-form_field_label" for="phone">Phone</label>
+  <div class="is-combined">
+    <div id="phone" class="e-form_input is-small">
+      <input type="text" value="+47">
+    </div>
+    <div class="e-form_input">
+      <input type="text" placeholder="Placeholder text">
+      <i class="e-icon-phone e-icon-xs"></i>
+    </div>
+  </div>
+</div>
+`;
+
+  example5 = `<div class="is-inline">
+  <div class="e-form_field">
+    <label class="e-form_field_label" for="city">City</label>
+    <div class="e-form_input">
+      <input id="city" type="text" placeholder="City">
+    </div>
+  </div>
+  <div class="e-form_field is-medium">
+    <label class="e-form_field_label" for="zip">Zip code</label>
+    <div class="e-form_input">
+      <input id="zip" type="text" placeholder="Zip code">
+    </div>
   </div>
 </div>
 `;
