291d304334d96e7d613335078e2addcc708aec48
Martin Espeland <m.espeland89@gmail.com>
2020-03-18 16:47:00 +0100

    Update link-doc.component.ts

--- src/app/doc-pages/components/link-doc/link-doc.component.ts
+++ src/app/doc-pages/components/link-doc/link-doc.component.ts
@@ -11,21 +11,21 @@ export class LinkDocComponent implements OnInit {
   componentStatus = getComponent('link-doc').status;
   componentClasses = ['e-link', 'e-link is-inline', 'e-link is-more', 'e-link is-back', 'e-link has-icon', 'e-link is-custom'];
 
-  example1 = `<a class="e-link" href="">Normal link</a>
+  example1 = `<a class="e-link" href="https://www.elvia.no/">Normal link</a>
 `;
-  example2 = `<p>Lorem ipsum <a class="e-link is-inline" href="">dolor</a> sit amet.
+  example2 = `<p>Lorem ipsum <a class="e-link is-inline" href="https://www.elvia.no/">dolor</a> sit amet.
   Soluta modi in rerum magnam blanditiis laboriosam architecto illum,
   nemo eaque voluptate consectetur nulla fugiat delectus vel quia tenetur eius pariatur magni!
 </p>
 `;
 
-  example3 = `<a class="e-link is-more" href="">Se mer</a>
+  example3 = `<a class="e-link is-more" href="https://www.elvia.no/">Se mer</a>
 `;
-  example4 = `<a class="e-link is-back" href="">Tilbake</a>
+  example4 = `<a class="e-link is-back" href="https://www.elvia.no/">Tilbake</a>
 `;
-  example5 = `<a class="e-link has-icon" href="">Last ned <i class="e-icon-download"></i></a>
+  example5 = `<a class="e-link has-icon" href="https://www.elvia.no/">Last ned <i class="e-icon-download"></i></a>
 `;
-  example6 = `<a class="e-link is-custom" href="">Se hva du kan gjøre</a>
+  example6 = `<a class="e-link is-custom" href="https://www.elvia.no/">Se hva du kan gjøre</a>
 `;
 
 
