10de935831399b9741619519e655ae066b043639
Fride Skarseth <fs@computas.com>
2020-03-24 09:30:52 +0100

    Added all greys

--- src/app/doc-pages/utilities/color-doc/color.ts
+++ src/app/doc-pages/utilities/color-doc/color.ts
@@ -4,29 +4,29 @@ export const colorItemsPrimary: Color[] = [
     {
         title: 'white',
         labels: ['elvia-on'],
-        hex: '#FFF',
-        rgba: '',
+        hex: '#FFFFFF',
+        rgba: 'rgb(255,255,255)',
         lg: true
     },
     {
         title: 'black',
         labels: ['elvia-off', 'font-color'],
-        hex: '#000',
-        rgba: '',
+        hex: '#000000',
+        rgba: 'rgb(0,0,0)',
         lg: true
     },
     {
         title: 'green-lime',
         labels: ['elvia-charge'],
         hex: '#29D305',
-        rgba: '',
+        rgba: 'rgb(41,211,5)',
         lg: true
     },
     {
         title: 'grey',
         labels: ['dark-color'],
         hex: '#262626',
-        rgba: '',
+        rgba: 'rgb(38,38,38)',
         lg: true
     },
 ];
@@ -34,79 +34,79 @@ export const colorItemsSecondary: Color[] = [
     {
         title: 'yellow',
         hex: '#FFFF00',
-        rgba: '',
+        rgba: 'rgb(255, 255, 0)',
     },
     {
         title: 'orange-peel',
         hex: '#FFA000',
-        rgba: '',
+        rgba: 'rgb(255, 160, 0)',
     },
     {
         title: 'red',
         hex: '#FF0000',
-        rgba: '',
+        rgba: 'rgb(255, 0, 0)',
     },
 ];
 export const colorItemsGreys: Color[] = [
     {
         title: 'grey',
         hex: '#262626',
-        rgba: 'rgba(38,38,38)',
+        rgba: 'rgb(38,38,38)',
     },
     {
         title: 'grey-90',
-        hex: '#3C3C3C',
-        rgba: 'rgba(38,38,38, 0.9)',
+        hex: '#3B3B3B',
+        rgba: 'rgba(38,38,38,0.9)',
     },
     {
         title: 'grey-80',
         hex: '#515151',
-        rgba: 'rgba(38,38,38, 0.8)',
+        rgba: 'rgba(38,38,38,0.8)',
     },
     {
         title: 'grey-70',
         hex: '#676767',
-        rgba: 'rgba(38,38,38, 0.7)',
+        rgba: 'rgba(38,38,38,0.7)',
     },
     {
         title: 'grey-60',
-        hex: '#7D7D7D',
-        rgba: 'rgba(38,38,38, 0.6)',
+        hex: '#7C7C7C',
+        rgba: 'rgba(38,38,38,0.6)',
     },
     {
         title: 'grey-50',
         hex: '#929292',
-        rgba: 'rgba(38,38,38, 0.5)',
+        rgba: 'rgba(38,38,38,0.5)',
     },
     {
         title: 'grey-40',
         hex: '#A8A8A8',
-        rgba: 'rgba(38,38,38, 0.4)',
+        rgba: 'rgba(38,38,38,0.4)',
     },
     {
         title: 'grey-30',
-        hex: '#BEBEBE',
-        rgba: 'rgba(38,38,38, 0.3)',
+        hex: '#BDBDBD',
+        rgba: 'rgba(38,38,38,0.3)',
     },
     {
         title: 'grey-20',
-        hex: '#D4D4D4',
-        rgba: 'rgba(38,38,38, 0.2)',
+        hex: '#D3D3D3',
+        rgba: 'rgba(38,38,38,0.2)',
     },
     {
         title: 'grey-10',
         hex: '#E9E9E9',
-        rgba: 'rgba(38,38,38, 0.1)',
+        rgba: 'rgba(38,38,38,0.1)',
     },
     {
         title: 'grey-05',
-        hex: '#E9E9E9',
-        rgba: 'rgba(38,38,38, 0.05)',
+        hex: '#F4F4F4',
+        rgba: 'rgba(38,38,38,0.05)',
     },
     {
         title: 'grey-02',
-        hex: '#E9E9E9',
-        rgba: 'rgba(38,38,38, 0.02)',
+        hex: '#FAFAFA',
+        rgba: 'rgba(38,38,38,0.02)',
     },
 ];
 
