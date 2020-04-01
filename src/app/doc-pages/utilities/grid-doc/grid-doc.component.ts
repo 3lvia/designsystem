f21112aa967f609e83f82156222f23b5b227b778
Fride Skarseth <fs@computas.com>
2020-04-01 11:03:41 +0200

    griding

--- src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
+++ src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
@@ -11,40 +11,40 @@ export class GridDocComponent implements OnInit {
   componentStatus = getUtilities('grid-doc').status;
 
   example1 = `<div class="e-bg-green">
-  <div class="row">
-    <div class="col-sm e-bg-yellow">
+  <div class="e-row e-m-0">
+    <div class="e-col-sm e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         One of three columns
       </div>
     </div>
-    <div class="col-sm e-bg-yellow">
+    <div class="e-col-sm e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         One of three columns
       </div>
     </div>
-    <div class="col-sm e-bg-yellow">
+    <div class="e-col-sm e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         One of three columns
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col e-bg-yellow">
+  <div class="e-row">
+    <div class="e-col e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         1/4
       </div>
     </div>
-    <div class="col e-bg-yellow">
+    <div class="e-col e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         2/4
       </div>
     </div>
-    <div class="col e-bg-yellow">
+    <div class="e-col e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         3/4
       </div>
     </div>
-    <div class="col e-bg-yellow">
+    <div class="e-col e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         4/4
       </div>
@@ -54,11 +54,11 @@ export class GridDocComponent implements OnInit {
 `;
 
   example2 = `<div class="e-bg-green">
-  <div class="row">
+  <div class="e-row">
     <div class="col-sm-9 e-bg-yellow">
       <div class="e-bg-green" style="border: 1px solid black">
         Level 1: .col-sm-9
-        <div class="row">
+        <div class="e-row">
           <div class="col-8 col-sm-6 e-bg-yellow"">
             <div class="e-bg-green" style="border: 1px solid black">
               Level 2: .col-8 .col-sm-6
