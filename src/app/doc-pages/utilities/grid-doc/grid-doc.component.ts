c440b5744e7aac60bad757dd99070ddd1287f234
Fride Skarseth <fs@computas.com>
2020-04-03 14:54:30 +0200

    Fixing examples

--- src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
+++ src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
@@ -10,69 +10,63 @@ export class GridDocComponent implements OnInit {
 
   componentStatus = getUtilities('grid-doc').status;
 
-  example1 = `<div class="e-grid">
-  <div class="e-bg-grey-10">
-    <div class="row">
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          One of three columns
-        </div>
+  example1 = `<div class="e-grid e-bg-grey-10">
+  <div class="row">
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
       </div>
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          One of three columns
-        </div>
+    </div>
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
       </div>
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          One of three columns
-        </div>
+    </div>
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
       </div>
     </div>
   </div>
 </div>
   `;
 
-  example2 = `<div class="e-grid">
-  <div class="e-bg-grey-10">
-    <div class="row e-m-0">
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          1/4
-        </div>
+  example2 = `<div class="e-grid e-bg-grey-10">
+  <div class="row e-m-0">
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        1/4
       </div>
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          2/4
-        </div>
+    </div>
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        2/4
       </div>
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          3/4
-        </div>
+    </div>
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        3/4
       </div>
-      <div class="col e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          4/4
-        </div>
+    </div>
+    <div class="col e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        4/4
       </div>
     </div>
   </div>
 </div>
 `;
 
-  example3 = `<div class="e-grid">
-  <div class="e-bg-grey-10">
-    <div class="row e-m-0">
-      <div class="col-sm-7 col-md-6 col-lg-8 e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          1
-        </div>
+  example3 = `<div class="e-grid e-bg-grey-10">
+  <div class="row e-m-0">
+    <div class="col-sm-7 col-md-6 col-lg-8 e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        1
       </div>
-      <div class="col-sm-5 col-md-6 col-lg-4 e-bg-grey-40">
-        <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
-          2
-        </div>
+    </div>
+    <div class="col-sm-5 col-md-6 col-lg-4 e-bg-grey-40">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        2
       </div>
     </div>
   </div>
