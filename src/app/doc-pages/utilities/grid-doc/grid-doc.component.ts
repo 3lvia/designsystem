a99b3088eaac49ddde90cf7b2f9906a5362ef2c2
Fride Skarseth <fs@computas.com>
2020-04-06 15:00:53 +0200

    Updated doc

--- src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
+++ src/app/doc-pages/utilities/grid-doc/grid-doc.component.ts
@@ -11,19 +11,19 @@ export class GridDocComponent implements OnInit {
   componentClasses = ['e-grid'];
   componentStatus = getUtilities('grid-doc').status;
 
-  example1 = `<div class="e-grid e-bg-grey-10">
+  example1 = `<div class="e-grid e-bg-grey-20">
   <div class="row">
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of three columns
       </div>
     </div>
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of three columns
       </div>
     </div>
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of three columns
       </div>
@@ -32,24 +32,45 @@ export class GridDocComponent implements OnInit {
 </div>
   `;
 
-  example2 = `<div class="e-grid e-bg-grey-10">
-  <div class="row e-no-margin e-no-gutters">
-    <div class="col e-bg-grey-40">
+  example2 = `<div class="e-grid e-bg-grey-20">
+  <div class="row e-grid-margin">
+    <div class="col">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
+      </div>
+    </div>
+    <div class="col">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
+      </div>
+    </div>
+    <div class="col">
+      <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
+        One of three columns
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+
+  example3 = `<div class="e-grid e-bg-grey-20">
+  <div class="row no-gutters">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         1/4
       </div>
     </div>
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         2/4
       </div>
     </div>
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         3/4
       </div>
     </div>
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         4/4
       </div>
@@ -58,14 +79,14 @@ export class GridDocComponent implements OnInit {
 </div>
 `;
 
-  example3 = `<div class="e-grid e-bg-grey-10">
-  <div class="row e-m-0">
-    <div class="col-sm-7 col-md-6 col-lg-8 e-bg-grey-40">
+  example4 = `<div class="e-grid e-bg-grey-20">
+  <div class="row">
+    <div class="col-sm-7 col-md-6 col-lg-8">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         1
       </div>
     </div>
-    <div class="col-sm-5 col-md-6 col-lg-4 e-bg-grey-40">
+    <div class="col-sm-5 col-md-6 col-lg-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         2
       </div>
@@ -74,38 +95,38 @@ export class GridDocComponent implements OnInit {
 </div>
 `;
 
-  example4 = `<div class="e-grid e-bg-grey-10">
+  example5 = `<div class="e-grid e-bg-grey-20">
   <div class="row justify-content-center">
-    <div class="col-4 e-bg-grey-40">
+    <div class="col-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
     </div>
-    <div class="col-4 e-bg-grey-40">
+    <div class="col-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
     </div>
   </div>
   <div class="row align-items-end" style="height: 100px;">
-    <div class="col-4 e-bg-grey-40">
+    <div class="col-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
     </div>
-    <div class="col-4 e-bg-grey-40">
+    <div class="col-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
     </div>
   </div>
-  <div class="row e-no-margin">
-    <div class="col-4 e-bg-grey-40">
+  <div class="row">
+    <div class="col-4">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
     </div>
-    <div class="col-4 offset-3 e-bg-grey-40">
+    <div class="col-4 offset-3">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         One of two columns
       </div>
@@ -114,19 +135,19 @@ export class GridDocComponent implements OnInit {
 </div>
 `;
 
-  example5 = `<div class="e-grid e-bg-grey-10">
+  example6 = `<div class="e-grid e-bg-grey-20">
   <div class="row">
-    <div class="col e-bg-grey-40">
+    <div class="col">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         First in DOM, no order applied
       </div>
     </div>
-    <div class="col order-12 e-bg-grey-40">
+    <div class="col order-12">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         Second in DOM, with a larger order
       </div>
     </div>
-    <div class="col order-1 e-bg-grey-40">
+    <div class="col order-1">
       <div class="e-bg-grey-70 e-p-2" style="border: 1px solid black;">
         Third in DOM, with an order of 1
       </div>
