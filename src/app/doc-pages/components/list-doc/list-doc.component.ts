3543a0433191ca753260d8b33b75ee13eaf51de7
Martin Espeland <m.espeland89@gmail.com>
2020-04-20 10:47:25 +0200

    Updated icons style. Now works better with multiline text

--- src/app/doc-pages/components/list-doc/list-doc.component.ts
+++ src/app/doc-pages/components/list-doc/list-doc.component.ts
@@ -29,11 +29,16 @@ export class ListDocComponent implements OnInit {
 </ul>
 `;
 
-  example3 = `<ol class="e-list e-icons">
-  <li><i class="e-icon-step-1-color"></i>Just a hunk, a hunk of burning list</li>
-  <li><i class="e-icon-step-2-color"></i>Just a hunk, a hunk of burning lists</li>
-  <li><i class="e-icon-money-color"></i>Just a hunk, a hunk of burning lists</li>
-  <li><i class="e-icon-information-circle"></i>Just a hunk, a hunk of burning lists</li>
+  example3 = `<ol class="e-list e-has-icon">
+  <li><span class="e-li"><i class="e-icon-step-1-color e-list-icon"></i></span>
+  <strong>Describing title :</strong> After, one or multiple sentences could
+  follow with more information. These sentences can span over multiple rows, but shouldn’t be too long.</li>
+  <li><span class="e-li"><i class="e-icon-step-2-color e-list-icon"></i></span>
+  <strong>This can also be the start of a sentence</strong> that continues in thinner font.
+  The bold part should be describing and the rest of the text informative.</li>
+  <li><span class="e-li"><i class="e-icon-step-3-color e-list-icon"></i></span>
+  <strong>Don’t use too many points</strong> and make sure the text is easy to read, 
+  informative and follows Elvia’s tone of voice.</li>
 </ol>
 `;
 
