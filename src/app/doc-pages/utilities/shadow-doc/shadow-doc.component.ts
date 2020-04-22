c0419caa7dc1a69cd910a78ee76082f18e093f4c
Martin Espeland <m.espeland89@gmail.com>
2020-04-22 10:04:57 +0200

    Updated guidelines

--- src/app/doc-pages/utilities/shadow-doc/shadow-doc.component.ts
+++ src/app/doc-pages/utilities/shadow-doc/shadow-doc.component.ts
@@ -12,17 +12,14 @@ export class ShadowDocComponent implements OnInit {
   @Input() selected = TabNames.Overview;
 
   tabNames = TabNames;
-  tabs = [TabNames.Overview, TabNames.Code, TabNames.Guidelines];
+  tabs = [TabNames.Overview, TabNames.Guidelines];
   // componentClasses = ['e-shadow-soft', 'e-shadow-medium', 'e-shadow-hard'];
   componentStatus = getUtilities('shadow-doc').status;
   externalUrl = getUtilities('shadow-doc').externalUrl;
 
-  doCodeCSS = `box-shadow: var(--e-shadow-2);
+  doCodeCSS = `box-shadow: var(--e-shadow-medium);
 box-shadow: none;`;
   dontCodeCSS = `box-shadow: var(--e-shadow-none);`;
-  example1 = `<span class="e-shadow-1 e-mb-16 e-mt-16 example-box"></span>
-<span class="e-shadow-2 e-mb-16 e-mt-16 example-box"></span>
-<span class="e-shadow-3 e-mb-16 e-mt-16 example-box"></span>`;
 
   constructor() { }
 
