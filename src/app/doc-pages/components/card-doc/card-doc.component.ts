6fb0f6f6c79b964c0c880e511a369f763aa08efd
Martin Espeland <m.espeland89@gmail.com>
2020-04-27 16:44:23 +0200

    Update example text

--- src/app/doc-pages/components/card-doc/card-doc.component.ts
+++ src/app/doc-pages/components/card-doc/card-doc.component.ts
@@ -14,11 +14,11 @@ export class CardDocComponent implements OnInit {
   example1 = `<div style="max-width:600px;margin:40px;">
   <div class="e-card">
     <div class="e-card__title">
-      A card should have a title
+      Strømregningen påvirkes av vinteren
     </div>
   <div class="e-card__content">
-    Best design once so-called "iPad killer" although Apple will only get better,
-    overall gorgeous on the contrary profit.
+    Hovedårsaken til at du får høyere strømregning på vinteren er at temperaturen ute synker,
+    mens de fleste holder en jevn temperatur på rundt 20 grader inne...
   </div>
 </div>
 `;
@@ -26,11 +26,11 @@ export class CardDocComponent implements OnInit {
   example2 = `<div style="max-width:600px;margin:40px;">
   <div class="e-card e-card--bg-white">
     <div class="e-card__title">
-      A card should have a title
+      Strømregningen påvirkes av vinteren
     </div>
     <div class="e-card__content">
-      Best design once so-called "iPad killer" although Apple will only get better,
-      overall gorgeous on the contrary profit.
+      Hovedårsaken til at du får høyere strømregning på vinteren er at temperaturen ute synker,
+      mens de fleste holder en jevn temperatur på rundt 20 grader inne...
     </div>
   </div>
 </div>`;
@@ -38,12 +38,11 @@ export class CardDocComponent implements OnInit {
   example3 = `<div style="max-width:600px;margin:40px;">
   <div class="e-card">
     <div class="e-card__title---above">
-      A card should have a title
+      Strømregningen påvirkes av vinteren
   </div>
     <div class="e-card__content">
-      Can also not have a title
-      Best design once so-called "iPad killer" although Apple will only get better,
-      overall gorgeous on the contrary profit.
+      Hovedårsaken til at du får høyere strømregning på vinteren er at temperaturen ute synker,
+      mens de fleste holder en jevn temperatur på rundt 20 grader inne...
     </div>
   </div>
 </div>
