9a256896907c1d6be655507306d7a04e8447547f
Martin Espeland <m.espeland89@gmail.com>
2020-04-27 14:59:51 +0200

    Updated Cars title line height, example div and width information for cards

--- src/app/doc-pages/components/card-doc/card-doc.component.ts
+++ src/app/doc-pages/components/card-doc/card-doc.component.ts
@@ -11,10 +11,11 @@ export class CardDocComponent implements OnInit {
   componentStatus = getComponent('card-doc').status;
   externalUrl = getComponent('card-doc').externalUrl;
 
-  example1 = `<div class="e-card">
-  <div class="e-card__title">
-    A card should have a title
-  </div>
+  example1 = `<div style="max-width:600px;margin:40px;">
+  <div class="e-card">
+    <div class="e-card__title">
+      A card should have a title
+    </div>
   <div class="e-card__content">
     Best design once so-called "iPad killer" although Apple will only get better,
     overall gorgeous on the contrary profit.
@@ -22,24 +23,28 @@ export class CardDocComponent implements OnInit {
 </div>
 `;
 
-  example2 = `<div class="e-card e-card--bg-white">
-   <div class="e-card__title">
-    A card should have a title
-  </div>
-  <div class="e-card__content">
-    Best design once so-called "iPad killer" although Apple will only get better,
-    overall gorgeous on the contrary profit.
+  example2 = `<div style="max-width:600px;margin:40px;">
+  <div class="e-card e-card--bg-white">
+    <div class="e-card__title">
+      A card should have a title
+    </div>
+    <div class="e-card__content">
+      Best design once so-called "iPad killer" although Apple will only get better,
+      overall gorgeous on the contrary profit.
+    </div>
   </div>
 </div>`;
 
-  example3 = `<div class="e-card__title-above">
-    A card should have a title
-</div>
-<div class="e-card">
-  <div class="e-card__content">
-    Can also not have a title
-    Best design once so-called "iPad killer" although Apple will only get better,
-    overall gorgeous on the contrary profit.
+  example3 = `<div style="max-width:600px;margin:40px;">
+  <div class="e-card__title-above">
+      A card should have a title
+  </div>
+  <div class="e-card">
+    <div class="e-card__content">
+      Can also not have a title
+      Best design once so-called "iPad killer" although Apple will only get better,
+      overall gorgeous on the contrary profit.
+    </div>
   </div>
 </div>
   `;
