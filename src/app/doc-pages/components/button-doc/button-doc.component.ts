3393f9ed84e00ac9687ea085033454403f400d29
Fride Skarseth <fs@computas.com>
2020-04-27 09:58:07 +0200

    Updated description

--- src/app/doc-pages/components/button-doc/button-doc.component.ts
+++ src/app/doc-pages/components/button-doc/button-doc.component.ts
@@ -78,7 +78,7 @@ example3 = `<div>
     <span class="e-btn__title">Standard</span>
   </button>
   <button class="e-btn e-btn--tertiary e-btn--lg e-m-16" disabled >
-    <span class="e-btn__icon"><i class="e-icon-download e-icon-disabled"></i></span>
+    <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Disabled</span>
   </button>
 </div>
@@ -90,7 +90,7 @@ example3 = `<div>
   </button>
   <button class="e-btn e-btn--tertiary e-m-16" disabled >
     <span class="e-btn__title">Disabled</span>
-    <span class="e-btn__icon"><i class="e-icon-download e-icon-disabled"></i></span>
+    <span class="e-btn__icon"><i class="e-icon-download"></i></span>
   </button>
 </div>
 <div>
@@ -100,7 +100,7 @@ example3 = `<div>
     <span class="e-btn__title">Standard</span>
   </button>
   <button class="e-btn e-btn--tertiary e-btn--sm e-m-16" disabled >
-    <span class="e-btn__icon"><i class="e-icon-download e-icon-disabled"></i></span>
+    <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Disabled</span>
   </button>
 </div>
@@ -110,31 +110,31 @@ example3 = `<div>
   <h3 class="e-title-small">Primary buttons</h3>
   <div>
     <button class="e-btn e-btn--lg e-m-16" >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-invert"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
     <button class="e-btn e-btn--lg e-m-16" disabled >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-disabled"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
   <div>
     <button class="e-btn e-m-16">
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-invert"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
     <button class="e-btn e-m-16" disabled >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-disabled"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
   <div>
     <button class="e-btn e-btn--sm e-m-16" >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-invert"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
     <button class="e-btn e-btn--sm e-m-16" disabled>
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled e-icon-disabled"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
