db81619f287bf978896c82be06f80d42bd686761
Fride Skarseth <fs@computas.com>
2020-04-27 10:07:45 +0200

    Updated doc descriptions

--- src/app/doc-pages/components/button-doc/button-doc.component.ts
+++ src/app/doc-pages/components/button-doc/button-doc.component.ts
@@ -15,91 +15,91 @@ export class ButtonDocComponent {
 
   example1 = `<div>
   <h3 class="e-title-small">Large</h3>
-  <button class="e-btn e-btn--lg e-m-16" >
-    Standard
+  <button class="e-btn e-btn--lg e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--lg e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-btn--lg e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Medium (default)</h3>
-  <button class="e-btn e-m-16" >
-    Standard
+  <button class="e-btn e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Small</h3>
-  <button class="e-btn e-btn--sm e-m-16" >
-    Standard
+  <button class="e-btn e-btn--sm e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--sm e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-btn--sm e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 `;
 
   example2 = `<div>
   <h3 class="e-title-small">Large</h3>
-  <button class="e-btn e-btn--secondary e-btn--lg e-m-16" >
-    Standard
+  <button class="e-btn e-btn--secondary e-btn--lg e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--secondary e-btn--lg e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-btn--secondary e-btn--lg e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Medium (default)</h3>
-  <button class="e-btn e-btn--secondary e-m-16" >
-    Standard
+  <button class="e-btn e-btn--secondary e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--secondary e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-btn--secondary e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Small</h3>
-  <button class="e-btn e-btn--secondary e-btn--sm e-m-16" >
-    Standard
+  <button class="e-btn e-btn--secondary e-btn--sm e-m-16">
+    <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--secondary e-btn--sm e-m-16" disabled >
-    Disabled
+  <button class="e-btn e-btn--secondary e-btn--sm e-m-16" disabled>
+    <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 `;
 
 example3 = `<div>
   <h3 class="e-title-small">Large</h3>
-  <button class="e-btn e-btn--tertiary e-btn--lg e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn--lg e-m-16">
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--tertiary e-btn--lg e-m-16" disabled >
+  <button class="e-btn e-btn--tertiary e-btn--lg e-m-16" disabled>
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Disabled</span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Medium (default)</h3>
-  <button class="e-btn e-btn--tertiary e-m-16" >
+  <button class="e-btn e-btn--tertiary e-m-16">
     <span class="e-btn__title">Standard</span>
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
   </button>
-  <button class="e-btn e-btn--tertiary e-m-16" disabled >
+  <button class="e-btn e-btn--tertiary e-m-16" disabled>
     <span class="e-btn__title">Disabled</span>
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Small</h3>
-  <button class="e-btn e-btn--tertiary e-btn--sm e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn--sm e-m-16">
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Standard</span>
   </button>
-  <button class="e-btn e-btn--tertiary e-btn--sm e-m-16" disabled >
+  <button class="e-btn e-btn--tertiary e-btn--sm e-m-16" disabled>
     <span class="e-btn__icon"><i class="e-icon-download"></i></span>
     <span class="e-btn__title">Disabled</span>
   </button>
@@ -109,32 +109,32 @@ example3 = `<div>
   example4 = `<div>
   <h3 class="e-title-small">Primary buttons</h3>
   <div>
-    <button class="e-btn e-btn--lg e-m-16" >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+    <button class="e-btn e-btn--lg e-m-16">
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
-    <button class="e-btn e-btn--lg e-m-16" disabled >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+    <button class="e-btn e-btn--lg e-m-16" disabled>
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
   <div>
     <button class="e-btn e-m-16">
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
-    <button class="e-btn e-m-16" disabled >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+    <button class="e-btn e-m-16" disabled>
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
   <div>
-    <button class="e-btn e-btn--sm e-m-16" >
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+    <button class="e-btn e-btn--sm e-m-16">
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
     <button class="e-btn e-btn--sm e-m-16" disabled>
-      <span class="e-btn__icon"><i class="e-icon-add-circle-filled"></i></span>
+      <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
   </div>
@@ -142,7 +142,7 @@ example3 = `<div>
 <div>
   <h3 class="e-title-small">Secondary buttons</h3>
   <div>
-    <button class="e-btn e-btn--secondary e-btn--lg e-m-16" >
+    <button class="e-btn e-btn--secondary e-btn--lg e-m-16">
       <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
@@ -152,7 +152,7 @@ example3 = `<div>
     </button>
   </div>
   <div>
-    <button class="e-btn e-btn--secondary e-m-16" >
+    <button class="e-btn e-btn--secondary e-m-16">
       <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
@@ -162,11 +162,11 @@ example3 = `<div>
     </button>
   </div>
   <div>
-    <button class="e-btn e-btn--secondary e-btn--sm e-m-16" >
+    <button class="e-btn e-btn--secondary e-btn--sm e-m-16">
       <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
-    <button class="e-btn e-btn--secondary e-btn--sm e-m-16" disabled >
+    <button class="e-btn e-btn--secondary e-btn--sm e-m-16" disabled>
       <span class="e-btn__icon"><i class="e-icon-download"></i></span>
       <span class="e-btn__title">Icon button</span>
     </button>
@@ -202,46 +202,46 @@ example3 = `<div>
 
   example6 = `<div>
   <h3 class="e-title-small">Primary buttons</h3>
-  <button class="e-btn e-btn---hover e-m-16" >
+  <button class="e-btn e-btn---hover e-m-16">
     Hover
   </button>
-  <button class="e-btn e-btn---active e-m-16" >
+  <button class="e-btn e-btn---active e-m-16">
     Active
   </button>
-  <button class="e-btn e-btn---focus e-m-16" >
+  <button class="e-btn e-btn---focus e-m-16">
     Focus
   </button>
-  <button class="e-btn e-btn---disabled e-m-16" >
+  <button class="e-btn e-btn---disabled e-m-16">
     Disabled
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Secondary buttons</h3>
-  <button class="e-btn e-btn--secondary e-btn---hover e-m-16" >
+  <button class="e-btn e-btn--secondary e-btn---hover e-m-16">
     Hover
   </button>
-  <button class="e-btn e-btn--secondary e-btn---active e-m-16" >
+  <button class="e-btn e-btn--secondary e-btn---active e-m-16">
     Active
   </button>
-  <button class="e-btn e-btn--secondary e-btn---focus e-m-16" >
+  <button class="e-btn e-btn--secondary e-btn---focus e-m-16">
     Focus
   </button>
-  <button class="e-btn e-btn--secondary e-btn---disabled e-m-16" >
+  <button class="e-btn e-btn--secondary e-btn---disabled e-m-16">
     Disabled
   </button>
 </div>
 <div>
   <h3 class="e-title-small">Tertiary buttons</h3>
-  <button class="e-btn e-btn--tertiary e-btn---hover e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn---hover e-m-16">
     Hover
   </button>
-  <button class="e-btn e-btn--tertiary e-btn---active e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn---active e-m-16">
     Active
   </button>
-  <button class="e-btn e-btn--tertiary e-btn---focus e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn---focus e-m-16">
     Focus
   </button>
-  <button class="e-btn e-btn--tertiary e-btn---disabled e-m-16" >
+  <button class="e-btn e-btn--tertiary e-btn---disabled e-m-16">
     Disabled
   </button>
 </div>
