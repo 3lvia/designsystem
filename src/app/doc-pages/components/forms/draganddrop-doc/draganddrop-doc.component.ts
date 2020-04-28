e4e0c324aa8e41484f8dedd8a38833eb72c7ce8a
Martin Espeland <m.espeland89@gmail.com>
2020-04-28 13:38:59 +0200

    Initial drag&drop style and doc

---
+++ src/app/doc-pages/components/forms/draganddrop-doc/draganddrop-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-draganddrop-doc',
+  templateUrl: './draganddrop-doc.component.html',
+  styleUrls: ['./draganddrop-doc.component.scss']
+})
+export class DraganddropDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
