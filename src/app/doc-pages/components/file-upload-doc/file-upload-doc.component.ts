3e95bed777b286f0962d79650edd1bfcbead7f63
Martin Espeland <m.espeland89@gmail.com>
2020-04-28 17:22:55 +0200

    Initial Card, this needs rework and discussion

--- src/app/doc-pages/components/file-upload-doc/file-upload-doc.component.ts
+++ src/app/doc-pages/components/file-upload-doc/file-upload-doc.component.ts
@@ -25,6 +25,26 @@ export class FileUploadDocComponent implements OnInit {
   </div>
 </div>`;
 
+  example3 = `<div class="e-card e-card--on-white" style="width:375px;height:80px">
+  <div class="e-grid">
+    <div class="row">
+      <div class="col-2">
+        <i class="e-icon e-icon--download"></i>
+      </div>
+      <div class="col-8">
+        <div class="e-progressbar" role="progressbar">
+          <div class="e-progressbar e-loading"></div>
+        </div>
+      </div>
+      <div class="col-2">
+       <i class="e-icon e-icon--close-menu"></i>
+      </div>
+    </div>
+  </div>
+</div>`;
+
+
+
 
   constructor() { }
 
