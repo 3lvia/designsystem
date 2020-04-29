1db8672c45a5a4f94ba69d7a04c3fd702e5c7816
Fride Skarseth <fs@computas.com>
2020-04-29 15:38:06 +0200

    Updated to icon classes

--- src/app/doc-pages/components/list-doc/list-doc.component.ts
+++ src/app/doc-pages/components/list-doc/list-doc.component.ts
@@ -31,17 +31,17 @@ export class ListDocComponent implements OnInit {
 
   example3 = `<ol class="e-list e-list--icons">
   <li>
-    <span class="e-list__icon"><i class="e-icon-md e-icon-step-1 e-list-icon"></i></span>
+    <span class="e-list__icon"><i class="e-icon e-icon--step-1 e-icon--md"></i></span>
     <strong>Describing title :</strong> After, one or multiple sentences could follow with more information.
     These sentences can span over multiple rows, but shouldn’t be too long.
   </li>
   <li>
-    <span class="e-list__icon"><i class="e-icon-md e-icon-step-2 e-list-icon"></i></span>
+    <span class="e-list__icon"><i class="e-icon e-icon--step-2 e-icon--md"></i></span>
     <strong>This can also be the start of a sentence</strong> that continues in thinner font.
     The bold part should be describing and the rest of the text informative.
   </li>
   <li>
-    <span class="e-list__icon"><i class="e-icon-md e-icon-step-3 e-list-icon"></i></span>
+    <span class="e-list__icon"><i class="e-icon e-icon--step-3 e-icon--md"></i></span>
     <strong>Don’t use too many points</strong> and make sure the text is easy to read,
     informative and follows Elvia’s tone of voice.
   </li>
