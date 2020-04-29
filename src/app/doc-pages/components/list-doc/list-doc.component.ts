651c6cfaf46b1c8d699931ad8df5e93ea1af2ba4
Fride Skarseth <fs@computas.com>
2020-04-29 12:48:44 +0200

    list fix

--- src/app/doc-pages/components/list-doc/list-doc.component.ts
+++ src/app/doc-pages/components/list-doc/list-doc.component.ts
@@ -14,40 +14,34 @@ export class ListDocComponent implements OnInit {
   componentStatus = getComponent('list-doc').status;
 
   example1 = `<ul class="e-list">
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
 </ul>
 `;
 
   example2 = `<ul class="e-list e-list--strong">
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
-  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
+  <li>Just a hunk, a hunk of burning list</li>
 </ul>
 `;
 
   example3 = `<ol class="e-list e-list--icons">
-  <li class="e-list__item">
-    <span class="e-list__icon">
-      <i class="e-icon-md e-icon-step-1 e-list-icon"></i>
-    </span>
+  <li>
+    <span class="e-list__icon"><i class="e-icon-md e-icon-step-1 e-list-icon"></i></span>
     <strong>Describing title :</strong> After, one or multiple sentences could follow with more information.
     These sentences can span over multiple rows, but shouldn’t be too long.
   </li>
-  <li class="e-list__item">
-    <span class="e-list__icon">
-      <i class="e-icon-md e-icon-step-2 e-list-icon"></i>
-    </span>
+  <li>
+    <span class="e-list__icon"><i class="e-icon-md e-icon-step-2 e-list-icon"></i></span>
     <strong>This can also be the start of a sentence</strong> that continues in thinner font.
     The bold part should be describing and the rest of the text informative.
   </li>
-  <li class="e-list__item">
-    <span class="e-list__icon">
-      <i class="e-icon-md e-icon-step-3 e-list-icon"></i>
-    </span>
+  <li>
+    <span class="e-list__icon"><i class="e-icon-md e-icon-step-3 e-list-icon"></i></span>
     <strong>Don’t use too many points</strong> and make sure the text is easy to read,
     informative and follows Elvia’s tone of voice.
   </li>
