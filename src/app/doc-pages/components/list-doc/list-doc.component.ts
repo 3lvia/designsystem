db5da3998b23a2a5d33487c19272c9c38d961407
Fride Skarseth <fs@computas.com>
2020-04-29 12:42:53 +0200

    List

--- src/app/doc-pages/components/list-doc/list-doc.component.ts
+++ src/app/doc-pages/components/list-doc/list-doc.component.ts
@@ -14,55 +14,45 @@ export class ListDocComponent implements OnInit {
   componentStatus = getComponent('list-doc').status;
 
   example1 = `<ul class="e-list">
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
 </ul>
 `;
 
-  example2 = `<ul class="e-list e-strong">
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
-  <li>Just a hunk, a hunk of burning list</li>
+  example2 = `<ul class="e-list e-list--strong">
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
+  <li class="e-list__item">Just a hunk, a hunk of burning list</li>
 </ul>
 `;
 
-  example3 = `
-<ol class="e-list e-has-icon">
-  <li>
-    <span class="e-list-icon-container">
+  example3 = `<ol class="e-list e-list--icons">
+  <li class="e-list__item">
+    <span class="e-list__icon">
       <i class="e-icon-md e-icon-step-1 e-list-icon"></i>
     </span>
-  <strong>Describing title :</strong> After, one or multiple sentences could follow with more information. 
-  These sentences can span over multiple rows, but shouldn’t be too long.
+    <strong>Describing title :</strong> After, one or multiple sentences could follow with more information.
+    These sentences can span over multiple rows, but shouldn’t be too long.
   </li>
-  <li>
-    <span class="e-list-icon-container">
+  <li class="e-list__item">
+    <span class="e-list__icon">
       <i class="e-icon-md e-icon-step-2 e-list-icon"></i>
     </span>
-  <strong>This can also be the start of a sentence</strong> that continues in thinner font.
-  The bold part should be describing and the rest of the text informative.
+    <strong>This can also be the start of a sentence</strong> that continues in thinner font.
+    The bold part should be describing and the rest of the text informative.
   </li>
-  <li>
-    <span class="e-list-icon-container">
+  <li class="e-list__item">
+    <span class="e-list__icon">
       <i class="e-icon-md e-icon-step-3 e-list-icon"></i>
     </span>
-  <strong>Don’t use too many points</strong> and make sure the text is easy to read, 
-  informative and follows Elvia’s tone of voice.
+    <strong>Don’t use too many points</strong> and make sure the text is easy to read,
+    informative and follows Elvia’s tone of voice.
   </li>
 </ol>
 `;
-
-// Old numbered lists. Might remove later
-//   example4 = `<ol class="e-list e-numbers">
-//   <li>Just a hunk, a hunk of burning list</li>
-//   <li>Just a hunk, a hunk of burning lists</li>
-//   <li>Just a hunk, a hunk of burning lists</li>
-//   <li>Just a hunk, a hunk of burning lists</li>
-// </ol>
-// `;
 
   constructor() { }
 
