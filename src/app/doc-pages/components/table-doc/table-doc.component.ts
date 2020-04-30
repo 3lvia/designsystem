bf04a948fbc08dbd1f34998369987e38be9e7a44
Fride Skarseth <fs@computas.com>
2020-04-30 09:51:34 +0200

    Proposal for adding table cell class

--- src/app/doc-pages/components/table-doc/table-doc.component.ts
+++ src/app/doc-pages/components/table-doc/table-doc.component.ts
@@ -20,28 +20,28 @@ export class TableDocComponent implements OnInit {
   </thead>
   <tbody>
     <tr>
-      <th scope="row">Administrativt arbeid i kundesystemet</th>
-      <td>420</td>
+      <th class="e-table__cell" scope="row">Administrativt arbeid i kundesystemet</th>
+      <td class="e-table__cell">420</td>
     </tr>
     <tr>
-      <th scope="row">Informasjon og purring til kunder</th>
-      <td>65</td>
+      <th class="e-table__cell" scope="row">Informasjon og purring til kunder</th>
+      <td class="e-table__cell">65</td>
     </tr>
     <tr>
-      <th scope="row">Arbeid relatert til booking av avlesning</th>
-      <td>140</td>
+      <th class="e-table__cell" scope="row">Arbeid relatert til booking av avlesning</th>
+      <td class="e-table__cell">140</td>
     </tr>
     <tr>
-      <th scope="row">Årlig kontrollavlesning hos kunde, inkl. kjøring</th>
-      <td>850</td>
+      <th class="e-table__cell" scope="row">Årlig kontrollavlesning hos kunde, inkl. kjøring</th>
+      <td class="e-table__cell">850</td>
     </tr>
     <tr>
-      <th scope="row">Arbeidsledelse og planlegging</th>
-      <td>150</td>
+      <th class="e-table__cell" scope="row">Arbeidsledelse og planlegging</th>
+      <td class="e-table__cell">150</td>
     </tr>
     <tr>
-      <th scope="row">Fradrag for besparelser i AMS-kostnader</th>
-      <td>-25</td>
+      <th class="e-table__cell" scope="row">Fradrag for besparelser i AMS-kostnader</th>
+      <td class="e-table__cell">-25</td>
     </tr>
   </tbody>
 </table>
@@ -51,41 +51,41 @@ export class TableDocComponent implements OnInit {
   <thead>
     <tr>
       <th class="e-table__cell" scope="col">Sikringsstørrelse</th>
-      <th scope="col" class="e-text-right">230 V (2-fase)</th>
-      <th scope="col" class="e-text-right">230 V (3-fase)</th>
-      <th scope="col" class="e-text-right">400 V (1-fase)</th>
-      <th scope="col" class="e-text-right">400 V (3-fase)</th>
+      <th class="e-table__cell" scope="col" class="e-text-right">230 V (2-fase)</th>
+      <th class="e-table__cell" scope="col" class="e-text-right">230 V (3-fase)</th>
+      <th class="e-table__cell" scope="col" class="e-text-right">400 V (1-fase)</th>
+      <th class="e-table__cell" scope="col" class="e-text-right">400 V (3-fase)</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">16 A</th>
-      <td class="e-table__cell--multiline e-text-right">340 kr/mnd.
+      <th class="e-table__cell" scope="row">16 A</th>
+      <td class="e-table__cell e-table__cell--multiline e-text-right">340 kr/mnd.
         This cell exceeds 300px, so the class 'e-table__cell--multiline' should be added.</td>
-      <td class="e-text-right">1065 kr/mnd</td>
-      <td class="e-text-right">900 kr/mnd</td>
-      <td class="e-text-right">900 kr/mnd</td>
+      <td class="e-table__cell e-text-right">1065 kr/mnd</td>
+      <td class="e-table__cell e-text-right">900 kr/mnd</td>
+      <td class="e-table__cell e-text-right">900 kr/mnd</td>
     </tr>
     <tr>
       <th scope="row">32 A</th>
-      <td class="e-text-right">150 kr/kW/mnd</td>
-      <td class="e-text-right">150 kr/kW/mnd</td>
-      <td class="e-text-right">122 kr/kW/mnd</td>
-      <td class="e-text-right">122 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">150 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">150 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">122 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">122 kr/kW/mnd</td>
     </tr>
     <tr>
       <th scope="row">40 A</th>
-      <td class="e-text-right">80 kr/kW/mnd</td>
-      <td class="e-text-right">80 kr/kW/mnd</td>
-      <td class="e-text-right">52 kr/kW/mnd</td>
-      <td class="e-text-right">52 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">80 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">80 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">52 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">52 kr/kW/mnd</td>
     </tr>
     <tr>
       <th scope="row">50 A</th>
-      <td class="e-text-right">23 kr/kW/mnd</td>
-      <td class="e-text-right">23 kr/kW/mnd</td>
-      <td class="e-text-right">17 kr/kW/mnd</td>
-      <td class="e-text-right">17 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">23 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">23 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">17 kr/kW/mnd</td>
+      <td class="e-table__cell e-text-right">17 kr/kW/mnd</td>
     </tr>
   </tbody>
 </table>
@@ -95,44 +95,44 @@ export class TableDocComponent implements OnInit {
   <table class="e-table-mobile">
     <thead>
       <tr>
-        <th scope="col">Sikringsstørrelse</th>
-        <th scope="col"></th>
+        <th class="e-table-mobile__cell" scope="col">Sikringsstørrelse</th>
+        <th class="e-table-mobile__cell" scope="col"></th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <th scope="col">16 A</th>
-        <th scope="col"></th>
+        <th class="e-table-mobile__cell" scope="col">16 A</th>
+        <th class="e-table-mobile__cell" scope="col"></th>
       </tr>
       <tr>
-        <th scope="row">230 V (2-fase)</td>
-        <td>340 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">230 V (2-fase)</td>
+        <td class="e-table-mobile__cell">340 kr/mnd</td>
       </tr>
       <tr>
-        <th scope="row">230 V (3-fase)</td>
-        <td>1065 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">230 V (3-fase)</td>
+        <td class="e-table-mobile__cell">1065 kr/mnd</td>
       </tr>
       <tr>
-        <th scope="row">400 V (1-fase)</td>
-        <td>900 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">400 V (1-fase)</td>
+        <td class="e-table-mobile__cell">900 kr/mnd</td>
       </tr>
     </tbody>
     <tbody>
       <tr>
-        <th scope="col">32 A</th>
-        <th scope="col"></th>
+        <th class="e-table-mobile__cell" scope="col">32 A</th>
+        <th class="e-table-mobile__cell" scope="col"></th>
       </tr>
       <tr>
-        <th scope="row">230 V (2-fase)</td>
-        <td>340 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">230 V (2-fase)</td>
+        <td class="e-table-mobile__cell">340 kr/mnd</td>
       </tr>
       <tr>
-        <th scope="row">230 V (3-fase)</td>
-        <td>1065 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">230 V (3-fase)</td>
+        <td class="e-table-mobile__cell">1065 kr/mnd</td>
       </tr>
       <tr>
-        <th scope="row">400 V (1-fase)</td>
-        <td>900 kr/mnd</td>
+        <th class="e-table-mobile__cell" scope="row">400 V (1-fase)</td>
+        <td class="e-table-mobile__cell">900 kr/mnd</td>
       </tr>
     </tbody>
   </table>
