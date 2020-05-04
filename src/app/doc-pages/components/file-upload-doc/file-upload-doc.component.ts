83da4bb30a8a764b20e215407b4b12a3e66bee5e
Martin Espeland <m.espeland89@gmail.com>
2020-05-04 13:47:31 +0200

    Inital live example donw with angular directive

--- src/app/doc-pages/components/file-upload-doc/file-upload-doc.component.ts
+++ src/app/doc-pages/components/file-upload-doc/file-upload-doc.component.ts
@@ -6,12 +6,13 @@ import { getComponent } from 'src/app/shared/e-items';
   templateUrl: './file-upload-doc.component.html',
   styleUrls: ['./file-upload-doc.component.scss']
 })
-export class FileUploadDocComponent implements OnInit {
+export class FileUploadDocComponent {
 
   externalUrl = getComponent('fileUpload-doc').externalUrl;
   componentStatus = getComponent('fileUpload-doc').status;
   fileUploadClasses = ['e-fileupload', 'e-fileupload__icon-container', 'e-fileupload__description', 'e-fileupload__input'];
 
+  // Code Block example code
   example1 = `<div class="e-fileupload" style="width:380px">
   <div class="e-fileupload__icon-container">
     <i class="e-icon e-icon--file-add"></i>
@@ -26,9 +27,65 @@ export class FileUploadDocComponent implements OnInit {
 </div>
 `;
 
-  constructor() { }
+  example2 = `<div
+class="e-fileupload"
+style="width:380px"
+appDnd
+>
+  <div class="e-fileupload__icon-container">
+    <i class="e-icon e-icon--file-add"></i>
+  </div>
+  <div class="e-fileupload__description">
+    Dra og slipp eller
+  </div>
+  <div class="e-fileupload__input">
+    <input
+    type="file"
+    id="fileUploadTemplate"
+    >
+    <label for="fileUploadTemplate"> <i class="e-icon e-icon--download"></i> Velg fil</label>
+  </div>
+</div>`;
+example3 = `
+import {
+    Directive,
+    Output,
+    Input,
+    EventEmitter,
+    HostBinding,
+    HostListener
+} from '@angular/core';
 
-  ngOnInit() {
-  }
+@Directive({
+    selector: '[appDnd]'
+})
+export class DndDirective {
+    @HostBinding('class.e-fileupload---hover') fileOver: boolean;
+    @Output() fileDropped = new EventEmitter<any>();
+
+    // Dragover listener
+    @HostListener('dragover', ['$event']) onDragOver(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        this.fileOver = true;
+    }
+
+    // Dragleave listener
+    @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        this.fileOver = false;
+    }
+
+    // Drop listener
+    @HostListener('drop', ['$event']) public ondrop(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        this.fileOver = false;
+        alert('Files got dropped! Don´t worry, we do not store anything that you dropped here =)');
+    }
+}
+`;
+
 
 }
--- src/app/doc-pages/components/file-upload-doc/file-upload-doc.module.ts
+++ src/app/doc-pages/components/file-upload-doc/file-upload-doc.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FileUploadDocComponent } from './file-upload-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
 import { CodeBlockModule } from 'src/app/shared/code-block/code-block.module';
+import { DndDirective } from './dnd.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentHeaderModule,
-    CodeBlockModule
+    CodeBlockModule,
   ],
-  declarations: [FileUploadDocComponent]
+  declarations: [FileUploadDocComponent, DndDirective]
 })
 export class FileUploadDocModule { }
