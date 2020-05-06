0f29c918620c79aa0e7353635f2daf651175bdc1
Fride Skarseth <fs@computas.com>
2020-05-06 11:35:05 +0200

    Popover live examples

--- src/app/doc-pages/components/popover-doc/popover-doc.component.ts
+++ src/app/doc-pages/components/popover-doc/popover-doc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { getComponent } from 'src/app/shared/e-items';
 
 @Component({
@@ -7,6 +7,8 @@ import { getComponent } from 'src/app/shared/e-items';
   styleUrls: ['./popover-doc.component.scss']
 })
 export class PopoverDocComponent implements OnInit {
+  @ViewChild('popover1') popover1: ElementRef;
+  @ViewChild('popover2') popover2: ElementRef;
 
   componentStatus = getComponent('popover-doc').status;
 
@@ -56,9 +58,41 @@ export class PopoverDocComponent implements OnInit {
 </div>
 `;
 
+  popupLiveCode = `function openPopover(popoverId) {
+  const el = document.querySelector('popoverId');
+  el.classList.add('e-popover---visible');
+}
+function closePopover(popoverId) {
+  const el = document.querySelector('popoverId');
+  if (el.classList.contains('e-popover---visible')) {
+    el.classList.remove('e-popover---visible');
+  }
+}`;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openPopover(popover: string) {
+    if (popover === 'popover1') {
+      this.popover1.nativeElement.classList.add('e-popover---visible');
+    }
+    if (popover === 'popover2') {
+      this.popover2.nativeElement.classList.add('e-popover---visible');
+    }
+  }
+  closePopover(popover: string) {
+    if (popover === 'popover1') {
+      if (this.popover1.nativeElement.classList.contains('e-popover---visible')) {
+        this.popover1.nativeElement.classList.remove('e-popover---visible');
+      }
+    }
+    if (popover === 'popover2') {
+      if (this.popover2.nativeElement.classList.contains('e-popover---visible')) {
+        this.popover2.nativeElement.classList.remove('e-popover---visible');
+      }
+    }
   }
 
 }
