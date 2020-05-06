0da35fca387deb8933f76afe1cc70eb675fb3547
Martin Espeland <m.espeland89@gmail.com>
2020-05-06 16:42:24 +0200

    Updated Icons example

--- src/app/doc-pages/components/list-doc/list-doc.component.ts
+++ src/app/doc-pages/components/list-doc/list-doc.component.ts
@@ -41,19 +41,19 @@ export class ListDocComponent implements OnInit {
 
   example4 = `<ol class="e-list e-list--icons">
   <li>
-    <span class="e-list__icon"><i class="e-icon e-icon--step-1 e-icon--md"></i></span>
-    <b>Describing title :</b> After, one or multiple sentences could follow with more information.
-    These sentences can span over multiple rows, but shouldn’t be too long.
+    <span class="e-list__icon"><i class="e-icon e-icon e-icon--powermeter-ams e-icon--md"></i></span>
+    <strong>Slik fungerer strømmåleren:</strong> Gjelder etablering og montering av nye målere og ved bytte av eksisterende målere.
   </li>
   <li>
-    <span class="e-list__icon"><i class="e-icon e-icon--step-2 e-icon--md"></i></span>
-    <strong>This can also be the start of a sentence</strong> that continues in thinner font.
-    The bold part should be describing and the rest of the text informative.
+    <span class="e-list__icon"><i class="e-icon e-icon e-icon--han-color e-icon--md"></i></span>
+    <strong>Han Porten:</strong> Ønsker du detaljert informasjon om strømforbruket ditt?
+    Vil du styre strømforbruket ditt bedre? Da kan det være en idé å bestille åpning av HAN-porten på strømmåleren din.
   </li>
   <li>
-    <span class="e-list__icon"><i class="e-icon e-icon--step-3 e-icon--md"></i></span>
-    <strong>Don’t use too many points</strong> and make sure the text is easy to read,
-    informative and follows Elvia’s tone of voice.
+    <span class="e-list__icon"><i class="e-icon e-icon e-icon--powermeter-old e-icon--md"></i></span>
+    <strong>Priser på målere og utstyr:</strong> Gjelder etablering og montering av nye målere og ved bytte av eksisterende målere.
+
+
   </li>
 </ol>
 `;
