2ac8cca0f9975ef5247ddd46b992a28c7a6ac71f
Martin Espeland <m.espeland89@gmail.com>
2020-05-08 09:13:42 +0200

    Autocomplete docpages inital setup

--- src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
+++ src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { getComponent } from 'src/app/shared/e-items';
 
 @Component({
   selector: 'app-autocomplete-doc',
   templateUrl: './autocomplete-doc.component.html',
-  styleUrls: ['./autocomplete-doc.component.css']
+  styleUrls: ['./autocomplete-doc.component.scss']
 })
 export class AutocompleteDocComponent implements OnInit {
+
+  externalUrl = getComponent('autocomplete-doc').externalUrl;
+  componentStatus = getComponent('autocomplete-doc').status;
+
+  example1 = `testing`;
+
+  example2 = `testing 2`;
 
   constructor() { }
 
--- src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.module.ts
+++ src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AutocompleteDocComponent } from './autocomplete-doc.component';
+import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
+import { CodeBlockModule } from 'src/app/shared/code-block/code-block.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentHeaderModule,
+    CodeBlockModule,
   ],
   declarations: [AutocompleteDocComponent]
 })
