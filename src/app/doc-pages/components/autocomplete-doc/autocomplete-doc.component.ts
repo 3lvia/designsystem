8ed299a4aae1392290263aaa7e9960a0347e3858
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-11 16:48:15 +0200

    Added autcomplete style again

--- src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
+++ src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
@@ -11,9 +11,29 @@ export class AutocompleteDocComponent implements OnInit {
   externalUrl = getComponent('autocomplete-doc').externalUrl;
   componentStatus = getComponent('autocomplete-doc').status;
 
-  example1 = `testing`;
+  example1 = `<div class="e-autocomplete">
+    <input placeholder="Placeholder text"></input>
+  </div>`;
 
-  example2 = `testing 2`;
+  example2 = `<div class="e-autocomplete">
+  <input placeholder="Placeholder text" value="Aus"></input>
+
+  <div class="e-autocomplete__content">
+    <span class="e-autocomplete__content__item">Australia</span>
+    <span class="e-autocomplete__content__item">Austria</span>
+  </div>
+</div>`;
+
+example3 = `<div class="e-autocomplete e-mt-16">
+<input placeholder="Normal state" value="Normal state"></input>
+</div>
+<div class="e-autocomplete e-autocomplete---active e-mt-16">
+<input placeholder="Active/Focus state" value="Active/Focus state"></input>
+</div>
+<div class="e-autocomplete e-autocomplete---invalid e-mt-16">
+<input placeholder="Invalid error state" value="Invalid error state"></input>
+</div>
+`;
 
   constructor() { }
 
