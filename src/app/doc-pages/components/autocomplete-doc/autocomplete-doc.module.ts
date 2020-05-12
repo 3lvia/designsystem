ab8c1ae4608bd95c875cdbc511770ea772ea3a5e
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-12 19:21:00 +0200

    Updated autocomplete style and example code

--- src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.module.ts
+++ src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AutocompleteDocComponent } from './autocomplete-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
 import { CodeBlockModule } from 'src/app/shared/code-block/code-block.module';
+import { CodeHighlighterModule } from 'src/app/shared/code-highlighter/code-highlighter.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentHeaderModule,
     CodeBlockModule,
+    CodeHighlighterModule
   ],
   declarations: [AutocompleteDocComponent]
 })
