cfe9b9b24e95ad2204289126b19363bd7251c985
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-13 14:46:16 +0200

    Added new icons, updated style and "live" examples

--- src/app/doc-pages/components/position-picker-doc/position-picker-doc.component.ts
+++ src/app/doc-pages/components/position-picker-doc/position-picker-doc.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { getComponent } from 'src/app/shared/e-items';
 
 @Component({
@@ -11,20 +11,22 @@ export class PositionPickerDocComponent implements OnInit {
   externalUrl = getComponent('position-picker-doc').externalUrl;
   componentStatus = getComponent('position-picker-doc').status;
 
+  @ViewChild('mapModal') mapModal: ElementRef;
+
+  example1Classes = ['e-position-picker', 'e-position-picker__icon', 'e-position-picker__action'];
+
   example1 = `<div class="e-position-picker" style="width:380px">
   <div class="e-position-picker__icon">
-    <span class="e-btn__icon"><i class="e-icon e-icon--modern-house-color"></i></span>
+    <span class="e-btn__icon"><i class="e-icon e-icon--map-pin"></i></span>
   </div>
   <div class="e-position-picker__action">
     <button class="e-btn e-btn--tertiary e-btn--lg e-m-16">
-      <span class="e-btn__icon"><i class="e-icon e-icon--download"></i></span>
+      <span class="e-btn__icon"><i class="e-icon e-icon--pin"></i></span>
       <span class="e-btn__title">Velg plassering</span>
     </button>
   </div>
 </div>
 `;
-
-
 
   example2 = `<div class="e-position-picker" style="width:380px">
   <div class="e-position-picker__description">
@@ -32,17 +34,30 @@ export class PositionPickerDocComponent implements OnInit {
   </div>
   <div class="e-position-picker__action">
     <button class="e-btn e-btn--tertiary e-btn--lg e-m-16">
-      <span class="e-btn__icon"><i class="e-icon e-icon--download"></i></span>
+      <span class="e-btn__icon"><i class="e-icon e-icon e-icon--pin"></i></span>
       <span class="e-btn__title">Endre plassering</span>
     </button>
   </div>
 </div>
 `;
 
-
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openMapExample(modal: string) {
+    if (modal === 'mapModal') {
+      this.mapModal.nativeElement.classList.add('e-modal---visible');
+    }
+  }
+
+  closeModal(modal: string) {
+    if (modal === 'mapModal') {
+      if (this.mapModal.nativeElement.classList.contains('e-modal---visible')) {
+        this.mapModal.nativeElement.classList.remove('e-modal---visible');
+      }
+    }
   }
 
 }
