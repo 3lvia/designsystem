09ca173fb595ae97bebce038e73677505b5ec2f3
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-13 10:02:15 +0200

    initial setup

---
+++ src/app/doc-pages/components/position-picker-doc/position-picker-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-position-picker-doc',
+  templateUrl: './position-picker-doc.component.html',
+  styleUrls: ['./position-picker-doc.component.scss']
+})
+export class PositionPickerDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/doc-pages/components/position-picker-doc/position-picker-doc.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PositionPickerDocComponent } from './position-picker-doc.component';
+import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
+import { CodeBlockModule } from 'src/app/shared/code-block/code-block.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ComponentHeaderModule,
+    CodeBlockModule
+  ],
+  declarations: [PositionPickerDocComponent]
+})
+export class PositionPickerDocModule { }
