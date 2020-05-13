e6990f6dc7894cba7fde5cf66c757595655b3455
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-13 16:52:31 +0200

    Update Example with code illustration

--- src/app/doc-pages/components/position-picker-doc/position-picker-doc.component.ts
+++ src/app/doc-pages/components/position-picker-doc/position-picker-doc.component.ts
@@ -42,6 +42,85 @@ export class PositionPickerDocComponent implements OnInit {
 </div>
 `;
 
+exampleTSCode = `
+@ViewChild('mapModal') mapModal: ElementRef;
+ShowPosition = false;
+
+openMapExample(modal: string) {
+  if (modal === 'mapModal') {
+    this.mapModal.nativeElement.classList.add('e-modal---visible');
+  }
+}
+
+closeModal(modal: string) {
+  if (modal === 'mapModal') {
+    if (this.mapModal.nativeElement.classList.contains('e-modal---visible')) {
+      this.mapModal.nativeElement.classList.remove('e-modal---visible');
+      this.ShowPosition = true;
+    }
+  }
+}
+`;
+
+exampleHTMLCode = `  <div class="e-position-picker" style="width:380px">
+<div class="e-position-picker__icon" *ngIf="!ShowPosition">
+  <span class="e-btn__icon"><i class="e-icon e-icon--map-pin"></i></span>
+</div>
+<div class="e-position-picker__description" *ngIf="ShowPosition">
+  60.026676, 10.798887
+</div>
+<div class="e-position-picker__action">
+  <button class="e-btn e-btn--tertiary e-btn--lg e-m-16" (click)="openMapExample('mapModal')">
+    <span class="e-btn__icon"><i class="e-icon e-icon--pin"></i></span>
+    <span class="e-btn__title">Velg plassering</span>
+  </button>
+</div>
+</div>
+<div class="e-modal" #mapModal>
+<div class="e-modal__content">
+  <button class="e-btn e-btn--icon" (click)="closeModal('mapModal')" id="examplebutton">
+    <span class="e-btn__icon"><i class="e-icon e-icon--remove-circle-color"></i></span>
+  </button>
+  <iframe
+ src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d7998.985919234493!2d10.685827403470268!3d59.9197547324057!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sno!4v1589372574967!5m2!1sen!2sno"
+  width="600"
+  height="450"
+  frameborder="0"
+  style="border:0;"
+  allowfullscreen=""
+  aria-hidden="false"
+  tabindex="0">
+</iframe>  <div class="e-form-field" id="exampleInput">
+    <div class="e-input">
+      <input id="normal" type="text" placeholder="Velg Adresse">
+      <i class="e-icon e-icon--search e-icon--xs"></i>
+    </div>
+  </div>
+</div>
+`;
+
+exampleSCSS = `
+iframe {
+  position: relative;
+}
+
+#examplebutton {
+  position: absolute;
+  z-index: 100000000;
+  top:1%;
+  right:1%;
+}
+
+#exampleInput {
+  position: absolute;
+  z-index: 100000000;
+  bottom:11%;
+  align-self: center;
+  width: 50%;
+}
+
+`;
+
   constructor() { }
 
   ngOnInit() {
