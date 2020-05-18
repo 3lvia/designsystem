65fa206c3c5f3f01b976786fd7a5ee67b169d0dc
Fride Skarseth <fs@computas.com>
2020-05-18 13:38:07 +0200

    Examples of input with date and time

--- src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.component.ts
+++ src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.component.ts
@@ -11,6 +11,22 @@ export class DatetimePickerDocComponent implements OnInit {
   externalUrl = getComponent('datetime-picker-doc').externalUrl;
   componentStatus = getComponent('datetime-picker-doc').status;
 
+  inputTimeExample = `<div class="e-form-field" style="width:140px">
+  <label class="e-form-field__label" for="normal">Normal input field </label>
+  <div class="e-input">
+    <i class="e-icon e-icon--clock e-icon--xs"></i>
+    <input id="normal" type="time" placeholder="">
+  </div>
+</div>`;
+
+  inputDateExample = `<div class="e-form-field" style="width:250px">
+  <label class="e-form-field__label" for="normal">Normal input field </label>
+  <div class="e-input">
+    <i class="e-icon e-icon--calendar e-icon--xs"></i>
+    <input id="normal" type="date" placeholder="">
+  </div>
+</div>`;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.module.ts
+++ src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DatetimePickerDocComponent } from './datetime-picker-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
 import { RouterModule } from '@angular/router';
+import { CodeBlockModule } from 'src/app/shared/code-block/code-block.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentHeaderModule,
-    RouterModule
+    RouterModule,
+    CodeBlockModule
   ],
   declarations: [DatetimePickerDocComponent]
 })
