745e05603a81dc8ec65f17f7cbef721cb5919df5
Fride Skarseth <fs@computas.com>
2020-05-18 10:42:37 +0200

    Datepicker documentation

--- src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.module.ts
+++ src/app/doc-pages/components/forms/datetime-picker-doc/datetime-picker-doc.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatetimePickerDocComponent } from './datetime-picker-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-header/component-header.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    ComponentHeaderModule
+    ComponentHeaderModule,
+    RouterModule
   ],
   declarations: [DatetimePickerDocComponent]
 })
