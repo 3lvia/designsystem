697cdaaa84d445d3587afe8310b40be83a76888a
Martin A. Espeland <m.espeland89@gmail.com>
2020-05-20 11:06:29 +0200

    Updated documentation

--- src/app/doc-pages/components/tooltip-doc/tooltip-doc.component.ts
+++ src/app/doc-pages/components/tooltip-doc/tooltip-doc.component.ts
@@ -11,18 +11,12 @@ export class TooltipDocComponent implements OnInit {
   externalUrl = getComponent('tooltip-doc').externalUrl;
   componentStatus = getComponent('tooltip-doc').status;
 
+  classes = ['e-tooltip', 'e-tooltip__content', 'e-tooltip__content--top', 'e-tooltip__content--bottom'];
+
   example1 = `<div style="display:flex;align-items: center;justify-content: space-around;height: 100px;width: 100%;">
 
   <span class="e-tooltip" tabindex=0>
     <i class="e-icon e-icon--information-circle e-icon--sm"></i>
-    <span class="e-tooltip__content">Above </span>
-  </span>
-  <span class="e-tooltip" tabindex=0>
-    <i class="e-icon e-icon--information-circle e-icon--lg"></i>
-    <span class="e-tooltip__content">Above </span>
-  </span>
-  <span class="e-tooltip" tabindex=0>
-    <i class="e-icon e-icon--information-circle e-icon--xxl"></i>
     <span class="e-tooltip__content">Above </span>
   </span>
 
@@ -31,9 +25,6 @@ export class TooltipDocComponent implements OnInit {
     <span class="e-tooltip__content e-tooltip__content--bottom">Below</span>
   </span>
   </div>
-
-
-
 `;
 
 constructor() { }
