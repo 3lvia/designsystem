fd27aafcb5e9e039f25bbe95de34567dba5da57f
Martin A. Espeland <m.espeland89@gmail.com>
2020-06-03 08:54:12 +0200

    updates & fixes for doc pages (#159)

--- src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
+++ src/app/doc-pages/components/autocomplete-doc/autocomplete-doc.component.ts
@@ -13,8 +13,8 @@ export class AutocompleteDocComponent implements OnInit {
   does = ['When you have many options in a list and the input must be validated'];
 
   example1 = `<div class="e-autocomplete">
-    <input placeholder="Placeholder text"/>
-  </div>`;
+  <input placeholder="Placeholder text"/>
+</div>`;
 
   example2 = `<div class="e-autocomplete" style="height:150px">
   <input placeholder="Placeholder text" value="Aus"/>
@@ -36,7 +36,7 @@ export class AutocompleteDocComponent implements OnInit {
 </div>
 `;
 
-exampleInHTML= `<div class="e-autocomplete">
+  exampleInHTML = `<div class="e-autocomplete">
   <input type="text" [value]="chosenLand" #searchTerm (keyup)="onSearch(searchTerm.value)" id="ChooseCountry"/>
   <div class="e-autocomplete__content" *ngIf="showResults">
     <div class="e-autocomplete__content__item" *ngFor="let country of results" (click)="SelectCountry(country)">
@@ -45,7 +45,7 @@ exampleInHTML= `<div class="e-autocomplete">
   </div>
 </div>
 `;
-exampleInTS= `results = [];
+  exampleInTS = `results = [];
 showResults = false;
 chosenLand = '';
 
@@ -96,55 +96,55 @@ countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","An
 "Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 `;
 
-results = [];
-showResults = false;
-chosenLand = '';
+  results = [];
+  showResults = false;
+  chosenLand = '';
 
-onSearch(searchTerm:string) {
+  onSearch(searchTerm: string) {
 
-  this.results = this.countries.filter(country => country.includes(searchTerm));
+    this.results = this.countries.filter(country => country.includes(searchTerm));
 
-  if(searchTerm.length === 0) {
-    this.showResults = false;
-  } else if(this.results.length === 0) {
-    this.showResults = false;
-  } else {
-    this.showResults = true;
+    if (searchTerm.length === 0) {
+      this.showResults = false;
+    } else if (this.results.length === 0) {
+      this.showResults = false;
+    } else {
+      this.showResults = true;
+    }
   }
-}
 
-SelectCountry(value:string) {
-  this.chosenLand = value;
-  this.showResults = false;
-}
+  SelectCountry(value: string) {
+    this.chosenLand = value;
+    this.showResults = false;
+  }
 
-countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda",
-"Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh",
-"Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina",
-"Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia",
-"Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China",
-"Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus",
-"Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador",
-"Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland",
-"France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana",
-"Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau",
-"Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland",
-"Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati",
-"Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein",
-"Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta",
-"Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia",
-"Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands",
-"Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea",
-"Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru",
-"Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda",
-"Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal",
-"Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia",
-"South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia",
-"St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan",
-"Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey",
-"Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom",
-"United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam",
-"Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+  countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda',
+    'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh',
+    'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina',
+    'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia',
+    'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central Arfrican Republic', 'Chad', 'Chile', 'China',
+    'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D Ivoire', 'Croatia', 'Cuba', 'Curacao', 'Cyprus',
+    'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador',
+    'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland',
+    'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana',
+    'Gibraltar', 'Greece', 'Greenland', 'Grenada', 'Guam', 'Guatemala', 'Guernsey', 'Guinea', 'Guinea Bissau',
+    'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland',
+    'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
+    'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein',
+    'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta',
+    'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia',
+    'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia', 'Nauro', 'Nepal', 'Netherlands',
+    'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea',
+    'Norway', 'Oman', 'Pakistan', 'Palau', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru',
+    'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda',
+    'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal',
+    'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia',
+    'South Africa', 'South Korea', 'South Sudan', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia',
+    'St Vincent', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan',
+    'Tanzania', 'Thailand', 'Timor L\'Este', 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey',
+    'Turkmenistan', 'Turks &amp; Caicos', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom',
+    'United States of America', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam',
+    'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'];
 
   constructor() { }
 
