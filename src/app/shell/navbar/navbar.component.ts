c13f2d4ab07616598503dde80a5fc0ab532ed06c
Fride Skarseth <fs@computas.com>
2020-06-23 12:07:26 +0200

    docs: reverting anchor routing

--- src/app/shell/navbar/navbar.component.ts
+++ src/app/shell/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { EItems } from 'src/app/shared/e-items.interface';
 import { GlobalService } from 'src/app/core/services/global.service';
 import { ScrollService } from 'src/app/core/services/scroll.service';
 import { NavbarAnchor } from 'src/app/shared/navbarAnchor.interface';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -22,47 +22,16 @@ export class NavbarComponent implements OnInit {
   toggleMenu = false;
   activeAnchor: NavbarAnchor;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private globalService: GlobalService,
-    private scrollService: ScrollService,
-  ) {
+  constructor(private router: Router, private globalService: GlobalService, private scrollService: ScrollService) {
     this.scrollService.listenAnchorAtCurrPos().subscribe((anchor: NavbarAnchor) => {
       this.activeAnchor = anchor;
     });
     this.scrollService.listenAnchors().subscribe((anchors: NavbarAnchor[]) => {
       this.navbarAnchors = anchors;
+      this.activeAnchor = this.navbarAnchors[0];
       this.navbarItems.forEach((item) => {
-        let currentRoute = this.router.url.slice(this.router.url.lastIndexOf('/') + 1);
-        if (currentRoute.includes('#')) {
-          currentRoute = currentRoute.slice(0, currentRoute.indexOf('#'));
-        }
-        if (item.docUrl === currentRoute) {
+        if (item.docUrl === this.router.url.slice(this.router.url.lastIndexOf('/') + 1)) {
           this.activeNavbarItem = item;
-        }
-      });
-
-      this.route.fragment.subscribe(fragment => {
-        if (fragment) {
-          const fragmentExists = this.navbarAnchors.find(anchor => {
-            const anchorRoute = anchor.title.toLocaleLowerCase().replace(/\s/g, '');
-            const fragmentRoute = fragment.toLocaleLowerCase().replace(/\s/g, '');
-            return anchorRoute === fragmentRoute;
-          });
-          if (fragmentExists) {
-            this.navbarAnchors.forEach(anchor => {
-              if (anchor.title.toLocaleLowerCase().replace(/\s/g, '') === fragment.toLocaleLowerCase().replace(/\s/g, '')) {
-                this.activeAnchor = anchor;
-                this.scrollService.scrollToElement(anchor.top);
-                return true;
-              }
-            });
-          } else {
-            this.activeAnchor = this.navbarAnchors[0];
-          }
-        } else {
-          this.activeAnchor = this.navbarAnchors[0];
         }
       });
     });
