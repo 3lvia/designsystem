969b2bd85b4808b3bb10d73d33b3d2c8c8193789
Fride Skarseth <skarseth.fride@gmail.com>
2020-06-24 10:55:09 +0200

    Anchor route (#190)
    * docs: anchor routing when no fragment
    
    * docs: updating anchor route when scrolling
    
    * docs: dash instead of spaces in url

--- src/app/shell/navbar/navbar.component.ts
+++ src/app/shell/navbar/navbar.component.ts
@@ -3,7 +3,8 @@ import { EItems } from 'src/app/shared/e-items.interface';
 import { GlobalService } from 'src/app/core/services/global.service';
 import { ScrollService } from 'src/app/core/services/scroll.service';
 import { NavbarAnchor } from 'src/app/shared/navbarAnchor.interface';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -21,18 +22,60 @@ export class NavbarComponent implements OnInit {
   filteredPages: EItems[];
   toggleMenu = false;
   activeAnchor: NavbarAnchor;
+  prevActiveAnchor: NavbarAnchor;
 
-  constructor(private router: Router, private globalService: GlobalService, private scrollService: ScrollService) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private globalService: GlobalService,
+    private scrollService: ScrollService,
+    private location: Location,
+  ) {
     this.scrollService.listenAnchorAtCurrPos().subscribe((anchor: NavbarAnchor) => {
+      if (this.prevActiveAnchor !== anchor) {
+        this.updateAnchorRoute(anchor);
+      }
+      this.prevActiveAnchor = this.activeAnchor;
       this.activeAnchor = anchor;
     });
+
     this.scrollService.listenAnchors().subscribe((anchors: NavbarAnchor[]) => {
       this.navbarAnchors = anchors;
-      this.activeAnchor = this.navbarAnchors[0];
       this.navbarItems.forEach((item) => {
-        if (item.docUrl === this.router.url.slice(this.router.url.lastIndexOf('/') + 1)) {
+        let currentRoute = this.router.url.slice(this.router.url.lastIndexOf('/') + 1);
+        if (currentRoute.includes('#')) {
+          currentRoute = currentRoute.slice(0, currentRoute.indexOf('#'));
+        }
+        if (item.docUrl === currentRoute) {
           this.activeNavbarItem = item;
         }
+      });
+
+      this.route.fragment.subscribe(fragment => {
+        if (!fragment) {
+          this.activeAnchor = this.navbarAnchors[0];
+          this.updateAnchorRoute(this.activeAnchor);
+          return;
+        }
+        const fragmentExists = this.navbarAnchors.find(anchor => {
+          const anchorRoute = anchor.title.toLocaleLowerCase().split(' ').join('-');
+          const fragmentRoute = fragment.toLocaleLowerCase().split(' ').join('-');
+          return anchorRoute === fragmentRoute;
+        });
+        if (!fragmentExists) {
+          this.activeAnchor = this.navbarAnchors[0];
+          this.updateAnchorRoute(this.activeAnchor);
+          return;
+        }
+        this.navbarAnchors.forEach(anchor => {
+          const anchorRoute = anchor.title.toLocaleLowerCase().split(' ').join('-');
+          const fragmentRoute = fragment.toLocaleLowerCase().split(' ').join('-');
+          if (anchorRoute === fragmentRoute) {
+            this.activeAnchor = anchor;
+            this.scrollService.scrollToElement(anchor.top);
+            this.updateAnchorRoute(this.activeAnchor);
+          }
+        });
       });
     });
   }
@@ -48,7 +91,16 @@ export class NavbarComponent implements OnInit {
     return this.globalService.headerWarning.show;
   }
 
-  markNewActiveAnchor(navbarItem: EItems): void {
+  updateAnchorRoute(anchor: NavbarAnchor): void {
+    let currentRoute = this.router.url;
+    if (currentRoute.includes('#')) {
+      currentRoute = currentRoute.slice(0, currentRoute.indexOf('#'));
+    }
+    const fragment = anchor.title.split(' ').join('-');
+    this.location.replaceState(currentRoute + '#' + fragment);
+  }
+
+  markNewActiveNavbarItem(navbarItem: EItems): void {
     if (navbarItem === this.activeNavbarItem) {
       return;
     }
@@ -60,5 +112,6 @@ export class NavbarComponent implements OnInit {
   scrollToElement(anchor: NavbarAnchor): void {
     this.activeAnchor = anchor;
     this.scrollService.newAnchorToScrollTo(anchor);
+    this.updateAnchorRoute(this.activeAnchor);
   }
 }
