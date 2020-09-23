0bc8f28a60ae1a1d03a80aeed929914101faf76f
Fride Skarseth <fs@computas.com>
2020-09-23 14:57:42 +0200

    docs: updates contact page

--- src/app/doc-pages/community/contact/contact.component.ts
+++ src/app/doc-pages/community/contact/contact.component.ts
@@ -9,5 +9,44 @@ import { getCommunity } from 'src/app/shared/e-items';
 export class ContactComponent {
 
   description = getCommunity('contact').description;
+  loadedImgMic = false;
+  loadedImgPat = false;
+  loadedImgFri = false;
+  loadedImgMar = false;
+  loadedImgHil = false;
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  hideContentLoaderMic(evt: any): void {
+    if (evt && evt.target) {
+      this.loadedImgMic = true;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  hideContentLoaderPat(evt: any): void {
+    if (evt && evt.target) {
+      this.loadedImgPat = true;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  hideContentLoaderFri(evt: any): void {
+    if (evt && evt.target) {
+      this.loadedImgFri = true;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  hideContentLoaderMar(evt: any): void {
+    if (evt && evt.target) {
+      this.loadedImgMar = true;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  hideContentLoaderHil(evt: any): void {
+    if (evt && evt.target) {
+      this.loadedImgHil = true;
+    }
+  }
 }
