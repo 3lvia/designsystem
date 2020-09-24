c081cdfc7a5264c7b8924edcc36ce43f82c8fa26
Fride Skarseth <fs@computas.com>
2020-09-24 11:34:05 +0200

    docs: doc for designers updated

--- src/app/doc-pages/get-started/get-started-designers/get-started-designers.module.ts
+++ src/app/doc-pages/get-started/get-started-designers/get-started-designers.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { GetStartedDesignersComponent } from './get-started-designers.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-structure/component-header/component-header.module';
 import { ComponentSectionModule } from 'src/app/shared/component-structure/component-section/component-section.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentHeaderModule,
     ComponentSectionModule,
+    RouterModule,
   ],
   declarations: [GetStartedDesignersComponent]
 })
