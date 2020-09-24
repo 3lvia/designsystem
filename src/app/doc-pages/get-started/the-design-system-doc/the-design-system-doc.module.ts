0003e784469e023f2bc097983005a36ee0bdfb8e
Martin Espeland <m.espeland89@gmail.com>
2020-09-24 11:37:25 +0200

    docs(cleanup): removed unused modules in the desisgn system doc page

--- src/app/doc-pages/get-started/the-design-system-doc/the-design-system-doc.module.ts
+++ src/app/doc-pages/get-started/the-design-system-doc/the-design-system-doc.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TheDesignSystemDocComponent } from './the-design-system-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-structure/component-header/component-header.module';
-import { CopyModule } from 'src/app/shared/copy/copy.module';
-import { CodeHighlighterModule } from 'src/app/shared/code-highlighter/code-highlighter.module';
 import { ComponentSectionModule } from 'src/app/shared/component-structure/component-section/component-section.module';
-import { ComponentSubsectionModule } from 'src/app/shared/component-structure/component-subsection/component-subsection.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     ComponentHeaderModule,
-    CopyModule,
-    CodeHighlighterModule,
     ComponentSectionModule,
-    ComponentSubsectionModule,
     RouterModule,
   ],
   declarations: [TheDesignSystemDocComponent]
