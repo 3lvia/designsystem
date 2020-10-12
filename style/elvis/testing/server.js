fb804c774d05200d83be17934054903f41951164
Martin Espeland <m.espeland89@gmail.com>
2020-10-12 10:43:06 +0200

    build(percy): removed nohup, created new folder for percy index

--- style/elvis/testing/server.js
+++ style/elvis/testing/server.js
@@ -5,7 +5,7 @@ const server = require('http').createServer(app);
 
 
 const port = process.env.PORT || 3333;
-app.use(express.static('dist/'));
+app.use(express.static('percy/'));
 app.use("/css/elvis.min.css", express.static(__dirname + '/../css/elvis.min.css'));
 app.use("/elvis.js", express.static(__dirname + '/../elvis.js'));
 server.listen(port);
