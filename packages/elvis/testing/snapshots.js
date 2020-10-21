ac5a569485d47b172e4edd40b5d1230bdac66e7b
Martin Espeland <m.espeland89@gmail.com>
2020-10-21 15:01:21 +0200

    test: updated percy snapshot.js

--- packages/elvis/testing/snapshots.js
+++ packages/elvis/testing/snapshots.js
@@ -2,13 +2,9 @@ const PercyScript = require('@percy/script');
 const breakpoints = [766, 1440];
 
 PercyScript.run(async (page, percySnapshot) => {
-    await page.await(2000)
+    await page.waitFor(2000)
     await page.goto('http://localhost:3333/');
-    await page.await(2000)
-
-    //clicakble components
-    await page.click('span[id="tooltip-top"]');
-    await page.click('span[id="tooltip-bottom"]');
+    await page.waitFor(2000)
     // Homepage
     await percySnapshot('Stilbibliotek', { widths: breakpoints });
 
