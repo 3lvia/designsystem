793ba96e44fafc8c29341973f9116c09466cf29d
Martin Espeland <m.espeland89@gmail.com>
2020-10-21 15:12:54 +0200

    test(percy snapshot): replaced depreccated waitFor function

--- packages/elvis/testing/snapshots.js
+++ packages/elvis/testing/snapshots.js
@@ -2,10 +2,9 @@ const PercyScript = require('@percy/script');
 const breakpoints = [766, 1440];
 
 PercyScript.run(async (page, percySnapshot) => {
-    await page.waitFor(2000)
+    await page.waitForTimeout(2000)
     await page.goto('http://localhost:3333/');
-    await page.waitFor(2000)
+    await page.waitForTimeout(2000)
     // Homepage
     await percySnapshot('Stilbibliotek', { widths: breakpoints });
-
 }); 
