7f86ece07579f6f81660dbc9d08cb129c49739c2
Martin Espeland <m.espeland89@gmail.com>
2020-10-21 14:57:42 +0200

    test: added test examples through iframes and update snapshot.js

--- packages/elvis/testing/snapshots.js
+++ packages/elvis/testing/snapshots.js
@@ -2,9 +2,9 @@ const PercyScript = require('@percy/script');
 const breakpoints = [766, 1440];
 
 PercyScript.run(async (page, percySnapshot) => {
-    await page.waitFor(2000)
+    await page.await(2000)
     await page.goto('http://localhost:3333/');
-    await page.waitFor(2000)
+    await page.await(2000)
 
     //clicakble components
     await page.click('span[id="tooltip-top"]');
