af2875d13fa6e98ed9080f610ee7e81547084b9e
Martin Espeland <m.espeland89@gmail.com>
2020-10-22 09:35:29 +0200

    update percy snapshow

--- packages/elvis/testing/snapshots.js
+++ packages/elvis/testing/snapshots.js
@@ -1,10 +1,11 @@
 const PercyScript = require('@percy/script');
 const breakpoints = [766, 1440];
+const minHeight = '100%';
 
 PercyScript.run(async (page, percySnapshot) => {
     await page.waitForTimeout(2000)
     await page.goto('http://localhost:3333/');
     await page.waitForTimeout(2000)
     // Homepage
-    await percySnapshot('Stilbibliotek', { widths: breakpoints });
+    await percySnapshot('Stilbibliotek', { widths: breakpoints, minHeight: minHeight });
 }); 
