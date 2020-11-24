da7f7185c8efcb808a800c264270daf37449b7ac
Martin Espeland <m.espeland89@gmail.com>
2020-11-24 09:48:38 +0100

    feat(error page): first draft of error page

--- packages/web/src/app/shell/main/main.component.ts
+++ packages/web/src/app/shell/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-main',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
+  bgClass = '';
+
+  constructor(
+    private router: Router,
+  ) {
+    // subscribe to router navigation
+    this.router.events.subscribe(event => {
+      // filter `NavigationEnd` events
+      if (event instanceof NavigationEnd) {
+        // get current route without leading slash `/`
+        const eventUrl = /(?<=\/).+/.exec(event.urlAfterRedirects);
+        const currentRoute = (eventUrl || []).join('');
+        // set bgClass property with the value of the current route
+        if (currentRoute === 'not-found') {
+          this.bgClass = currentRoute;
+        } else {
+          this.bgClass = '';
+        }
+      }
+    });
+  }
 }
