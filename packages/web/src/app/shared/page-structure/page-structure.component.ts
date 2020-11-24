88dea4ccfd33fbdbd2cf3e21c4cd9cf2e885d422
Fride Skarseth <fs@computas.com>
2020-11-24 09:41:51 +0100

    docs(feedback): link to feedback section

--- packages/web/src/app/shared/page-structure/page-structure.component.ts
+++ packages/web/src/app/shared/page-structure/page-structure.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { EItems } from '../e-items.interface';
 import { SectionAnimation, slideIn } from 'src/app/shared/animations';
+import { ScrollService } from 'src/app/core/services/scroll.service';
 
 @Component({
   selector: 'app-page-structure',
@@ -11,5 +12,12 @@ import { SectionAnimation, slideIn } from 'src/app/shared/animations';
 export class PageStructureComponent {
 
   @Input() pages: EItems[] = [];
+
+  constructor(private scrollService: ScrollService) { }
+
+  scrollToFeedback(): void {
+    const offsetTop = document.body.scrollHeight;
+    this.scrollService.scrollToElement(offsetTop);
+  }
 
 }
