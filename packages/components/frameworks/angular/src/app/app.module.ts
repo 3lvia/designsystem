0c8d12a2a86bea3363ed684b7d20f81d83ed921d
Martin Espeland <m.espeland89@gmail.com>
2020-11-30 16:42:30 +0100

    feat: added progressvar to angular preview

--- packages/components/frameworks/angular/src/app/app.module.ts
+++ packages/components/frameworks/angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 
 import '@elvia/elvis-checkbox';
 import '@elvia/elvis-popover';
+import '@elvia/elvis-progressbar';
 
 @NgModule({
   declarations: [
