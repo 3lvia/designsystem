f5593e2b5510fbdc281adb8437b0be80c8e05197
Martin Espeland <m.espeland89@gmail.com>
2020-12-01 10:36:31 +0100

    feat: progressbar with state

--- packages/components/components/elvis-progressbar/src/react/elvis-progressbar.tsx
+++ packages/components/components/elvis-progressbar/src/react/elvis-progressbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './style.scss';
 
 export interface ProgressbarProps {
@@ -16,11 +16,22 @@ const Progressbar: React.FC<ProgressbarProps> = ({ }) => {
     link.type = 'text/css';
     document.head.appendChild(link);
   });
+  // inital state
+  const [percentRange, setProgress] = useState(0);
+
 
   return (
     <span>
-      <div className="ewc-progress">
-        <div className="ewc-progress__bar"></div>
+      <div className="ewc-progress--linear">
+          <div className="ewc-progress--linear__range" style={{width: `${percentRange}%`}}></div>
+      </div>
+    
+      <div style={{margin: "20px"}}>
+                  <button onClick={() => setProgress(percentRange > 0 ?
+                    percentRange - 10 : 0)}>Decrease
+                </button>
+                <button onClick={() => setProgress(percentRange < 100 ? percentRange + 10 : 100)}>Increase</button>
+                <button onClick={() => setProgress(0)}>Reset</button>
       </div>
     </span>
   );
