a3e40c12ab0d675747a64e9f3a950546ccd72943
Martin Espeland <m.espeland89@gmail.com>
2020-12-03 10:17:19 +0100

    docs: updated angular preview of elvis-progressbar

--- packages/components/frameworks/angular/src/app/app.component.ts
+++ packages/components/frameworks/angular/src/app/app.component.ts
@@ -9,6 +9,11 @@ export class AppComponent implements OnInit {
   @ViewChild('checkbox1', { static: true }) checkbox: any;
   checkBoxVal = true;
 
+  @ViewChild('progressbarExamp', { static: true }) progressbar: any;
+  progressValue = 10;
+  indeterminate = false;
+  progressError = false;
+
   ngOnInit(): void {
     // Listen checkbox changes
     this.checkbox.nativeElement.addEventListener('props-changed', (event: any) => {
@@ -16,5 +21,14 @@ export class AppComponent implements OnInit {
     });
     this.checkbox.nativeElement.setProps({ checked: true });
     this.checkbox.nativeElement.getProps();
+
+    // Listen progressbar changes
+    this.progressbar.nativeElement.addEventListener('props-changed', (event: any) => {
+      this.progressValue = event.detail.rangeValue;
+      this.indeterminate = event.detail.indeterminate;
+      this.progressError = event.detail.error;
+    });
+    this.progressbar.nativeElement.setProps({ indeterminate: true });
+    this.progressbar.nativeElement.getProps();
   }
 }
