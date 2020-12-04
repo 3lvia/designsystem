eb87e7445f0258228637c64b4b8ffd661648b5d5
Michael Pande <michaelpande@gmail.com>
2020-12-04 12:06:23 +0100

    Buggy implementation of yarn workspaces, still need to fix dart-sass / node-sass auto-detection issue

--- packages/components/frameworks/react/craco.config.js
+++ packages/components/frameworks/react/craco.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); const resolvePackage = relativePath => path.resolve(__dirname, relativePath); module.exports = {
     webpack: {
         alias: {
-            react: resolvePackage('./node_modules/react')
+            //react: resolvePackage('./node_modules/react')
         }
     }
 }
