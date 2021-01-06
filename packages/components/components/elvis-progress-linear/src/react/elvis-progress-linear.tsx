5a0f321dc03eb0d2f289ccdb55d2744a1fa2d0b6
Michael Pande <michaelpande@gmail.com>
2021-01-06 14:56:52 +0100

    Formatted component after the prettier  r config we use gier config we use globally in the designsystem

--- packages/components/components/elvis-progress-linear/src/react/elvis-progress-linear.tsx
+++ packages/components/components/elvis-progress-linear/src/react/elvis-progress-linear.tsx
@@ -1,27 +1,24 @@
 import * as React from 'react';
 import './style.scss';
-import classnames from 'classnames'
+import classnames from 'classnames';
 
 export interface ProgressbarProps {
   value: number;
-  isIndeterminate: boolean ;
+  isIndeterminate: boolean;
   isError: boolean;
 }
 
-const ProgressLinear: React.FC<ProgressbarProps> = ({ value , isIndeterminate , isError }) => {
+const ProgressLinear: React.FC<ProgressbarProps> = ({ value, isIndeterminate, isError }) => {
   const classes = classnames({
     ['ewc-progress-linear--range']: !isIndeterminate && !isError,
-    ['ewc-progress-linear--indeterminate']: isIndeterminate &&  !isError,
+    ['ewc-progress-linear--indeterminate']: isIndeterminate && !isError,
     ['ewc-progress-linear--error']: isError,
   });
 
   return (
-      <div className='ewc-progress-linear'>
-          <div
-            className={classes}
-            style={{width: `${value}%`}}
-            ></div>
-      </div>
+    <div className="ewc-progress-linear">
+      <div className={classes} style={{ width: `${value}%` }}></div>
+    </div>
   );
 };
 
