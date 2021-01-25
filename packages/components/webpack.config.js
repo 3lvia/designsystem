d6451defad0bd77289d61e2d07e8020342efe72a
Michael Pande <michaelpande@gmail.com>
2021-01-25 20:26:53 +0100

    Basic oversized bundling & testing grounds

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -3,55 +3,55 @@ const elviaComponents = require('./elvia-components.config.js');
 const builds = [];
 
 elviaComponents.forEach((component) => {
-  builds.push({
-    entry: `./components/${component.name}/src/web_component/${component.name}.ts`,
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
-          },
-          exclude: /node_modules/,
+    builds.push({
+        entry: `./components/${component.name}/dist/web_component/js/${component.elementName}.js`,
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                        },
+                    },
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: false,
+                            },
+                        },
+                    ],
+                },
+            ],
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            'style-loader',
-            // Translates CSS into CommonJS
-            'css-loader',
-            // Compiles Sass to CSS
-            'sass-loader',
-          ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.css', '.scss'],
         },
-        {
-          test: /\.css$/i,
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: false,
-              },
-            },
-          ],
+        output: {
+            filename: `${component.elementName}.js`,
+            path: path.resolve(__dirname, `./components/${component.name}/dist/cdn/`),
         },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.css', '.scss'],
-    },
-    output: {
-      filename: `${component.name}.js`,
-      path: path.resolve(__dirname, `./components/${component.name}/dist/cdn/`),
-    },
-    devServer: {
-      contentBase: 'dist',
-    },
-  });
+        devServer: {
+            contentBase: 'dist',
+        },
+    });
 });
 
 module.exports = builds;
