c3bd53b8e94649e7fd5e2657aa365bc255750ca6
Fride Skarseth <fs@computas.com>
2021-02-01 15:55:59 +0100

    fix(links): added back link class

--- packages/web/src/app/doc-pages/components/link-doc/link-doc.component.ts
+++ packages/web/src/app/doc-pages/components/link-doc/link-doc.component.ts
@@ -9,8 +9,8 @@ import { getComponent } from 'src/app/shared/e-items';
 export class LinkDocComponent {
   figmaUrl = getComponent('link').figmaUrl;
   description = getComponent('link').description;
-  doExample = `<a class="e-link e-link--lg" href="https://design.elvia.io/components/link-doc#Overview">Se vårt strømsbruddskart</a>`;
-  dontExample = `<a class="e-link e-link--lg" href="https://design.elvia.io/components/link-doc#Overview">Klikk her</a>`;
+  doExample = `<a class="e-link e-link--lg" href="https://design.elvia.io/components/link#Overview">Se vårt strømsbruddskart</a>`;
+  dontExample = `<a class="e-link e-link--lg" href="https://design.elvia.io/components/link#Overview">Klikk her</a>`;
 
   doesExample6 = [
     'Use jumbo links for a group of links on the same level, to gather and provide an overview.',
@@ -34,19 +34,19 @@ export class LinkDocComponent {
     'Do not use both standard and shortcut on the same page. The same goes for the description version, use description on all or none.',
   ];
 
-  previewLinkHTML = `<a class="e-link" href="https://design.elvia.io/components/link-doc#Overview">Link</a>`;
+  previewLinkHTML = `<a class="e-link" href="https://design.elvia.io/components/link#Overview">Link</a>`;
 
-  example1 = `<a class="e-link" href="https://design.elvia.io/components/link-doc#Overview">Standard link</a>
+  example1 = `<a class="e-link" href="https://design.elvia.io/components/link#Overview">Standard link</a>
 `;
 
-  example1Inverted = `<a class="e-link e-link--inverted" href="https://design.elvia.io/components/link-doc#Overview">Standard link</a>
+  example1Inverted = `<a class="e-link e-link--inverted" href="https://design.elvia.io/components/link#Overview">Standard link</a>
 `;
 
-  example2 = `<div style="font-size: 20px;">Dette er en <a class="e-link e-link--inline" href="https://design.elvia.io/components/link-doc#Overview">inline link</a></div>
+  example2 = `<div style="font-size: 20px;">Dette er en <a class="e-link e-link--inline" href="https://design.elvia.io/components/link#Overview">inline link</a></div>
 `;
 
   // tslint:disable-next-line: max-line-length
-  example2Inverted = `<div style="font-size: 20px; e-text-white">Dette er en <a class="e-link e-link--inline e-link--inverted" href="https://design.elvia.io/components/link-doc#Overview">inline link</a></div>
+  example2Inverted = `<div style="font-size: 20px; e-text-white">Dette er en <a class="e-link e-link--inline e-link--inverted" href="https://design.elvia.io/components/link#Overview">inline link</a></div>
 `;
 
   exampleExternal = `<a class="e-link e-link--external" href="https://www.elvia.no/">
@@ -61,19 +61,19 @@ export class LinkDocComponent {
 </a>
 `;
 
-  exampleNewTab = `<a class="e-link e-link--new-tab" href="https://design.elvia.io/components/link-doc#Overview">
+  exampleNewTab = `<a class="e-link e-link--new-tab" href="https://design.elvia.io/components/link#Overview">
     <span class="e-link__title">New tab link</span>
     <span class="e-link__icon"><i class="e-icon e-icon--new_tab-bold"></i></span>
   </a>
 `;
 
-  exampleNewTabInverted = `<a class="e-link e-link--new-tab e-link--inverted" href="https://design.elvia.io/components/link-doc#Overview">
+  exampleNewTabInverted = `<a class="e-link e-link--new-tab e-link--inverted" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__title">New tab link</span>
   <span class="e-link__icon"><i class="e-icon e-icon--new_tab-bold e-icon--inverted"></i></span>
 </a>
 `;
 
-  exampleAction = `<a class="e-link e-link--action" href="https://design.elvia.io/components/link-doc#Overview">
+  exampleAction = `<a class="e-link e-link--action" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__title">Action link</span>
   <span class="e-link__icon">
     <i class="e-icon e-icon--arrow_right_circle-color"></i>
@@ -82,7 +82,7 @@ export class LinkDocComponent {
 </a>
 `;
 
-  exampleBack = `<a class="e-link e-link--action" href="https://design.elvia.io/components/link-doc#Overview">
+  exampleBack = `<a class="e-link e-link--back" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__icon">
     <i class="e-icon e-icon--arrow_left_circle-color"></i>
     <i class="e-icon e-icon--arrow_left_circle-filled-color"></i>
@@ -91,21 +91,21 @@ export class LinkDocComponent {
 </a>
 `;
 
-  example6 = `<a class="e-link e-link--jumbo e-mb-40" href="https://design.elvia.io/components/link-doc#Overview">
+  example6 = `<a class="e-link e-link--jumbo e-mb-40" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__title">Jumbo link</span>
   <span class="e-link__icon"><i class="e-icon e-icon--arrow_long_right-bold e-icon--xs"></i></span>
 </a>
-<a class="e-link e-link--jumbo" href="https://design.elvia.io/components/link-doc#Overview">
+<a class="e-link e-link--jumbo" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__icon"><i class="e-icon e-icon--powerline e-icon--xl"></i></span>
   <span class="e-link__title">Jumbo link with icon</span>
   <span class="e-link__icon"><i class="e-icon e-icon--arrow_long_right-bold e-icon--xs"></i></span>
 </a>
 `;
-  example6External = `<a class="e-link e-link--jumbo e-mb-40" href="https://design.elvia.io/components/link-doc#Overview">
+  example6External = `<a class="e-link e-link--jumbo e-mb-40" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__title">External jumbo link</span>
   <span class="e-link__icon"><i class="e-icon e-icon--arrow_external-bold e-icon--xs"></i></span>
 </a>
-<a class="e-link e-link--jumbo" href="https://design.elvia.io/components/link-doc#Overview">
+<a class="e-link e-link--jumbo" href="https://design.elvia.io/components/link#Overview">
   <span class="e-link__icon"><i class="e-icon e-icon--powerline e-icon--xl"></i></span>
   <span class="e-link__title">External jumbo link with icon</span>
   <span class="e-link__icon"><i class="e-icon e-icon--arrow_external-bold e-icon--xs"></i></span>
@@ -116,7 +116,7 @@ export class LinkDocComponent {
 <div style="width: 150px;">
   <a
     class="e-link e-link--card"
-    href="https://design.elvia.io/components/link-doc#Overview"
+    href="https://design.elvia.io/components/link#Overview"
   >
     <div class="e-link__content">
       <span class="e-link__icon">
@@ -132,7 +132,7 @@ export class LinkDocComponent {
 <div style="width: 150px;">
   <a
     class="e-link e-link--card e-link--shortcut"
-    href="https://design.elvia.io/components/link-doc#Overview"
+    href="https://design.elvia.io/components/link#Overview"
   >
     <div class="e-link__content">
       <span class="e-link__icon">
@@ -153,13 +153,13 @@ export class LinkDocComponent {
 
   exampleSizes = `<div style="display: flex; align-items: center; flex-wrap: wrap;">
   <div class="e-mt-16 e-mr-16">
-    <a class="e-link e-link--sm" href="https://design.elvia.io/components/link-doc#Overview">Small link</a>
+    <a class="e-link e-link--sm" href="https://design.elvia.io/components/link#Overview">Small link</a>
   </div>
   <div class="e-mt-16 e-mr-16">
-    <a class="e-link" href="https://design.elvia.io/components/link-doc#Overview">Medium link</a>
+    <a class="e-link" href="https://design.elvia.io/components/link#Overview">Medium link</a>
   </div>
   <div class="e-mt-16">
-    <a class="e-link e-link--lg" href="https://design.elvia.io/components/link-doc#Overview">Large link</a>
+    <a class="e-link e-link--lg" href="https://design.elvia.io/components/link#Overview">Large link</a>
   </div>
 </div>
 `;
