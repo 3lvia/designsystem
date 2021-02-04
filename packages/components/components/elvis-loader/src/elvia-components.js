43dcdae73e99a70b7eff3c95e5010057163b4ed4
Michael Pande <michaelpande@gmail.com>
2021-02-04 12:24:26 +0100

    Elvis loader uses CDN

--- packages/components/components/elvis-loader/src/elvia-components.js
+++ packages/components/components/elvis-loader/src/elvia-components.js
@@ -1,52 +1,60 @@
 /* This file is going to be a part of the designsystem and elvis-bundle.js in the future */
-components = require('../../../elvia-components.config');
+const components = require('../../../elvia-components.config');
+const injector = require('./script-injector');
 
-/** TODO: Replace with mutationobserver implementation */
 document.addEventListener('DOMContentLoaded', function () {
-  const componentList = [];
-  for (var i = 0; i < components.length; i++) {
-    componentList.push(components[i]);
-  }
+  const elementNames = {};
 
-  for (var i = 0; i < componentList.length; i++) {
-    console.log(componentList[i].name);
-    const elements = document.querySelectorAll(componentList[i].elementName);
-    if (elements && elements.length > 0) {
-      console.log('[Success] Detected webcomponent: ' + componentList[i].name);
-      injectScript(componentList[i]);
+  components.forEach(comp => {
+    elementNames[comp.elementName] = comp;
+  })
+
+  function searchForComponents(domElement) {
+    for (var i = 0; i < components.length; i++) {
+      const elements = domElement.querySelectorAll(components[i].elementName);
+      if (elements && elements.length > 0) {
+        injector(components[i].name);
+      }
     }
   }
 
-  function injectScript(component) {
-    // Only inject legacy script on IE11;
-    if (!!window.MSInputMethodContext && !!document.documentMode) {
-      document.body.appendChild(createScript(component));
-      //document.body.appendChild(createLegacyScript(name));
-    } else {
-      //document.body.appendChild(createLegacyScript(name));
-      document.body.appendChild(createScript(component));
+  // Initial Load
+  searchForComponents(document);
+  startMutationObserver();
+
+
+
+  function startMutationObserver() {
+    let mo = new MutationObserver(function (mutations) {
+      for (let i = 0; i < mutations.length; i++) {
+        injectScriptIfEligible(mutations[i].target, mutations[i]);
+      }
+    });
+
+    function injectScriptIfEligible(node, mutation) {
+      if (mutation.addedNodes.length > 0) {
+        for (let i = 0; i < mutation.addedNodes.length; i++) {
+          let addedNode = mutation.addedNodes[i];
+          const component = elementNames[addedNode.localName];
+          if (component) {
+            injector(component.name);
+          }
+          if (addedNode.querySelectorAll) {
+            searchForComponents(addedNode);
+          }
+        }
+      }
+      const component = elementNames[node.localName];
+      if (component) {
+        injector(component.name);
+      }
     }
-  }
 
-  /*function createLegacyScript(name) {
-        const scriptTag = document.createElement('script');
-        scriptTag.src = name + '.legacy.js';
-        scriptTag.setAttribute('nomodule', '');
-        return scriptTag;
-    }*/
-
-  function createScript(component) {
-    const scriptTag = document.createElement('script');
-    scriptTag.src = '../../' + component.name + '/dist/cdn/' + component.elementName + '.js';
-    //scriptTag.setAttribute('type', 'module');
-    return scriptTag;
+    mo.observe(document.documentElement, {
+      childList: true,
+      attributes: true,
+      characterData: true,
+      subtree: true,
+    });
   }
-  //<script src="components/elvis-popover/dist/cdn/elvia-popover.js"></script>
-  /*
-    function createScript(name) {
-      const scriptTag = document.createElement('script');
-      scriptTag.src = '/cdn/components/' + name + '.js';
-      //scriptTag.setAttribute('type', 'module');
-      return scriptTag;
-    }*/
 });
