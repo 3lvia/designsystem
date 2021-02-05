e2952e0a67c91413a397cff36da691979c397f12
Fride Skarseth <fs@computas.com>
2021-02-05 14:56:05 +0100

    fix(tabs): style fix (#204)
    * fix(tabs): style fix

--- packages/components/components/elvis-tabs/src/react/elvia-tabs.tsx
+++ packages/components/components/elvis-tabs/src/react/elvia-tabs.tsx
@@ -34,7 +34,7 @@ const Tabs: FC<TabsProps> = ({ items, value, valueOnChange, webcomponent }) => {
       return;
     }
     const throttledResizeCount = toolbox.throttle(updateArrowVisibility, 500);
-    const throttledScrollCount = toolbox.throttle(updateArrowVisibility, 250);
+    const throttledScrollCount = toolbox.throttle(updateArrowVisibility, 150);
 
     window.addEventListener('resize', throttledResizeCount);
     itemsRef.current.addEventListener('scroll', throttledScrollCount);
@@ -131,7 +131,7 @@ const Tabs: FC<TabsProps> = ({ items, value, valueOnChange, webcomponent }) => {
         }}
       >
         <i
-          className="ewc-icon ewc-icon--arrow_left-bold ewc-icon--xxs"
+          className="ewc-icon ewc-icon--arrow_left-bold"
           style={{
             backgroundImage: `url("data:image/svg+xml,%3csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cg clip-path='url(%23clip0)'%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M18.716 23.122a2.89 2.89 0 00-.08-4.04L11.373 12l7.261-7.082a2.89 2.89 0 00.081-4.04A2.782 2.782 0 0014.74.796L5.365 9.939A2.88 2.88 0 004.5 12c0 .778.312 1.522.865 2.061l9.375 9.143a2.782 2.782 0 003.976-.082z' fill='black'/%3e%3c/g%3e%3cdefs%3e%3cclipPath id='clip0'%3e%3cpath d='M0 0h24v24H0V0z' fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e")`,
           }}
