53549a67bec9d1549c0b822f3438cf3c51d50c0c
Martin Espeland <m.espeland89@gmail.com>
2021-02-19 14:45:28 +0100

    refactor: updates after QA

--- packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.component.ts
+++ packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.component.ts
@@ -17,28 +17,28 @@ export class SearchDocComponent {
   isExampleInput = false;
 
   overviewExample = `<div class="e-form-field">
-<div
-  class="e-search e-search--local--instant"
-  style="width: 250px"
-  #exampleSearch
->
-  <div class="e-input">
-    <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
-      #exampleInput
-      (keyup)="onInput(exampleInput.value)"
-    />
-  </div>
-  <i class="e-icon e-icon--search-color"></i>
-  <button
-    class="e-btn e-btn--icon e-btn--lg"
-    (click)="clearExample()"
+  <div
+    class="e-search e-search--instant"
+    style="width: 250px"
+    #exampleSearch
   >
-    <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
-  </button>
-</div>
+    <div class="e-input">
+      <input
+        aria-label="Search"
+        type="search"
+        placeholder="Search"
+        #exampleInput
+        (keyup)="onInput(exampleInput.value)"
+      />
+    </div>
+    <i class="e-icon e-icon--search-color"></i>
+    <button
+      class="e-btn e-btn--icon e-btn--lg"
+      (click)="clearExample()"
+    >
+      <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
+    </button>
+  </div>
 </div>
 `;
 
@@ -65,82 +65,30 @@ clearExample(): void {
 `;
 
   searchExample = `<div class="e-form-field">
-<div
-  class="e-search e-search--instant"
-  style="width: 250px"
->
-  <div class="e-input">
-    <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
-    />
-  </div>
-  <i class="e-icon e-icon--search-color"></i>
-  <button
-    class="e-btn e-btn--icon"
-  >
-    <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
-  </button>
-</div>
-</div>
-`;
-
-  afterSearchExample = `<div class="e-form-field">
-<div
-  class="e-search e-search--instant e-search--searched"
-  style="width: 250px"
->
-  <div class="e-input">
-    <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
-      value="Elvia"
-    />
-  </div>
-  <i class="e-icon e-icon--search-color"></i>
-  <button
-    class="e-btn e-btn--icon"
-    (click)="clearExample()"
-  >
-    <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
-  </button>
-</div>
-</div>
-`;
-
-  searhOnSubmitExample = `<div style="display: flex; justify-content: start; align-items: flex-end;">
-<div class="e-form-field">
   <div
-  class="e-search e-search--on-submit"
-  style="width: 250px"
+    class="e-search e-search--instant"
+    style="width: 250px"
   >
     <div class="e-input">
       <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
+        aria-label="Search"
+        type="search"
+        placeholder="Search"
       />
     </div>
-    <button class="e-btn e-btn--icon">
+    <i class="e-icon e-icon--search-color"></i>
+    <button
+      class="e-btn e-btn--icon"
+    >
       <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
     </button>
   </div>
 </div>
-  <button class="e-btn e-btn--lg e-ml-16">
-    <span class="e-btn__icon">
-      <i class="e-icon e-icon--search-color e-icon--inverted"></i>
-      <i class="e-icon e-icon--search-color"></i>
-    </span>
-    <span class="e-btn__title">Search</span>
-  </button>
-</div>
 `;
-  searhOnSubmitAfterExample = `<div style="display: flex; justify-content: start; align-items: flex-end;">
-<div class="e-form-field">
+
+  afterSearchExample = `<div class="e-form-field">
   <div
-    class="e-search e-search--on-submit e-search--searched"
+    class="e-search e-search--instant e-search--searched"
     style="width: 250px"
   >
     <div class="e-input">
@@ -151,20 +99,72 @@ clearExample(): void {
         value="Elvia"
       />
     </div>
+    <i class="e-icon e-icon--search-color"></i>
     <button
       class="e-btn e-btn--icon"
+      (click)="clearExample()"
     >
       <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
     </button>
   </div>
 </div>
-<button class="e-btn e-btn--lg e-ml-16">
-  <span class="e-btn__icon">
-    <i class="e-icon e-icon--search-color e-icon--inverted"></i>
-    <i class="e-icon e-icon--search-color"></i>
-  </span>
-  <span class="e-btn__title">Search</span>
-</button>
+`;
+
+  searhOnSubmitExample = `<div style="display: flex; justify-content: start; align-items: flex-end;">
+  <div class="e-form-field">
+    <div
+    class="e-search e-search--on-submit"
+    style="width: 250px"
+    >
+      <div class="e-input">
+        <input
+        aria-label="Search"
+        type="search"
+        placeholder="Search"
+        />
+      </div>
+      <button class="e-btn e-btn--icon">
+        <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
+      </button>
+    </div>
+  </div>
+  <button class="e-btn e-btn--lg e-ml-16">
+    <span class="e-btn__icon">
+      <i class="e-icon e-icon--search-color e-icon--inverted"></i>
+      <i class="e-icon e-icon--search-color"></i>
+    </span>
+    <span class="e-btn__title">Search</span>
+  </button>
+</div>
+`;
+  searhOnSubmitAfterExample = `<div style="display: flex; justify-content: start; align-items: flex-end;">
+  <div class="e-form-field">
+    <div
+      class="e-search e-search--on-submit e-search--searched"
+      style="width: 250px"
+    >
+      <div class="e-input">
+        <input
+          aria-label="Search"
+          type="search"
+          placeholder="Search"
+          value="Elvia"
+        />
+      </div>
+      <button
+        class="e-btn e-btn--icon"
+      >
+        <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
+      </button>
+    </div>
+  </div>
+  <button class="e-btn e-btn--lg e-ml-16">
+    <span class="e-btn__icon">
+      <i class="e-icon e-icon--search-color e-icon--inverted"></i>
+      <i class="e-icon e-icon--search-color"></i>
+    </span>
+    <span class="e-btn__title">Search</span>
+  </button>
 </div>
 `;
 
@@ -270,34 +270,6 @@ clearExample(): void {
       <i class="e-icon e-icon--search-color"></i>
     </span>
     <span class="e-btn__title">Search</span>
-  </button>
-</div>
-`;
-
-  searchGlobalExample = `<h3>Before search</h3>
-<div class="e-search e-search--global" style="width:340px;">
-  <div class="e-input">
-    <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
-    />
-  </div>
-  <button class="e-btn e-btn--icon" (click)="searchString = ''">
-    <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
-  </button>
-</div>
-<h3>After search (e-search--searched)</h3>
-<div class="e-search e-search--global e-search--searched" style="width:340px;">
-  <div class="e-input">
-    <input
-      aria-label="Search"
-      type="search"
-      placeholder="Search"
-    />
-  </div>
-  <button class="e-btn e-btn--icon" (click)="searchString = ''">
-    <span class="e-btn__icon"><i class="e-icon e-icon--close-bold"></i></span>
   </button>
 </div>
 `;
