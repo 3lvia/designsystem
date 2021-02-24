bf5d55937d0563bb5b9b7d048f32e7455bca65c9
Fride Skarseth <fs@computas.com>
2021-02-24 10:01:04 +0100

    docs(tabs): cleanup

--- packages/web/src/app/doc-pages/components/tabs-doc/tabs-doc.module.ts
+++ packages/web/src/app/doc-pages/components/tabs-doc/tabs-doc.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import '@elvia/elvis-tabs';
 import { TabsDocComponent } from './tabs-doc.component';
@@ -21,6 +21,7 @@ import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
     ComponentInstallationModule,
     WhenToUseModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [TabsDocComponent],
 })
 export class TabsDocModule {}
