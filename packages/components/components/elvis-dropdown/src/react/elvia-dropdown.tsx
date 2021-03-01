536817295beaf81dff557644e5e2a6289f4deb9d
Martin Espeland <m.espeland89@gmail.com>
2021-03-01 12:29:23 +0100

    style: update to dropdown after no need for shadow-dom in webcomps

--- packages/components/components/elvis-dropdown/src/react/elvia-dropdown.tsx
+++ packages/components/components/elvis-dropdown/src/react/elvia-dropdown.tsx
@@ -37,6 +37,8 @@ const Dropdown: React.FC<DropdownProps> = ({
             className="ewc-icon"
             style={{
               backgroundImage: `url("data:image/svg+xml,%3csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M.476 5.994a.75.75 0 011.06.012L12 16.707 22.464 6.006a.75.75 0 011.072 1.048l-10.481 10.72A1.483 1.483 0 0112 18.22a1.469 1.469 0 01-1.055-.445L.464 7.054a.75.75 0 01.012-1.061z' fill='black'/%3e%3c/svg%3e")`,
+              height: '24px',
+              width: '24px',
             }}
           ></i>
         </span>
