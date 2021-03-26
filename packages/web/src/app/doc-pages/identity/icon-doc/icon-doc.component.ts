8a79f50bf097a12ba3a83a8010d33986e633c9b6
Martin A. Espeland <m.espeland89@gmail.com>
2021-03-26 13:35:38 +0100

    Lego 734 - Elvis builds icons from elvis-assets package (#258)
    * refactor(elvis & elvis-assets): elvis builds icons from elvis-assets package
    
    + updated elvis-assets package with new icons
    
    * refactor: added elvis-assets icon package as dependency to web
    
    * refactor(web): updated angular.json
    
    * Added missing folder structure
    
    * Support for downloading icons
    
    * docs(icons): removed figma icon from elvis-assets-icon. Placed figma icon in web assets
    
    * revert(figma icon): added figma icon to elvis-assets-icons package again
    
    * refactor(icon doc): updated linting
    
    * (refactor): update web config for png icon files
    
    redirect to src instead of dist folder to enable download on netlify
    
    * build(web): updated build to build elvis-assets-icons package on deploy, and revert angular.json
    
    * Copy icons.config from elvis-assets-icons
    
    * Removed generated file
    
    * Gitignore for gfenerated file
    
    * Fixed docs
    
    Co-authored-by: Michael Pande <michaelpande@gmail.com>

--- packages/web/src/app/doc-pages/identity/icon-doc/icon-doc.component.ts
+++ packages/web/src/app/doc-pages/identity/icon-doc/icon-doc.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, HostListener } from '@angular/core';
 import { Icon } from 'src/app/shared/icon.interface';
 import { getIdentity } from 'src/app/shared/e-items';
-import * as icons from '@elvia/elvis/src/config/icons.config';
+import * as icons from '@elvia/elvis-assets-icons/config/icons.config.js';
 import { CopyToClipboardService } from 'src/app/core/services/copy-to-clipboard.service';
 
 @Component({
@@ -54,14 +54,14 @@ export class IconDocComponent implements OnInit {
 <i class="e-icon e-icon--chat e-icon--color-disabled e-icon--inverted"></i>
 `;
 
-  importCodeTS = `import { addCircle } from '@elvia/elvis/icons'`;
+  importCodeTS = `import { addCircle } from '@elvia/elvis-assets-icons/icons'`;
   scriptCodeHTML = `<script src="path_to_file/elvis.js"></script>;`;
   iconExample = `<i class="e-icon e-icon--chat e-icon--md"></i>`;
 
   term = '';
   IconClassList: Icon[] = [];
 
-  constructor(private copyService: CopyToClipboardService) {}
+  constructor(private copyService: CopyToClipboardService) { }
 
   @HostListener('document:click', ['$event', '$event.target'])
   onClick(event: MouseEvent, targetElement: HTMLElement): void {
