e5fd2899e6d72af17ad6c7010cc3e9bf8bd18e67
Martin Espeland <m.espeland89@gmail.com>
2021-04-06 12:54:20 +0200

    refactor(accordion): added size prop and small prop cleanup in type checking

--- packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
+++ packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 export interface AccordionProps {
   label: string;
   position: string;
+  size: string;
   content: string;
 }
 
@@ -23,7 +24,7 @@ const AccordionArea = styled.div`
 
 const AccordionButtonArea = styled.div`
   display: inline-flex;
-  justify-content: ${(props) => props.position};
+  justify-content: ${(props: { position: string }) => props.position};
   width: 100%;
   flex-direction: row;
 `;
@@ -32,8 +33,15 @@ const AccordionLabel = styled.label`
   display: flex;
   font-family: 'Red Hat Display', Verdana, sans-serif;
   font-weight: 500;
-  font-size: 14px;
-  line-height: 24px;
+  font-size: ${(props: { size: string }) =>
+    props.size !== 'medium'
+      ? props.size === 'small'
+        ? '14px'
+        : props.size === 'large'
+        ? '20px'
+        : '16px'
+      : '16px'};
+  line-height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
   text-align: left;
 
   padding-right: 8px;
@@ -52,8 +60,8 @@ const AccordionButton = styled.button`
       : `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12zm0-22.5C6.21 1.5 1.5 6.21 1.5 12S6.21 22.5 12 22.5 22.5 17.79 22.5 12 17.79 1.5 12 1.5z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e")`};
   background-color: ${ElviaColors.elviaOn};
   display: inline-block;
-  height: 24px;
-  width: 24px;
+  height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
+  width: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
   outline: none;
 
   &:hover {
@@ -68,23 +76,24 @@ const AccordionContent = styled.div`
   display: flex;
   width: 100%;
   // open
-  padding-top: ${(props) => (props.isContentOpen ? '24px' : '0')};
-  pointer-events: ${(props) => (props.isContentOpen ? 'auto' : 'none')};
-  max-height: ${(props) => (props.isContentOpen ? '10000px' : '0')};
-  opacity: ${(props) => (props.isContentOpen ? '1' : '0')};
+  padding-top: ${(props: { isContentOpen: boolean }) => (props.isContentOpen ? '24px' : '0')};
+  pointer-events: ${(props: { isContentOpen: boolean }) => (props.isContentOpen ? 'auto' : 'none')};
+  max-height: ${(props: { isContentOpen: boolean }) => (props.isContentOpen ? '10000px' : '0')};
+  opacity: ${(props: { isContentOpen: boolean }) => (props.isContentOpen ? '1' : '0')};
 
   transition: all 0.25s cubic-bezier(0.4, 0.01, 0.12, 0.32), opacity 0.3s cubic-bezier(0, 0.3, 0.83, 0.97);
 `;
 
-const Accordion: FC<AccordionProps> = ({ label, position = 'center', content }) => {
+const Accordion: FC<AccordionProps> = ({ label, position = 'center', size = 'medium', content }) => {
   const [contentOpen, setContentOpen] = useState(false);
 
   return (
     <AccordionArea>
       <AccordionButtonArea position={position}>
-        <AccordionLabel>{label}</AccordionLabel>
+        <AccordionLabel size={size}>{label}</AccordionLabel>
         <AccordionButton
           isContentOpen={contentOpen}
+          size={size}
           onClick={() => setContentOpen((contentOpen) => !contentOpen)}
         ></AccordionButton>
       </AccordionButtonArea>
