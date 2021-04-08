36100311c4920cb9d31478d899c71b1ed592ee80
Martin Espeland <m.espeland89@gmail.com>
2021-04-08 16:17:01 +0200

    refactor(accordion & ceg): updated Accordion after QA and small fix to Ceg

--- packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
+++ packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
@@ -27,53 +27,48 @@ const AccordionButtonArea = styled.div`
   display: inline-flex;
   justify-content: ${(props: { position: string }) =>
     (props.position === 'center' && 'center;') ||
-    (props.position === 'end' && 'flex-end;') ||
-    (props.position === 'start' && 'start;')}
+    (props.position === 'right' && 'flex-end;') ||
+    (props.position === 'left' && 'start;')}
   flex-direction: row;
   width: 100%;
   margin-top: ${(props: { type: string }) => (props.type !== 'overflow' ? '0' : '16px')};
 `;
 
-const AccordionLabel = styled.label`
+const AccordionButton = styled.button`
+  border: none;
+  background: transparent;
   display: flex;
   font-family: 'Red Hat Display', Verdana, sans-serif;
   font-weight: 500;
   font-size: ${(props: { size: string }) =>
-    (props.size === 'medium' && '16px;') ||
     (props.size === 'large' && '20px;') ||
+    (props.size === 'medium' && '16px;') ||
     (props.size === 'small' && '14px;')}
   line-height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
   text-align: left;
   cursor: pointer;
-  padding-right: 8px;
-  max-width: 80%;
-`;
-
-const AccordionButton = styled.button`
-  border: none;
-  border-radius: 50%;
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-image: ${(props: { isContentOpen: boolean }) =>
-    props.isContentOpen
-      ? `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12zm0-22.5C6.21 1.5 1.5 6.21 1.5 12S6.21 22.5 12 22.5 22.5 17.79 22.5 12 17.79 1.5 12 1.5z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M11.187 9.106a1.149 1.149 0 011.626 0l4.237 4.237a.853.853 0 11-1.207 1.207L12 10.707 8.157 14.55a.853.853 0 01-1.207-1.207l4.237-4.237z' fill='black'/%3e%3c/svg%3e")`
-      : `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12zm0-22.5C6.21 1.5 1.5 6.21 1.5 12S6.21 22.5 12 22.5 22.5 17.79 22.5 12 17.79 1.5 12 1.5z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e")`};
-  background-color: ${ElviaColors.elviaOn};
-  display: inline-block;
-  height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
-  width: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
-  cursor: pointer;
-
   &:hover {
-    background-image: ${(props: { isContentOpen: boolean }) =>
-      props.isContentOpen
-        ? `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M0 12c0 6.617 5.383 12 12 12s12-5.383 12-12S18.617 0 12 0 0 5.383 0 12z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M11.187 9.106a1.149 1.149 0 011.626 0l4.237 4.237a.853.853 0 11-1.207 1.207L12 10.707 8.157 14.55a.853.853 0 01-1.207-1.207l4.237-4.237z' fill='black'/%3e%3c/svg%3e")`
-        : `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e")`};
+    i {
+      background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e");
+    }
   }
-
-  // TODO TOOLBOX
-  // outline: none;
+  
+  i {
+    margin-left: 8px;
+    border: none;
+    border-radius: 50%;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12zm0-22.5C6.21 1.5 1.5 6.21 1.5 12S6.21 22.5 12 22.5 22.5 17.79 22.5 12 17.79 1.5 12 1.5z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e");
+    background-color: ${ElviaColors.elviaOn};
+    display: inline-block;
+    height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
+    width: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
+    transition: transform 300ms; 
+    transform: ${(props: { isContentOpen: boolean }) =>
+      (props.isContentOpen && ' rotate(180deg)') || (props.isContentOpen === false && ' rotate(0deg)')};
+  }
 `;
 
 const AccordionContent = styled.div`
@@ -82,8 +77,7 @@ const AccordionContent = styled.div`
   width: 100%;
   font-size: 16px;
   line-height:inherit;
-  // open
-  padding-top: ${(props: { isContentOpen: boolean; type: string }) =>
+  margin-top: ${(props: { isContentOpen: boolean; type: string }) =>
     props.isContentOpen && props.type === 'normal' ? '24px' : '0'};
   pointer-events: ${(props: { isContentOpen: boolean }) => (props.isContentOpen ? 'auto' : 'none')};
   height:auto;
@@ -106,7 +100,7 @@ const AccordionContent = styled.div`
 
 const Accordion: FC<AccordionProps> = ({
   content,
-  label = ['Show more', 'Hide content'],
+  label = ['Show', 'Hide'],
   position = 'center',
   size = 'medium',
   type = 'normal',
@@ -153,14 +147,14 @@ const Accordion: FC<AccordionProps> = ({
           </AccordionContent>
         ) : null}
         <AccordionButtonArea position={position} type={type}>
-          <AccordionLabel size={size} onClick={() => setContentOpen((contentOpen) => !contentOpen)}>
-            {contentOpen ? label[1] : label[0]}
-          </AccordionLabel>
           <AccordionButton
             isContentOpen={contentOpen}
             size={size}
             onClick={() => setContentOpen((contentOpen) => !contentOpen)}
-          ></AccordionButton>
+          >
+            {contentOpen ? label[1] : label[0]}
+            <i></i>
+          </AccordionButton>
         </AccordionButtonArea>
         {type === 'normal' ? (
           <AccordionContent isContentOpen={contentOpen} type={type}>
