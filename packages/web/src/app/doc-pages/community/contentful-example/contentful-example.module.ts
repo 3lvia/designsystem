7588feb750e627dbc7cd190148fcf4c18d01b44b
Michael Pande <michaelpande@gmail.com>
2021-04-14 14:35:37 +0200

    Contentful merge

--- packages/web/src/app/doc-pages/community/contentful-example/contentful-example.module.ts
+++ packages/web/src/app/doc-pages/community/contentful-example/contentful-example.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentfulExampleComponent } from './contentful-example.component';
-import { ComponentHeaderModule } from 'src/app/shared/component-structure/component-header/component-header.module';
+import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 
 @NgModule({
   imports: [CommonModule, ComponentHeaderModule],
