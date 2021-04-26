143ff64ee6d8de482dbda38eef4cea045da09802
Fride Skarseth <fs@computas.com>
2021-04-26 22:53:45 +0200

    fix(datepicker): testing

--- packages/web/src/app/shell/main/main.module.ts
+++ packages/web/src/app/shell/main/main.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { RouterModule } from '@angular/router';
 import { HeaderModule } from '../header/header.module';
 import { NavbarModule } from '../navbar/navbar.module';
 import { FooterModule } from '../footer/footer.module';
+import '@elvia/elvis-datepicker';
 
 @NgModule({
   imports: [CommonModule, RouterModule, HeaderModule, NavbarModule, FooterModule],
   declarations: [MainComponent],
   exports: [MainComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MainModule {}
