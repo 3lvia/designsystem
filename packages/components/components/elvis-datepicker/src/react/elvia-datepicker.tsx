d14a3bfd0cea8d8ef22729693e6b9c8e65273388
Fride Skarseth <fs@computas.com>
2021-04-26 16:28:20 +0200

    fix(datepicker): testing

--- packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
+++ packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
@@ -3,8 +3,8 @@ import './style.scss';
 import classnames from 'classnames';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import PickerToolbar from '@material-ui/pickers/_shared/PickerToolbar';
+// import Button from '@material-ui/core/Button';
+// import PickerToolbar from '@material-ui/pickers/_shared/PickerToolbar';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
 import toolbox from '@elvia/elvis-toolbox';
@@ -185,7 +185,7 @@ export const Datepicker: FC<DatepickerProps> = ({
   };
 
   const getCustomToolbar = (props: any) => {
-    const { date, title } = props;
+    const { date } = props;
     //   const toggleYearView = () => () => {
     //     openView === 'year' ? setOpenView('date') : setOpenView('year');
     //   };
@@ -200,24 +200,7 @@ export const Datepicker: FC<DatepickerProps> = ({
     //   removeOutlineFix(datepickerPopoverRef.current);
     // }
 
-    return (
-      <PickerToolbar title={title}>
-        {selectedDate !== null ? (
-          <div className="ewc-datepicker--toolbar-today">{date.format('dddd DD. MMMM')}</div>
-        ) : (
-          <div></div>
-        )}
-        <Button className="ewc-datepicker--toolbar-dropdown">
-          <div className="ewc-datepicker__toolbar-year">{date.format('YYYY')}</div>
-          {/* <i
-            className={dropdownIconClasses}
-            style={{
-              backgroundImage: `url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M.389 5.869a1.328 1.328 0 011.878 0L12 15.6l9.733-9.732a1.328 1.328 0 011.878 1.878L13.443 17.915h-.001a2.04 2.04 0 01-2.885 0L.39 7.747a1.328 1.328 0 010-1.878z' fill='black'/%3e%3c/svg%3e")`,
-            }}
-          ></i> */}
-        </Button>
-      </PickerToolbar>
-    );
+    return <div className="ewc-datepicker__toolbar-year">{date.format('YYYY')}</div>;
   };
 
   const formatDate = (inputString: string) => {
