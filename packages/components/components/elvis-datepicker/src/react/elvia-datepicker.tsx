445a942bb6a9a3073cc9d718f7fd09c715c39721
Fride Skarseth <fs@computas.com>
2021-04-26 15:49:25 +0200

    fix(datepicker): testing

--- packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
+++ packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
@@ -1,13 +1,13 @@
 import React, { FC, useState, useRef, useEffect } from 'react';
 import './style.scss';
-// import classnames from 'classnames';
+import classnames from 'classnames';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
-// import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 // import Button from '@material-ui/core/Button';
 // import PickerToolbar from '@material-ui/pickers/_shared/PickerToolbar';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
-// import toolbox from '@elvia/elvis-toolbox';
+import toolbox from '@elvia/elvis-toolbox';
 
 export interface DatepickerProps {
   value: Date | number | null;
@@ -26,96 +26,96 @@ export interface DatepickerProps {
 export const Datepicker: FC<DatepickerProps> = ({
   value,
   // label = 'Velg dato',
-  // isCompact = false,
-  // isDisabled = false,
-  // errorMessage = '',
-  // isFullWidth = false,
-  // minDate = undefined,
-  // maxDate = undefined,
-  // startDate = undefined,
+  isCompact = false,
+  isDisabled = false,
+  errorMessage = '',
+  isFullWidth = false,
+  minDate = undefined,
+  maxDate = undefined,
+  startDate = undefined,
   valueOnChange,
   webcomponent,
 }) => {
   const [selectedDate, setSelectedDate] = useState(value);
   const datepickerRef = useRef<HTMLDivElement>(null);
-  // const datepickerPopoverRef = useRef<HTMLDivElement>(null);
-  // const inputRef = useRef<HTMLInputElement>(null);
-  // const placeholderString = 'dd.mm.yyyy';
+  const datepickerPopoverRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const placeholderString = 'dd.mm.yyyy';
 
   // Styling
-  // const datePickerClasses = classnames('ewc-datepicker', {
-  //   ['ewc-datepicker--error']: errorMessage !== '',
-  //   ['ewc-datepicker--compact']: isCompact !== false,
-  //   ['ewc-datepicker--unselected']: value === null,
-  // });
+  const datePickerClasses = classnames('ewc-datepicker', {
+    ['ewc-datepicker--error']: errorMessage !== '',
+    ['ewc-datepicker--compact']: isCompact !== false,
+    ['ewc-datepicker--unselected']: value === null,
+  });
   // const datePickerLabelClasses = classnames('ewc-datepicker__label', {
   //   ['ewc-datepicker__label--disabled']: isDisabled,
   // });
-  // const materialTheme = createMuiTheme({
-  //   props: {
-  //     MuiButtonBase: {
-  //       disableRipple: true,
-  //     },
-  //   },
-  //   overrides: {
-  //     MuiIconButton: {
-  //       root: {
-  //         padding: '8px',
-  //         '&:hover': {
-  //           backgroundColor: '#29d305',
-  //         },
-  //         '&.Mui-focusVisible': {
-  //           outline: '2px solid #0064fa !important',
-  //           outlineOffset: '2px !important',
-  //         },
-  //         '&:active': {
-  //           transform: 'scale(0.93)',
-  //         },
-  //       },
-  //     },
-  //     MuiPickersDay: {
-  //       daySelected: {
-  //         background: selectedDate === null ? '#f4f4f4 !important' : '#29d305 !important',
-  //         border: selectedDate === null ? '0 !important' : '1px solid black !important',
-  //         '&:hover': {
-  //           border: selectedDate === null ? '1px solid #29d305 !important' : '0 !important',
-  //         },
-  //       },
-  //       current: {
-  //         background: '#f4f4f4 !important',
-  //         border: '0 !important',
-  //         '&:hover': {
-  //           border: '1px solid #29d305 !important',
-  //         },
-  //       },
-  //     },
-  //   },
-  // });
+  const materialTheme = createMuiTheme({
+    props: {
+      MuiButtonBase: {
+        disableRipple: true,
+      },
+    },
+    overrides: {
+      MuiIconButton: {
+        root: {
+          padding: '8px',
+          '&:hover': {
+            backgroundColor: '#29d305',
+          },
+          '&.Mui-focusVisible': {
+            outline: '2px solid #0064fa !important',
+            outlineOffset: '2px !important',
+          },
+          '&:active': {
+            transform: 'scale(0.93)',
+          },
+        },
+      },
+      MuiPickersDay: {
+        daySelected: {
+          background: selectedDate === null ? '#f4f4f4 !important' : '#29d305 !important',
+          border: selectedDate === null ? '0 !important' : '1px solid black !important',
+          '&:hover': {
+            border: selectedDate === null ? '1px solid #29d305 !important' : '0 !important',
+          },
+        },
+        current: {
+          background: '#f4f4f4 !important',
+          border: '0 !important',
+          '&:hover': {
+            border: '1px solid #29d305 !important',
+          },
+        },
+      },
+    },
+  });
 
-  // useEffect(() => {
-  //   moment.updateLocale('nb', {
-  //     week: {
-  //       dow: 1,
-  //     },
-  //     weekdaysShort: ['sø', 'ma', 'ti', 'on', 'to', 'fr', 'lø'],
-  //     weekdays: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
-  //   });
+  useEffect(() => {
+    moment.updateLocale('nb', {
+      week: {
+        dow: 1,
+      },
+      weekdaysShort: ['sø', 'ma', 'ti', 'on', 'to', 'fr', 'lø'],
+      weekdays: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+    });
 
-  //   addOutlineFix(datepickerRef.current);
+    addOutlineFix(datepickerRef.current);
 
-  //   return () => {
-  //     removeOutlineFix(datepickerRef.current);
-  //   };
-  // }, []);
+    return () => {
+      removeOutlineFix(datepickerRef.current);
+    };
+  }, []);
 
   // Needed for webcomponent
   useEffect(() => {
     handleDateChange(value);
   }, [value]);
 
-  // useEffect(() => {
-  //   updateCaretPositionDot();
-  // }, [selectedDate]);
+  useEffect(() => {
+    updateCaretPositionDot();
+  }, [selectedDate]);
 
   const handleDateChange = (date: number | Date | null) => {
     setSelectedDate(date);
@@ -127,36 +127,36 @@ export const Datepicker: FC<DatepickerProps> = ({
     }
   };
 
-  // const updateCaretPositionDot = () => {
-  //   if (!inputRef.current) {
-  //     return;
-  //   }
-  //   if (inputRef.current.value.length === 3 || inputRef.current.value.length === 6) {
-  //     inputRef.current.selectionStart = inputRef.current.value.length + 1;
-  //     inputRef.current.selectionEnd = inputRef.current.value.length + 1;
-  //   }
-  // };
+  const updateCaretPositionDot = () => {
+    if (!inputRef.current) {
+      return;
+    }
+    if (inputRef.current.value.length === 3 || inputRef.current.value.length === 6) {
+      inputRef.current.selectionStart = inputRef.current.value.length + 1;
+      inputRef.current.selectionEnd = inputRef.current.value.length + 1;
+    }
+  };
 
-  // const updateCaretPositionSpace = () => {
-  //   setTimeout(() => {
-  //     if (!inputRef.current) {
-  //       return;
-  //     }
-  //     const index = inputRef.current.value.indexOf(' ');
-  //     if (index > 0) {
-  //       inputRef.current.selectionStart = index;
-  //       inputRef.current.selectionEnd = index;
-  //     }
-  //   }, 10);
-  // };
+  const updateCaretPositionSpace = () => {
+    setTimeout(() => {
+      if (!inputRef.current) {
+        return;
+      }
+      const index = inputRef.current.value.indexOf(' ');
+      if (index > 0) {
+        inputRef.current.selectionStart = index;
+        inputRef.current.selectionEnd = index;
+      }
+    }, 10);
+  };
 
-  // const addOutlineFix = (ref: HTMLDivElement | null) => {
-  //   toolbox.outlineListener(ref);
-  // };
+  const addOutlineFix = (ref: HTMLDivElement | null) => {
+    toolbox.outlineListener(ref);
+  };
 
-  // const removeOutlineFix = (ref: HTMLDivElement | null) => {
-  //   toolbox.outlineListener(ref, true);
-  // };
+  const removeOutlineFix = (ref: HTMLDivElement | null) => {
+    toolbox.outlineListener(ref, true);
+  };
 
   // const getCalIcon = () => {
   //   const calendar = `url("data:image/svg+xml,%3csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cg clip-path='url(%23clip0)' fill='black'%3e%3cpath d='M2.251 24a2.252 2.252 0 01-2.25-2.25V5.25A2.252 2.252 0 012.251 3h3.75V.75a.75.75 0 011.5 0V3h9V.75a.75.75 0 011.5 0V3h3.75a2.252 2.252 0 012.25 2.25v16.5a2.252 2.252 0 01-2.25 2.25h-19.5zm-.75-2.25c0 .414.336.75.75.75h19.5a.75.75 0 00.75-.75V10.5h-21v11.25zm21-12.75V5.25a.75.75 0 00-.75-.75h-3.75V6a.75.75 0 01-1.5 0V4.5h-9V6a.75.75 0 01-1.5 0V4.5h-3.75a.75.75 0 00-.75.75V9h21z'/%3e%3crect x='3' y='19.5' width='6' height='1.5' rx='.75'/%3e%3c/g%3e%3cdefs%3e%3cclipPath id='clip0'%3e%3cpath d='M0 0h24v24H0V0z' fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e")`;
@@ -220,63 +220,63 @@ export const Datepicker: FC<DatepickerProps> = ({
   //   );
   // };
 
-  // const formatDate = (inputString: string) => {
-  //   const digits = (inputString.match(/\d+/g) || []).join('');
-  //   let res = digits
-  //     .split('')
-  //     .reduce((num1, num2, index) => (index === 2 || index === 4 ? `${num1}.${num2}` : `${num1}${num2}`), '')
-  //     .substr(0, 10);
+  const formatDate = (inputString: string) => {
+    const digits = (inputString.match(/\d+/g) || []).join('');
+    let res = digits
+      .split('')
+      .reduce((num1, num2, index) => (index === 2 || index === 4 ? `${num1}.${num2}` : `${num1}${num2}`), '')
+      .substr(0, 10);
 
-  //   if (res.length === 2 || res.length === 5) {
-  //     res = `${res}.`;
-  //   }
-  //   if (res.length > 6 && res.length < 10) {
-  //     res += ' ';
-  //   }
-  //   return res;
-  // };
+    if (res.length === 2 || res.length === 5) {
+      res = `${res}.`;
+    }
+    if (res.length > 6 && res.length < 10) {
+      res += ' ';
+    }
+    return res;
+  };
 
   return (
     <div ref={datepickerRef}>
-      {/* <div className={datePickerClasses}> */}
-      {/* Label */}
-      {/* {label !== '' && <label className={datePickerLabelClasses}>{label}</label>} */}
+      <div className={datePickerClasses}>
+        {/* Label */}
+        {/* {label !== '' && <label className={datePickerLabelClasses}>{label}</label>} */}
 
-      {/* MUI Datepicker */}
-      {/* <ThemeProvider theme={materialTheme}> */}
-      <MuiPickersUtilsProvider utils={MomentUtils} libInstance={moment}>
-        <KeyboardDatePicker
-          // autoOk={true} -> Skru på når ferdig testet
-          variant="inline"
-          // inputProps={{ ref: inputRef }}
-          value={selectedDate}
-          onChange={handleDateChange}
-          // onFocus={updateCaretPositionSpace}
-          // placeholder={placeholderString}
-          // format="DD.MM.yyyy"
-          // rifmFormatter={formatDate}
-          // disabled={isDisabled === true || isDisabled === 'true'}
-          // fullWidth={isFullWidth === true || isFullWidth === 'true'}
-          // minDate={startDate ? startDate : minDate}
-          // maxDate={maxDate}
-          // ToolbarComponent={getCustomToolbar}
-          // keyboardIcon={getCalIcon()}
-          // leftArrowIcon={getArrowIcon(true)}
-          // rightArrowIcon={getArrowIcon(false)}
-          // PopoverProps={{
-          //   anchorOrigin: { horizontal: 'left', vertical: 'bottom' },
-          //   transformOrigin: { horizontal: 'left', vertical: 'top' },
-          //   ref: datepickerPopoverRef,
-          // }}
-          // KeyboardButtonProps={{
-          //   'aria-label': 'endre dato',
-          // }}
-        />
-      </MuiPickersUtilsProvider>
-      {/* </ThemeProvider> */}
+        {/* MUI Datepicker */}
+        <ThemeProvider theme={materialTheme}>
+          <MuiPickersUtilsProvider utils={MomentUtils} libInstance={moment}>
+            <KeyboardDatePicker
+              // autoOk={true} -> Skru på når ferdig testet
+              variant="inline"
+              inputProps={{ ref: inputRef }}
+              value={selectedDate}
+              onChange={handleDateChange}
+              onFocus={updateCaretPositionSpace}
+              placeholder={placeholderString}
+              format="DD.MM.yyyy"
+              rifmFormatter={formatDate}
+              disabled={isDisabled === true || isDisabled === 'true'}
+              fullWidth={isFullWidth === true || isFullWidth === 'true'}
+              minDate={startDate ? startDate : minDate}
+              maxDate={maxDate}
+              // ToolbarComponent={getCustomToolbar}
+              // keyboardIcon={getCalIcon()}
+              // leftArrowIcon={getArrowIcon(true)}
+              // rightArrowIcon={getArrowIcon(false)}
+              PopoverProps={{
+                anchorOrigin: { horizontal: 'left', vertical: 'bottom' },
+                transformOrigin: { horizontal: 'left', vertical: 'top' },
+                ref: datepickerPopoverRef,
+              }}
+              KeyboardButtonProps={{
+                'aria-label': 'endre dato',
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </ThemeProvider>
 
-      {/* Helper text */}
-      {/* {errorMessage && (
+        {/* Helper text */}
+        {/* {errorMessage && (
           <div className="ewc-datepicker__error">
             <i
               className="ewc-datepicker__icon ewc-datepicker__icon--error"
@@ -286,8 +286,8 @@ export const Datepicker: FC<DatepickerProps> = ({
             ></i>
             <div className="ewc-datepicker__helper-text">{errorMessage}</div>
           </div>
-        )}
-      </div> */}
+        )} */}
+      </div>
     </div>
   );
 };
