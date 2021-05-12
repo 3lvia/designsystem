5299c3efcb751a756d3e9013f3aba1201a9854f5
Michael Pande <michaelpande@gmail.com>
2021-05-12 13:23:30 +0000

    Lego 907 build command cleanup (#299)
    * Removed unused build command for react
    
    * Cleanup of commands & removed buggy syntax from popover
    
    * Removed unecessary cleanup function
    
    * Fixed broken command

--- packages/components/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-
-const port = process.env.PORT || 3333;
-app.use(express.static('dist/'));
-server.listen(port);
-console.log('Listening on port ' + port);
