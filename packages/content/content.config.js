e4905ec34a7849e0e72776df3c588f4cc45c3186
Michael Pande <michaelpande@gmail.com>
2021-05-27 10:45:05 +0200

    CMS

---
+++ packages/content/content.config.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        path: "community/cms-test",
+        contentful: {
+            entry_id: "6agg697xYesUFEdPfooJee"
+        }
+    }
+]
---
+++ packages/content/getContent.js
@@ -0,0 +1,6 @@
+const dotenv = require('dotenv').config();
+const content = require('./content.config');
+
+console.log(content);
+console.log(dotenv);
+// TODO: Request data from contentful
