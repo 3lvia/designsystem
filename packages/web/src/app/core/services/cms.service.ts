5dd0540774253f43bb77bc34215997f8159221c0
Michael Pande <michaelpande@gmail.com>
2021-05-27 17:54:27 +0200

    Improvements to HTML structure

--- packages/web/src/app/core/services/cms.service.ts
+++ packages/web/src/app/core/services/cms.service.ts
@@ -14,13 +14,14 @@ export class CMSService {
       [MARKS.BOLD]: text => `<b>${text}</b>`
     },
     renderNode: {
+      [BLOCKS.HEADING_1]: (node, next) => `<h2 class="e-title-md elvis-anchor-title e-mb-24">${next(node.content)}</h2>`, // We don't want want to use h1 tags for this purpose
       [BLOCKS.HEADING_2]: (node, next) => `<h2 class="e-title-md elvis-anchor-title e-mb-24">${next(node.content)}</h2>`,
       [BLOCKS.HEADING_3]: (node, next) => `<h3 class="e-title-sm">${next(node.content)}</h3>`,
-      [BLOCKS.PARAGRAPH]: (node, next) => `<p class="e-text-body component-section__content">${next(node.content)}</p>`,
+      [BLOCKS.PARAGRAPH]: (node, next) => `<p class="e-text-body">${next(node.content)}</p>`,
       [BLOCKS.UL_LIST]: (node, next) => `<ul class="e-list">${next(node.content)}</ul>`,
       [INLINES.HYPERLINK]: (node, next) => `<a class="e-link e-link--inline" href="${node.data.uri}">${next(node.content)}</a>`,
-      [BLOCKS.EMBEDDED_ASSET]: (node, next) => `<img src="${node.data.target.fields.file.url}"/>`,
-      [BLOCKS.HR]: (node, next) => `<hr class="e-mb-24" style="height:1px !important; border:0;"></hr>`
+      [BLOCKS.EMBEDDED_ASSET]: (node, next) => `<img class="cms-img" src="${node.data.target.fields.file.url}"/>`,
+      [BLOCKS.HR]: (node, next) => `<hr class="cms-hr e-mb-24"></hr>`
     }
   }
 
