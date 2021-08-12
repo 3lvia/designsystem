afdc46b53279491d674e049e899d9ee4a7767f5c
Fride Skarseth <fs@computas.com>
2021-08-12 10:02:04 +0200

    Trying making a compiler

--- packages/web/src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module.ts
+++ packages/web/src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module.ts
@@ -1,15 +1,25 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { Compiler, CompilerFactory, COMPILER_OPTIONS, CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentExampleGeneratorComponent } from './component-example-generator.component';
 import { ComponentExampleCodeModule } from '../component-example-code/component-example-code.module';
 import { ComponentExampleAccordionModule } from '../component-example-accordion/component-example-accordion.module';
 import { CegFiltersModule } from './ceg-filters/ceg-filters.module';
 import '@elvia/elvis-dropdown';
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   imports: [CommonModule, ComponentExampleCodeModule, ComponentExampleAccordionModule, CegFiltersModule],
   declarations: [ComponentExampleGeneratorComponent],
   exports: [ComponentExampleGeneratorComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    { provide: COMPILER_OPTIONS, useValue: {}, multi: true },
+    { provide: CompilerFactory, useClass: JitCompilerFactory, deps: [COMPILER_OPTIONS] },
+    { provide: Compiler, useFactory: createCompiler, deps: [CompilerFactory] },
+  ],
 })
 export class ComponentExampleGeneratorModule {}
+
+export function createCompiler(compilerFactory: CompilerFactory) {
+  return compilerFactory.createCompiler();
+}
