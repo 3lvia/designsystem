34c1f7ca36b45754804cf81ad741ca510d39bfc6
Fride Skarseth <fs@computas.com>
2021-08-31 09:28:58 +0200

    Doc: Using new accordion component

--- packages/web/src/app/doc-pages/components/accordion-doc/accordion-doc.module.ts
+++ packages/web/src/app/doc-pages/components/accordion-doc/accordion-doc.module.ts
@@ -9,7 +9,6 @@ import { ComponentPropertiesModule } from 'src/app/shared/component-documentatio
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
-import { ComponentExampleAccordionModule } from 'src/app/shared/component-documentation/component-example/component-example-accordion/component-example-accordion.module';
 import { ComponentPropertiesTableModule } from 'src/app/shared/component-documentation/component-properties-table/component-properties-table.module';
 import { ComponentExampleGeneratorModule } from 'src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
@@ -25,7 +24,6 @@ import { ComponentInstallationModule } from 'src/app/shared/component-documentat
     ComponentSectionModule,
     ComponentSubsectionModule,
     WhenToUseModule,
-    ComponentExampleAccordionModule,
     ComponentPropertiesTableModule,
     ComponentExampleGeneratorModule,
     ComponentSubsubsectionModule,
@@ -33,4 +31,4 @@ import { ComponentInstallationModule } from 'src/app/shared/component-documentat
   ],
   declarations: [AccordionDocComponent],
 })
-export class AccordionDocModule {}
+export class AccordionDocModule { }
