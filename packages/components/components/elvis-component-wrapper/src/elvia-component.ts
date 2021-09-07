70d11705a016d66c78345189f4746d5de294e463
Fride Skarseth <fs@computas.com>
2021-09-07 16:13:28 +0200

    Components wrapper: Slot fix + box componen doc available (#381)
    * Components wrapper: Slot fix + box componen doc available
    
    * Component wrapper: Bumped package
    
    * Component wrapper: Updated changelog

--- packages/components/components/elvis-component-wrapper/src/elvia-component.ts
+++ packages/components/components/elvis-component-wrapper/src/elvia-component.ts
@@ -48,6 +48,10 @@ export class ElvisComponentWrapper extends HTMLElement {
   }
 
   attributeChangedCallback(): void {
+    // Slot items
+    if (this.webComponent.getComponentData().slotItems === true) {
+      this.storeAllSlots();
+    }
     this.throttleRenderReactDOM();
   }
 
@@ -144,24 +148,24 @@ export class ElvisComponentWrapper extends HTMLElement {
   private logErrorMessage(functionName: string, error: string): void {
     console.error(
       '[' +
-        this.webComponent.getComponentData().name +
-        '] elvia-component-wrapper: ' +
-        "Failed at function '" +
-        functionName +
-        "'. " +
-        error,
+      this.webComponent.getComponentData().name +
+      '] elvia-component-wrapper: ' +
+      "Failed at function '" +
+      functionName +
+      "'. " +
+      error,
     );
   }
 
   private logWarnMessage(functionName: string, warn: string): void {
     console.warn(
       '[' +
-        this.webComponent.getComponentData().name +
-        '] elvia-component-wrapper: ' +
-        "Failed at function '" +
-        functionName +
-        "'. " +
-        warn,
+      this.webComponent.getComponentData().name +
+      '] elvia-component-wrapper: ' +
+      "Failed at function '" +
+      functionName +
+      "'. " +
+      warn,
     );
   }
 
@@ -222,8 +226,8 @@ export class ElvisComponentWrapper extends HTMLElement {
         this.logErrorMessage(
           'convertString',
           ': The property "' +
-            attrName +
-            '" is not a valid JSON object. This is probably because the JSON object is containing single quotes instead of double quotes.',
+          attrName +
+          '" is not a valid JSON object. This is probably because the JSON object is containing single quotes instead of double quotes.',
         );
       }
     }
