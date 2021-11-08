f420fc60231fc2458daad088305a0ffd612b8328
Fride Skarseth <fs@computas.com>
2021-11-08 16:03:48 +0100

    Testing: tests in pre-push script

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+cd packages/components
+yarn test 
+echo SUCCESS
 echo 'Elvis - Building project before pushing...'
+cd ../..
 cd packages/web 
 node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --source-map=false --progress=false
 cd ../..
