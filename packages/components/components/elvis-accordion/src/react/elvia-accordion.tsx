f376e2c9711dfba9c59de5d632bb3e061a8008d7
Martin Espeland <m.espeland89@gmail.com>
2021-11-10 12:49:02 +0100

    defined btn color in accordion

--- packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
+++ packages/components/components/elvis-accordion/src/react/elvia-accordion.tsx
@@ -16,9 +16,9 @@ export interface AccordionProps {
 }
 
 const ElviaColors = {
-  elviaCharge: '#29d305',
-  elviaOn: '#ffffff',
-  elviaOff: '#000000',
+  green: '#29d305',
+  white: '#ffffff',
+  black: '#000000',
 };
 
 const AccordionArea = styled.div`
@@ -86,6 +86,8 @@ const AccordionButton = styled.button<AccordionButton>`
   line-height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
   text-align: left;
   cursor: pointer;
+  color: ${ElviaColors.black};
+
   &:hover {
     i {
       background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e");
@@ -94,25 +96,26 @@ const AccordionButton = styled.button<AccordionButton>`
 
   i {
     margin-left: ${(props: { openLabel: string; closeLabel: string }) => {
-      if (props.openLabel !== undefined || props.closeLabel !== undefined) {
-        return '8px;';
-      }
-      return '0px;';
-    }};
+    if (props.openLabel !== undefined || props.closeLabel !== undefined) {
+      return '8px;';
+    }
+    return '0px;';
+  }};
     border: none;
     border-radius: 50%;
     background-size: contain;
     background-position: center;
     background-repeat: no-repeat;
     background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 24 24' aria-hidden='true' width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12 24C5.383 24 0 18.617 0 12S5.383 0 12 0s12 5.383 12 12-5.383 12-12 12zm0-22.5C6.21 1.5 1.5 6.21 1.5 12S6.21 22.5 12 22.5 22.5 17.79 22.5 12 17.79 1.5 12 1.5z' fill='%2329D305'/%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M12.813 15.393a1.149 1.149 0 01-1.626 0L6.95 11.157A.853.853 0 118.157 9.95L12 13.793l3.843-3.843a.853.853 0 011.207 1.207l-4.237 4.236z' fill='black'/%3e%3c/svg%3e");
-    background-color: ${ElviaColors.elviaOn};
+    background-color: ${ElviaColors.white};
     display: inline-block;
     height: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
     width: ${(props: { size: string }) => (props.size === 'small' ? '16px' : '24px')};
     transition: transform 300ms;
     transform: ${(props: { isContentOpen: boolean }) =>
-      (props.isContentOpen && ' rotate(180deg)') || (props.isContentOpen === false && ' rotate(0deg)')};
+    (props.isContentOpen && ' rotate(180deg)') || (props.isContentOpen === false && ' rotate(0deg)')};
   }
+
 `;
 
 const decideContentMarginTop = (contentOpen: boolean, type: string, size: string): string => {
