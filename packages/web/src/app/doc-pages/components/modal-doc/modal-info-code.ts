5c1fe343f01769fc2f18b0bf1eaea8686eea9231
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2021-11-15 10:22:43 +0100

    Modal: removed duplicated example code scripts

--- packages/web/src/app/doc-pages/components/modal-doc/modal-info-code.ts
+++ packages/web/src/app/doc-pages/components/modal-doc/modal-info-code.ts
@@ -38,21 +38,8 @@ const modalInfoCode = {
   <button slot="primaryButton" class="e-btn e-btn--primary e-btn--lg">Primary action</button>
 </elvia-modal>
 `,
-  codeNativeScript: `  const modal = document.getElementById('example-elvia-modal');
-  const button = document.getElementById('example-modal-button;
-  let isModalShowing = false;
-
-  modal.setProps({isShowing: isModalShowing });
-  modal.setProps({title: "Title of content" });
-  modal.addEventListener('onHide', () => {
-    modal.setProps({isShowing: !isModalShowing });
-    isModalShowing = !isModalShowing;
-  });
-  button.addEventListener("click", () => {
-    modal.setProps({isShowing: !isModalShowing });
-    isModalShowing = !isModalShowing;
-  });
-`,
+  codeNativeScript: ``,
+// all scripts for the examples are in modal-data.ts
 };
 
 export { modalInfoCode };
