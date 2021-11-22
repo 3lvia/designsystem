1b362985150fd1034d48f95a61fd26c11c4d3580
Martin Espeland <m.espeland89@gmail.com>
2021-11-22 16:25:55 +0100

    Reorder the box jest test to be in one describe

--- packages/components/components/elvis-box/src/react/elvia-box.test.js
+++ packages/components/components/elvis-box/src/react/elvia-box.test.js
@@ -1,65 +1,66 @@
 import 'jsdom-global/register';
 import '@testing-library/jest-dom';
+import 'jest-styled-components';
 import { Box } from '@elvia/elvis-box/react';
 import React from 'react';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import enzyme from 'enzyme';
+import { querySelectorAll } from 'dom-helpers';
+import { getChildren } from 'domutils';
+
 enzyme.configure({ adapter: new Adapter() });
 
 describe('Elvis Box ', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = enzyme.mount(
-      <Box
-        hasBorder={true}
-        isColored={true}
-        title={<h1>Box title</h1>}
-        content={<div>Box content</div>}
-      ></Box>,
-    );
+  describe(' with border & colored line', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = enzyme.mount(
+        <Box
+          hasBorder={true}
+          isColored={true}
+          title={<h1>Box title</h1>}
+          content={<div>Box content</div>}
+        ></Box>,
+      );
+    });
+
+    it('should show title "Box title"', function (done) {
+      expect(wrapper.find('div').at(1).text()).toBe('Box title');
+      done();
+    });
+
+    it('should show content of "Box content"', function (done) {
+      expect(wrapper.find('div').at(2).text()).toBe('Box content');
+      done();
+    });
+
+    it('should have a div with elvia green background color"', function (done) {
+      expect(wrapper.find('div').at(2).find('div').at(1).getDOMNode()).toHaveStyle('background: #29d305');
+      done();
+    });
+
+    it('should show border', function (done) {
+      expect(wrapper.find('div').at(2).getDOMNode()).toHaveStyle('border: 1px solid #E9E9E9');
+      done();
+    });
   });
 
-  it('should show title "Box title"', function (done) {
-    expect(wrapper.find('div').at(1).text()).toBe('Box title');
-    done();
-  });
+  describe(' without border & colored line', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = enzyme.mount(
+        <Box
+          hasBorder={false}
+          isColored={false}
+          title={<h1>Box title</h1>}
+          content={<div>Box content</div>}
+        ></Box>,
+      );
+    });
 
-  it('should show content of "Box content"', function (done) {
-    expect(wrapper.find('div').at(2).text()).toBe('Box content');
-    done();
-  });
-
-  it('should have a div with elvia green background color"', function (done) {
-    expect(wrapper.find('div').at(2).find('div').at(1).getDOMNode()).toHaveStyle('background: #29d305');
-    done();
-  });
-
-  it('should show border', function (done) {
-    expect(wrapper.find('div').at(2).getDOMNode()).toHaveStyle('border: 1px solid #E9E9E9');
-    done();
-  });
-});
-
-describe('Elvis Box with no border & color', () => {
-  let wrapper;
-  beforeEach(() => {
-    wrapper = enzyme.mount(
-      <Box
-        hasBorder={false}
-        isColored={false}
-        title={<h1>Box title</h1>}
-        content={<div>Box content</div>}
-      ></Box>,
-    );
-  });
-
-  it('should not have a div with elvia green background color"', function (done) {
-    expect(wrapper.find('div').at(2).find('div').at(1).getDOMNode()).not.toHaveStyle('background: #29d305');
-    done();
-  });
-
-  it('should not show border', function (done) {
-    expect(wrapper.find('div').at(2).getDOMNode()).not.toHaveStyle('border: 1px solid #E9E9E9');
-    done();
+    it('should not show border', function (done) {
+      expect(wrapper.find('div').at(2).getDOMNode()).not.toHaveStyle('border: 1px solid #E9E9E9');
+      done();
+    });
   });
 });
