88af3835c972288185d01af8bd422a0b8e0fda7c
Fride Skarseth <fs@computas.com>
2021-11-23 09:46:40 +0100

    CEG: Interfaces

---
+++ packages/web/src/app/shared/component-documentation/component-example/component-example-generator/ceg.interface.ts
@@ -0,0 +1,25 @@
+export interface CegFormGroup {
+  label: string;
+  type: string;
+  defaultValue?: string | number;
+  propValue?: string;
+  propName?: string;
+  propSlot?: string;
+  formGroupOptions?: CegFormGroupOption[];
+  counterMax?: number;
+  counterMin?: number;
+  counterStepValue?: number;
+  counterType?: string;
+  dependency?: CegFormDependency;
+}
+export interface CegFormGroupOption {
+  name: string;
+  defaultValue: string;
+  propValue?: string;
+  propName?: string;
+  dependency?: CegFormDependency;
+}
+export interface CegFormDependency {
+  name: string;
+  value: string | string[];
+}
