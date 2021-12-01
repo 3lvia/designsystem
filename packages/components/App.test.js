e39a63e4a8da39a767d1cb1a155cb91bd9843056
Fride Skarseth <fs@computas.com>
2021-12-01 18:19:16 +0100

    Testing: Removed redundant test

--- packages/components/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Accordion } from '@elvia/elvis-accordion/react';
-import { shallow } from 'enzyme';
-
-it('should render my component', () => {
-  const wrapper = shallow(<Accordion />);
-});
