e82acc106710821d87294fe2adf11493536f598d
Fride Skarseth <fs@computas.com>
2021-12-06 15:50:21 +0100

    Build: Running build on pre-push

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -2,6 +2,7 @@
 cd packages/components 
 echo SUCCESS
 echo 'Elvis - Building project before pushing...'
+yarn run build
 cd ../..
 cd packages/web 
 node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --source-map=false --progress=false
