9beb5dbec902d820d368a8c233cf8f68cb0150a7
Fride Skarseth <fs@computas.com>
2021-12-07 12:33:37 +0100

    Prepush: updated prepush messages

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd packages/components 
 echo SUCCESS
-echo 'Elvis - Building project before pushing...'
+echo 'Components - Building components project'
 yarn run build
 cd ../..
 cd packages/web 
+echo SUCCESS
+echo 'Web - Building web prosject'
 node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --source-map=false --progress=false
 cd ../..
