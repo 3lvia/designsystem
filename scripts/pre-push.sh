3a4a89d40e2558662e9310c720dc5d3c02788816
Fride Skarseth <fs@computas.com>
2021-12-07 14:02:00 +0100

    Lego 1314 build (#518)
    * Build: Update watch to not run declarations and test
    
    * Build: Updated pre-push

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd packages/components 
-echo SUCCESS
-echo 'Elvis - Building project before pushing...'
+cd packages/components
+echo 'Components - Building components...'
+yarn build
 cd ../..
 cd packages/web 
-node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --source-map=false --progress=false
-cd ../..
+echo 'Web - Building web...'
+yarn build
