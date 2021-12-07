6c81df8051d3af8077b4135f7ff36dd5c7f565e5
Fride Skarseth <fs@computas.com>
2021-12-07 09:49:05 +0100

    Tabs: tests added (#517)
    * Tabs: tests added

---
+++ packages/components/components/elvis-tabs/src/react/elvia-tabs.test.js
@@ -0,0 +1,67 @@
+import Tabs from './elvia-tabs.tsx';
+import React from 'react';
+import { mount } from 'enzyme';
+
+describe('Elvis Tabs', () => {
+  let wrapper;
+  let items = ['Oranges', 'Apples', 'Pears'];
+  let tabsElements;
+
+  describe('Default', () => {
+    beforeEach(() => {
+      wrapper = mount(<Tabs items={items} />);
+      tabsElements = wrapper.find('.ewc-tabs__label');
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it('should have correct number of tabs', function (done) {
+      expect(tabsElements.length).toEqual(3);
+      done();
+    });
+    it('should have default value 0', function (done) {
+      const selectedTab = wrapper.find('.ewc-tabs__label--selected');
+      expect(tabsElements.at(0).getDOMNode()).toEqual(selectedTab.getDOMNode());
+      done();
+    });
+    it('should update value when clicking new tab', function (done) {
+      tabsElements.at(1).simulate('click');
+      expect(tabsElements.at(1).getDOMNode().classList.contains('ewc-tabs__label--selected')).toBe(true);
+      done();
+    });
+    it('should have correct labels', function (done) {
+      expect(tabsElements.at(0).text()).toBe('Oranges');
+      expect(tabsElements.at(1).text()).toBe('Apples');
+      expect(tabsElements.at(2).text()).toBe('Pears');
+      done();
+    });
+  });
+  describe('Value = 2', () => {
+    beforeEach(() => {
+      wrapper = mount(<Tabs items={items} value={2}></Tabs>);
+      tabsElements = wrapper.find('.ewc-tabs__label');
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it('should have new default value 2', function (done) {
+      expect(tabsElements.at(2).getDOMNode().classList.contains('ewc-tabs__label--selected')).toBe(true);
+      done();
+    });
+  });
+  describe('Inverted', () => {
+    let tabsContainer;
+    beforeEach(() => {
+      wrapper = mount(<Tabs items={items} isInverted={true}></Tabs>);
+      tabsContainer = wrapper.find({ 'data-testid': 'tabs-container' });
+      tabsElements = wrapper.find('.ewc-tabs__label');
+    });
+    afterEach(() => {
+      wrapper.unmount();
+    });
+    it('should have class inverted', function (done) {
+      expect(tabsContainer.getDOMNode().classList.contains('ewc-tabs--inverted')).toBe(true);
+      done();
+    });
+  });
+});
