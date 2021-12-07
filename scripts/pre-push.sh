722df8a187850a89ea6cb944d0531441a332cb40
Fride Skarseth <fs@computas.com>
2021-12-07 12:40:09 +0100

    Prepush: testing prepush

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-cd packages/components 
-echo SUCCESS
-echo 'Components - Building components project'
-yarn run build
-cd ../..
-cd packages/web 
-echo SUCCESS
-echo 'Web - Building web prosject'
+echo 'Building project'
 node --max_old_space_size=4096 ./node_modules/@angular/cli/bin/ng build --source-map=false --progress=false
-cd ../..
