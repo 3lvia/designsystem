6c013ce2283b7f97d2ded027f453d493c09fe058
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2022-01-05 10:46:35 +0100

    fix typo in docs

--- packages/web/src/app/doc-pages/components/breadcrumb-doc/breadcrumb-event-code.ts
+++ packages/web/src/app/doc-pages/components/breadcrumb-doc/breadcrumb-event-code.ts
@@ -8,7 +8,7 @@ const breadcrumbEventCode = {
     {title: 'Components'},
     {title: 'Breadcrumbs'},
   ]}
-  breadcrumsOnChange={(event) => handleOnChange(event)}
+  breadcrumbsOnChange={(event) => handleOnChange(event)}
 ></Breadcrumb>`,
   codeAngular: `<elvia-breadcrumb 
   [breadcrumbs]="[
@@ -16,7 +16,7 @@ const breadcrumbEventCode = {
     {title: 'Components'},
     {title: 'Breadcrumbs'},
   ]"
-  (breadcrumsOnChange)="handleOnChange($event)"
+  (breadcrumbsOnChange)="handleOnChange($event)"
 ></elvia-breadcrumb>`,
   codeVue: `<elvia-breadcrumb 
   :breadcrumbs="[
@@ -24,7 +24,7 @@ const breadcrumbEventCode = {
     {title: 'Components'},
     {title: 'Breadcrumbs'},
   ]"
-  @breadcrums-on-change="handleOnChange($event)"
+  @breadcrumbs-on-change="handleOnChange($event)"
 ></elvia-breadcrumb>`,
   codeNativeHTML: `<elvia-breadcrumb id="example-elvia-breadcrumb-2"></elvia-breadcrumb>`,
   codeNativeScript: ` const breadcrumb = document.getElementById('example-elvia-breadcrumb-2');
