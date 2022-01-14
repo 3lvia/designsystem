524ff87cdf067d6ed474753e50415737574c9a92
Fride Skarseth <fs@computas.com>
2022-01-14 09:53:13 +0100

    Revert "CMS - Small fix"
    This reverts commit e1c5580f49d89d85ebbaa0308eeffcb6067e9000.

--- packages/web/contentful/syncFromContentful.js
+++ packages/web/contentful/syncFromContentful.js
@@ -1,4 +1,5 @@
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path: '../.env' });
 const contentful = require('contentful');
 const del = require('del');
 const fs = require('fs');
