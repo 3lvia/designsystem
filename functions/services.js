e92450e90c1ea8f906a3eb55e64f325486781573
Fride Skarseth <fs@computas.com>
2022-01-31 10:48:13 +0100

    Fix

--- functions/services.js
+++ functions/services.js
@@ -8,8 +8,7 @@ exports.handler = async (event, context) => {
 	const client = contentful.createClient({
 		host: 'preview.contentful.com',
 		space: process.env.CONTENTFUL_SPACE,
-		accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
-		resolveLinks: false
+		accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
 	})
 	const entry = await client.getEntry(event.queryStringParameters.id, { locale: '*', include: 10 })
 		.then((entry) => {
