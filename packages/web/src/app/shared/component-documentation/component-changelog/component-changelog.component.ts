a2090f065d17f5aa91d4c23e2571dac8c5e7cfe8
Kristin Pettersen <julie.kristin.pettersen@elvia.no>
2022-03-15 12:07:02 +0100

    Testing json script

--- packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
+++ packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-component-changelog',
   templateUrl: './component-changelog.component.html',
   styleUrls: ['./component-changelog.component.scss'],
 })
-export class ComponentChangelogComponent {}
+export class ComponentChangelogComponent {
+  @Input() componentData;
+  changelogSrc;
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  ngOnInit() {
+    this.changelogSrc = `assets/changelog/${this.componentData.name}/CHANGELOG_new.md`;
+  }
+}
