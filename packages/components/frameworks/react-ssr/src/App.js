f34333f3b436d2b34e01788e32a2b1405799d5ac
Martin Espeland <m.espeland89@gmail.com>
2022-03-16 15:34:16 +0100

    update yarn lock, yarnrc lint & reactexample with dropdown

--- packages/components/frameworks/react-ssr/src/App.js
+++ packages/components/frameworks/react-ssr/src/App.js
@@ -86,6 +86,14 @@ function App() {
         <div className="example-wrapper">
           {/* Test the component here (delete what was here previously). When done add it to the list alphabetically */}
           <h3>Test your component here</h3>
+
+          <h3>Dropdown</h3>
+          <Dropdown
+            options={dropdownOptions}
+            defaultValue={defaultDropdownOptions}
+            label="test"
+            isMulti
+          ></Dropdown>
           {/* Normal version */}
           <div className="e-bg-white"></div>
           {/* Inverted version */}
