09fd97643c99addd4cd44d871c78a859b300ee0e
kristin-pettersen <42831119+kristin-pettersen@users.noreply.github.com>
2022-04-01 09:21:37 +0200

    Cleanup

--- packages/web/src/app/shared/component-documentation/component-installation/component-installation.component.ts
+++ packages/web/src/app/shared/component-documentation/component-installation/component-installation.component.ts
@@ -7,9 +7,4 @@ import { Component, Input } from '@angular/core';
 })
 export class ComponentInstallationComponent {
   @Input() componentData;
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  ngOnInit() {
-    console.log(this.componentData);
-  }
 }
