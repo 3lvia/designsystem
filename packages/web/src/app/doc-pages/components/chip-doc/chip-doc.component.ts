6819802966effd4e9fc3b382d418a471b32c8428
Fride Skarseth <fs@computas.com>
2022-04-16 01:43:42 +0200

    Chip - Accessiblity fixes

--- packages/web/src/app/doc-pages/components/chip-doc/chip-doc.component.ts
+++ packages/web/src/app/doc-pages/components/chip-doc/chip-doc.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { getComponent } from 'src/app/shared/doc-pages';
+import { chipAccessibilityCode } from './chip-accessibility-code';
 import { chipData } from './chip-data';
 
 @Component({
@@ -13,6 +14,7 @@ export class ChipDocComponent {
   does = chipData.does;
   donts = chipData.donts;
   componentData = chipData;
+  chipAccessibilityCode = chipAccessibilityCode;
 
   filteredValues = { 2019: false, 2020: true, 2021: true, 2022: false, 2023: true, 2024: true };
   filteredKeys = Object.keys(this.filteredValues);
