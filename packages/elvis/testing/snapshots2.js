794f237126a34449978ca8f6adfd24120659011d
Trygve Scheline Urdahl <trur@computas.com>
2022-04-20 14:38:42 +0200

    change percy method

--- packages/elvis/testing/snapshots2.js
+++ packages/elvis/testing/snapshots2.js
@@ -1,9 +1,24 @@
-import Percy from '@percy/core';
+const Percy = require('@percy/core');
 
 const percy = new Percy();
 percy.snapshot({
   baseUrl: 'localhost:3333',
-  snapshots: ['/', '/components/accordion.html', '/components/alert.html'],
+  snapshots: [
+    '/',
+    '/components/accordion.html',
+    '/components/alert.html',
+    '/components/autocomplete.html',
+    '/components/buttons.html',
+    '/components/cards.html',
+    '/components/checkbox.html',
+    '/components/content-loader.html',
+    '/components/datepicker.html',
+    '/components/drag-and-drop.html',
+    '/components/file-upload.html',
+    '/components/header.html',
+    '/components/input.html',
+    '/components/label.html',
+  ],
   options: {
     widths: [766, 1440],
   },
