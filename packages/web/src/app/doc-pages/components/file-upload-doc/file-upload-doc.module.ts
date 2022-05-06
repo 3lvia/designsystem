fa387e215c8c8ec59390ba55916a082e53c31efb
Fride Skarseth <fs@computas.com>
2022-05-06 09:36:43 +0200

    Lego 1576 accessibility (#787)
    * Accessibility - Start of documentation
    
    * Accessibility - Documentation for accessbility on each component

--- packages/web/src/app/doc-pages/components/file-upload-doc/file-upload-doc.module.ts
+++ packages/web/src/app/doc-pages/components/file-upload-doc/file-upload-doc.module.ts
@@ -8,6 +8,7 @@ import { ComponentExampleCodeModule } from 'src/app/shared/component-documentati
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { ComponentSectionModule } from 'src/app/shared/component-documentation/c
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
+    RouterModule,
   ],
   declarations: [FileUploadDocComponent, DndDirective],
 })
