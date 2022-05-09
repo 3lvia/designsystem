580108e074a229bd3e2e616e8c18d02530d1e44f
Trygve Scheline Urdahl <trur@computas.com>
2022-05-09 14:59:28 +0200

    var -> let

--- packages/components/frameworks/react-ssr/src/index.js
+++ packages/components/frameworks/react-ssr/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = document.getElementById('root');
-var renderMethod;
+let renderMethod;
 if (root && root.innerHTML !== '') {
   renderMethod = ReactDOM.hydrate;
 } else {
