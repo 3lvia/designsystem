7df135651da7db9e7cb56772e7a5c201908e22a2
Fride Skarseth <fs@computas.com>
2022-05-09 13:33:38 +0200

    Fix

--- packages/components/frameworks/react-ssr/src/App.js
+++ packages/components/frameworks/react-ssr/src/App.js
@@ -162,8 +162,8 @@ function App() {
           <Chip
             type="legend"
             valueOnChange={() => setChipSelected(!chipSelected)}
-            selected={chipSelected} aria-hidden="true"></i>
-            value="2022" aria-hidden="true"></i>
+            selected={chipSelected}
+            value="2022"
             onClick={() => console.log('Clicked')}
           ></Chip>
         </div>
