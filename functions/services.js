e8a934d58d7ae0c65979a01449b23ed6f752cb25
Fride Skarseth <fs@computas.com>
2022-05-09 14:50:13 +0200

    Fix

--- functions/services.js
+++ functions/services.js
@@ -1,13 +1,13 @@
 const contentful = require('contentful');
 const safeJsonStringify = require('safe-json-stringify');
 
-exports.handler = async (event) => {
+exports.handler = (event) => {
   const client = contentful.createClient({
     host: 'preview.contentful.com',
     space: process.env.CONTENTFUL_SPACE,
     accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   });
-  return await client
+  return client
     .getEntry(event.queryStringParameters.id, { locale: '*', include: 10 })
     .then((entryResult) => {
       return {
