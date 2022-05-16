a334af1fb2dbb632b09ed10e30db4733c3e51551
Kristin Pettersen <kristin.pettersen@elvia.no>
2022-05-16 11:53:21 +0200

    Documentation fix

--- packages/components/components/elvis-breadcrumb/src/react/config.ts
+++ packages/components/components/elvis-breadcrumb/src/react/config.ts
@@ -21,13 +21,13 @@ export const breadcrumbConfig: ComponentConfig = {
 export const breadcrumbLinkConfig: ComponentConfig = {
   componentName: 'Breadcrumb',
   deprecatedProps: {
-    // Rule 1.12: List of elements
+    // Rule 1.14: HTML-attributes
     url: {
       version: '2.0.0',
       newProp: 'href',
       isDirectReplacement: true,
     },
-    // Rule 1.4, event handlers
+    // Rule 1.14: HTML-attributes / Rule 1.8: Do not use title
     title: {
       version: '2.0.0',
       newProp: 'text',
