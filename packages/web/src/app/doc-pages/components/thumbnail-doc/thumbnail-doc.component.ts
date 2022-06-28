81475c127109e40beb440b393d0a7934b3a86b7a
Fride Skarseth <fs@computas.com>
2022-06-28 16:04:32 +0200

    Thumbnail

--- packages/web/src/app/doc-pages/components/thumbnail-doc/thumbnail-doc.component.ts
+++ packages/web/src/app/doc-pages/components/thumbnail-doc/thumbnail-doc.component.ts
@@ -9,4 +9,19 @@ import { getComponent } from 'src/app/shared/doc-pages';
 export class ThumbnailDocComponent {
   figmaUrl = getComponent('thumbnail').figmaUrl;
   description = getComponent('thumbnail').description;
+
+  thumbnailExample = `<div class="e-flex e-gap-8">
+  <button class="e-thumbnail">
+    <img src="assets/thumbnail/image.png" alt="Thumbnail example image 1" />
+  </button>
+  <button class="e-thumbnail">
+    <img src="assets/thumbnail/image-1.png" alt="Thumbnail example image 2" />
+  </button>
+  <button class="e-thumbnail">
+    <img src="assets/thumbnail/image-2.png" alt="Thumbnail example image 3" />
+  </button>
+  <button class="e-thumbnail e-thumbnail--selected">
+    <img src="assets/thumbnail/image-3.png" alt="Thumbnail example image 4" />
+  </button>
+</div>`;
 }
