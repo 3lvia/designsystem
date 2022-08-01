f0283ab793cda9dac5b318dad830d92077142c67
Trygve Scheline Urdahl <trur@computas.com>
2022-08-01 07:36:17 +0200

    test more labels

--- packages/components/components/elvis-icon/src/react/elvia-icon.tsx
+++ packages/components/components/elvis-icon/src/react/elvia-icon.tsx
@@ -73,7 +73,7 @@ export const Icon: React.FC<IconProps> = ({
 
   const displayIcon = getIcon(name, size, color, customSize);
 
-  // Remove children from rest because dangerouslySetInnerHTML is used
+  // Remove children from rest because dangerouslySetInnerHTML is used to set content
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { children, ...restWithoutChildren } = rest;
 
