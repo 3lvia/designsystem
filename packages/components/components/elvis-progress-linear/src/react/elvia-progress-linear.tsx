0cdb41d087689980a962a220b1c85f468033a1e8
Fride Skarseth <fs@computas.com>
2022-08-15 09:49:00 +0200

    Progressbar - size prop

--- packages/components/components/elvis-progress-linear/src/react/elvia-progress-linear.tsx
+++ packages/components/components/elvis-progress-linear/src/react/elvia-progress-linear.tsx
@@ -6,8 +6,9 @@ export interface ProgressbarProps {
   value: number;
   isIndeterminate: boolean;
   isError: boolean;
-  className?: string;
   ariaValueText: string;
+  size: 'medium' | 'large';
+  className?: string;
   inlineStyle?: { [style: string]: CSSProperties };
 }
 
@@ -16,14 +17,16 @@ const ProgressLinear: FC<ProgressbarProps> = ({
   isIndeterminate,
   isError,
   ariaValueText,
+  size = 'medium',
   className,
   inlineStyle,
   ...rest
 }) => {
-  const classes = classnames({
+  const classes = classnames('ewc-progress-linear', {
     ['ewc-progress-linear--range']: !isIndeterminate && !isError,
     ['ewc-progress-linear--indeterminate']: isIndeterminate && !isError,
     ['ewc-progress-linear--error']: isError,
+    ['ewc-progress-linear--lg']: size === 'large',
   });
 
   return (
@@ -37,8 +40,12 @@ const ProgressLinear: FC<ProgressbarProps> = ({
       aria-valuetext={ariaValueText ? ariaValueText : 'Progresjonen er nå på ' + value + '%.'}
       {...rest}
     >
-      <div className={'ewc-progress-linear'}>
-        <div className={classes} style={{ width: `${value}%` }} data-testid="progress-linear"></div>
+      <div className={classes} data-testid="progress-linear">
+        <div
+          className="ewc-progress-linear__progress"
+          style={{ width: `${value}%` }}
+          data-testid="progress-linear-progress"
+        ></div>
       </div>
     </div>
   );
