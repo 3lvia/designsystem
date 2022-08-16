ab2c14917e94f5e9f41fba40e8e53dfbf1d60bbc
Fride Skarseth <fs@computas.com>
2022-08-16 15:14:36 +0200

    Progressbar - size prop (#929)
    * Progressbar - size prop

--- packages/components/components/elvis-progress-linear/src/react/elvia-progress-linear.test.js
+++ packages/components/components/elvis-progress-linear/src/react/elvia-progress-linear.test.js
@@ -5,49 +5,46 @@ import { mount } from 'enzyme';
 describe('Elvis Progress Linear', () => {
   let wrapper;
   let progressWrapper;
-  let progressLinear;
+  let progressLinearElement;
+  let progressLinearProgressElement;
 
   describe('Default', () => {
     beforeEach(() => {
       wrapper = mount(<ProgressLinear></ProgressLinear>);
-      progressLinear = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
+      progressLinearElement = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
     });
     it('should have class ewc-progress-linear--range', function (done) {
-      expect(progressLinear.getDOMNode()).toHaveClass('ewc-progress-linear--range');
-      done();
-    });
-    it('should default value 0', function (done) {
-      expect(progressLinear.getDOMNode()).toHaveStyle('width: 0%');
+      expect(progressLinearElement.getDOMNode()).toHaveClass('ewc-progress-linear--range');
       done();
     });
   });
   describe('Value = 48', () => {
     beforeEach(() => {
       wrapper = mount(<ProgressLinear value="48"></ProgressLinear>);
-      progressLinear = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
+      progressLinearProgressElement = wrapper.find({ 'data-testid': 'progress-linear-progress' }).at(0);
     });
     it('should have value 48', function (done) {
-      expect(progressLinear.getDOMNode()).toHaveStyle('width: 48%');
+      expect(progressLinearProgressElement.getDOMNode()).toHaveStyle('width: 48%');
       done();
     });
   });
   describe('Indeterminate', () => {
     beforeEach(() => {
       wrapper = mount(<ProgressLinear isIndeterminate></ProgressLinear>);
-      progressLinear = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
+      progressLinearElement = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
     });
     it('should have class ewc-progress-linear--indeterminate', function (done) {
-      expect(progressLinear.getDOMNode()).toHaveClass('ewc-progress-linear--indeterminate');
+      expect(progressLinearElement.getDOMNode()).toHaveClass('ewc-progress-linear--indeterminate');
       done();
     });
   });
   describe('Error', () => {
     beforeEach(() => {
       wrapper = mount(<ProgressLinear isError></ProgressLinear>);
-      progressLinear = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
+      progressLinearElement = wrapper.find({ 'data-testid': 'progress-linear' }).at(0);
     });
     it('should have class ewc-progress-linear--error', function (done) {
-      expect(progressLinear.getDOMNode()).toHaveClass('ewc-progress-linear--error');
+      expect(progressLinearElement.getDOMNode()).toHaveClass('ewc-progress-linear--error');
       done();
     });
   });
