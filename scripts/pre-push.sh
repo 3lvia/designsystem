4586c90a01d5325d14ef8cc69a2a030e0f6f5171
Fride Skarseth <fs@computas.com>
2022-09-01 17:03:01 +0200

    Test

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # TODO: Build components only when changed
+echo 'Tokens - Building color, typography and icons...'
+yarn build:tokens
 echo 'Components - Building components...'
 yarn build:components
 echo 'Web - Building web...'
