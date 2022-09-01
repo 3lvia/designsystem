defbd17e9c9606d8f59ec2b4f19aa28e0c3936d0
Fride Skarseth <fs@computas.com>
2022-09-01 15:12:24 +0200

    Fix

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo
 echo 'Tokens - Building elvis-colors and elvis-typography'
 yarn build:tokens
 # TODO: Build components only when changed
-echo 'Components - Building components...'
+echo 'Components - Building components'
 yarn build:components
-echo 'Web - Building web...'
+echo 'Web - Building web'
 yarn build:web
