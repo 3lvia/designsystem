672321eb5a30ab65ce44adbb4f54b67d637530ff
Fride Skarseth <fs@computas.com>
2022-09-05 23:29:45 +0200

    Testing

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -4,7 +4,5 @@ echo 'Tokens - Building colors, typography and icons...'
 yarn build:tokens
 echo 'Components - Building components...'
 yarn build:components
-echo 'Components - Running components tests...'
-yarn test
 echo 'Web - Building web...'
 yarn build:web
