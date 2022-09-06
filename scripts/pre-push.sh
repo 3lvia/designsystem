896c3fe3a68778551a208054a14319f8151fa86e
Fride Skarseth <fs@computas.com>
2022-09-06 15:38:33 +0200

    Removed tokens from pre-push aswell

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # TODO: Build components only when changed
-echo 'Tokens - Building colors, typography and icons...'
-yarn build:tokens
 echo 'Components - Building components...'
 yarn build:components
 echo 'Web - Building web...'
