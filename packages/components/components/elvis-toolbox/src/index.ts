09c131bd350a763e16741a2b46c0b0f67977758b
Erik Tallang <et@computas.com>
2022-09-14 14:53:05 +0200

    feat(toolbox): add hook for detecting long presses

--- packages/components/components/elvis-toolbox/src/index.ts
+++ packages/components/components/elvis-toolbox/src/index.ts
@@ -2,3 +2,4 @@ export * from './elvis-toolbox';
 export { isSsr } from './isSsr';
 export { useConnectedOverlay } from './hooks/useConnectedOverlay';
 export { useFocusTrap } from './hooks/useFocusTrap';
+export { useLongPress } from './hooks/useLongPress';
