95dc26e912337e1dee89ce497db68643b79f4a55
Trygve Scheline Urdahl <trur@computas.com>
2022-09-14 08:56:01 +0200

    frix dropdown doc code indentation

--- packages/web/src/app/doc-pages/components/dropdown-doc/dropdown-load-more-items-code.ts
+++ packages/web/src/app/doc-pages/components/dropdown-doc/dropdown-load-more-items-code.ts
@@ -48,42 +48,42 @@ const dropdownLoadMoreItemsCode = {
   id="example-elvia-dropdown-loadmore"
 ></elvia-dropdown>`,
 
-  codeNativeScript: `// This code causes the "load more" button to be 
-// clickable two times before being removed
-const dropdown = document.getElementById('example-elvia-dropdown-loadmore');
-const items = [
-  { value: 'Nord-Korea', label: 'Nord-Korea' },
-  { value: 'Nord-Makedonia', label: 'Nord-Makedonia' },
-  { value: 'Norge', label: 'Norge' },
-  { value: 'Oman', label: 'Oman' },
-  { value: 'Russland', label: 'Russland' },
-  { value: 'Sveits', label: 'Sveits' },
-  { value: 'Sverige', label: 'Sverige' },
-];
-dropdown.setProps({ items: items });
-let timesLoadedMoreItems = 0;
-dropdown.addEventListener('onLoadMoreItems', () => {
-  dropdown.setProps({ isLoadingMoreItems: 'true' });
-  setTimeout(() => {
-    dropdown.setProps({ isLoadingMoreItems: 'false' });
-    if (timesLoadedMoreItems === 0) {
-      items.push({ value: 'Tyskland', label: 'Tyskland' });
-      items.push({ value: 'Ungarn', label: 'Ungarn' });
-      items.push({ value: 'USA', label: 'USA' });
-      items.push({ value: 'Vatikanstaten', label: 'Vatikanstaten' });
-    } else if (timesLoadedMoreItems === 1) {
-      items.push({ value: 'Venezuela', label: 'Venezuela' });
-      items.push({ value: 'Vietnam', label: 'Vietnam' });
-      items.push({ value: 'Østerrike', label: 'Østerrike' });
-      items.push({ value: 'Øst-Timor', label: 'Øst-Timor' });
-    }
-    dropdown.setProps({ items: items });
-    timesLoadedMoreItems++;
-    if (timesLoadedMoreItems > 1) {
-      dropdown.setProps({ hasLoadMoreItemsButton: 'false' });
-    }
-  }, 2000);
-});
+  codeNativeScript: `  // This code causes the "load more" button to be 
+  // clickable two times before being removed
+  const dropdown = document.getElementById('example-elvia-dropdown-loadmore');
+  const items = [
+    { value: 'Nord-Korea', label: 'Nord-Korea' },
+    { value: 'Nord-Makedonia', label: 'Nord-Makedonia' },
+    { value: 'Norge', label: 'Norge' },
+    { value: 'Oman', label: 'Oman' },
+    { value: 'Russland', label: 'Russland' },
+    { value: 'Sveits', label: 'Sveits' },
+    { value: 'Sverige', label: 'Sverige' },
+  ];
+  dropdown.setProps({ items: items });
+  let timesLoadedMoreItems = 0;
+  dropdown.addEventListener('onLoadMoreItems', () => {
+    dropdown.setProps({ isLoadingMoreItems: 'true' });
+    setTimeout(() => {
+      dropdown.setProps({ isLoadingMoreItems: 'false' });
+      if (timesLoadedMoreItems === 0) {
+        items.push({ value: 'Tyskland', label: 'Tyskland' });
+        items.push({ value: 'Ungarn', label: 'Ungarn' });
+        items.push({ value: 'USA', label: 'USA' });
+        items.push({ value: 'Vatikanstaten', label: 'Vatikanstaten' });
+      } else if (timesLoadedMoreItems === 1) {
+        items.push({ value: 'Venezuela', label: 'Venezuela' });
+        items.push({ value: 'Vietnam', label: 'Vietnam' });
+        items.push({ value: 'Østerrike', label: 'Østerrike' });
+        items.push({ value: 'Øst-Timor', label: 'Øst-Timor' });
+      }
+      dropdown.setProps({ items: items });
+      timesLoadedMoreItems++;
+      if (timesLoadedMoreItems > 1) {
+        dropdown.setProps({ hasLoadMoreItemsButton: 'false' });
+      }
+    }, 2000);
+  });
 `,
 };
 
