e694f0848b0202947a677c8155145f54baa3f8d2
Erik Tallang <et@computas.com>
2022-09-26 08:33:57 +0200

    feat(toolbox): add hook useDeviceCheck

---
+++ packages/components/components/elvis-toolbox/src/hooks/useDeviceCheck.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+
+type BreakPoint = 'gt-mobile' | 'gt-tablet';
+
+export const useDeviceCheck = (deviceType: BreakPoint): boolean => {
+  const [matches, setMatches] = useState(false);
+  let mediaQueryList: MediaQueryList | undefined;
+
+  useEffect(() => {
+    const onScreenChange = (ev: MediaQueryListEvent): void => {
+      setMatches(ev.matches);
+    };
+
+    const query = deviceType === 'gt-mobile' ? '(min-width: 768px)' : '(min-width: 1024px)';
+    mediaQueryList = window.matchMedia(query);
+    setMatches(mediaQueryList.matches);
+    mediaQueryList.addEventListener('change', onScreenChange);
+
+    return () => {
+      mediaQueryList?.removeEventListener('change', onScreenChange);
+    };
+  }, []);
+
+  return matches;
+};
