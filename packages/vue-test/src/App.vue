0d9fbc3ab96e631afed4150ffbf795986edf8287
Erik Tallang <et@computas.com>
2022-10-03 08:22:38 +0200

    fix(vue): vue is misinterpreted as react

--- packages/vue-test/src/App.vue
+++ packages/vue-test/src/App.vue
@@ -18,7 +18,7 @@
 
 <script lang="ts">
 import { NavItem } from '@elvia/elvis-header';
-import { computed, defineComponent } from 'vue';
+import { computed, defineComponent, ref, watch } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import TooltipHeader from './components/TooltipHeader.vue';
 
@@ -28,19 +28,21 @@ export default defineComponent({
     const router = useRouter();
     const route = useRoute();
 
-    const navItems: NavItem[] = [
+    const navItems = ref<NavItem[]>([
       {
         url: '/',
         iconName: 'dashboard',
         name: 'Components',
         strictMatching: true,
+        isActive: true,
       },
       {
         url: '/tooltip',
         iconName: 'touchFingerColor',
         name: 'Tooltip',
+        isActive: false,
       },
-    ];
+    ]);
 
     const pageTitle = computed(() => {
       return route.meta.title;
@@ -49,6 +51,19 @@ export default defineComponent({
     const navigate = (navItem: NavItem): void => {
       router.push(navItem.url);
     };
+
+    watch(
+      () => route.path,
+      () => {
+        const clone = navItems.value.slice();
+        clone.forEach((item) => {
+          console.log(route.path);
+          item.isActive = item.url === route.path;
+        });
+
+        navItems.value = clone;
+      },
+    );
 
     return { pageTitle, navItems, navigate };
   },
