68834cb7a998c0745c87f800a8dd7a730824bceb
Erik Tallang <et@computas.com>
2022-10-12 19:46:22 +0200

    test(box): convert to react-testing-library

---
+++ packages/components/components/elvis-box/src/react/elvia-box.test.tsx
@@ -0,0 +1,79 @@
+import Box from './elvia-box.tsx';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { getColor } from '@elvia/elvis-colors';
+
+const colors = {
+  green: getColor('green'),
+  grey10: getColor('grey-10'),
+};
+
+const htmlCode = <div>Html content</div>;
+
+describe('Elvis Box', () => {
+  describe('Title = Hello', () => {
+    beforeEach(() => {
+      render(<Box title="Hello Box"></Box>);
+    });
+
+    it('should have title containing "Hello Box"', () => {
+      const boxTitle = screen.getByTestId('box-title');
+      expect(boxTitle).toHaveTextContent('Hello Box');
+    });
+
+    it('should not have className undefined', () => {
+      const boxArea = screen.getByTestId('box-area');
+      expect(boxArea).not.toHaveClass('undefined');
+    });
+  });
+
+  describe('Content = "Some content", isColored = true', () => {
+    beforeEach(() => {
+      render(<Box isColored content="Some content"></Box>);
+    });
+
+    it('should show content of string "Some content"', () => {
+      const boxContent = screen.getByTestId('box-content');
+      expect(boxContent).toHaveTextContent('Some content');
+    });
+
+    it('should have green boxColored line', () => {
+      const boxColoredLineWithContent = screen.getByTestId('box-colored-line');
+      expect(boxColoredLineWithContent).toHaveStyle(`background: ${colors.green}`);
+    });
+  });
+
+  describe('Content = html', () => {
+    beforeEach(() => {
+      render(<Box isColored content={htmlCode}></Box>);
+    });
+
+    it('should show content of html code containing "Html content"', () => {
+      const boxContent = screen.getByTestId('box-content');
+      expect(boxContent).toHaveTextContent('Html content');
+    });
+  });
+
+  describe('hasBorder = true', () => {
+    beforeEach(() => {
+      render(<Box hasBorder></Box>);
+    });
+
+    it('should show box content with a grey border', () => {
+      const boxContent = screen.getByTestId('box-no-content');
+      expect(boxContent).toHaveStyle(`border: 1px solid ${colors.grey10}`);
+    });
+  });
+
+  describe('className and inlineStyle passed to wrapper', () => {
+    beforeEach(() => {
+      render(<Box className={'test-class'} inlineStyle={{ margin: '24px' }}></Box>);
+    });
+
+    it('should have className and inlineStyle from props', () => {
+      const boxArea = screen.getByTestId('box-area').parentElement;
+      expect(boxArea).toHaveStyle(`margin: 24px`);
+      expect(boxArea).toHaveClass('test-class');
+    });
+  });
+});
