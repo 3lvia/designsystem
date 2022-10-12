ace7522947dd3d8b2eae4230087586568aeec1ab
Erik Tallang <et@computas.com>
2022-10-12 19:32:40 +0200

    test(badge): convert to react-testing-library

--- packages/components/components/elvis-badge/src/react/elvia-badge.test.tsx
+++ packages/components/components/elvis-badge/src/react/elvia-badge.test.tsx
@@ -1,6 +1,6 @@
 import Badge from './elvia-badge.tsx';
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import { getColor } from '@elvia/elvis-colors';
 
 const colors = {
@@ -11,13 +11,9 @@ const colors = {
 };
 
 describe('Elvis Badge', () => {
-  let wrapper;
-  let badgeCircle;
-
-  //COLOR
   describe('the default background color of the badge', () => {
     beforeEach(() => {
-      wrapper = mount(
+      render(
         <Badge
           content={
             <span className="e-btn__icon">
@@ -26,19 +22,18 @@ describe('Elvis Badge', () => {
           }
         />,
       );
-      badgeCircle = wrapper.find({ 'data-testid': 'badge-circle' }).at(0);
     });
 
-    it('is green', (done) => {
-      expect(badgeCircle.getDOMNode()).toHaveStyle(`background-color: ${colors.elviaCharge}`);
-      done();
+    it('is green', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`background-color: ${colors.elviaCharge}`);
     });
   });
 
   //NUMBERED under 100
   describe('the number inside the badge circle when it is under 100', () => {
     beforeEach(() => {
-      wrapper = mount(
+      render(
         <Badge
           count={53}
           content={
@@ -48,29 +43,28 @@ describe('Elvis Badge', () => {
           }
         />,
       );
-      badgeCircle = wrapper.find({ 'data-testid': 'badge-circle' }).at(0);
     });
 
-    it('should display the actual number', (done) => {
-      expect(badgeCircle.at(0).text()).toBe('53');
-      done();
+    it('should display the actual number', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveTextContent('53');
     });
 
-    it('should have normal padding', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`padding: 2px 0px`);
-      done();
+    it('should have normal padding', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`padding: 2px 0px`);
     });
 
-    it('should have a fixed width to stay round', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`width: 16px`);
-      done();
+    it('should have a fixed width to stay round', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`width: 16px`);
     });
   });
 
   //NUMBERED over 100
   describe('the number inside the badge circle when it is over 100', () => {
     beforeEach(() => {
-      wrapper = mount(
+      render(
         <Badge
           count={101}
           content={
@@ -80,29 +74,28 @@ describe('Elvis Badge', () => {
           }
         />,
       );
-      badgeCircle = wrapper.find({ 'data-testid': 'badge-circle' }).at(0);
     });
 
-    it('should display "99+"', (done) => {
-      expect(badgeCircle.at(0).text()).toBe('99+');
-      done();
+    it('should display "99+"', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveTextContent('99+');
     });
 
-    it('have some extra padding', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`padding: 2px 4px`);
-      done();
+    it('have some extra padding', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`padding: 2px 4px`);
     });
 
-    it('should not have a fixed width', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`width: unset`);
-      done();
+    it('should not have a fixed width', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`width: unset`);
     });
   });
 
   //NUMBERED with red color
   describe('the text color when the badge is red', () => {
     beforeEach(() => {
-      wrapper = mount(
+      render(
         <Badge
           count={101}
           badgeColor="red"
@@ -113,17 +106,16 @@ describe('Elvis Badge', () => {
           }
         />,
       );
-      badgeCircle = wrapper.find({ 'data-testid': 'badge-circle' }).at(0);
     });
 
-    it('should have a red background color', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`background-color: ${colors.elviaRed}`);
-      done();
+    it('should have a red background color', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`background-color: ${colors.elviaRed}`);
     });
 
-    it('should have a white text color', (done) => {
-      expect(badgeCircle.at(0).getDOMNode()).toHaveStyle(`color: ${colors.elviaWhite}`);
-      done();
+    it('should have a white text color', () => {
+      const badgeCircle = screen.getByTestId('badge-circle');
+      expect(badgeCircle).toHaveStyle(`color: ${colors.elviaWhite}`);
     });
   });
 });
