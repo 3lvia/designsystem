6dac4b4d5bc876509ef2d808fd66c0317376037a
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2022-10-28 15:35:25 +0200

    Create elviaPopover.types.tsx

---
+++ packages/components/components/elvis-popover/src/react/elviaPopover.types.tsx
@@ -0,0 +1,49 @@
+import type { ElvisComponentWrapper } from '@elvia/elvis-component-wrapper';
+import { CSSProperties } from 'react';
+
+export type VerticalPosition = 'bottom' | 'top';
+export type HorizontalPosition = 'left' | 'center' | 'right';
+
+export interface PopoverProps {
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by `heading`.
+   */
+  header?: string;
+  heading?: string;
+  content?: string | JSX.Element;
+  type?: 'informative' | 'list';
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by `isSelectable`.
+   */
+  selectable?: boolean;
+  isSelectable?: boolean;
+  hasDivider?: boolean;
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by `horizontalPosition`.
+   */
+  posX?: 'left' | 'right' | 'center';
+  horizontalPosition?: HorizontalPosition;
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by `verticalPosition`.
+   */
+  posY?: 'top' | 'bottom';
+  verticalPosition?: VerticalPosition;
+
+  trigger?: JSX.Element;
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by 'hasCloseButton'.
+   */
+  hasCloseBtn?: boolean;
+  hasCloseButton?: boolean;
+  isShowing?: boolean;
+  /**
+   * @deprecated Deprecated in version 5.0.0. Replaced by 'onOpen' & 'onClose'
+   */
+  isShowingOnChange?: (isShowing: boolean) => void;
+  onOpen?: () => void;
+  onClose?: () => void;
+  disableAutoClose?: boolean;
+  className?: string;
+  inlineStyle?: CSSProperties;
+  webcomponent?: ElvisComponentWrapper;
+}
