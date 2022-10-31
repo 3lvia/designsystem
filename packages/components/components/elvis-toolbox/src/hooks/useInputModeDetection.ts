6009966bff1e1b631c8c767b4734d88847ea9f32
Erik Tallang <e.tallang@gmail.com>
2022-10-31 10:17:46 +0100

    feat(toolbox): add hook for input mode detection (#1085)
    feat(toolbox): add hook for input mode detection

---
+++ packages/components/components/elvis-toolbox/src/hooks/useInputModeDetection.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+type InputMode = 'mouse' | 'keyboard' | 'touch';
+
+/**
+ * Detects whether mouse or keyboard is being used for interaction.
+ *
+ * @param element An optional element to observe. If not provided, `document.body` is being observed.
+ *
+ * @example
+ * export const Component: FC<Props> = () => {
+ *   const { inputMode, isMouse } = useInputModeDetection();
+ *   ...
+ * }
+ */
+export const useInputModeDetection = (
+  element?: React.RefObject<HTMLElement>,
+): { inputMode: InputMode; isMouse: boolean } => {
+  const [inputMode, setInputMode] = useState<InputMode>('mouse');
+
+  useEffect(() => {
+    const setIsKeyboard = () => setInputMode('keyboard');
+    const setIsMouse = () => setInputMode('mouse');
+    const setIsTouch = () => setInputMode('touch');
+
+    const elementToObserve = element?.current ?? document.body;
+    elementToObserve.addEventListener('mousemove', setIsMouse);
+    elementToObserve.addEventListener('keydown', setIsKeyboard);
+    elementToObserve.addEventListener('touchstart', setIsTouch);
+
+    return () => {
+      elementToObserve.removeEventListener('mousemove', setIsMouse);
+      elementToObserve.removeEventListener('keydown', setIsKeyboard);
+      elementToObserve.removeEventListener('touchstart', setIsTouch);
+    };
+  }, [element]);
+
+  return { inputMode, isMouse: inputMode === 'mouse' };
+};
