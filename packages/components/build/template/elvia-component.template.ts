8af4d544dbcbf13641680021b4eeb5bff3c2e310
Erik Tallang <et@computas.com>
2022-10-31 15:57:22 +0100

    chore: remove old attributes from component wrapper template

--- packages/components/build/template/elvia-component.template.ts
+++ packages/components/build/template/elvia-component.template.ts
@@ -1,7 +1,5 @@
 import * as ReactComponent from '../../../react.js';
 import { ElvisComponentWrapper } from '@elvia/elvis-component-wrapper';
-const style = `{{INSERT_STYLE_HERE}}`;
-
 
 export default class ElviaComponent extends ElvisComponentWrapper {
     static get observedAttributes(): any[] {
@@ -12,7 +10,7 @@ export default class ElviaComponent extends ElvisComponentWrapper {
 
     //{{INSERT_SETTERS_AND_GETTERS}}
     constructor() {
-        super(ElviaComponent, ReactComponent['{{INSERT_REACT_NAME}}'], style);
+        super(ElviaComponent, ReactComponent['{{INSERT_REACT_NAME}}']);
     }
 }
 
