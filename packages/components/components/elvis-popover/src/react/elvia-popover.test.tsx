16626745b9b6320b38bdcbbc0e8c44e2bef2db16
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2022-11-03 09:43:48 +0100

    fix className and inlineStyle popover test (#1097)
    * fix className and inlineStyle popover test

--- packages/components/components/elvis-popover/src/react/elvia-popover.test.tsx
+++ packages/components/components/elvis-popover/src/react/elvia-popover.test.tsx
@@ -148,10 +148,15 @@ describe('Elvis Popover', () => {
       );
     });
 
-    it('should have className and inlineStyle', () => {
-      const popoverWrapper = screen.getByRole('dialog');
-      expect(popoverWrapper).toHaveStyle('margin: 24px');
-      expect(popoverWrapper).toHaveClass('test-class');
+    it('should have className and inlineStyle', async () => {
+      const user = userEvent.setup();
+      const popoverTrigger = screen.getByRole('button', { name: 'Trigger' });
+
+      await user.click(popoverTrigger);
+
+      const popoverContent = screen.getByTestId('popover-content');
+      expect(popoverContent).toHaveStyle('margin: 24px');
+      expect(popoverContent).toHaveClass('test-class');
     });
   });
 });
