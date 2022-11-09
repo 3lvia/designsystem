ebe5eb052d56e25c67435e1c15cd131efc34880f
Fride Skarseth <fs@computas.com>
2022-11-09 13:40:23 +0100

    docs(segmented controls): Updated selected icons to colored icons i doc

--- packages/web/src/app/doc-pages/components/forms/segmented-controls-doc/segmented-controls-icon-code.ts
+++ packages/web/src/app/doc-pages/components/forms/segmented-controls-doc/segmented-controls-icon-code.ts
@@ -5,32 +5,32 @@ const segmentedControlIconCode: ComponentTypeData = {
   codeReact: `<SegmentedControl
   type={"icon"}
   items={[
-    {iconName: 'thumbnail', iconNameSelected: 'thumbnail', ariaLabel: 'Segmented control example label'}, 
-    {iconName: 'list', iconNameSelected: 'list', ariaLabel: 'Segmented control example label'}
+    {iconName: 'thumbnail', iconNameSelected: 'thumbnailColor', ariaLabel: 'Segmented control example label'}, 
+    {iconName: 'list', iconNameSelected: 'listColor', ariaLabel: 'Segmented control example label'}
   ]}
   valueOnChange={(event) => handleOnChange(event)}
 ></SegmentedControl>`,
   codeAngular: `<elvia-segmented-control
   [type]="'icon'"
   [items]="[
-    {iconName: 'thumbnail', iconNameSelected: 'thumbnail', ariaLabel: 'Segmented control example label'}, 
-    {iconName: 'list', iconNameSelected: 'list', ariaLabel: 'Segmented control example label'}
+    {iconName: 'thumbnail', iconNameSelected: 'thumbnailColor', ariaLabel: 'Segmented control example label'}, 
+    {iconName: 'list', iconNameSelected: 'listColor', ariaLabel: 'Segmented control example label'}
   ]"
   (valueOnChange)="handleOnChange($event.detail.value)"
 ></elvia-segmented-control>`,
   codeVue: `<elvia-segmented-control
   :type="'icon'"
   :items="[
-    {iconName: 'thumbnail', iconNameSelected: 'thumbnail', ariaLabel: 'Segmented control example label'}, 
-    {iconName: 'list', iconNameSelected: 'list', ariaLabel: 'Segmented control example label'}
+    {iconName: 'thumbnail', iconNameSelected: 'thumbnailColor', ariaLabel: 'Segmented control example label'}, 
+    {iconName: 'list', iconNameSelected: 'listColor', ariaLabel: 'Segmented control example label'}
   ]"
   @value-on-change="handleOnChange($event.detail.value)"
 ></elvia-segmented-control>`,
   codeNativeHTML: `<elvia-segmented-control
   type="icon"
   items="[
-    {iconName: 'thumbnail', iconNameSelected: 'thumbnail', ariaLabel: 'Segmented control example label'}, 
-    {iconName: 'list', iconNameSelected: 'list', ariaLabel: 'Segmented control example label'}
+    {iconName: 'thumbnail', iconNameSelected: 'thumbnailColor', ariaLabel: 'Segmented control example label'}, 
+    {iconName: 'list', iconNameSelected: 'listColor', ariaLabel: 'Segmented control example label'}
   ]"
   id="example-elvia-segmented-control"
 ></elvia-segmented-control>`,
