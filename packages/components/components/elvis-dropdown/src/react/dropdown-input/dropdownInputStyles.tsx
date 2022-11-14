4db4824345ca5ed9c9689577072db34b46bc74b4
Erik Tallang <et@computas.com>
2022-11-14 20:00:01 +0100

    feat(dropdown): make input selection color transparent

--- packages/components/components/elvis-dropdown/src/react/dropdown-input/dropdownInputStyles.tsx
+++ packages/components/components/elvis-dropdown/src/react/dropdown-input/dropdownInputStyles.tsx
@@ -4,4 +4,13 @@ import styled from 'styled-components';
 export const Input = styled(FormFieldInput)`
   flex: 1;
   text-overflow: ellipsis;
+
+  ::-moz-selection {
+    /* Code for Firefox */
+    background: transparent;
+  }
+
+  ::selection {
+    background: transparent;
+  }
 `;
