ad222d049b1dec833988933e3b9d05fe04c60ff9
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2022-11-16 10:55:11 +0100

    Lego 2011: toolbox sett opp enhetstester av custom hooks (#1126)
    * add @testing-library/react-hooks
    
    * update toolbox build to not include test files
    
    * add useWebComponentState tests

---
+++ packages/components/components/elvis-toolbox/src/hooks/useWebComponentState.test.ts
@@ -0,0 +1,109 @@
+import { useWebComponentState } from './useWebComponentState';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+describe('useWebComponentState', () => {
+  describe('with Web Component', () => {
+    let webcomponent: {
+      setProps: jest.Mock;
+      triggerEvent: jest.Mock;
+    };
+    const stateValue = 'test';
+
+    beforeEach(() => {
+      webcomponent = {
+        setProps: jest.fn(),
+        triggerEvent: jest.fn(),
+      };
+    });
+
+    it('should render hook', () => {
+      const { result } = renderHook(() => useWebComponentState(stateValue, 'state', webcomponent, undefined));
+
+      expect(result.current[0]).toBe(stateValue);
+      expect(typeof result.current[0]).toBe(typeof stateValue);
+      expect(typeof result.current[1]).toBe('function');
+    });
+    it('should update value', () => {
+      const { result } = renderHook(() => useWebComponentState(stateValue, 'state', webcomponent, undefined));
+
+      act(() => {
+        result.current[1]('new value');
+      });
+      expect(result.current[0]).toBe('new value');
+    });
+    it('should update value with callback', () => {
+      const { result } = renderHook(() => useWebComponentState(stateValue, 'state', webcomponent, undefined));
+
+      act(() => {
+        result.current[1]((oldValue) => oldValue + 'updated');
+      });
+      expect(result.current[0]).toBe(stateValue + 'updated');
+    });
+    it('should call webcomponent setProps and triggerEvent', () => {
+      const { result } = renderHook(() => useWebComponentState(stateValue, 'state', webcomponent, undefined));
+
+      act(() => {
+        result.current[1]('new value');
+      });
+      expect(webcomponent.setProps).toHaveBeenCalledWith({ state: 'new value' }, true);
+      expect(webcomponent.triggerEvent).toHaveBeenCalledWith('stateOnChange', 'new value');
+    });
+  });
+
+  describe('with React', () => {
+    let reactCallback: jest.Mock;
+    const stateValue = 'test';
+
+    beforeEach(() => {
+      reactCallback = jest.fn();
+    });
+
+    it('should render hook', () => {
+      const { result } = renderHook(() =>
+        useWebComponentState(stateValue, 'state', undefined, reactCallback),
+      );
+
+      expect(result.current[0]).toBe(stateValue);
+      expect(typeof result.current[0]).toBe(typeof stateValue);
+      expect(typeof result.current[1]).toBe('function');
+    });
+    it('should update value', () => {
+      const { result } = renderHook(() =>
+        useWebComponentState(stateValue, 'state', undefined, reactCallback),
+      );
+
+      act(() => {
+        result.current[1]('new value');
+      });
+      expect(result.current[0]).toBe('new value');
+    });
+    it('should update value with callback', () => {
+      const { result } = renderHook(() =>
+        useWebComponentState<string, any>(stateValue, 'state', undefined, reactCallback),
+      );
+
+      act(() => {
+        result.current[1]((oldValue) => oldValue + 'updated');
+      });
+      expect(result.current[0]).toBe(stateValue + 'updated');
+    });
+    it('should call React callback', () => {
+      const { result } = renderHook(() =>
+        useWebComponentState(stateValue, 'state', undefined, reactCallback),
+      );
+
+      act(() => {
+        result.current[1]('new value');
+      });
+      expect(reactCallback).toHaveBeenCalledWith('new value');
+    });
+    it('should work without any callback', () => {
+      const { result } = renderHook(() => useWebComponentState(stateValue, 'state', undefined, undefined));
+
+      act(() => {
+        result.current[1]('new value');
+      });
+      expect(result.current[0]).toBe('new value');
+    });
+  });
+});
