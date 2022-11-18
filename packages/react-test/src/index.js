f382bb63596628e231199c2833455a71699a8646
Trygve Scheline Urdahl <trur@computas.com>
2022-11-18 14:19:12 +0100

    add strict mode back to react test env

--- packages/react-test/src/index.js
+++ packages/react-test/src/index.js
@@ -9,7 +9,11 @@ import '@elvia/elvis/elvis.js';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(
+  <React.StrictMode>
+    <App tab="home" />
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
