e0890fdfb21d0bd3a79182e247a21cb12be0834f
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2022-11-21 09:53:02 +0100

    Lego 1827 React - få komponentene til å støtte React 18 (#1128)
    * add react 18 support, migrate react-test to react 18
    
    * fix flashing usePortal components
    
    * update react testing library
    
    * remove react type resolution
    
    * add wrapper react root useId prefix
    
    * remove react-hook testing library, use default from testing library react
    
    * add changelogs
    
    * bump dependency on component wrapper

--- packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
+++ packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
@@ -14,6 +14,7 @@ import { DatepickerInput } from './datepickerInput';
 import { DatepickerError } from './error/datepickerError';
 import { getErrorText } from './getErrorText';
 import { copyDay, isValidDate } from './dateHelpers';
+import { flushSync } from 'react-dom';
 
 export const Datepicker: React.FC<DatepickerProps> = ({
   clearButtonText = 'Nullstill',
@@ -117,7 +118,7 @@ export const Datepicker: React.FC<DatepickerProps> = ({
   };
 
   const setVisibility = (isShowing: boolean): void => {
-    setIsShowing(isShowing);
+    flushSync(() => setIsShowing(isShowing));
 
     if (!isShowing) {
       openPopoverButtonRef.current?.focus();
--- packages/react-test/src/index.js
+++ packages/react-test/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,18 +7,12 @@ import reportWebVitals from './reportWebVitals';
 import '@elvia/elvis/css/elvis.min.css';
 import '@elvia/elvis/elvis.js';
 
-const root = document.getElementById('root');
-let renderMethod;
-if (root && root.innerHTML !== '') {
-  renderMethod = ReactDOM.hydrate;
-} else {
-  renderMethod = ReactDOM.render;
-}
-renderMethod(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
