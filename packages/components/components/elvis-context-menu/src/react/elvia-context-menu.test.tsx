290586a30a6f687e3d5f35d255249f54f3dfe2d6
Erik Tallang <et@computas.com>
2022-11-25 12:14:10 +0100

    test(context-menu): fix broken tests

--- packages/components/components/elvis-context-menu/src/react/elvia-context-menu.test.tsx
+++ packages/components/components/elvis-context-menu/src/react/elvia-context-menu.test.tsx
@@ -9,7 +9,7 @@ describe('Elvis Context Menu', () => {
       <ContextMenu
         trigger={<button data-testid="trigger">Trigger</button>}
         content={
-          <>
+          <div data-testid="menu">
             <div className="ewc-popover__list-group">
               <button>
                 <span>Be om tilgang</span>
@@ -23,7 +23,7 @@ describe('Elvis Context Menu', () => {
                 <span>Endre passord</span>
               </a>
             </div>
-          </>
+          </div>
         }
       />,
     );
@@ -55,9 +55,8 @@ describe('Elvis Context Menu', () => {
     });
 
     it('the first option receives focus', () => {
-      const dropdownList = screen.getByTestId('dropdown-list');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> =
-        dropdownList.querySelectorAll('button, a');
+      const menu = screen.getByTestId('menu');
+      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
       expect(buttons.length).toBe(3);
       expect(buttons.item(0).tabIndex).toBe(0);
       expect(buttons.item(1).tabIndex).toBe(-1);
@@ -68,9 +67,8 @@ describe('Elvis Context Menu', () => {
       const user = userEvent.setup();
       await user.keyboard('{arrowdown}');
 
-      const dropdownList = screen.getByTestId('dropdown-list');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> =
-        dropdownList.querySelectorAll('button, a');
+      const menu = screen.getByTestId('menu');
+      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
       expect(buttons.item(0).tabIndex).toBe(-1);
       expect(buttons.item(1).tabIndex).toBe(0);
       expect(buttons.item(2).tabIndex).toBe(-1);
@@ -80,9 +78,8 @@ describe('Elvis Context Menu', () => {
       const user = userEvent.setup();
       await user.keyboard('{arrowup}');
 
-      const dropdownList = screen.getByTestId('dropdown-list');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> =
-        dropdownList.querySelectorAll('button, a');
+      const menu = screen.getByTestId('menu');
+      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
       expect(buttons.item(0).tabIndex).toBe(-1);
       expect(buttons.item(1).tabIndex).toBe(-1);
       expect(buttons.item(2).tabIndex).toBe(0);
