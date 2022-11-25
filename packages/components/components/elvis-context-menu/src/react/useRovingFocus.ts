342adcc1b078b62700270985bd2fb1490c48de85
Erik Tallang <et@computas.com>
2022-11-25 10:35:46 +0100

    chore: reduce cognitive complexity

--- packages/components/components/elvis-context-menu/src/react/useRovingFocus.ts
+++ packages/components/components/elvis-context-menu/src/react/useRovingFocus.ts
@@ -1,54 +1,61 @@
 import { RefObject, useEffect } from 'react';
 
 export const useRovingFocus = (listRef?: RefObject<HTMLElement>): void => {
+  const getNewFocusedIndex = (
+    ev: KeyboardEvent,
+    items: NodeListOf<HTMLElement>,
+    currentIndex: number,
+  ): number => {
+    if (ev.code === 'ArrowRight' || ev.code === 'ArrowDown') {
+      ev.preventDefault();
+      if (currentIndex === items.length - 1) {
+        return 0;
+      } else {
+        return ++currentIndex;
+      }
+    } else if (ev.code === 'ArrowLeft' || ev.code === 'ArrowUp') {
+      ev.preventDefault();
+      if (currentIndex === 0) {
+        return items.length - 1;
+      } else {
+        return --currentIndex;
+      }
+    }
+
+    return currentIndex;
+  };
+
   useEffect(() => {
     const list = listRef?.current;
 
-    if (list) {
-      let focusedIndex = 0;
-
-      const tabbableItems: NodeListOf<HTMLButtonElement | HTMLAnchorElement> =
-        list.querySelectorAll('button, a');
-
-      tabbableItems.forEach((item, index) => {
-        if (index === 0) {
-          item.tabIndex = 0;
-        } else {
-          item.tabIndex = -1;
-        }
-      });
-
-      const setNewFocusedIndex = (ev: KeyboardEvent): void => {
-        if (ev.code === 'ArrowRight' || ev.code === 'ArrowDown') {
-          ev.preventDefault();
-          if (focusedIndex === tabbableItems.length - 1) {
-            focusedIndex = 0;
-          } else {
-            ++focusedIndex;
-          }
-        } else if (ev.code === 'ArrowLeft' || ev.code === 'ArrowUp') {
-          ev.preventDefault();
-          if (focusedIndex === 0) {
-            focusedIndex = tabbableItems.length - 1;
-          } else {
-            --focusedIndex;
-          }
-        }
-      };
-
-      const onKeyDown = (ev: KeyboardEvent): void => {
-        tabbableItems.item(focusedIndex).tabIndex = -1;
-
-        setNewFocusedIndex(ev);
-
-        const item = tabbableItems.item(focusedIndex);
-        item.focus();
-        item.tabIndex = 0;
-      };
-
-      list.addEventListener('keydown', onKeyDown);
-
-      return () => list.removeEventListener('keydown', onKeyDown);
+    if (!list) {
+      return;
     }
+
+    let focusedIndex = 0;
+    const tabbableItems: NodeListOf<HTMLButtonElement | HTMLAnchorElement> =
+      list.querySelectorAll('button, a');
+
+    tabbableItems.forEach((item, index) => {
+      if (index === 0) {
+        item.tabIndex = 0;
+      } else {
+        item.tabIndex = -1;
+      }
+    });
+
+    const onKeyDown = (ev: KeyboardEvent): void => {
+      tabbableItems.item(focusedIndex).tabIndex = -1;
+
+      focusedIndex = getNewFocusedIndex(ev, tabbableItems, focusedIndex);
+
+      const item = tabbableItems.item(focusedIndex);
+      item.focus();
+      item.tabIndex = 0;
+    };
+
+    list.addEventListener('keydown', onKeyDown);
+
+    return () => list.removeEventListener('keydown', onKeyDown);
   }, [listRef, listRef?.current]);
 };
