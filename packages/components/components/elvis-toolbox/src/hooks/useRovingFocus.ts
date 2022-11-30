7d2e28cef1be1134876e72947d8b83c0a6dae571
Erik Tallang <et@computas.com>
2022-11-30 07:58:05 +0100

    feat(toolbox): make roving focus hook configurable

--- packages/components/components/elvis-toolbox/src/hooks/useRovingFocus.ts
+++ packages/components/components/elvis-toolbox/src/hooks/useRovingFocus.ts
@@ -1,13 +1,24 @@
 import { RefObject, useEffect, useRef } from 'react';
 
+interface Options<T extends HTMLElement> {
+  dir: 'vertical' | 'horizontal' | 'both';
+  elementRef?: RefObject<T>;
+}
+
 /**
  * useRovingFocus is a custom hook that allows you to implement a roving focus.
  *
  * @internal
  * @since 8.0.0
  */
-export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
-  const ref = useRef<T>(null);
+export const useRovingFocus = <T extends HTMLElement>(options?: Partial<Options<T>>): RefObject<T> => {
+  const defaultRef = useRef<T>(null);
+  const ref = options?.elementRef ?? defaultRef;
+
+  const opts: Options<T> = {
+    dir: 'both',
+    ...options,
+  };
 
   const focusedItem = useRef<HTMLElement>();
   const focusedIndex = useRef(0);
@@ -27,33 +38,45 @@ export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
     }) as HTMLElement[];
   };
 
+  const getPrevItemKeys = (): string[] => {
+    if (opts.dir === 'both') {
+      return ['ArrowUp', 'ArrowLeft'];
+    } else if (opts.dir === 'horizontal') {
+      return ['ArrowLeft'];
+    }
+    return ['ArrowUp'];
+  };
+
+  const getNextItemKeys = (): string[] => {
+    if (opts.dir === 'both') {
+      return ['ArrowDown', 'ArrowRight'];
+    } else if (opts.dir === 'horizontal') {
+      return ['ArrowRight'];
+    }
+    return ['ArrowDown'];
+  };
+
   const getNewIndex = (ev: KeyboardEvent, listLength: number, currentIndex: number): number => {
-    switch (ev.key) {
-      case 'ArrowUp':
-      case 'ArrowLeft':
-        ev.preventDefault();
-        if (currentIndex === 0) {
-          return listLength - 1;
-        } else {
-          return currentIndex - 1;
-        }
-
-      case 'ArrowDown':
-      case 'ArrowRight':
-        ev.preventDefault();
-        if (currentIndex === listLength - 1) {
-          return 0;
-        } else {
-          return currentIndex + 1;
-        }
-
-      case 'Home':
-        ev.preventDefault();
-        return 0;
-
-      case 'End':
-        ev.preventDefault();
+    if (getPrevItemKeys().includes(ev.code)) {
+      ev.preventDefault();
+      if (currentIndex === 0) {
         return listLength - 1;
+      } else {
+        return currentIndex - 1;
+      }
+    } else if (getNextItemKeys().includes(ev.code)) {
+      ev.preventDefault();
+      if (currentIndex === listLength - 1) {
+        return 0;
+      } else {
+        return currentIndex + 1;
+      }
+    } else if (ev.code === 'Home') {
+      ev.preventDefault();
+      return 0;
+    } else if (ev.code === 'End') {
+      ev.preventDefault();
+      return listLength - 1;
     }
 
     return currentIndex;
@@ -61,15 +84,20 @@ export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
 
   // Restore focused item and focused index after the list has mutated
   const restoreTabPosition = (updatedItemList: HTMLElement[]): void => {
-    const updatedIndex = focusedItem.current ? updatedItemList.indexOf(focusedItem.current) : -1;
+    const updatedIndex = focusedItem.current
+      ? updatedItemList.map((item) => item.innerHTML).indexOf(focusedItem.current.innerHTML)
+      : -1;
 
+    // TODO: This is duplicated thrice
     if (updatedIndex !== -1) {
       focusedIndex.current = updatedIndex;
+      focusedItem.current = updatedItemList[updatedIndex];
+      updatedItemList[updatedIndex]?.focus();
     } else if (focusedIndex.current !== -1) {
-      const clampedIndex = Math.min(updatedItemList.length, focusedIndex.current);
-      updatedItemList[clampedIndex]?.focus();
+      const clampedIndex = Math.min(updatedItemList.length - 1, focusedIndex.current);
       focusedItem.current = updatedItemList[clampedIndex];
       focusedIndex.current = clampedIndex;
+      updatedItemList[clampedIndex]?.focus();
     } else {
       focusedItem.current = updatedItemList[0];
       focusedIndex.current = 0;
@@ -81,7 +109,6 @@ export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
       list.forEach((item) => (item.tabIndex = item === focusedItem.current ? 0 : -1));
     } else {
       list.forEach((item, index) => (item.tabIndex = index === 0 ? 0 : -1));
-      focusedItem.current = list.find((item) => item.tabIndex === 0);
     }
   };
 
@@ -95,6 +122,13 @@ export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
     const observer = new MutationObserver(() => {
       unsubscriber.current?.();
       const items = getFocusableItems(container);
+
+      if (focusedItem.current) {
+        restoreTabPosition(items);
+      }
+
+      setTabIndexes(items);
+
       unsubscriber.current = initializeKeydownHandler(container, items);
     });
 
@@ -109,12 +143,6 @@ export const useRovingFocus = <T extends HTMLElement>(): RefObject<T> => {
   }, [ref.current]);
 
   const initializeKeydownHandler = (container: T, items: HTMLElement[]): (() => void) => {
-    if (focusedItem.current) {
-      restoreTabPosition(items);
-    }
-
-    setTabIndexes(items);
-
     const handleKeyDown = (event: KeyboardEvent) => {
       const newIndex = getNewIndex(event, items.length, focusedIndex.current);
 
