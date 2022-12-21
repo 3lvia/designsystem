c9c54f87bd7e0dd80b45bc01d129c8c213255592
Trygve Scheline Urdahl <trur@computas.com>
2022-12-21 08:38:54 +0100

    add light and dark theme

---
+++ packages/elvis-colors/src/themes/darkTheme.ts
@@ -0,0 +1,204 @@
+import { BaseColors, Theme } from '../theme';
+
+export const darkThemeColors: BaseColors = {
+  'primary-colors': {
+    white: {
+      color: '#EDEDED',
+      contrastText: '#171717',
+    },
+    green: {
+      color: '#35BB18',
+      contrastText: '#171717',
+    },
+    black: {
+      color: '#171717',
+      contrastText: '#EDEDED',
+    },
+  },
+  'signal-colors': {
+    yellow: {
+      color: '#EBEB18',
+      contrastText: '#171717',
+    },
+    orange: {
+      color: '#E59712',
+      contrastText: '#171717',
+    },
+    red: {
+      color: '#C82520',
+      contrastText: '#171717',
+    },
+  },
+  'data-colors': {
+    'green-apple': {
+      color: '#2d9f15',
+      contrastText: '#171717',
+    },
+    'violet-grape': {
+      color: '#692CA5',
+      contrastText: '#EDEDED',
+    },
+    'blue-berry': {
+      color: '#0967C6',
+      contrastText: '#EDEDED',
+    },
+    'purple-plum': {
+      color: '#9963CF',
+      contrastText: '#171717',
+    },
+    'orange-mango': {
+      color: '#C5670C',
+      contrastText: '#171717',
+    },
+    'red-tomato': {
+      color: '#9E1111',
+      contrastText: '#EDEDED',
+    },
+  },
+  'grey-colors': {
+    'grey-80': {
+      color: '#1F1F1F',
+      contrastText: '#EDEDED',
+    },
+    'grey-70': {
+      color: '#242424',
+      contrastText: '#EDEDED',
+    },
+    'grey-60': {
+      color: '#333333',
+      contrastText: '#EDEDED',
+    },
+    'grey-50': {
+      color: '#424242',
+      contrastText: '#EDEDED',
+    },
+    'grey-40': {
+      color: '#5E5E5E',
+      contrastText: '#EDEDED',
+    },
+    'grey-20': {
+      color: '#BDBDBD',
+      contrastText: '#171717',
+    },
+    'grey-10': {
+      color: '#C4C4C4',
+      contrastText: '#171717',
+    },
+  },
+  'internal-colors': {
+    'focus-outline': {
+      color: '#0064fa',
+    },
+  },
+} as const;
+
+export const darkTheme: Theme = {
+  background: {
+    'color-background-primary': {
+      hex: darkThemeColors['grey-colors']['grey-80'].color,
+      contrast: darkThemeColors['grey-colors']['grey-80'].contrastText,
+    },
+    'color-background-secondary': {
+      hex: darkThemeColors['grey-colors']['grey-80'].color,
+      contrast: darkThemeColors['grey-colors']['grey-80'].contrastText,
+    },
+    'color-background-tertiary': {
+      hex: darkThemeColors['grey-colors']['grey-80'].color,
+      contrast: darkThemeColors['grey-colors']['grey-80'].contrastText,
+    },
+    'color-background-element': {
+      hex: darkThemeColors['grey-colors']['grey-80'].color,
+      contrast: darkThemeColors['grey-colors']['grey-80'].contrastText,
+    },
+    'color-background-element-zebra': {
+      hex: darkThemeColors['grey-colors']['grey-70'].color,
+      contrast: darkThemeColors['grey-colors']['grey-70'].contrastText,
+    },
+    'color-background-overlay': {
+      hex: darkThemeColors['grey-colors']['grey-70'].color,
+      contrast: darkThemeColors['grey-colors']['grey-70'].contrastText,
+    },
+    'color-background-header': {
+      hex: darkThemeColors['primary-colors'].black.color,
+      contrast: darkThemeColors['primary-colors'].black.contrastText,
+    },
+    'color-background-accent': {
+      hex: darkThemeColors['grey-colors']['grey-60'].color,
+      contrast: darkThemeColors['grey-colors']['grey-60'].contrastText,
+    },
+    'color-background-accent-strong': {
+      hex: darkThemeColors['grey-colors']['grey-40'].color,
+      contrast: darkThemeColors['grey-colors']['grey-40'].contrastText,
+    },
+  },
+  text: {
+    'color-text-primary': {
+      hex: darkThemeColors['primary-colors'].white.color,
+    },
+    'color-text-secondary': {
+      hex: darkThemeColors['grey-colors']['grey-10'].color,
+    },
+    'color-text-placeholder': {
+      hex: darkThemeColors['grey-colors']['grey-20'].color,
+    },
+  },
+  state: {
+    'color-state-on': {
+      hex: darkThemeColors['primary-colors'].green.color,
+      contrast: darkThemeColors['primary-colors'].green.contrastText,
+    },
+    'color-state-caution': {
+      hex: darkThemeColors['signal-colors'].yellow.color,
+      contrast: darkThemeColors['signal-colors'].yellow.contrastText,
+    },
+    'color-state-warning': {
+      hex: darkThemeColors['signal-colors'].orange.color,
+      contrast: darkThemeColors['signal-colors'].orange.contrastText,
+    },
+    'color-state-error': {
+      hex: darkThemeColors['signal-colors'].red.color,
+      contrast: darkThemeColors['signal-colors'].red.contrastText,
+    },
+    'color-state-hover-green': {
+      hex: darkThemeColors['primary-colors'].green.color,
+      contrast: darkThemeColors['primary-colors'].green.contrastText,
+    },
+    'color-state-hover-grey': {
+      hex: darkThemeColors['grey-colors']['grey-60'].color,
+      contrast: darkThemeColors['grey-colors']['grey-60'].contrastText,
+    },
+    'color-state-disabled': {
+      hex: darkThemeColors['grey-colors']['grey-40'].color,
+      contrast: darkThemeColors['grey-colors']['grey-40'].contrastText,
+    },
+    'color-state-focus': {
+      hex: darkThemeColors['internal-colors']['focus-outline'].color,
+    },
+  },
+  data: {
+    'color-data-green': {
+      hex: darkThemeColors['data-colors']['green-apple'].color,
+      contrast: darkThemeColors['data-colors']['green-apple'].contrastText,
+    },
+    'color-data-violet': {
+      hex: darkThemeColors['data-colors']['violet-grape'].color,
+      contrast: darkThemeColors['data-colors']['violet-grape'].contrastText,
+    },
+    'color-data-blue': {
+      hex: darkThemeColors['data-colors']['blue-berry'].color,
+      contrast: darkThemeColors['data-colors']['blue-berry'].contrastText,
+    },
+    'color-data-purple': {
+      hex: darkThemeColors['data-colors']['purple-plum'].color,
+      contrast: darkThemeColors['data-colors']['purple-plum'].contrastText,
+    },
+    'color-data-orange': {
+      hex: darkThemeColors['data-colors']['orange-mango'].color,
+      contrast: darkThemeColors['data-colors']['orange-mango'].contrastText,
+    },
+    'color-data-red': {
+      hex: darkThemeColors['data-colors']['red-tomato'].color,
+      contrast: darkThemeColors['data-colors']['red-tomato'].contrastText,
+    },
+  },
+} as const;
---
+++ packages/elvis-colors/src/themes/lightTheme.ts
@@ -0,0 +1,224 @@
+import { BaseColors, Theme } from '../theme';
+
+export const lightThemeColors: BaseColors = {
+  'primary-colors': {
+    white: {
+      color: '#fff',
+      contrastText: '#000',
+    },
+    green: {
+      color: '#29d305',
+      contrastText: '#000',
+    },
+    black: {
+      color: '#000',
+      contrastText: '#fff',
+    },
+    grey: {
+      color: '#262626',
+      contrastText: '#fff',
+    },
+  },
+  'signal-colors': {
+    yellow: {
+      color: '#ffff00',
+      contrastText: '#000',
+    },
+    orange: {
+      color: '#ffa000',
+      contrastText: '#000',
+    },
+    red: {
+      color: '#ee0701',
+      contrastText: '#000',
+    },
+  },
+  'data-colors': {
+    'green-apple': {
+      color: '#21ac04',
+      contrastText: '#000',
+    },
+    'violet-grape': {
+      color: '#490192',
+      contrastText: '#fff',
+    },
+    'blue-berry': {
+      color: '#006ddb',
+      contrastText: '#fff',
+    },
+    'purple-plum': {
+      color: '#b66dff',
+      contrastText: '#000',
+    },
+    'orange-mango': {
+      color: '#db6d00',
+      contrastText: '#000',
+    },
+    'red-tomato': {
+      color: '#b90202',
+      contrastText: '#fff',
+    },
+  },
+  'grey-colors': {
+    'grey-90': {
+      color: '#3b3b3b',
+      contrastText: '#fff',
+    },
+    'grey-80': {
+      color: '#515151',
+      contrastText: '#fff',
+    },
+    'grey-70': {
+      color: '#676767',
+      contrastText: '#fff',
+    },
+    'grey-60': {
+      color: '#7c7c7c',
+      contrastText: '#000',
+    },
+    'grey-50': {
+      color: '#929292',
+      contrastText: '#000',
+    },
+    'grey-40': {
+      color: '#a8a8a8',
+      contrastText: '#000',
+    },
+    'grey-30': {
+      color: '#bdbdbd',
+      contrastText: '#000',
+    },
+    'grey-20': {
+      color: '#d3d3d3',
+      contrastText: '#000',
+    },
+    'grey-10': {
+      color: '#e9e9e9',
+      contrastText: '#000',
+    },
+    'grey-05': {
+      color: '#f4f4f4',
+      contrastText: '#000',
+    },
+    'grey-02': {
+      color: '#fafafa',
+      contrastText: '#000',
+    },
+  },
+  'internal-colors': {
+    'focus-outline': {
+      color: '#0064fa',
+    },
+  },
+} as const;
+
+export const lightTheme: Theme = {
+  background: {
+    'color-background-primary': {
+      hex: lightThemeColors['primary-colors'].white.color,
+      contrast: lightThemeColors['primary-colors'].white.contrastText,
+    },
+    'color-background-secondary': {
+      hex: lightThemeColors['grey-colors']['grey-05'].color,
+      contrast: lightThemeColors['grey-colors']['grey-05'].contrastText,
+    },
+    'color-background-tertiary': {
+      hex: lightThemeColors['primary-colors'].grey.color,
+      contrast: lightThemeColors['primary-colors'].grey.contrastText,
+    },
+    'color-background-element': {
+      hex: lightThemeColors['primary-colors'].white.color,
+      contrast: lightThemeColors['primary-colors'].white.contrastText,
+    },
+    'color-background-element-zebra': {
+      hex: lightThemeColors['grey-colors']['grey-05'].color,
+      contrast: lightThemeColors['grey-colors']['grey-05'].contrastText,
+    },
+    'color-background-overlay': {
+      hex: lightThemeColors['primary-colors'].white.color,
+      contrast: lightThemeColors['primary-colors'].white.contrastText,
+    },
+    'color-background-header': {
+      hex: lightThemeColors['primary-colors'].white.color,
+      contrast: lightThemeColors['primary-colors'].white.contrastText,
+    },
+    'color-background-accent': {
+      hex: lightThemeColors['grey-colors']['grey-10'].color,
+      contrast: lightThemeColors['grey-colors']['grey-10'].contrastText,
+    },
+    'color-background-accent-strong': {
+      hex: lightThemeColors['grey-colors']['grey-20'].color,
+      contrast: lightThemeColors['grey-colors']['grey-20'].contrastText,
+    },
+  },
+  text: {
+    'color-text-primary': {
+      hex: lightThemeColors['primary-colors'].black.color,
+    },
+    'color-text-secondary': {
+      hex: lightThemeColors['grey-colors']['grey-80'].color,
+    },
+    'color-text-placeholder': {
+      hex: lightThemeColors['grey-colors']['grey-70'].color,
+    },
+  },
+  state: {
+    'color-state-on': {
+      hex: lightThemeColors['primary-colors'].green.color,
+      contrast: lightThemeColors['primary-colors'].green.contrastText,
+    },
+    'color-state-caution': {
+      hex: lightThemeColors['signal-colors'].yellow.color,
+      contrast: lightThemeColors['signal-colors'].yellow.contrastText,
+    },
+    'color-state-warning': {
+      hex: lightThemeColors['signal-colors'].orange.color,
+      contrast: lightThemeColors['signal-colors'].orange.contrastText,
+    },
+    'color-state-error': {
+      hex: lightThemeColors['signal-colors'].red.color,
+      contrast: lightThemeColors['signal-colors'].red.contrastText,
+    },
+    'color-state-hover-green': {
+      hex: lightThemeColors['primary-colors'].green.color,
+      contrast: lightThemeColors['primary-colors'].green.contrastText,
+    },
+    'color-state-hover-grey': {
+      hex: lightThemeColors['grey-colors']['grey-05'].color,
+      contrast: lightThemeColors['grey-colors']['grey-05'].contrastText,
+    },
+    'color-state-disabled': {
+      hex: lightThemeColors['grey-colors']['grey-30'].color,
+      contrast: lightThemeColors['grey-colors']['grey-30'].contrastText,
+    },
+    'color-state-focus': {
+      hex: lightThemeColors['internal-colors']['focus-outline'].color,
+    },
+  },
+  data: {
+    'color-data-green': {
+      hex: lightThemeColors['data-colors']['green-apple'].color,
+      contrast: lightThemeColors['data-colors']['green-apple'].contrastText,
+    },
+    'color-data-violet': {
+      hex: lightThemeColors['data-colors']['violet-grape'].color,
+      contrast: lightThemeColors['data-colors']['violet-grape'].contrastText,
+    },
+    'color-data-blue': {
+      hex: lightThemeColors['data-colors']['blue-berry'].color,
+      contrast: lightThemeColors['data-colors']['blue-berry'].contrastText,
+    },
+    'color-data-purple': {
+      hex: lightThemeColors['data-colors']['purple-plum'].color,
+      contrast: lightThemeColors['data-colors']['purple-plum'].contrastText,
+    },
+    'color-data-orange': {
+      hex: lightThemeColors['data-colors']['orange-mango'].color,
+      contrast: lightThemeColors['data-colors']['orange-mango'].contrastText,
+    },
+    'color-data-red': {
+      hex: lightThemeColors['data-colors']['red-tomato'].color,
+      contrast: lightThemeColors['data-colors']['red-tomato'].contrastText,
+    },
+  },
+} as const;
