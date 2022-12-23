abcd01b0d2c9175687494e0edee06e57d90df20f
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2022-12-23 12:41:28 +0100

    feat(component-changelog): add filterChangelog method
    allows to filter by a single component

--- packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
+++ packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-component-changelog',
   templateUrl: './component-changelog.component.html',
   styleUrls: ['./component-changelog.component.scss'],
 })
-export class ComponentChangelogComponent {
+export class ComponentChangelogComponent implements OnInit {
   @Input() changelog;
   /**
    * If enabled the changelog-list will be in an accordion
@@ -15,4 +15,35 @@ export class ComponentChangelogComponent {
    * Will hide all but first element
    */
   @Input() isFrontpage = false;
+
+  /**
+   * The component to filter for (only display changelog for this component)
+   */
+  @Input() componentToFilter: string;
+
+  constructor() {}
+
+  filteredChangelog = [];
+
+  filterChangelog(componentToFilter) {
+    this.changelog.forEach((elvisChangelogEntry) => {
+      elvisChangelogEntry.changelog.forEach(({ components }) => {
+        components?.forEach(({ displayName }) => {
+          if (
+            displayName.toLowerCase() === componentToFilter.toLowerCase() &&
+            !this.filteredChangelog.includes(elvisChangelogEntry)
+          ) {
+            this.filteredChangelog.push(elvisChangelogEntry);
+          }
+        });
+      });
+    });
+  }
+
+  ngOnInit() {
+    if (this.componentToFilter) {
+      this.filterChangelog(this.componentToFilter);
+      this.changelog = this.filteredChangelog;
+    }
+  }
 }
