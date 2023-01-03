28c21ede42f81411d6fc68433df555c820b2dbff
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-01-03 15:31:01 +0100

    doc(component-changelog): improve doc

--- packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
+++ packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.component.ts
@@ -7,7 +7,7 @@ import elvisChangelogJson from 'src/assets/changelogs/elvis/CHANGELOG.json';
   styleUrls: ['./component-changelog.component.scss'],
 })
 export class ComponentChangelogComponent implements OnInit {
-  @Input() changelog;
+  @Input() changelog?;
   /**
    * If enabled the changelog-list will be in an accordion
    */
@@ -17,20 +17,23 @@ export class ComponentChangelogComponent implements OnInit {
    */
   @Input() isFrontpage = false;
   /**
-   * The component to filter for (only display changelog for this component)
+   * The Elvis component to filter for (only display changelog for this component).
+   * When set, the component will automatically use the Elvis Changelog.
+   * NOTE: Not intended for use with V2+ components.
    */
   @Input() elvisComponentToFilter?: string;
 
   filteredChangelog = [];
 
+  /**
+   * Filters the the Elvis changelog for a specific Elvis component.
+   * Counts the number of releases between each mention of the component it filters
+   */
   private filterChangelog(elvisComponentToFilter: string) {
     let numberOfReleasesSkipped = 0;
-    // Loop through each entry in the changelog.
     this.changelog.forEach((elvisChangelogEntry) => {
       let wasSkipped = true;
-      // Loop through each component mentioned in the changelog entry.
       elvisChangelogEntry.changelog.forEach(({ components }) => {
-        // Loop through each component name in the entry.
         components?.forEach(({ displayName }) => {
           if (
             displayName.toLowerCase() === elvisComponentToFilter.toLowerCase() &&
