ef9d019c12cc614cd4f9d92df4e67a94f1a7343f
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-01-04 19:41:44 +0100

    test(pagination): remove skip from axe test

--- packages/components/components/elvis-pagination/src/react/elvia-pagination.test.tsx
+++ packages/components/components/elvis-pagination/src/react/elvia-pagination.test.tsx
@@ -135,13 +135,12 @@ describe('Elvis Pagination', () => {
     });
   });
 
-  describe.skip('the accessibility', () => {
+  describe('the accessibility', () => {
     /* will fail if there are multiple paginators on same page -> considered user error */
     /* depends on fixes in dropdown before this test can be un-skipped */
     it('should have no axe violations', async () => {
       render(
-        <div data-testid="paginations">
-          <Pagination />
+        <div data-testid="pagination-wrapper">
           <Pagination
             numberOfElements={100}
             alignment={'right'}
@@ -150,8 +149,8 @@ describe('Elvis Pagination', () => {
         </div>,
       );
 
-      const paginations = screen.getByTestId('paginations');
-      const results = await axe(paginations);
+      const pagination = screen.getByTestId('pagination-wrapper');
+      const results = await axe(pagination);
 
       expect(results).toHaveNoViolations();
     });
