82c2260188b2164f10918d03b6338151fe09eec4
Trygve Scheline Urdahl <trur@computas.com>
2023-01-04 09:02:40 +0100

    add useCurrentTheme hook to toolbox

---
+++ packages/components/components/elvis-toolbox/src/hooks/useCurrentTheme.ts
@@ -0,0 +1,28 @@
+import React, { useLayoutEffect, useState } from 'react';
+import { ThemeName, ThemeClassName } from '@elvia/elvis-colors';
+
+interface UseCurrentThemeApi {
+  currentTheme: ThemeName;
+  themeClass: ThemeClassName;
+}
+
+/**
+ *
+ * @param ref Ref to a DOM element inside the current component. This is used to find the closest parent with a theme class.
+ * @returns
+ */
+export const useCurrentTheme = (ref: React.RefObject<HTMLElement>): UseCurrentThemeApi => {
+  const [currentTheme, setCurrentTheme] = useState<ThemeName>('light');
+  const [themeClass, setThemeClass] = useState<ThemeClassName>('e-theme-light');
+
+  useLayoutEffect(() => {
+    const closestParentWithThemeClass = ref.current?.closest('.e-theme-dark, .e-theme-light');
+    if (closestParentWithThemeClass) {
+      const themeClass = closestParentWithThemeClass.className.match(/e-theme-(dark|light)/)?.[0];
+      setThemeClass(themeClass as ThemeClassName);
+      setCurrentTheme(themeClass === 'e-theme-dark' ? 'dark' : 'light');
+    }
+  }, []);
+
+  return { currentTheme, themeClass };
+};
