d39feba3e248e12f5bcedd9f910ba9fe5e2c75a7
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-01-12 12:19:34 +0100

    Lego 1546 webstruktur gjore doc siden for komponenter mer gjenbrukbar (#1226)
    * feat(shared): add SharedDocumentationModule
    
    * refactor(accordion-doc): import SharedDocumentationModule
    
    * refactor(accordion-group-doc): import SharedDocumentationModule
    
    See https://angular.io/guide/sharing-ngmodules

--- packages/web/src/app/doc-pages/components/accordion-doc/accordion-doc.module.ts
+++ packages/web/src/app/doc-pages/components/accordion-doc/accordion-doc.module.ts
@@ -1,36 +1,10 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import '@elvia/elvis-accordion';
 import { AccordionDocComponent } from './accordion-doc.component';
-import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
-import { ComponentExampleCodeModule } from 'src/app/shared/component-documentation/component-example/component-example-code/component-example-code.module';
-import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
-import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
-import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
-import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
-import { ComponentPropertiesTableModule } from 'src/app/shared/component-documentation/component-properties-table/component-properties-table.module';
-import { ComponentExampleGeneratorModule } from 'src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module';
-import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
-import { ComponentInstallationModule } from 'src/app/shared/component-documentation/component-installation/component-installation.module';
-import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
+import { SharedDocumentationModule } from 'src/app/shared/component-documentation/shared-component-documentation.module';
+import '@elvia/elvis-accordion';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ComponentHeaderModule,
-    ComponentExampleV1Module,
-    ComponentExampleCodeModule,
-    ComponentPropertiesModule,
-    ComponentSectionModule,
-    ComponentSubsectionModule,
-    WhenToUseModule,
-    ComponentPropertiesTableModule,
-    ComponentExampleGeneratorModule,
-    ComponentSubsubsectionModule,
-    ComponentInstallationModule,
-    ComponentChangelogModule,
-  ],
+  imports: [SharedDocumentationModule],
   declarations: [AccordionDocComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
