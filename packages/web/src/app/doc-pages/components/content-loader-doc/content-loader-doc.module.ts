924eb52e3110d3c2fc8cf05864bcbe55136e43b8
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-01-13 15:19:39 +0100

    Lego 2184 web legge til filtrert elvis changelog pa resterende dokumentasjonssider (#1224)
    * refactor(component-changelog): the first element in the array can not be a "skipped" element
    
    * refactor(docs): add changelog to Elvis components
    
    * refactor(Elvis): add accordion group to the changelog
    
    * refactor(Elvis): add drag&drop change
    
    mest relevante jeg fant: https://github.com/3lvia/designsystem/commit/e6dd73776ad836b99a5e2c3e76d10da1fcb449c7

--- packages/web/src/app/doc-pages/components/content-loader-doc/content-loader-doc.module.ts
+++ packages/web/src/app/doc-pages/components/content-loader-doc/content-loader-doc.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContentLoaderDocComponent } from './content-loader-doc.component';
-import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
 import { ComponentExampleCodeModule } from 'src/app/shared/component-documentation/component-example/component-example-code/component-example-code.module';
+import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
+import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
+import { ContentLoaderDocComponent } from './content-loader-doc.component';
 
 @NgModule({
+  declarations: [ContentLoaderDocComponent],
   imports: [
     CommonModule,
     ComponentHeaderModule,
@@ -19,7 +21,7 @@ import { ComponentSubsectionModule } from 'src/app/shared/component-documentatio
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
+    ComponentChangelogModule,
   ],
-  declarations: [ContentLoaderDocComponent],
 })
 export class ContentLoaderDocModule {}
--- packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.module.ts
+++ packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchDocComponent } from './search-doc.component';
-import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
-import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
-import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
-import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
-import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
+import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
+import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
+import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
+import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
+import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
+import { SearchDocComponent } from './search-doc.component';
+import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
 
 @NgModule({
+  declarations: [SearchDocComponent],
   imports: [
     CommonModule,
     ComponentHeaderModule,
@@ -19,7 +21,7 @@ import { RouterModule } from '@angular/router';
     ComponentSubsectionModule,
     WhenToUseModule,
     RouterModule,
+    ComponentChangelogModule,
   ],
-  declarations: [SearchDocComponent],
 })
 export class SearchDocModule {}
