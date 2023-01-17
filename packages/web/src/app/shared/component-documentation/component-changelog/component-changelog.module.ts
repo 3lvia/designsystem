434d7f13f50ca8a18660b367d19e48a5cbcb9984
Trygve Scheline Urdahl <trur@computas.com>
2023-01-17 08:28:15 +0100

    add formsModule to component-changelog.module

--- packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.module.ts
+++ packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.module.ts
@@ -1,17 +1,16 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Input, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ComponentChangelogComponent } from './component-changelog.component';
 import { ChangelogTypePipe } from './component-changelog-pipe';
 import '@elvia/elvis-accordion';
 import '@elvia/elvis-divider';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   declarations: [ComponentChangelogComponent, ChangelogTypePipe],
   exports: [ComponentChangelogComponent, ChangelogTypePipe],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ComponentChangelogModule {
-  @Input() changelog;
-}
+export class ComponentChangelogModule {}
