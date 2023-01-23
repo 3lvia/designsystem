be0a86ee40298fc660f28dff081721a9f5487e66
Trygve Scheline Urdahl <trur@computas.com>
2023-01-23 08:01:03 +0100

    improve group accordion documentation

--- packages/web/src/app/doc-pages/components/accordion-doc/accordion-group-data.ts
+++ packages/web/src/app/doc-pages/components/accordion-doc/accordion-group-data.ts
@@ -1,10 +1,100 @@
 import { exampleContents } from 'src/app/shared/example-contents';
 
 export const accordionGroupData = {
-  codeReact: `<div></div>`,
-  codeAngular: `<div></div>`,
-  codeVue: `<div></div>`,
-  codeNativeHTML: `<div style="display: flex; flex-direction: column; gap: 16px;">
+  codeReact: `<div style={{display: 'flex'; flexDirection: 'column'; gap: '16px'}}>
+  <Accordion 
+    isFullWidth="true"
+    typography="text-lg-strong"
+    openLabel="${exampleContents.texts.md['eng-GBR'].title}" 
+    closeLabel="${exampleContents.texts.md['eng-GBR'].title}"
+    content="${exampleContents.texts.md['eng-GBR'].description}"
+  /></Accordion>
+  <Divider></Divider>
+  <Accordion 
+    isFullWidth="true"
+    typography="text-lg-strong"
+    openLabel="${exampleContents.texts.sm['eng-GBR'].title}" 
+    closeLabel="${exampleContents.texts.sm['eng-GBR'].title}"
+    content="${exampleContents.texts.sm['eng-GBR'].description}"
+  ></Accordion>
+  <Divider></Divider>
+  <Accordion 
+    isFullWidth="true"
+    typography="text-lg-strong"
+    openLabel="${exampleContents.texts.lg['eng-GBR'].title}" 
+    closeLabel="${exampleContents.texts.lg['eng-GBR'].title}"
+    content="${exampleContents.texts.lg['eng-GBR'].description}"
+  ></Accordion>
+</div>`,
+  codeAngular: `<div style="display: flex; flex-direction: column; gap: 16px">
+  <elvia-accordion 
+    [isFullWidth]="true"
+    [typography]="'text-lg-strong'"
+    [openLabel]="'${exampleContents.texts.md['eng-GBR'].title}'" 
+    [closeLabel]="'${exampleContents.texts.md['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.md['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+  <elvia-divider></elvia-divider>
+  <elvia-accordion 
+    [isFullWidth]="true"
+    [typography]="'text-lg-strong'"
+    [openLabel]="'${exampleContents.texts.sm['eng-GBR'].title}'" 
+    [closeLabel]="'${exampleContents.texts.sm['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.sm['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+  <elvia-divider></elvia-divider>
+  <elvia-accordion 
+    [isFullWidth]="true"
+    [typography]="'text-lg-strong'"
+    [openLabel]="'${exampleContents.texts.lg['eng-GBR'].title}'" 
+    [closeLabel]="'${exampleContents.texts.lg['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.lg['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+</div>`,
+  codeVue: `<div style="display: flex; flex-direction: column; gap: 16px">
+  <elvia-accordion 
+    :isFullWidth="true"
+    :typography="'text-lg-strong'"
+    :openLabel="'${exampleContents.texts.md['eng-GBR'].title}'" 
+    :closeLabel="'${exampleContents.texts.md['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.md['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+  <elvia-divider></elvia-divider>
+  <elvia-accordion 
+    :isFullWidth="true"
+    :typography="'text-lg-strong'"
+    :openLabel="'${exampleContents.texts.sm['eng-GBR'].title}'" 
+    :closeLabel="'${exampleContents.texts.sm['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.sm['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+  <elvia-divider></elvia-divider>
+  <elvia-accordion 
+    :isFullWidth="true"
+    :typography="'text-lg-strong'"
+    :openLabel="'${exampleContents.texts.lg['eng-GBR'].title}'" 
+    :closeLabel="'${exampleContents.texts.lg['eng-GBR'].title}'"
+  >
+    <div slot="content">
+        ${exampleContents.texts.lg['eng-GBR'].description}
+    </div>
+  </elvia-accordion>
+</div>`,
+  codeNativeHTML: `<div style="display: flex; flex-direction: column; gap: 16px">
   <elvia-accordion 
     isFullWidth="true"
     typography="text-lg-strong"
