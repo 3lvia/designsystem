f4b21daf18411503cc35626d9eb7f4761be8ed1c
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-01-24 15:41:23 +0100

    Lego 2172: Changelog - kunne søke og filtrere (#1244)
    * make searchService injectable per component
    
    * update changelog typescript interface
    
    * add filter and search to changelogs
    
    * refactor no-properties-found style
    
    * make properties search normal size
    
    * refactor changelog id generating functions to pipe
    
    * set fixed hight on changelog empty state to fix accordion

--- packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.module.ts
+++ packages/web/src/app/shared/component-documentation/component-changelog/component-changelog.module.ts
@@ -1,17 +1,16 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Input, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { ComponentChangelogComponent } from './component-changelog.component';
 import { ChangelogTypePipe } from './component-changelog-pipe';
 import '@elvia/elvis-accordion';
-import '@elvia/elvis-divider';
+import { ChangelogIdPipe } from './component-changelog-id-pipe';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
-  declarations: [ComponentChangelogComponent, ChangelogTypePipe],
-  exports: [ComponentChangelogComponent, ChangelogTypePipe],
+  imports: [CommonModule, RouterModule, FormsModule],
+  declarations: [ComponentChangelogComponent, ChangelogTypePipe, ChangelogIdPipe],
+  exports: [ComponentChangelogComponent, ChangelogTypePipe, ChangelogIdPipe],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ComponentChangelogModule {
-  @Input() changelog;
-}
+export class ComponentChangelogModule {}
