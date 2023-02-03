1a7bc3282e3d2f5d4f27a8453939c20064421f4a
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-02-03 10:41:05 +0100

    Lego 2264: Theme - progressbar (#1282)
    * add dark theme to progressbar
    
    * fix progressbar test
    
    * destructure props in progressbar styledComponents

--- packages/components/components/elvis-progress-linear/src/react/styledComponents.tsx
+++ packages/components/components/elvis-progress-linear/src/react/styledComponents.tsx
@@ -1,12 +1,7 @@
 import styled, { css, keyframes } from 'styled-components';
-import { getColor } from '@elvia/elvis-colors';
+import { getThemeColor } from '@elvia/elvis-colors';
 import { ProgressLinearSize } from './elvia-progress-linear.types';
 
-export const colors = {
-  Grey10: getColor('grey-10'),
-  ElviaCharge: getColor('elvia-charge'),
-  Red: getColor('red'),
-};
 const loading = keyframes`
 	0% { width: 0%; margin-left: 0%; }
 	15% { width: 15%; margin-left: 0%; }
@@ -21,9 +16,9 @@ type ProgressLinearWrapperProps = {
 export const ProgressLinearWrapper = styled.div<ProgressLinearWrapperProps>`
   display: flex;
   width: 100%;
-  height: ${(props) => (props.currSize === 'medium' ? '8px' : '4px')};
+  height: ${({ currSize }) => (currSize === 'medium' ? '8px' : '4px')};
   border-radius: 50px;
-  background-color: ${colors.Grey10};
+  background-color: ${getThemeColor('background-accent')};
   margin: 0;
 `;
 
@@ -36,14 +31,14 @@ type ProgressLinearProgressProps = {
 export const ProgressLinearProgress = styled.div<ProgressLinearProgressProps>`
   border-radius: 50px;
   align-self: center;
-  height: ${(props) => (props.currSize === 'medium' ? '16px' : '8px')};
+  height: ${({ currSize }) => (currSize === 'medium' ? '16px' : '8px')};
   margin-left: 0;
-  background-color: ${(props) => (props.isError ? colors.Red : colors.ElviaCharge)};
-  transition: ${(props) => (props.isIndeterminate ? 'none' : 'width 0.3s ease-in;')};
-  ${(props) => decideProgressValue(props.isIndeterminate, props.isError)};
+  background-color: ${({ isError }) => (isError ? getThemeColor('state-error') : getThemeColor('state-on'))};
+  transition: ${({ isIndeterminate }) => (isIndeterminate ? 'none' : 'width 0.3s ease-in;')};
+  ${({ isIndeterminate, isError }) => decideProgressValue(isIndeterminate, isError)};
   // Indeterminate
-  ${(props) =>
-    props.isIndeterminate && !props.isError
+  ${({ isIndeterminate, isError }) =>
+    isIndeterminate && !isError
       ? css`
           animation: ${loading} 1s infinite;
         `
