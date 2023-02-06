cd17b8ced9419608b472818f88ea9e4d0a68bd9b
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-02-06 09:18:40 +0100

    Update config.ts

--- packages/components/components/elvis-slider/src/react/config.ts
+++ packages/components/components/elvis-slider/src/react/config.ts
@@ -3,12 +3,16 @@ import { ComponentConfig } from '@elvia/elvis-toolbox';
 export const config: ComponentConfig = {
   name: 'Slider',
   attributes: [
+    { name: 'ariaLabel', type: 'string' },
     { name: 'className', type: 'string' },
+    { name: 'errorOptions', type: 'object' },
     { name: 'hasHintValues', type: 'boolean' },
     { name: 'hasInputField', type: 'boolean' },
     { name: 'hasPercent', type: 'boolean' },
     { name: 'hasTooltip', type: 'boolean' },
+    { name: 'heading', type: 'string' },
     { name: 'inlineStyle', type: 'object' },
+    { name: 'isCompact', type: 'boolean' },
     { name: 'isDisabled', type: 'boolean' },
     { name: 'label', type: 'string' },
     { name: 'max', type: 'number' },
