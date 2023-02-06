25698e636ca78ad1408b99fb30f2bf5b555d1c27
Trygve Scheline Urdahl <trur@computas.com>
2023-02-06 10:40:35 +0100

    improve useCurrentTheme hook in toolbox

--- packages/components/components/elvis-toolbox/src/hooks/useCurrentTheme.ts
+++ packages/components/components/elvis-toolbox/src/hooks/useCurrentTheme.ts
@@ -1,9 +1,10 @@
-import React, { useLayoutEffect, useMemo, useState } from 'react';
+import React, { useCallback, useLayoutEffect, useMemo, useState } from 'react';
 import { ThemeName, ThemeClassName } from '@elvia/elvis-colors';
 
 interface UseCurrentThemeApi {
   currentTheme: ThemeName;
   themeClass: ThemeClassName;
+  updateCurrentTheme: () => void;
 }
 
 /**
@@ -17,13 +18,19 @@ export const useCurrentTheme = (ref: React.RefObject<HTMLElement>): UseCurrentTh
   const [themeClass, setThemeClass] = useState<ThemeClassName>('e-theme-light');
   const currentTheme = useMemo(() => (themeClass === 'e-theme-dark' ? 'dark' : 'light'), [themeClass]);
 
-  useLayoutEffect(() => {
+  const updateCurrentTheme = useCallback(() => {
     const closestParentWithThemeClass = ref.current?.closest('.e-theme-dark, .e-theme-light');
     if (closestParentWithThemeClass) {
-      const themeClass = closestParentWithThemeClass.className.match(/e-theme-(dark|light)/)?.[0];
-      setThemeClass(themeClass as ThemeClassName);
+      const newThemeClass = Array.from(closestParentWithThemeClass.classList).find((className) =>
+        className.match(/e-theme-(dark|light)/),
+      ) as ThemeClassName;
+      setThemeClass(newThemeClass);
     }
   }, [ref]);
 
-  return { currentTheme, themeClass };
+  useLayoutEffect(() => {
+    updateCurrentTheme();
+  }, [ref]);
+
+  return { currentTheme, themeClass, updateCurrentTheme };
 };
