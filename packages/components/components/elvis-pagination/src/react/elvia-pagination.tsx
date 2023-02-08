584d44aa98ccdeb54453912c29d67b38290fc659
Fride Skarseth <fs@computas.com>
2023-02-08 11:14:05 +0100

    fix(pagination): Hide page-numbers when only one page

--- packages/components/components/elvis-pagination/src/react/elvia-pagination.tsx
+++ packages/components/components/elvis-pagination/src/react/elvia-pagination.tsx
@@ -97,6 +97,10 @@ const Pagination: FC<PaginationProps> = function ({
 
   useEffect(() => {
     setNumberOfPages(Math.ceil(numberOfElements / selectedDropdownValue));
+    console.log('CHANGED');
+    if (Math.ceil(numberOfElements / selectedDropdownValue) < 2) {
+      setShowPaginationNumbers(false);
+    }
   }, [numberOfElements, selectedDropdownValue]);
 
   useEffect(() => {
@@ -196,17 +200,18 @@ const Pagination: FC<PaginationProps> = function ({
           {labelOptionsState.of} {numberOfElements.toLocaleString('nb-NO')} {labelOptionsState.label}
         </PaginatorInfoAmount>
       </PaginatorInfoContainer>
-      <PaginatorSelectorArea role="navigation" ref={listContainerRef}>
-        <PaginatorSelectorArrowBtn
-          visible={shouldHaveLeftArrow()}
-          aria-hidden={!shouldHaveLeftArrow()}
-          onClick={() => setSelectedPageNumber((previousPageNumber) => previousPageNumber - 1)}
-          data-testid="selector-arrow-btn-left"
-          aria-label="Forrige side"
-        >
-          <IconWrapper icon={arrowLongLeft} size="xs" />
-        </PaginatorSelectorArrowBtn>
-        {showPaginationNumbers && (
+      {showPaginationNumbers && (
+        <PaginatorSelectorArea role="navigation" ref={listContainerRef}>
+          <PaginatorSelectorArrowBtn
+            visible={shouldHaveLeftArrow()}
+            aria-hidden={!shouldHaveLeftArrow()}
+            onClick={() => setSelectedPageNumber((previousPageNumber) => previousPageNumber - 1)}
+            data-testid="selector-arrow-btn-left"
+            aria-label="Forrige side"
+          >
+            <IconWrapper icon={arrowLongLeft} size="xs" />
+          </PaginatorSelectorArrowBtn>
+
           <PaginatorNumbersAndDots
             numberOfPages={numberOfPages}
             selectedPageNumber={selectedPageNumber}
@@ -214,17 +219,17 @@ const Pagination: FC<PaginationProps> = function ({
             numberOfElements={numberOfElements}
             lastNumberLimit={lastNumberLimit}
           />
-        )}
-        <PaginatorSelectorArrowBtn
-          visible={shouldHaveRightArrow()}
-          aria-hidden={!shouldHaveRightArrow()}
-          onClick={() => setSelectedPageNumber((previousPageNumber) => previousPageNumber + 1)}
-          data-testid="selector-arrow-btn-right"
-          aria-label="Neste side"
-        >
-          <IconWrapper icon={arrowLongRight} size="xs" />
-        </PaginatorSelectorArrowBtn>
-      </PaginatorSelectorArea>
+          <PaginatorSelectorArrowBtn
+            visible={shouldHaveRightArrow()}
+            aria-hidden={!shouldHaveRightArrow()}
+            onClick={() => setSelectedPageNumber((previousPageNumber) => previousPageNumber + 1)}
+            data-testid="selector-arrow-btn-right"
+            aria-label="Neste side"
+          >
+            <IconWrapper icon={arrowLongRight} size="xs" />
+          </PaginatorSelectorArrowBtn>
+        </PaginatorSelectorArea>
+      )}
     </Paginator>
   );
 };
