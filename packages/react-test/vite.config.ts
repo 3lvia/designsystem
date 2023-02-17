6c2096dca3e9e84d144ebf0678f38795fcf0331d
Trygve Scheline Urdahl <trur@computas.com>
2023-02-17 15:07:58 +0100

    migrate react-test to vite

---
+++ packages/react-test/vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@elvia/elvis-accordion/react',
+      '@elvia/elvis-badge/react',
+      '@elvia/elvis-box/react',
+      '@elvia/elvis-breadcrumb/react',
+      '@elvia/elvis-card/react',
+      '@elvia/elvis-carousel/react',
+      '@elvia/elvis-chip/react',
+      '@elvia/elvis-context-menu/react',
+      '@elvia/elvis-datepicker/react',
+      '@elvia/elvis-datepicker-range/react',
+      '@elvia/elvis-divider/react',
+      '@elvia/elvis-dropdown/react',
+      '@elvia/elvis-header/react',
+      '@elvia/elvis-icon/react',
+      '@elvia/elvis-modal/react',
+      '@elvia/elvis-pagination/react',
+      '@elvia/elvis-popover/react',
+      '@elvia/elvis-progress-linear/react',
+      '@elvia/elvis-radio-filter/react',
+      '@elvia/elvis-segmented-control/react',
+      '@elvia/elvis-slider/react',
+      '@elvia/elvis-spotlight/react',
+      '@elvia/elvis-tabs/react',
+      '@elvia/elvis-timepicker/react',
+    ],
+  },
+});
