c3997d4996a7f2dbd12edca9ebd95057ce216de8
Trygve Scheline Urdahl <trur@computas.com>
2023-02-20 08:15:34 +0100

    test react-test prod deploy

--- packages/web/src/app/app.module.ts
+++ packages/web/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
 import { ComponentsModule } from './doc-pages/components/components.module';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { HttpClientModule } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 import { ErrorModule } from './shared/error/error.module';
 import { CMSPageModule } from './doc-pages/cms/cms-page/cms-page.module';
 import { MobileHeaderModule } from './shell/header/mobile-menu/mobile-menu.module';
@@ -33,6 +34,7 @@ import { OtherPagesModule } from './doc-pages/other-pages/other-pages.module';
     MobileHeaderModule,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/react-preview' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
