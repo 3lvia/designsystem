4ad789cc0ff734682e42167754719a6f34f05ef6
Trygve Scheline Urdahl <trur@computas.com>
2023-02-20 08:47:10 +0100

    add rollup plugin for commonjs

--- packages/react-test/vite.config.ts
+++ packages/react-test/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -34,35 +35,8 @@ export default defineConfig({
     ],
   },
   build: {
-    // target: 'modules',
     rollupOptions: {
-      external: [
-        /@elvia/,
-        // '@elvia/elvis-accordion/react',
-        // '@elvia/elvis-badge/react',
-        // '@elvia/elvis-box/react',
-        // '@elvia/elvis-breadcrumb/react',
-        // '@elvia/elvis-card/react',
-        // '@elvia/elvis-carousel/react',
-        // '@elvia/elvis-chip/react',
-        // '@elvia/elvis-context-menu/react',
-        // '@elvia/elvis-datepicker/react',
-        // '@elvia/elvis-datepicker-range/react',
-        // '@elvia/elvis-divider/react',
-        // '@elvia/elvis-dropdown/react',
-        // '@elvia/elvis-header/react',
-        // '@elvia/elvis-icon/react',
-        // '@elvia/elvis-modal/react',
-        // '@elvia/elvis-pagination/react',
-        // '@elvia/elvis-popover/react',
-        // '@elvia/elvis-progress-linear/react',
-        // '@elvia/elvis-radio-filter/react',
-        // '@elvia/elvis-segmented-control/react',
-        // '@elvia/elvis-slider/react',
-        // '@elvia/elvis-spotlight/react',
-        // '@elvia/elvis-tabs/react',
-        // '@elvia/elvis-timepicker/react',
-      ],
+      plugins: [commonjs()],
     },
   },
 });
