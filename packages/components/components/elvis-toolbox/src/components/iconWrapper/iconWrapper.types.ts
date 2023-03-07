4e3fbaf4f93f09fbb12d5b02e6a2b59582f06145
Trygve Scheline Urdahl <trur@computas.com>
2023-03-07 08:26:31 +0100

    chore(iconWrapper): update iconWrapper type to support new getThemeColor labels

--- packages/components/components/elvis-toolbox/src/components/iconWrapper/iconWrapper.types.ts
+++ packages/components/components/elvis-toolbox/src/components/iconWrapper/iconWrapper.types.ts
@@ -1,24 +1,17 @@
 import React, { FC } from 'react';
-import { ElviaColor } from '@elvia/elvis-colors';
+import { ColorLabel } from '@elvia/elvis-colors';
 
-type IconSizes =
-  | 'xxs'
-  | 'xs'
-  | 'sm'
-  | 'md'
-  | 'lg'
-  | 'xl'
-  | 'xxl'
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  | (string & {});
+// eslint-disable-next-line @typescript-eslint/ban-types
+type AnyString = string & {};
+type IconSizes = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | AnyString;
 
-export interface IconWrapperProps<TIcon extends { getIcon: (color?: ElviaColor | 'inverted') => string }>
+export interface IconWrapperProps<TIcon extends { getIcon: (color?: ColorLabel | AnyString) => string }>
   extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
   /**
    * The icon to render from `@elvia/elvis-assets-icons`.
    */
   icon: TIcon;
-  color?: ElviaColor | 'inverted';
+  color?: ColorLabel | AnyString;
   /**
    * Either a size from the `IconSizes` enum (e.g. `'sm'` or `'xl'`) or a custom size in pixels.
    */
@@ -27,7 +20,7 @@ export interface IconWrapperProps<TIcon extends { getIcon: (color?: ElviaColor |
 
 /** Can be replaced with the use of `IconType` from `@elvia/elvis-assets-icons` if we are ok with a dependency from toolbox on assets-icons */
 export type IconWrapperType<
-  TIcon extends { getIcon: (color?: ElviaColor | 'inverted') => string } = {
-    getIcon: (color?: ElviaColor | 'inverted') => string;
+  TIcon extends { getIcon: (color?: ColorLabel | AnyString) => string } = {
+    getIcon: (color?: ColorLabel | AnyString) => string;
   },
 > = FC<IconWrapperProps<TIcon>>;
