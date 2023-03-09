cfcc91ca132454e60466b2a14b1521edfc02089b
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-03-09 05:31:08 -0800

    Lego 2270: Theme - timepicker (#1284)
    * add dark theme to timepicker
    
    * bump date range picker for time picker new version
    
    * remove explicit icon coloring
    
    * use new disabled-foreground label
    
    * update datepicker year view dark theme colors
    
    * chore: bump dependencies on assets-icons
    
    * chore: update changelog dates
    
    * chore(timepicker): fix elvia-colors dependency version

--- packages/components/components/elvis-timepicker/src/react/error/timepickerError.tsx
+++ packages/components/components/elvis-timepicker/src/react/error/timepickerError.tsx
@@ -3,6 +3,7 @@ import { ErrorType } from '../elviaTimepicker.types';
 import { getErrorText } from '../getErrorText';
 import { FormFieldErrorContainer, FormFieldError, IconWrapper } from '@elvia/elvis-toolbox';
 import removeCircle from '@elvia/elvis-assets-icons/dist/icons/removeCircle';
+import { getThemeColor } from '@elvia/elvis-colors';
 
 interface Props {
   errorType?: ErrorType;
@@ -22,7 +23,7 @@ export const TimepickerError: React.FC<Props> = ({ errorType, customText }) => {
 
   return (
     <FormFieldErrorContainer>
-      <IconWrapper icon={removeCircle} color="error" size="xs" />
+      <IconWrapper icon={removeCircle} color={getThemeColor('state-error')} size="xs" />
       <FormFieldError data-testid="error">{errorText}</FormFieldError>
     </FormFieldErrorContainer>
   );
