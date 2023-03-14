ecc6bac780733fce9096841b3ea6950ad0168931
Erik Tallang <et@computas.com>
2023-03-14 09:01:37 +0100

    test(toast): add tests

--- packages/components/components/elvis-toast/src/react/elvia-toast.test.tsx
+++ packages/components/components/elvis-toast/src/react/elvia-toast.test.tsx
@@ -1,21 +1,104 @@
 import React from 'react';
 import Toast from './elvia-toast';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { openElviaToast } from './publicApi';
+import { ToastConfig } from './elviaToast.types';
+import userEvent from '@testing-library/user-event';
 
-const renderToast = () => {
-  render(<Toast></Toast>);
+const toastTitle = 'Toast title';
+const toastBody = 'Toast description';
+const toastDuration = 10;
+
+const mockMatchMedia = (opts?: Partial<{ isGtMobile: boolean }>) => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: opts?.isGtMobile,
+      media: query,
+      onchange: null,
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
 };
 
-const toastDescription = 'Toast description';
+const openToast = (config?: Partial<ToastConfig>) => {
+  act(() => {
+    openElviaToast({
+      title: toastTitle,
+      body: toastBody,
+      duration: toastDuration,
+      ...config,
+    });
+  });
+};
+
 describe('Elvis Toast', () => {
-  describe('with default settings', () => {
-    beforeEach(() => {
-      renderToast();
+  beforeEach(() => {
+    mockMatchMedia({ isGtMobile: true });
+    render(<Toast></Toast>);
+  });
+
+  it('should initially not show an toast', () => {
+    const toast = screen.queryByText(toastBody);
+    expect(toast).not.toBeInTheDocument();
+  });
+
+  it('should show the toast title when it is open', async () => {
+    openToast();
+
+    await waitFor(() => {
+      const element = screen.getByText(toastTitle);
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  it('should show the toast body when it is open', async () => {
+    openToast();
+
+    await waitFor(() => {
+      const element = screen.getByText(toastBody);
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  it('should not have a close button initially', async () => {
+    openToast();
+
+    await waitFor(() => {
+      const element = screen.queryByText('close-btn');
+      expect(element).not.toBeInTheDocument();
+    });
+  });
+
+  it('should have a close button when the config says so', async () => {
+    openToast({ closable: true });
+
+    await waitFor(() => {
+      const element = screen.queryByTestId('close-btn');
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  it('should close when the close button is pressed', async () => {
+    const user = userEvent.setup();
+    openToast({ closable: true });
+
+    await waitFor(() => {
+      const closeBtn = screen.getByTestId('close-btn');
+      user.click(closeBtn);
     });
 
-    it('should initially not show an toast', () => {
-      const toast = screen.queryByText(toastDescription);
-      expect(toast).not.toBeInTheDocument();
+    await waitForElementToBeRemoved(screen.queryByRole('status'));
+  });
+
+  it('should be possible to have a custom icon', async () => {
+    openToast({ customIcon: '<p data-testid="custom-icon">Custom icon</p>' });
+
+    await waitFor(() => {
+      const element = screen.queryByTestId('custom-icon');
+      expect(element).toBeInTheDocument();
     });
   });
 });
