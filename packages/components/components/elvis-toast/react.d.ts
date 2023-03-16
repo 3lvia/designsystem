2669a399d801ecca35f8fdbb214efb1112fdb2e2
Erik Tallang <et@computas.com>
2023-03-16 11:59:08 +0100

    fix(toast): improper react type export

--- packages/components/components/elvis-toast/react.d.ts
+++ packages/components/components/elvis-toast/react.d.ts
@@ -1,6 +1,6 @@
 declare module '@elvia/elvis-toast/react';
 export { default as Toast } from './dist/react/js/elvia-toast';
-export { openElviaToast } from './dist/react/js/publicApi';
 export type { ToastProps } from './dist/react/js/elvia-toast';
-export type * from './dist/react/js/elviaToast';
+export type * from './dist/react/js/elvia-toast';
+export { openElviaToast } from './dist/react/js/publicApi';
 export type * from './dist/react/js/elviaToast.types';
