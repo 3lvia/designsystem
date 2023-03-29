7a0549f821864f66fa8ab9d6df3a991e4727c053
Trygve Scheline Urdahl <trur@computas.com>
2023-03-29 08:28:31 +0200

    feat(localization.service): reset localization on routing outside of brandC

--- packages/web/src/app/core/services/localization.service.ts
+++ packages/web/src/app/core/services/localization.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 export enum Locale {
@@ -12,6 +13,18 @@ export enum Locale {
 export class LocalizationService {
   private defaultLocale = Locale['en-GB'];
   private localizationSubject = new BehaviorSubject<Locale>(this.defaultLocale);
+
+  constructor(private router: Router) {
+    // Set localization to english on route change outside of brand
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        if (url.split('/')[1] !== 'brand') {
+          this.setLocalization(Locale['en-GB']);
+        }
+      }
+    });
+  }
 
   listenLocalization(): BehaviorSubject<Locale> {
     return this.localizationSubject;
