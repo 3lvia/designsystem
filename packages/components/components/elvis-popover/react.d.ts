e2b2e6257fc579ac917a1a30fbebed00b768a002
Erik Tallang <et@computas.com>
2023-03-31 15:31:36 +0200

    fix(popover): incorrect type exports

--- packages/components/components/elvis-popover/react.d.ts
+++ packages/components/components/elvis-popover/react.d.ts
@@ -1,4 +1,5 @@
 declare module '@elvia/elvis-popover/react';
 export { default as Popover } from './dist/react/js/elvia-popover';
-export type { PopoverProps } from './dist/react/js/elvia-popover';
+export type { PopoverProps } from './dist/react/js/elviaPopover.types';
 export type * from './dist/react/js/elvia-popover';
+export type * from './dist/react/js/elviaPopover.types';
