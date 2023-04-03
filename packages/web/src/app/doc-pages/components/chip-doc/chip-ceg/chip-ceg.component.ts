4d31897518eaf5e495d07c706fc79686b910233c
Trygve Scheline Urdahl <trur@computas.com>
2023-04-03 09:31:09 +0200

    feat(docs:chip): use new CEG

---
+++ packages/web/src/app/doc-pages/components/chip-doc/chip-ceg/chip-ceg.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { CegControlManager, ComponentExample } from 'src/app/shared/component-documentation/ceg';
+import { ChipProps } from '@elvia/elvis-chip/react';
+
+@Component({
+  selector: 'app-chip-ceg',
+  templateUrl: './chip-ceg.component.html',
+  providers: [{ provide: ComponentExample, useExisting: ChipCegComponent }],
+})
+export class ChipCegComponent implements ComponentExample {
+  elementName = 'chip';
+  cegContent = new CegControlManager<ChipProps>([
+    {
+      name: 'Removable',
+      controls: {
+        color: {
+          type: 'radioGroup',
+          radios: [
+            { label: 'Green', value: 'green' },
+            { label: 'Blue', value: 'blue' },
+            { label: 'Orange', value: 'orange' },
+            { label: 'Purple', value: 'purple' },
+            { label: 'Red', value: 'red' },
+            { label: 'Violet', value: 'violet' },
+          ],
+          value: 'green',
+          group: 'Color',
+        },
+        isDisabled: {
+          label: 'Disabled',
+          type: 'checkbox',
+          value: false,
+          group: 'State',
+        },
+        value: {
+          type: 'text',
+          value: 'Chip',
+          label: 'Value',
+          group: 'Content',
+          inputType: 'input',
+        },
+      },
+      groupOrder: ['Color', 'State'],
+    },
+    {
+      name: 'Legend',
+      controls: {
+        color: {
+          type: 'radioGroup',
+          radios: [
+            { label: 'Green', value: 'green' },
+            { label: 'Blue', value: 'blue' },
+            { label: 'Orange', value: 'orange' },
+            { label: 'Purple', value: 'purple' },
+            { label: 'Red', value: 'red' },
+            { label: 'Violet', value: 'violet' },
+          ],
+          value: 'green',
+          group: 'Color',
+        },
+        isDisabled: {
+          label: 'Disabled',
+          type: 'checkbox',
+          value: false,
+          group: 'State',
+        },
+        value: {
+          type: 'text',
+          value: 'Chip',
+          label: 'Value',
+          group: 'Content',
+          inputType: 'input',
+        },
+      },
+      groupOrder: ['Color', 'State'],
+    },
+    {
+      name: 'Choice',
+      controls: {
+        isDisabled: {
+          label: 'Disabled',
+          type: 'checkbox',
+          value: false,
+          group: 'State',
+        },
+        value: {
+          type: 'text',
+          value: 'Chip',
+          label: 'Value',
+          group: 'Content',
+          inputType: 'input',
+        },
+      },
+      groupOrder: ['State'],
+    },
+  ]);
+
+  handleOnChange(value: number): void {
+    console.log('Selected value: ', value);
+  }
+}
