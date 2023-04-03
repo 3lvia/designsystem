c3263c5d30d5b91d3af117be17bd08e5d1ce001d
Erik Tallang <e.tallang@gmail.com>
2023-04-03 10:16:49 +0200

    Lego 2417 replace tooltip ceg (#1369)
    docs(tooltip): use new ceg

--- packages/components/components/elvis-tooltip/react.d.ts
+++ packages/components/components/elvis-tooltip/react.d.ts
@@ -1,5 +1,5 @@
 declare module '@elvia/elvis-tooltip/react';
 export { default as Tooltip } from './dist/react/js/elvia-tooltip';
-export type { TooltipProps } from './dist/react/js/elvia-tooltip';
+export type { TooltipProps } from './dist/react/js/elviaTooltip.types';
 export type * from './dist/react/js/elvia-tooltip';
-export type * from './dist/react/js/elvia-tooltip.types';
+export type * from './dist/react/js/elviaTooltip.types';
