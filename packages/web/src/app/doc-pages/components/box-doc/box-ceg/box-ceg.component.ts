150f74e4b5368cc8be6aa00985ef48e872a3fb9c
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-04-04 09:31:19 +0200

    refactor(box): replace old CEG with new CEG

---
+++ packages/web/src/app/doc-pages/components/box-doc/box-ceg/box-ceg.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { BoxProps } from '@elvia/elvis-box/react';
+import { CegControlManager, ComponentExample } from 'src/app/shared/component-documentation/ceg';
+import { exampleContents } from 'src/app/shared/example-contents';
+
+@Component({
+  selector: 'app-box-ceg',
+  templateUrl: './box-ceg.component.html',
+  providers: [{ provide: ComponentExample, useExisting: BoxCegComponent }],
+})
+export class BoxCegComponent implements ComponentExample {
+  elementName = 'box';
+  cegContent = new CegControlManager<BoxProps>([
+    {
+      controls: {
+        isColored: {
+          group: 'Options',
+          type: 'switch',
+          label: 'Colored',
+        },
+        title: {
+          group: 'Title',
+          type: 'text',
+          label: 'Title',
+          value: this.exampleTitle,
+        },
+      },
+      groupOrder: ['Options', 'Title'],
+    },
+  ]);
+
+  get exampleText() {
+    return exampleContents.texts.sm['eng-GBR'].description;
+  }
+
+  get exampleTitle() {
+    return exampleContents.texts.sm['eng-GBR'].title;
+  }
+
+  get tabsItems() {
+    return exampleContents.words.categories['eng-GBR'];
+  }
+}
