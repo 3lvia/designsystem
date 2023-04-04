a3cec264b04040d40928f6323a512b1f01a0bc53
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-04-04 09:55:50 +0200

    docs(box): remove old ceg data

--- packages/web/src/app/doc-pages/components/box-doc/box-data.ts
+++ packages/web/src/app/doc-pages/components/box-doc/box-data.ts
@@ -15,21 +15,12 @@ const boxData: ComponentData = {
       isRequired: false,
       type: `string | HTMLElement | JSX.Element`,
       description: `Title for the box (slot in webcomponent).`,
-      cegDisplayName: 'Title',
-      cegType: 'boolean',
-      cegFormType: 'toggle',
-      cegDefault: false,
-      cegSlot: '<h1>Customer relations</h1>',
     },
     hasBorder: {
       isRequired: false,
       type: 'boolean',
       description: 'Use this prop if the box is on a white background.',
       default: 'false',
-      cegDefault: 1,
-      cegType: 'boolean',
-      cegFormType: 'background',
-      cegOptions: ['White', 'Light grey'],
     },
     isColored: {
       isRequired: false,
@@ -37,11 +28,6 @@ const boxData: ComponentData = {
       description:
         'Green line on top of box. Use the green line to draw attention, but avoid using multiple boxes with green lines on the same page.',
       default: 'false',
-      cegDisplayName: 'Colored',
-      cegType: 'boolean',
-      cegFormType: 'toggle',
-      cegDefault: false,
-      cegOption: 'true',
     },
     className: {
       isRequired: false,
@@ -55,35 +41,6 @@ const boxData: ComponentData = {
         "Custom CSS style object that can be added to the box. Example: {marginTop: '8px', width: '100%'}",
     },
   },
-  codeReact: `<Box
-  content={<div>
-    <Tabs items={['AMS-meter', 'Electric car', 'HAN-port']}></Tabs>
-    <div className="e-mt-16">${exampleContents.texts.sm['eng-GBR'].description}</div>
-  </div>}
-></Box>`,
-  codeAngular: `<elvia-box 
->
-  <div slot="content">
-    <elvia-tabs [items]="['AMS-meter', 'Electric car', 'HAN-port']"></elvia-tabs>
-    <div class="e-mt-16">${exampleContents.texts.sm['eng-GBR'].description}</div>
-  </div>
-</elvia-box>`,
-  codeVue: `<elvia-box 
->
-  <div slot="content">
-    <elvia-tabs :items="['AMS-meter', 'Electric car', 'HAN-port']"></elvia-tabs>
-    <div class="e-mt-16">${exampleContents.texts.sm['eng-GBR'].description}</div>
-  </div>
-</elvia-box>`,
-  codeNativeHTML: `<elvia-box 
->
-  <div slot="content">
-    <elvia-tabs items='["AMS-meter", "Electric car", "HAN-port"]'></elvia-tabs>
-    <div class="e-mt-16">${exampleContents.texts.sm['eng-GBR'].description}</div>
-  </div>
-</elvia-box>`,
-
-  codeNativeScript: ``,
 };
 
 export { boxData };
