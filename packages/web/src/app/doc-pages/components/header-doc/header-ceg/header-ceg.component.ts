e77b163000a2bd5ee617a483090b02c66031071f
Erik Tallang <et@computas.com>
2023-04-11 18:55:06 +0200

    docs(header): use new ceg

---
+++ packages/web/src/app/doc-pages/components/header-doc/header-ceg/header-ceg.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CegControlManager, ComponentExample } from 'src/app/shared/component-documentation/ceg';
+
+import { HeaderProps } from '@elvia/elvis-header/react';
+
+@Component({
+  selector: 'app-header-ceg',
+  templateUrl: './header-ceg.component.html',
+  providers: [{ provide: ComponentExample, useExisting: HeaderCegComponent }],
+})
+export class HeaderCegComponent implements ComponentExample {
+  headerIsVisible = false;
+  elementName = 'header';
+  cegContent = new CegControlManager<HeaderProps>([
+    {
+      controls: {
+        username: { type: 'text', group: 'Username', label: 'Username', value: 'Ola Nordmann' },
+        email: { type: 'text', group: 'Email', label: 'Email', value: 'ola.nordmann@elvia.no' },
+        appTitle: { type: 'text', group: 'App Title', label: 'App Title', value: 'Designsystemet' },
+        pageTitle: { type: 'text', group: 'Page Title', label: 'Page Title', value: 'Dashbord' },
+      },
+      groupOrder: ['Username', 'Email', 'App Title', 'Page Title'],
+    },
+  ]);
+
+  toggleHeader() {
+    const externalHeader = document.getElementById('header');
+    externalHeader.classList.toggle('e-none');
+
+    const mainElement = document.getElementById('main-content');
+    mainElement.classList.toggle('e-bg-grey-05');
+
+    this.headerIsVisible = !this.headerIsVisible;
+  }
+}
--- packages/web/src/app/doc-pages/components/header-doc/header-data.ts
+++ packages/web/src/app/doc-pages/components/header-doc/header-data.ts
@@ -9,38 +9,22 @@ export const headerData: ComponentData = {
       isRequired: true,
       type: 'string',
       description: 'The username of the signed in user. Can be fetched from the JWT.',
-      cegDisplayName: 'User name',
-      cegType: 'string',
-      cegFormType: 'custom-text',
-      cegDefault: 'Ola Jansen',
     },
     email: {
       isRequired: true,
       type: 'string',
       description: 'The email of the signed in user. Can be fetched from the JWT.',
-      cegDisplayName: 'E-mail',
-      cegType: 'string',
-      cegFormType: 'custom-text',
-      cegDefault: 'e12345@elvia.no',
     },
     appTitle: {
       isRequired: false,
       type: 'string',
       description: 'The name of the application. If no title is provided, the title is assumed from the URL.',
-      cegDisplayName: 'App title',
-      cegType: 'string',
-      cegFormType: 'custom-text',
-      cegDefault: 'Drops',
     },
     pageTitle: {
       isRequired: true,
       type: 'string | HTMLElement | JSX.Element',
       description:
         'The name of the current page. Can be text, or rich content (use slot in web component if not just text).',
-      cegDisplayName: 'Page title',
-      cegType: 'string',
-      cegFormType: 'custom-text',
-      cegDefault: 'Kart og analyse',
     },
     navItems: {
       isRequired: false,
@@ -76,146 +60,4 @@ export const headerData: ComponentData = {
         "Custom CSS style object that can be added to the header. Example: {marginTop: '8px', width: '100%'}.",
     },
   },
-  codeReact: `<Header
-  appContent={
-    <h1 class="e-title-md">
-      This is the page content
-    </h1>
-  }
-  navItems={
-    <div className="e-sidenav__container">
-      <Link to="/" className="e-sidenav__item e-sidenav__item--active" aria-label="Dashbord">
-        <div className="e-sidenav__icon-container">
-          <i class="e-icon e-icon--sm e-icon--search"></i>
-          <i class="e-icon e-icon--sm e-icon--search-color"></i>
-        </div>
-        <div className="e-sidenav__item-text">Dashbord</div>
-      </Link>
-      <Link to="/search" className="e-sidenav__item" aria-label="Søk">
-        <div className="e-sidenav__icon-container">
-          <i class="e-icon e-icon--sm e-icon--upload"></i>
-          <i class="e-icon e-icon--sm e-icon--upload e-icon--upload-color"></i>
-        </div>
-        <div className="e-sidenav__item-text">Søk</div>
-      </Link>
-      <Link to="/pin" className="e-sidenav__item" aria-label="Analyse">
-        <div className="e-sidenav__icon-container">
-          <i class="e-icon e-icon--sm e-icon--full_battery"></i>
-          <i class="e-icon e-icon--sm e-icon--full_battery-color"></i>
-        </div>
-        <div className="e-sidenav__item-text">Analyse</div>
-      </Link>
-    </div>
-  }
-></Header>`,
-  codeAngular: `<elvia-header
->
-  <div slot="appContent">
-    <h1 class="e-title-md">
-      This is the page content
-    </h1>
-  </div>
-  <div slot="navItems" class="e-sidenav__container">
-    <a routerLink="/" class="e-sidenav__item e-sidenav__item--active" aria-label="Dashbord">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--search"></i>
-        <i class="e-icon e-icon--sm e-icon--search-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Dashbord</div>
-    </a>
-    <a routerLink="/search" class="e-sidenav__item" aria-label="Søk">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--upload"></i>
-        <i class="e-icon e-icon--sm e-icon--upload e-icon--upload-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Søk</div>
-    </a>
-    <a routerLink="/pin" class="e-sidenav__item" aria-label="Analyse">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--full_battery"></i>
-        <i class="e-icon e-icon--sm e-icon--full_battery-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Analyse</div>
-    </a>
-  </div>
-</elvia-header>`,
-  codeVue: `<elvia-header
->
-  <div slot="appContent">
-    <h1 class="e-title-md">
-      This is the page content
-    </h1>
-  </div>
-  <div slot="navItems" class="e-sidenav__container">
-    <router-link to="/" class="e-sidenav__item e-sidenav__item--active" aria-label="Dashbord">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--search"></i>
-        <i class="e-icon e-icon--sm e-icon--search-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Dashbord</div>
-    </router-link>
-    <router-link to="/search" class="e-sidenav__item" aria-label="Søk">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--upload"></i>
-        <i class="e-icon e-icon--sm e-icon--upload e-icon--upload-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Søk</div>
-    </router-link>
-    <router-link to="/pin" class="e-sidenav__item" aria-label="Analyse">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--full_battery"></i>
-        <i class="e-icon e-icon--sm e-icon--full_battery-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Analyse</div>
-    </router-link>
-  </div>
-</elvia-header>`,
-  codeNativeHTML: `<button class="e-btn" id="header-trigger" style="position: relative;
-  z-index: 999999;">Toggle internal header</button>
-<elvia-header
-  class="e-none"
-  id="internal-header"
->
-  <div slot="navItems" class="e-sidenav__container">
-    <a href="#" class="e-sidenav__item e-sidenav__item--active" aria-label="Dashbord">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--search"></i>
-        <i class="e-icon e-icon--sm e-icon--search-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Dashbord</div>
-    </a>
-    <a href="#" class="e-sidenav__item" aria-label="Søk">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--upload"></i>
-        <i class="e-icon e-icon--sm e-icon--upload e-icon--upload-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Søk</div>
-    </a>
-    <a href="#" class="e-sidenav__item" aria-label="Analyse">
-      <div class="e-sidenav__icon-container">
-        <i class="e-icon e-icon--sm e-icon--full_battery"></i>
-        <i class="e-icon e-icon--sm e-icon--full_battery-color"></i>
-      </div>
-      <div class="e-sidenav__item-text">Analyse</div>
-    </a>
-  </div>
-</elvia-header>`,
-  codeNativeScript: `
-const header = document.getElementById('internal-header');
-const externalHeader = document.getElementById('header');
-const mainElement = document.getElementById('main-content');
-const headerTrigger = document.getElementById('header-trigger');
-
-headerTrigger.addEventListener('click', (event) => {
-  if (header.classList.contains('e-none')) {
-    header.classList.remove('e-none');
-    externalHeader.classList.add('e-none');
-    mainElement.classList.add('e-bg-grey-05');
-  } else {
-    header.classList.add('e-none');
-    externalHeader.classList.remove('e-none');
-    mainElement.classList.remove('e-bg-grey-05');
-  }
-});
-  `,
 };
