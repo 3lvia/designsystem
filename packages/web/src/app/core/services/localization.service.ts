a3b1740b0b0b5708ab0ec528ba4d38c9e229790f
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-04-12 08:39:31 +0200

    Lego 963: CMS - localization (#1359)
    * feat(web): add locale fallback to cms service
    
    * fix(web): enable locale switch outside localhost
    
    * feat(web): make sidenav listen to locale, add fallback for docpages without current locale
    
    * feat(web): improve generated contentful model types
    
    * feat(web): cms locale data extractor function
    
    * refactor(cms.service): make private functions private
    
    * feat(web): add locale switch to sidebar
    
    * feat(localization.service): reset localization on routing outside of brand
    
    * feat(navbar): add new localization switch only on brand
    
    * fix(navbar): fix initial render locale switch visibility
    
    * fix(navbar): dont show locale toggle when still loading from cms
    
    * refactor(navbar): move --navbar-blur-amount css variable from document to navbar-element
    
    * refactor(cms-transform.service): use extractLocale for getDocumentationPageHTML
    
    * feat(navbar): make locale switch sticky, add mobile locale switch
    
    * refactor(footer): remove unused localization
    
    * refactor(cms-transform.service): make extractLocale facade to avoid repeating this.locale
    
    * fix(localization.service): make listenLocalization return observable instead of BehaviorSubject
    
    * feat(localization): store preferred localization in local storage

--- packages/web/src/app/core/services/localization.service.ts
+++ packages/web/src/app/core/services/localization.service.ts
@@ -1,22 +1,47 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export enum Locale {
   'en-GB' = 0,
   'nb-NO' = 1,
 }
 
+const LOCALIZATION_STORAGE_KEY = 'preferredDesignElviaIoLocale';
 @Injectable({
   providedIn: 'root',
 })
 export class LocalizationService {
-  private defaultLocale = Locale['en-GB'];
+  readonly defaultLocale = Locale['en-GB'];
   private localizationSubject = new BehaviorSubject<Locale>(this.defaultLocale);
 
-  listenLocalization(): BehaviorSubject<Locale> {
-    return this.localizationSubject;
+  constructor(private router: Router) {
+    // Set localization to english on route change outside of brand
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects;
+        if (url.split('/')[1] !== 'brand') {
+          this.setLocalization(Locale['en-GB']);
+        } else {
+          // On route change to brand, check if a preferred locale is set
+          const preferredLocale = localStorage.getItem(LOCALIZATION_STORAGE_KEY);
+          if (preferredLocale) {
+            this.setLocalization(Locale[preferredLocale]);
+          }
+        }
+      }
+    });
   }
+
+  listenLocalization(): Observable<Locale> {
+    return this.localizationSubject.asObservable();
+  }
+
   setLocalization(locale: Locale): void {
     this.localizationSubject.next(locale);
+  }
+
+  setPreferredLocalization(locale: Locale): void {
+    localStorage.setItem(LOCALIZATION_STORAGE_KEY, Locale[locale]);
   }
 }
