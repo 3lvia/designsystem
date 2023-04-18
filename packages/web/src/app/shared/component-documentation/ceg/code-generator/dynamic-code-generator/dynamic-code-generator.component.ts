38c7996038cb7386e2bb22a39b9cc1965bcb8476
Erik Tallang <et@computas.com>
2023-04-18 16:53:16 +0200

    feat(ceg): update the slot code real time

--- packages/web/src/app/shared/component-documentation/ceg/code-generator/dynamic-code-generator/dynamic-code-generator.component.ts
+++ packages/web/src/app/shared/component-documentation/ceg/code-generator/dynamic-code-generator/dynamic-code-generator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subject, Observable, combineLatest } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { UnknownCegControlManager } from '../../cegControlManager';
@@ -25,6 +25,8 @@ export class DynamicCodeGeneratorComponent implements OnInit, OnDestroy {
   reactCode = '';
   vueCode = '';
 
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
   ngOnInit() {
     this.initialProps = this.getFlatPropList(
       this.controlManager.getControlSnapshot() ?? {},
@@ -45,6 +47,14 @@ export class DynamicCodeGeneratorComponent implements OnInit, OnDestroy {
         this.angularCode = this.createWebComponentCode(props, slots, '[', ']');
         this.vueCode = this.createWebComponentCode(props, slots, ':');
         this.reactCode = this.createReactCode(props, slots);
+
+        /**
+         * For some reason, changes in slots in our web components does
+         * not trigger a change detection cycle in Angular, thus e.g. changes
+         * in applied CSS classes are "late" and gets rendered on the next tick.
+         * We fix this by explicitly telling Angular to trigger change detection.
+         */
+        this.changeDetectorRef.detectChanges();
       });
   }
 
