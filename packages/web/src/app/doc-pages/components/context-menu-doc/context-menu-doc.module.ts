f4e259c5e7c38d8b1c5fbd6c217127844401bbf6
Erik Tallang <et@computas.com>
2023-04-19 12:05:28 +0200

    docs(context-menu): clean up module imports

--- packages/web/src/app/doc-pages/components/context-menu-doc/context-menu-doc.module.ts
+++ packages/web/src/app/doc-pages/components/context-menu-doc/context-menu-doc.module.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import '@elvia/elvis-context-menu';
+import { CommonModule } from '@angular/common';
 import { CegModule } from 'src/app/shared/component-documentation/ceg/ceg.module';
 import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
@@ -8,14 +8,14 @@ import { ComponentPropertiesTableModule } from 'src/app/shared/component-documen
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
-import { ContextMenuCegComponent } from './context-menu-ceg/context-menu-ceg.component';
-import { ContextMenuHeadingsCegComponent } from './context-menu-headings-ceg/context-menu-headings-ceg.component';
 import { ContextMenuDocComponent } from './context-menu-doc.component';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
-import { CommonModule } from '@angular/common';
+import { ContextMenuCegComponent } from './context-menu-ceg/context-menu-ceg.component';
+import { ContextMenuHeadingsCegComponent } from './context-menu-headings-ceg/context-menu-headings-ceg.component';
 import { ContextMenuIconsCegComponent } from './context-menu-icons-ceg/context-menu-icons-ceg.component';
 import { ContextMenuSelectableCegComponent } from './context-menu-selectable-ceg/context-menu-selectable-ceg.component';
 import { ContextMenuDisabledCegComponent } from './context-menu-disabled-ceg/context-menu-disabled-ceg.component';
+import '@elvia/elvis-context-menu';
 
 @NgModule({
   imports: [
