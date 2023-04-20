9ea8446f950a6d5eebbbe057b1e706043672b767
Erik Tallang <e.tallang@gmail.com>
2023-04-20 14:33:23 +0200

    docs(tabs): replace ceg (#1395)
    docs(tabs): replace ceg

--- packages/web/src/app/doc-pages/components/tabs-doc/tabs-doc.module.ts
+++ packages/web/src/app/doc-pages/components/tabs-doc/tabs-doc.module.ts
@@ -1,29 +1,30 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import '@elvia/elvis-tabs';
 import { TabsDocComponent } from './tabs-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentPropertiesTableModule } from 'src/app/shared/component-documentation/component-properties-table/component-properties-table.module';
-import { ComponentExampleGeneratorModule } from 'src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module';
 import { ComponentInstallationModule } from 'src/app/shared/component-documentation/component-installation/component-installation.module';
 import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
+import { TabsCegComponent } from './tabs-ceg/tabs-ceg.component';
+import { CegModule } from 'src/app/shared/component-documentation/ceg/ceg.module';
+import '@elvia/elvis-tabs';
 
 @NgModule({
   imports: [
+    CegModule,
     CommonModule,
     ComponentHeaderModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
     ComponentPropertiesTableModule,
-    ComponentExampleGeneratorModule,
     ComponentInstallationModule,
     ComponentChangelogModule,
     WhenToUseModule,
   ],
-  declarations: [TabsDocComponent],
+  declarations: [TabsDocComponent, TabsCegComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class TabsDocModule {}
