90222ece7f1b5882c8b6156bd62525c36195f862
Fride Skarseth <fs@computas.com>
2023-04-20 10:01:49 +0200

    feat(card): Card ceg with slottoggles (#1394)
    * feat(card): Card ceg with slottoggles
    
    * tests(card): Updated tests
    
    * feat(card): Using static CEG

--- packages/web/src/app/doc-pages/components/card-doc/card-doc.module.ts
+++ packages/web/src/app/doc-pages/components/card-doc/card-doc.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import '@elvia/elvis-card';
 import { CardDocComponent } from './card-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
@@ -8,10 +9,12 @@ import { ComponentSubsectionModule } from 'src/app/shared/component-documentatio
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
 import { ComponentInstallationModule } from 'src/app/shared/component-documentation/component-installation/component-installation.module';
-import { ComponentExampleGeneratorModule } from 'src/app/shared/component-documentation/component-example/component-example-generator/component-example-generator.module';
 import { ComponentPropertiesTableModule } from 'src/app/shared/component-documentation/component-properties-table/component-properties-table.module';
 import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
 import { RouterModule } from '@angular/router';
+import { CardCegComponent } from './card-ceg/card-ceg.component';
+import { CardLinksCegComponent } from './card-links-ceg/card-links-ceg.component';
+import { CegModule } from 'src/app/shared/component-documentation/ceg/ceg.module';
 
 @NgModule({
   imports: [
@@ -23,11 +26,12 @@ import { RouterModule } from '@angular/router';
     ComponentSubsubsectionModule,
     WhenToUseModule,
     ComponentInstallationModule,
-    ComponentExampleGeneratorModule,
     ComponentPropertiesTableModule,
     ComponentChangelogModule,
     RouterModule,
+    CegModule,
   ],
-  declarations: [CardDocComponent],
+  declarations: [CardDocComponent, CardCegComponent, CardLinksCegComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class CardDocModule {}
