ec02cbeea8b95a125ec1ba5b0394ad0a9e0c0e55
Fride Skarseth <fs@computas.com>
2023-04-24 11:15:02 +0200

    docs(layout): Added translations (#1409)
    * docs(layout): Added translations

--- packages/web/src/app/doc-pages/other-pages/layout-doc/layout-doc.component.ts
+++ packages/web/src/app/doc-pages/other-pages/layout-doc/layout-doc.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { getDocPagesNotFromCMS } from 'src/app/shared/doc-pages';
 import { spacingItems } from './spacing';
 import { Title } from '@angular/platform-browser';
+import { Locale, LocalizationService } from 'src/app/core/services/localization.service';
+import { Subscription } from 'rxjs';
+import { LOCALE_CODE } from 'contentful/types';
 
 @Component({
   selector: 'app-layout-doc',
   templateUrl: './layout-doc.component.html',
   styleUrls: ['./layout-doc.component.scss'],
 })
-export class LayoutDocComponent {
-  description = getDocPagesNotFromCMS('layout')?.description;
-  figmaUrl = getDocPagesNotFromCMS('layout')?.figmaUrl;
+export class LayoutDocComponent implements OnDestroy {
+  localizationSubscriber: Subscription;
   title = getDocPagesNotFromCMS('layout')?.title;
+  titleNo = getDocPagesNotFromCMS('layout')?.titleNo;
+  description = getDocPagesNotFromCMS('layout')?.description;
+  descriptionNo = getDocPagesNotFromCMS('layout')?.descriptionNo;
+  figmaUrl = getDocPagesNotFromCMS('layout')?.figmaUrl;
   spacingItems = spacingItems;
+  locale: LOCALE_CODE = 'en-GB';
 
-  constructor(private titleServcie: Title) {
-    this.titleServcie.setTitle(this.title + ' | Elvia design system');
+  constructor(private titleService: Title, private localizationService: LocalizationService) {
+    this.setTabTitle();
+    this.localizationSubscriber = this.localizationService.listenLocalization().subscribe((locale) => {
+      this.locale = locale === Locale['en-GB'] ? 'en-GB' : 'nb-NO';
+      this.setTabTitle();
+    });
   }
+
+  ngOnDestroy(): void {
+    this.localizationSubscriber && this.localizationSubscriber.unsubscribe();
+  }
+
+  setTabTitle = (): void => {
+    this.titleService.setTitle(
+      (this.locale === 'nb-NO' && this.titleNo ? this.titleNo : this.title) + ' | Elvia design system',
+    );
+  };
 
   doCodeCSS = `padding: var(--e-spacing-16);
   margin: var(--e-spacing-48);`;
