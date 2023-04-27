e1d402a62fd396bbb33868c189a469eb8750508c
Fride Skarseth <fs@computas.com>
2023-04-27 15:55:21 +0200

    docs(thumbnail): Refactor and added static ceg

--- packages/web/src/app/doc-pages/components/thumbnail-doc/thumbnail-doc.module.ts
+++ packages/web/src/app/doc-pages/components/thumbnail-doc/thumbnail-doc.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ThumbnailDocComponent } from './thumbnail-doc.component';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
+import { ThumbnailCegComponent } from './thumbnail-ceg/thumbnail-ceg.component';
+import { CegModule } from 'src/app/shared/component-documentation/ceg/ceg.module';
+import { ThumbnailSelectedCegComponent } from './thumbnail-selected-ceg/thumbnail-selected-ceg.component';
 
 @NgModule({
-  declarations: [ThumbnailDocComponent],
+  declarations: [ThumbnailDocComponent, ThumbnailCegComponent, ThumbnailSelectedCegComponent],
   imports: [
     CommonModule,
     ComponentHeaderModule,
-    ComponentExampleV1Module,
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
     WhenToUseModule,
     ComponentChangelogModule,
+    CegModule,
   ],
 })
 export class ThumbnailDocModule {}
