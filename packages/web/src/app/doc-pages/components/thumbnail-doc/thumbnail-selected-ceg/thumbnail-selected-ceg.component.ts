5e55ad131c4d2c2e275fd64ea6c51bd052706773
Fride Skarseth <fs@computas.com>
2023-04-28 09:40:31 +0200

    docs(thumbnail): Refactor and added static ceg (#1439)
    * docs(thumbnail): Refactor and added static ceg

---
+++ packages/web/src/app/doc-pages/components/thumbnail-doc/thumbnail-selected-ceg/thumbnail-selected-ceg.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { StaticComponentExample } from 'src/app/shared/component-documentation/ceg';
+
+import * as template from 'html-loader!./thumbnail-selected-ceg.component.html';
+
+@Component({
+  selector: 'app-thumbnail-selected-ceg',
+  templateUrl: './thumbnail-selected-ceg.component.html',
+  providers: [{ provide: StaticComponentExample, useExisting: ThumbnailSelectedCegComponent }],
+})
+export class ThumbnailSelectedCegComponent implements StaticComponentExample {
+  html = template.default;
+}
