f2d7499dd8d9221cd19f6674f74c810536236c1e
Erik Tallang <et@computas.com>
2023-04-28 09:44:21 +0200

    refactor: reuse router service in localization service

--- packages/web/src/app/core/services/router.service.ts
+++ packages/web/src/app/core/services/router.service.ts
@@ -10,20 +10,22 @@ export class RouterService {
   constructor(private router: Router) {}
 
   /**
-   * Only emit routing events when the path (URL without query params) has changed
+   * Only emit routing events when the path (URL without query params) has changed.
+   * @returns An Observable containing the new path.
    */
-  urlPathChange(): Observable<void> {
+  urlPathChange(): Observable<string> {
     return this.router.events.pipe(
       filter((event) => event instanceof NavigationEnd),
       startWith(null), // Start with a null value to make the first navigation pass the pairwise pipe (which requires two events to have been fired)
       pairwise(),
-      filter(([oldRoute, newRoute]) => {
-        const oldUrl = (oldRoute as NavigationEnd)?.urlAfterRedirects.split('?')[0];
-        const newUrl = (newRoute as NavigationEnd).urlAfterRedirects.split('?')[0];
+      map(([oldRoute, newRoute]) => {
+        const oldPath = (oldRoute as NavigationEnd)?.urlAfterRedirects.split('?')[0];
+        const newPath = (newRoute as NavigationEnd).urlAfterRedirects.split('?')[0];
 
-        return oldUrl !== newUrl;
+        return [oldPath, newPath];
       }),
-      map(() => {}),
+      filter(([oldPath, newPath]) => oldPath !== newPath),
+      map(([_, newPath]) => newPath),
     );
   }
 }
