d5849cc404ab6324660ab82c7ff6afbfda5307d6
Erik Tallang <et@computas.com>
2023-05-02 13:00:56 +0200

    chore(slider): remove old ceg config

--- packages/web/src/app/doc-pages/components/slider-doc/slider-data.ts
+++ packages/web/src/app/doc-pages/components/slider-doc/slider-data.ts
@@ -11,14 +11,12 @@ export const sliderData: ComponentData = {
       isRequired: false,
       type: '“simple” | “range”',
     },
-
     hasInputField: {
       default: 'false',
       description: 'Decides if the slider should have input field(s).',
       isRequired: false,
       type: 'boolean',
     },
-
     hasHintValues: {
       default: 'false',
       description:
@@ -26,7 +24,6 @@ export const sliderData: ComponentData = {
       isRequired: false,
       type: 'boolean',
     },
-
     hasTooltip: {
       default: 'true',
       description:
@@ -34,56 +31,27 @@ export const sliderData: ComponentData = {
       isRequired: false,
       type: 'boolean',
     },
-
     hasPercent: {
       default: 'false',
       description:
         'The tooltip should display the percentage of the distance between minimum and maximum values rather than the actual value itself. Only available on a simple slider.',
       isRequired: false,
       type: 'boolean',
-
-      cegDefault: false,
-      cegDependency: [
-        { name: 'type', value: 'simple' },
-        { name: 'hasTooltip', value: 'true' },
-      ],
-      cegDisplayGroup: 'Options',
-      cegDisplayName: 'Display percentage',
-      cegFormType: 'checkbox',
-      cegOption: 'true',
-      cegType: 'boolean',
     },
-
     unit: {
       isRequired: false,
       description:
         'A custom unit displayed in the tooltip. For example "kWh", "kr/mnd". The unit will be overwritten if hasPercent is set to true.',
       type: 'string',
       default: '',
-
-      cegFormType: 'custom-text',
-      cegCustomTextType: 'input',
-      cegDefault: '',
-      cegDependency: [
-        { name: 'hasTooltip', value: 'true' },
-        { name: 'hasPercent', value: 'false' },
-      ],
     },
-
     label: {
       isRequired: false,
       description:
         'A custom label displayed above the input(s). For example "Kilowatt hours". Labels can be set individually for range sliders by using a object: {left:"min", right:"max"}',
       type: 'string | {left: string, right: string}',
       default: '',
-
-      cegFormType: 'custom-text',
-      cegCustomTextType: 'input',
-      cegDefault: '',
-      cegDependency: [{ name: 'hasInputField', value: 'true' }],
     },
-
-    /* TODO: Legge til max i CEG, men bare dersom counten til max aldri kan bli mindre enn min */
     max: {
       default: 100,
       description:
@@ -91,8 +59,6 @@ export const sliderData: ComponentData = {
       isRequired: false,
       type: 'number',
     },
-
-    /* TODO: Legge til min i CEG, men bare dersom counten til min aldri kan bli større enn max */
     min: {
       default: 1,
       description:
@@ -100,25 +66,17 @@ export const sliderData: ComponentData = {
       isRequired: false,
       type: 'number',
     },
-
     value: {
       default: '{left: 1, right: 100}',
       description: 'The default value of the slider. An object for range sliders.',
       isRequired: false,
       type: 'number | {left: number, right: number}',
     },
-
     isDisabled: {
       default: 'false',
       description: 'Set the slider to a disabled state.',
       isRequired: false,
       type: 'boolean',
-
-      cegDisplayGroup: 'State',
-      cegDisplayName: 'Disabled',
-      cegFormType: 'checkbox',
-      cegOption: 'true',
-      cegType: 'boolean',
     },
     className: {
       description: 'Custom CSS classes that can be added to the slider.',
@@ -133,37 +91,8 @@ export const sliderData: ComponentData = {
     },
     valueOnChange: {
       isRequired: false,
-      type: '(value: Number | object ) => CustomEvent',
+      type: '(value: number | object ) => CustomEvent',
       description: 'Gets called every time the value is changed. Only returns a value if the input is valid.',
     },
   },
-  codeReact: `<Slider 
-  min={1} 
-  max={100}
-  valueOnChange={(event) => handleOnChange(event.detail.value)}
-></Slider>
-`,
-  codeAngular: `<elvia-slider
-  [min]="1"
-  [max]="100"
-  (valueOnChange)="updateValue($event.detail.value)"
-></elvia-slider>
-`,
-  codeVue: `<elvia-slider
-  :min="1"
-  :max="100"
-  @value-on-change="updateValue($event.detail.value)"
-></elvia-slider>
-`,
-  codeNativeHTML: `<elvia-slider
-  min="1"
-  max="100"
-  id="example-elvia-slider"
-></elvia-slider>
-`,
-  codeNativeScript: `  const slider = document.getElementById('example-elvia-slider');
-  slider.addEventListener('valueOnChange', (event) => {
-    console.log('Elvia Slider value:',event.detail.value.min && event.detail.value.max ? event.detail.value.min+" and "+event.detail.value.max : event.detail.value);
-  });
-`,
 };
