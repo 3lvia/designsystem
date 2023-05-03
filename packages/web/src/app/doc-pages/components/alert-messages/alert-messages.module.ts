a059df7a5c7884d9e004a09b0c85cd0209a8d5b5
Trygve Scheline Urdahl <trur@computas.com>
2023-05-03 08:47:48 +0200

    chore(docs: alert): remove unused import

--- packages/web/src/app/doc-pages/components/alert-messages/alert-messages.module.ts
+++ packages/web/src/app/doc-pages/components/alert-messages/alert-messages.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertMessagesComponent } from './alert-messages.component';
 import { ComponentChangelogModule } from '../../../shared/component-documentation/component-changelog/component-changelog.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
@@ -41,7 +40,6 @@ import { AlertRoleNoteCegComponent } from './alert-role-note-ceg/alert-role-note
     CommonModule,
     CegModule,
     ComponentHeaderModule,
-    ComponentExampleV1Module,
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
