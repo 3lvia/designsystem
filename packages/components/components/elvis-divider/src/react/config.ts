40d23ed669b0380280c91aa59fa5a01ee6785c9a
Fride Skarseth <fs@computas.com>
2023-05-05 14:09:59 +0200

    BREAKING CHANGE(divider): Deprecated title prop and added isInverted to the deprecated config (#1479)
    BREAKING CHANGE(divider): Deprecated title prop and added isInverted to the deprecated config

--- packages/components/components/elvis-divider/src/react/config.ts
+++ packages/components/components/elvis-divider/src/react/config.ts
@@ -3,11 +3,21 @@ import { ComponentConfig } from '@elvia/elvis-toolbox';
 export const config: ComponentConfig = {
   name: 'Divider',
   attributes: [
-    { name: 'type', type: 'string' },
-    { name: 'title', type: 'string' },
+    { name: 'type', type: 'string' }, // type="title" DEPRECATED - Replaced by type="heading"
+    { name: 'heading', type: 'string' },
     { name: 'typography', type: 'string' },
     { name: 'orientation', type: 'string' },
     { name: 'className', type: 'string' },
     { name: 'inlineStyle', type: 'object' },
+    {
+      name: 'isInverted',
+      type: 'boolean',
+      deprecatedDetails: { version: '2.0.0', explanation: 'Will be replaced be dark theme.' },
+    },
+    {
+      name: 'title',
+      type: 'string',
+      deprecatedDetails: { version: '3.0.0', newProp: 'heading', isDirectReplacement: true },
+    },
   ],
 };
