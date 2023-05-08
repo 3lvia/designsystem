4d6a0ca0f6fb8513d875f1afd0d4a5cb3fd36a1b
Trygve Scheline Urdahl <trur@computas.com>
2023-05-08 09:56:01 +0200

    docs(search): use new ceg

--- packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.module.ts
+++ packages/web/src/app/doc-pages/components/forms/search-doc/search-doc.module.ts
@@ -9,13 +9,33 @@ import { ComponentSectionModule } from 'src/app/shared/component-documentation/c
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { SearchDocComponent } from './search-doc.component';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
+import { CegModule } from 'src/app/shared/component-documentation/ceg/ceg.module';
+import { SearchCegComponent } from './search-ceg/search-ceg.component';
+import { SearchInstantCegComponent } from './search-instant-ceg/search-instant-ceg.component';
+import { SearchInstantSearchedCegComponent } from './search-instant-searched-ceg/search-instant-searched-ceg.component';
+import { SearchOnSubmitCegComponent } from './search-on-submit-ceg/search-on-submit-ceg.component';
+import { SearchOnSubmitSearchedCegComponent } from './search-on-submit-searched-ceg/search-on-submit-searched-ceg.component';
+import { SearchFullwidthCegComponent } from './search-fullwidth-ceg/search-fullwidth-ceg.component';
+import { SearchSizeNormalCegComponent } from './search-size-normal-ceg/search-size-normal-ceg.component';
+import { SearchSizeCompactCegComponent } from './search-size-compact-ceg/search-size-compact-ceg.component';
 
 @NgModule({
-  declarations: [SearchDocComponent],
+  declarations: [
+    SearchDocComponent,
+    SearchCegComponent,
+    SearchInstantCegComponent,
+    SearchInstantSearchedCegComponent,
+    SearchOnSubmitCegComponent,
+    SearchOnSubmitSearchedCegComponent,
+    SearchFullwidthCegComponent,
+    SearchSizeNormalCegComponent,
+    SearchSizeCompactCegComponent,
+  ],
   imports: [
     CommonModule,
     ComponentHeaderModule,
     ComponentExampleV1Module,
+    CegModule,
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
