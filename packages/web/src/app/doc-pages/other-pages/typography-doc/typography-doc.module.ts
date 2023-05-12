73d51f81935c98d1c6c47aee77f6b377470bfb49
Trygve Scheline Urdahl <trur@computas.com>
2023-05-12 12:26:04 +0200

    docs(typography): remove unused import

--- packages/web/src/app/doc-pages/other-pages/typography-doc/typography-doc.module.ts
+++ packages/web/src/app/doc-pages/other-pages/typography-doc/typography-doc.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TypographyDocComponent } from './typography-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
-import { CopyModule } from 'src/app/shared/copy/copy.module';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
