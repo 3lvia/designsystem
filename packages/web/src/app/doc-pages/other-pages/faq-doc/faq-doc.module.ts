35f09d3d441c537423f2ff4026adfd96e55ce7db
Trygve Scheline Urdahl <trur@computas.com>
2023-05-16 11:53:53 +0200

    chore(web:faq): replace deprecated group accordions with components

--- packages/web/src/app/doc-pages/other-pages/faq-doc/faq-doc.module.ts
+++ packages/web/src/app/doc-pages/other-pages/faq-doc/faq-doc.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaqDocComponent } from './faq-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { RouterModule } from '@angular/router';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
+import '@elvia/elvis-accordion';
 
 @NgModule({
   imports: [CommonModule, ComponentHeaderModule, RouterModule, ComponentSubsubsectionModule],
   declarations: [FaqDocComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FaqDocModule {}
