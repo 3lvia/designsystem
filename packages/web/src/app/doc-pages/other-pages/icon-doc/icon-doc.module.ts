f054d108bb64e0579f07deac69f31f4c5021271d
Erik Tallang <et@computas.com>
2023-05-19 09:11:47 +0200

    docs(icon): remove import of old ceg modules

--- packages/web/src/app/doc-pages/other-pages/icon-doc/icon-doc.module.ts
+++ packages/web/src/app/doc-pages/other-pages/icon-doc/icon-doc.module.ts
@@ -2,12 +2,10 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IconDocComponent } from './icon-doc.component';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
 import { CopyModule } from 'src/app/shared/copy/copy.module';
 import { IconSearchPipe } from './icon-search.pipe';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
-import { ComponentExampleCodeModule } from 'src/app/shared/component-documentation/component-example/component-example-code/component-example-code.module';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
@@ -26,12 +24,10 @@ import { IconInvertedDisabledCegComponent } from './icon-inverted-disabled-ceg/i
 @NgModule({
   imports: [
     CommonModule,
-    ComponentExampleV1Module,
     FormsModule,
     ComponentHeaderModule,
     CopyModule,
     ComponentPropertiesModule,
-    ComponentExampleCodeModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
     ComponentSubsubsectionModule,
