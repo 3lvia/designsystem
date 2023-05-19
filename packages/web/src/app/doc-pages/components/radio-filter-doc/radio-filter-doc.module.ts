72ab44600b739a9687ee7d23f6e588871d5e0138
Erik Tallang <et@computas.com>
2023-05-19 09:21:42 +0200

    chore: remove unused references to ComponentExampleV1Module

--- packages/web/src/app/doc-pages/components/radio-filter-doc/radio-filter-doc.module.ts
+++ packages/web/src/app/doc-pages/components/radio-filter-doc/radio-filter-doc.module.ts
@@ -2,7 +2,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RadioFilterDocComponent } from './radio-filter-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
-import { ComponentExampleV1Module } from 'src/app/shared/component-documentation/component-example/component-example-v1/component-example-v1.module';
 import { ComponentPropertiesModule } from 'src/app/shared/component-documentation/component-properties/component-properties.module';
 import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
 import { WhenToUseModule } from 'src/app/shared/when-to-use/when-to-use.module';
@@ -18,7 +17,6 @@ import { RadioFilterIconCegComponent } from './radio-filter-icon-ceg/radio-filte
   imports: [
     CommonModule,
     ComponentHeaderModule,
-    ComponentExampleV1Module,
     ComponentPropertiesModule,
     ComponentSectionModule,
     ComponentSubsectionModule,
