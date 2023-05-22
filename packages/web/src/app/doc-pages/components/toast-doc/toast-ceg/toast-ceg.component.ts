abf8b77b27398ed6d277039645245c43ef81cba7
Erik Tallang <et@computas.com>
2023-05-22 10:29:15 +0200

    docs(toast): use CegModule

--- packages/web/src/app/doc-pages/components/toast-doc/toast-ceg/toast-ceg.component.ts
+++ packages/web/src/app/doc-pages/components/toast-doc/toast-ceg/toast-ceg.component.ts
@@ -1,53 +1,81 @@
 import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { openElviaToast } from '@elvia/elvis-toast';
+import { ToastConfig } from '@elvia/elvis-toast';
+import {
+  CegControlManager,
+  ComponentExample,
+  TypescriptComponentExample,
+} from 'src/app/shared/component-documentation/ceg';
 
 @Component({
   selector: 'app-toast-ceg',
   templateUrl: './toast-ceg.component.html',
-  styleUrls: ['./toast-ceg.component.scss'],
+  providers: [
+    { provide: ComponentExample, useExisting: ToastCegComponent },
+    { provide: TypescriptComponentExample, useExisting: ToastCegComponent },
+  ],
 })
-export class ToastCegComponent {
-  status: 'informative' | 'positive' = 'positive';
-  closable = false;
-  title = 'Short title';
-  body = 'A successful confirmation message.';
-  popoverIsOpen = false;
+export class ToastCegComponent extends TypescriptComponentExample implements ComponentExample {
+  elementName = 'toast';
+  toastConfig: Record<string, string | boolean | number | undefined>;
+  cegContent = new CegControlManager<ToastConfig>([
+    {
+      controls: {
+        status: {
+          type: 'radioGroup',
+          group: 'Status',
+          value: 'positive',
+          radios: [
+            { label: 'Positive', value: 'positive' },
+            { label: 'Informative', value: 'informative' },
+          ],
+          excludedFromDomProps: true,
+        },
+        closable: { type: 'switch', group: 'Options', label: 'Closable', excludedFromDomProps: true },
+        title: {
+          type: 'text',
+          group: 'Title',
+          label: 'Title',
+          value: 'Short title',
+          excludedFromDomProps: true,
+        },
+        body: {
+          type: 'text',
+          group: 'Body',
+          label: 'Body',
+          value: 'A successful confirmation message.',
+          excludedFromDomProps: true,
+        },
+      },
+      groupOrder: ['Status', 'Options', 'Title', 'Body'],
+    },
+  ]);
 
-  exampleInHTML = `<!-- Place this element at the root of your app -->
-<elvia-toast></elvia-toast>
-  
-<!-- Then trigger the toast e.g. from a button -->
-<button class="e-btn" (click)="showToast()">
-  Show example toast
-</button>
-`;
+  constructor() {
+    super();
 
-  get exampleInTS() {
-    return `import { openElviaToast } from '@elvia/elvis-toast';
+    this.cegContent
+      .getCurrentControls()
+      .pipe(takeUntilDestroyed())
+      .subscribe((controls) => {
+        if (controls) {
+          this.toastConfig = {};
+          Object.entries(controls).forEach(([controlName, control]) => {
+            const key = controlName as keyof ToastConfig;
+            this.toastConfig[key] = control.value;
+          });
 
-showToast() {
-  openElviaToast({
-    title: '${this.title}',
-    body: '${this.body}',
-    duration: 5000,
-    closable: ${this.closable},
-    status: '${this.status}'
-  });
-}`;
+          this.setTypeScript(`import { openElviaToast } from '@elvia/elvis-toast';
+
+          export const showToast = () => {
+            openElviaToast(${JSON.stringify(this.toastConfig)});
+          }`);
+        }
+      });
   }
 
-  showToast() {
-    openElviaToast({
-      title: this.title,
-      body: this.body,
-      duration: 5000,
-      closable: this.closable,
-      status: this.status,
-    });
-  }
-
-  resetText() {
-    this.title = 'Short title';
-    this.body = 'A successful confirmation message.';
+  showToast(): void {
+    openElviaToast(this.toastConfig);
   }
 }
