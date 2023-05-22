a7a18186cfe5ff905151088334458cbc9165b774
Trygve Scheline Urdahl <trur@computas.com>
2023-05-22 10:39:59 +0200

    chore(docs: get started): remove deprecated accordions

--- packages/web/src/app/doc-pages/other-pages/get-started-doc/get-started-doc.module.ts
+++ packages/web/src/app/doc-pages/other-pages/get-started-doc/get-started-doc.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GetStartedDocComponent } from './get-started-doc.component';
 import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
@@ -8,6 +8,8 @@ import { ComponentSectionModule } from 'src/app/shared/component-documentation/c
 import { RouterModule } from '@angular/router';
 import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
 import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
+import '@elvia/elvis-accordion';
+import '@elvia/elvis-divider';
 
 @NgModule({
   imports: [
@@ -21,5 +23,6 @@ import { ComponentSubsubsectionModule } from 'src/app/shared/component-documenta
     RouterModule,
   ],
   declarations: [GetStartedDocComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class GetStartedDocModule {}
