d4f187c1b830f7bc6a098975c39af4f56bb7df86
Trygve Scheline Urdahl <trur@computas.com>
2023-05-23 13:29:56 +0200

    fix

--- packages/elvis-colors/src/index.ts
+++ packages/elvis-colors/src/index.ts
@@ -1,6 +1,4 @@
-export { getThemeColor, getThemeColorContrast, getCustomThemeColor, getBaseColor } from './getThemeColor';
 export * from './getThemeColor';
-export { ThemeName, ThemeClassName, ColorLabel } from './theme';
 export * from './theme';
 
 export { shadows, getShadow } from './shadows';
