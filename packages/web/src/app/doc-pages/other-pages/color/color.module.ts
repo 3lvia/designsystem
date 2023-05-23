cbbf92dfe399885abba8e6a6bf9f9ef852fce3e4
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-05-23 15:58:15 +0200

    Lego 2571: feat(web:color): add minimal skeleton for new color doc page (#1520)
    * feat(web:color): add minimal skeleton for new color doc page
    
    * chore(web:color): rename module from color2 to color

---
+++ packages/web/src/app/doc-pages/other-pages/color/color.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ColorComponent } from './color.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { ColorTokenTableComponent } from './color-token-table/color-token-table.component';
+import { ComponentHeaderModule } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.module';
+import { ComponentSectionModule } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.module';
+import { ComponentSubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.module';
+import { ComponentSubsubsectionModule } from 'src/app/shared/component-documentation/component-structure/component-subsubsection/component-subsubsection.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ComponentHeaderModule,
+    ComponentSectionModule,
+    ComponentSubsectionModule,
+    ComponentSubsubsectionModule,
+  ],
+  declarations: [ColorComponent, ColorPickerComponent, ColorTokenTableComponent],
+})
+export class ColorModule {}
--- packages/web/src/app/doc-pages/other-pages/color2/color2.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Color2Component } from './color2.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [Color2Component],
-})
-export class Color2Module {}
