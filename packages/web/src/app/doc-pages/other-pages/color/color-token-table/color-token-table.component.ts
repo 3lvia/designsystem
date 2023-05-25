0e6ed9f177f38e657cd7f69b17287d0ae15aa626
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-05-25 09:58:30 +0200

    Lego 2574: feat(web:color): define data structure for label table (#1523)
    * feat(web:color): define data structure for label table

--- packages/web/src/app/doc-pages/other-pages/color/color-token-table/color-token-table.component.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color-token-table/color-token-table.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  TableColorArray,
+  backgroundColorsDefault,
+  backgroundColorsElement,
+  backgroundColorsOverlay,
+  backgroundColorsStates,
+  textColorsDefault,
+  textColorsState,
+} from './colors';
 
 @Component({
   selector: 'app-color-token-table',
   templateUrl: './color-token-table.component.html',
   styleUrls: ['./color-token-table.component.scss'],
 })
-export class ColorTokenTableComponent {}
+export class ColorTokenTableComponent {
+  textColorsDefault = textColorsDefault;
+  textColorsState = textColorsState;
+  backgroundColorsDefault = backgroundColorsDefault;
+  backgroundColorsElement = backgroundColorsElement;
+  backgroundColorsOverlay = backgroundColorsOverlay;
+  backgroundColorsState = backgroundColorsStates;
+
+  /** Hack to fix type in ng-template context, don't do this in prod (zone re-renders) */
+  asColorArray = (array: unknown): TableColorArray => {
+    return array as any;
+  };
+}
