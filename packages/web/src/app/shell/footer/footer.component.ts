bb7a3c5104ccc843f9ed3a773fb8f2ccb930df47
Trygve Scheline Urdahl <trur@computas.com>
2023-05-31 10:48:54 +0200

    chore(web): remove comments

--- packages/web/src/app/shell/footer/footer.component.ts
+++ packages/web/src/app/shell/footer/footer.component.ts
@@ -14,13 +14,9 @@ export class FooterComponent implements OnInit {
   currentYear: number;
 
   constructor(private router: Router) {
-    // subscribe to router navigation
     this.router.events.pipe(takeUntilDestroyed()).subscribe((event) => {
-      // filter `NavigationEnd` events
       if (event instanceof NavigationEnd) {
-        // get current route without leading slash `/`
         const eventUrl = event.urlAfterRedirects;
-        // set bgClass property with the value of the current route
         if (eventUrl === '/not-found') {
           this.bgClass = 'not-found';
         } else {
