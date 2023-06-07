1ee024aba75de28cb8b28e5b755f9b29e2b8c0f9
majabnils <mani@computas.com>
2023-06-07 13:21:03 +0200

    docs(color): trying out formula for constrasts

--- packages/web/src/app/doc-pages/other-pages/color/color-picker/colors-dark.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color-picker/colors-dark.ts
@@ -8,34 +8,33 @@ import {
   IconColors as IconLabels,
   ThemeName,
   DarkThemeColorName,
-  LightThemeColorName,
   getThemeColor,
-  lightThemeColors,
-  lightTheme,
   getBaseColor,
+  darkThemeColors,
+  darkTheme,
 } from '@elvia/elvis-colors';
 
-type LightThemeColors = typeof lightThemeColors;
-type PrimaryColors = LightThemeColors['primary-colors'];
+type DarkThemeColors = typeof darkThemeColors;
+type PrimaryColors = DarkThemeColors['primary-colors'];
 type PrimaryColor = keyof PrimaryColors;
-const primaryColors = Object.keys(lightThemeColors['primary-colors']) as PrimaryColor[];
+const primaryColors = Object.keys(darkThemeColors['primary-colors']) as PrimaryColor[];
 
-type SignalColors = LightThemeColors['signal-colors'];
+type SignalColors = DarkThemeColors['signal-colors'];
 type SignalColor = keyof SignalColors;
-const signalColors = Object.keys(lightThemeColors['signal-colors']) as SignalColor[];
+const signalColors = Object.keys(darkThemeColors['signal-colors']) as SignalColor[];
 
-type DataColors = LightThemeColors['data-colors'];
+type DataColors = DarkThemeColors['data-colors'];
 type DataColor = keyof DataColors;
-const dataColors = Object.keys(lightThemeColors['data-colors']) as DataColor[];
+const dataColors = Object.keys(darkThemeColors['data-colors']) as DataColor[];
 
-type GreyColors = LightThemeColors['grey-colors'];
+type GreyColors = DarkThemeColors['grey-colors'];
 type GreyColor = keyof GreyColors;
-const greyColors = Object.keys(lightThemeColors['grey-colors']) as GreyColor[];
+const greyColors = Object.keys(darkThemeColors['grey-colors']) as GreyColor[];
 
 interface ColorElement {
   name: string;
   hex: string;
-  contrast: { white: '' | 'AA' | 'AAA'; black: '' | 'AA' | 'AAA' };
+  contrast: { white: false | 'AA' | 'AAA'; black: false | 'AA' | 'AAA' };
   token: ColorLabel[];
 }
 
@@ -48,7 +47,7 @@ interface ColorTypes {
 
 const getTokens = (hex: string) => {
   const foundLabels: ColorLabel[] = [];
-  Object.values(lightTheme).forEach((category) =>
+  Object.values(darkTheme).forEach((category) =>
     Object.entries(category).forEach(([colorName, label]) => {
       if (label.hex === hex) {
         foundLabels.push(colorName as ColorLabel);
@@ -57,6 +56,14 @@ const getTokens = (hex: string) => {
   );
   return foundLabels;
 };
+
+const getColorElement = (name: DarkThemeColorName) =>
+  <ColorElement>{
+    name: name,
+    hex: getBaseColor(name),
+    contrast: { white: false, black: 'AA' },
+    token: getTokens(getBaseColor(name)),
+  };
 
 const contrasts = {
   'primary-colors': {
@@ -213,64 +220,7 @@ const contrasts = {
   },
 };
 
-const hexToRgb = (hex: string): number[] | null => {
-  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  hex = makeHexValue6Length(hex);
-
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  const rgb = result ? [parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)] : null;
-  return rgb;
-};
-
-const makeHexValue6Length = (hex: string): string => {
-  if (hex.length === 4) {
-    hex = `#${hex.charAt(1)}${hex.charAt(1)}${hex.charAt(2)}${hex.charAt(2)}${hex.charAt(3)}${hex.charAt(3)}`;
-  }
-  return hex.toUpperCase();
-};
-
-const getLuminanace = (values: number[]) => {
-  const rgb = values.map((v) => {
-    const val = v / 255;
-    return val <= 0.03928 ? val / 12.92 : ((val + 0.055) / 1.055) ** 2.4;
-  });
-  return Number((0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]).toFixed(3));
-};
-
-const getContrastRatio = (colorA: number[], colorB: number[]) => {
-  const lumA = getLuminanace(colorA);
-  const lumB = getLuminanace(colorB);
-
-  return (Math.max(lumA, lumB) + 0.05) / (Math.min(lumA, lumB) + 0.05);
-};
-const greenRgb = hexToRgb(lightThemeColors['primary-colors'].green.color);
-const blackRgb = hexToRgb(lightThemeColors['primary-colors'].black.color);
-if (greenRgb && blackRgb) {
-  const cont = getContrastRatio(greenRgb, blackRgb);
-  console.log();
-}
-
-const getContrastValues = (color1: LightThemeColorName, color2: LightThemeColorName) => {
-  const colorRgb1 = hexToRgb(getBaseColor(color1));
-  const colorRgb2 = hexToRgb(getBaseColor(color2));
-  if (colorRgb1 && colorRgb2) {
-    const contrast = getContrastRatio(colorRgb1, colorRgb2);
-  }
-};
-
-type colorType = keyof typeof contrasts;
-const whiteColor = 'white';
-const ex = contrasts['primary-colors'][whiteColor].contrasts;
-
-const getColorElement = (name: LightThemeColorName) =>
-  <ColorElement>{
-    name: name,
-    hex: getBaseColor(name),
-    contrast: { white: '', black: '' },
-    token: getTokens(getBaseColor(name)),
-  };
-
-export const lightColors: ColorTypes = {
+export const darkColors: ColorTypes = {
   primary: primaryColors.map((colorName) => getColorElement(colorName)),
   signal: signalColors.map((colorName) => getColorElement(colorName)),
   data: dataColors.map((colorName) => getColorElement(colorName)),
