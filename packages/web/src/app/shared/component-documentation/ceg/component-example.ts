a3236398cd73172288a3461caed6a9d899f82acd
Erik Tallang <e.tallang@gmail.com>
2023-06-12 16:33:36 +0200

    feat(ceg): add prop for defining custom react slots (#1573)
    Co-authored-by: Erik Tallang <et@computas.com>

--- packages/web/src/app/shared/component-documentation/ceg/component-example.ts
+++ packages/web/src/app/shared/component-documentation/ceg/component-example.ts
@@ -12,4 +12,10 @@ export abstract class ComponentExample<T extends Record<string, any> = any> {
    * The CEG configuration.
    */
   readonly cegContent: CegControlManager<T>;
+
+  /**
+   * Manual replacement of slots for React. This is only necessary in rare cases where
+   * the DOM differs between the React-component and the web-component.
+   */
+  readonly reactSlotReplacement?: Partial<Record<keyof T, string>>;
 }
