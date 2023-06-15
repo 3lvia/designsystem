141f1824d09727df56c81e8e142e8f946a939f3a
majabnils <mani@computas.com>
2023-06-15 22:12:58 +0200

    docs(color): make visualization for color list and animations, needs to correct checkmark color

--- packages/web/src/app/doc-pages/other-pages/color/color-picker/color-picker.component.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color-picker/color-picker.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { lightColors } from './colors-light';
 import { darkColors } from './colors-dark';
+import { ColorElement } from './colors-types';
 
 @Component({
   selector: 'app-color-picker',
@@ -9,10 +10,25 @@ import { darkColors } from './colors-dark';
 })
 export class ColorPickerComponent {
   isDarkTheme = false;
+  colorList = this.isDarkTheme ? darkColors : lightColors;
+  currentColor = this.colorList.primary[0];
   toggleTheme = () => {
     document.getElementsByClassName('theme-container')[0].classList.toggle('e-theme-dark');
     this.isDarkTheme = !this.isDarkTheme;
+    this.colorList = this.isDarkTheme ? darkColors : lightColors;
   };
 
-  colorList = this.isDarkTheme ? darkColors : lightColors;
+  getColorsFromCategory = (category: 'primary' | 'signal' | 'data' | 'grey') => {
+    return this.colorList?.[category];
+  };
+
+  needsBorder = (color: ColorElement) => {
+    return (
+      !this.isDarkTheme && (color.name === 'white' || color.name === 'grey-02' || color.name === 'grey-05')
+    );
+  };
+
+  chooseColor = (color: ColorElement) => {
+    this.currentColor = color;
+  };
 }
