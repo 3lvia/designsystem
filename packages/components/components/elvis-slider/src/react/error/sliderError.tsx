dca15cb582f07240159eec5fd73cd097b086bbc4
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-06-16 10:47:42 +0200

    Create sliderError.tsx

---
+++ packages/components/components/elvis-slider/src/react/error/sliderError.tsx
@@ -0,0 +1,31 @@
+import { removeCircle } from '@elvia/elvis-assets-icons';
+import { FormFieldError, FormFieldErrorContainer, IconWrapper } from '@elvia/elvis-toolbox';
+import React, { useEffect, useState } from 'react';
+import { BothSliders, ErrorOptions, ErrorType } from '../elvia-slider.types';
+import { getErrorOptionsText, getInternalErrorText } from '../utils/getError';
+
+interface Props {
+  errorType?: Partial<BothSliders<ErrorType>>;
+  errorOptions?: ErrorOptions;
+}
+
+export const SliderError: React.FC<Props> = ({ errorType, errorOptions }) => {
+  const [errorText, setErrorText] = useState('');
+
+  useEffect(() => {
+    if (errorOptions) {
+      setErrorText(getErrorOptionsText(errorOptions));
+    } else if (errorType) {
+      setErrorText(getInternalErrorText(errorType));
+    } else {
+      setErrorText('');
+    }
+  }, [errorType, errorOptions]);
+
+  return (
+    <FormFieldErrorContainer>
+      <IconWrapper icon={removeCircle} color={'error'} size="xs" />
+      <FormFieldError data-testid="error">{errorText}</FormFieldError>
+    </FormFieldErrorContainer>
+  );
+};
