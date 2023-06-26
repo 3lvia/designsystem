421f29fb0309656996d28aa8fa11820ee8ddf21f
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-06-26 11:16:52 +0200

    feat(color): add 188n + usage

--- packages/web/src/app/doc-pages/other-pages/color/color.component.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { LOCALE_CODE } from 'contentful/types';
+import { LocalizationService, Locale } from 'src/app/core/services/localization.service';
+import { Title } from '@angular/platform-browser';
+import { getDocPagesNotFromCMS } from 'src/app/shared/doc-pages';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import '@elvia/elvis-accordion';
 import '@elvia/elvis-divider';
 
 @Component({
   selector: 'app-color',
   templateUrl: './color.component.html',
+  styleUrls: ['./color.component.scss'],
 })
-export class ColorComponent {}
+export class ColorComponent {
+  description = getDocPagesNotFromCMS('color')?.description;
+  descriptionNo = getDocPagesNotFromCMS('color')?.descriptionNo;
+  locale: LOCALE_CODE = 'en-GB';
+  title = getDocPagesNotFromCMS('color')?.title;
+  titleNo = getDocPagesNotFromCMS('color')?.titleNo;
+
+  constructor(private titleService: Title, private localizationService: LocalizationService) {
+    this.setTabTitle();
+    this.localizationService
+      .listenLocalization()
+      .pipe(takeUntilDestroyed())
+      .subscribe((locale) => {
+        this.locale = locale === Locale['en-GB'] ? 'en-GB' : 'nb-NO';
+        this.setTabTitle();
+      });
+  }
+
+  setTabTitle = (): void => {
+    this.titleService.setTitle(
+      (this.locale === 'nb-NO' && this.titleNo ? this.titleNo : this.title) + ' | Elvia design system',
+    );
+  };
+}
