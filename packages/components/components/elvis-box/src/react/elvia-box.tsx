9b10b7901576c59140f49ef04401a2f117134524
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-06-26 12:50:52 +0200

    BREAKING(box): replace title with heading

--- packages/components/components/elvis-box/src/react/elvia-box.tsx
+++ packages/components/components/elvis-box/src/react/elvia-box.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
-import { BoxArea, BoxColoredLine, BoxTitle, BoxContent } from './styledComponents';
+import { BoxArea, BoxColoredLine, BoxHeading, BoxContent } from './styledComponents';
 import { useSlot, warnDeprecatedProps } from '@elvia/elvis-toolbox';
 import { BoxProps } from './elvia-box.types';
 import { config } from './config';
 
 const Box: FC<BoxProps> = function ({
+  heading,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   title,
   content,
   isColored = false,
@@ -16,13 +18,11 @@ const Box: FC<BoxProps> = function ({
   warnDeprecatedProps(config, arguments[0]);
 
   const { ref: boxContent } = useSlot<HTMLDivElement>('content', webcomponent);
-  const { ref: boxTitle } = useSlot<HTMLDivElement>('title', webcomponent);
+  const { ref: boxHeading } = useSlot<HTMLDivElement>('heading', webcomponent);
 
   return (
     <BoxArea className={className} style={inlineStyle} {...rest} data-testid="box-area">
-      <BoxTitle ref={boxTitle} data-testid="box-title">
-        {title}
-      </BoxTitle>
+      <BoxHeading ref={boxHeading}>{heading}</BoxHeading>
       <BoxContent data-testid="box-content">
         {isColored && <BoxColoredLine data-testid="box-colored-line"></BoxColoredLine>}
         {content ? content : <div ref={boxContent}></div>}
--- packages/components/components/elvis-box/src/react/elvia-box.types.ts
+++ packages/components/components/elvis-box/src/react/elvia-box.types.ts
@@ -1,6 +1,9 @@
 import { BaseProps } from '@elvia/elvis-toolbox';
 
 export interface BoxProps extends BaseProps {
+  /**
+   * @deprecated Deprecated in version 2.0.0. Use the `heading` prop instead.
+   */
   title?: string | JSX.Element;
   content: string | JSX.Element;
   isColored?: boolean;
@@ -8,4 +11,5 @@ export interface BoxProps extends BaseProps {
    * @deprecated Deprecated in version 1.5.0. Box now has the same border on all backgrounds. No replacement needed.
    */
   hasBorder?: boolean;
+  heading?: string | JSX.Element;
 }
