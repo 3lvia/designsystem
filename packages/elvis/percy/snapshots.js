a9da5add58c56f3ba4b668764ca7127a2c36a047
Trygve Scheline Urdahl <trur@computas.com>
2023-06-29 11:49:01 +0200

    test(tag): add dark theme percy test

--- packages/elvis/percy/snapshots.js
+++ packages/elvis/percy/snapshots.js
@@ -175,6 +175,14 @@ module.exports = [
     name: 'Tag',
     url: '/components/tag.html',
     widths: [766, 1440],
+    additionalSnapshots: [
+      {
+        suffix: ' - dark theme',
+        execute() {
+          document.body.classList.add('e-theme-dark');
+        },
+      },
+    ],
   },
   {
     name: 'Toggle',
