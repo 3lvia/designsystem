f6843751b730361257b0cecaf2f75b4be78ec155
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-07-03 11:24:52 +0200

    Update home.module.ts

--- packages/web/src/app/shell/home/home.module.ts
+++ packages/web/src/app/shell/home/home.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 import { ComponentChangelogModule } from 'src/app/shared/component-documentation/component-changelog/component-changelog.module';
+import { ShortcutComponent } from './shortcut/shortcut.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, ComponentChangelogModule],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, ShortcutComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule {}
