a63ad12668d9b32513cad82c75c9052e9f7f9d5d
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-07-03 15:51:53 +0200

    Revert "test(datepicker-range): add hasAutoOpenEndDatepicker test"
    This reverts commit 0c0b03ec11f07003cf5b8221b128803c45fe4514.

--- packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.test.tsx
+++ packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import DatepickerRange from './elvia-datepicker-range';
 import { axe } from 'jest-axe';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 
 describe('Elvis DatepickerRange', () => {
   describe('Default', () => {
@@ -42,36 +41,6 @@ describe('Elvis DatepickerRange', () => {
     });
   });
 
-  describe('with hasAutoOpenEndDatepicker', () => {
-    beforeEach(() => {
-      render(
-        <DatepickerRange
-          hasAutoOpenEndDatepicker
-          value={{
-            start: new Date('2023-01-01'),
-            end: null,
-          }}
-        />,
-      );
-    });
-
-    it('should open the end Date Picker after the start Date Picker is closed', async () => {
-      const user = userEvent.setup();
-      const today = new Date();
-      const formattedToday = today.toLocaleString('nb-NO', {
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-      });
-
-      await user.click(screen.getAllByRole('button', { name: /åpne datovelger/i })[0]);
-      await user.click(screen.getByRole('button', { name: /^4. januar 2023$/i }));
-
-      expect(screen.queryByTestId('popover')).toBeInTheDocument();
-      await waitFor(() => expect(screen.getByLabelText('Til dato')).toHaveValue(formattedToday));
-    });
-  });
-
   describe('className and inlineStyle passed to wrapper', () => {
     beforeEach(() => {
       render(<DatepickerRange className="testclass" inlineStyle={{ paddingTop: '24px' }}></DatepickerRange>);
@@ -106,7 +75,7 @@ describe('Elvis DatepickerRange', () => {
         expect(datepicker).toHaveStyle('width: 100%');
       });
     });
-    it('should have both date pickers size="small"', () => {
+    it('should have both date pickers compact', () => {
       const datePickers = screen.getAllByTestId('wrapper');
       datePickers.forEach((datepicker) => {
         expect(datepicker).toHaveStyle('padding-top: 0.5rem');
