f3fe0554cef50799f6c9e0f66b49c66fd903aedf
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-07-06 16:06:56 +0200

    feat(color-picker): add "color-picker-exhibit"-component

--- packages/web/src/app/doc-pages/other-pages/color/color.module.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color.module.ts
@@ -18,6 +18,7 @@ import { IllustrationBaseDirective } from './illustrations/illustration-base.dir
 import { ColorListBaseDirective } from './color-picker/color-list-base.directive';
 import { DoDontModule } from 'src/app/shared/do-dont/do-dont.module';
 import { DoDontTextModule } from 'src/app/shared/do-dont-text/do-dont-text.module';
+import { ColorPickerExhibitComponent } from './color-picker/color-picker-exhibit/color-picker-exhibit.component';
 
 @NgModule({
   imports: [
@@ -43,6 +44,7 @@ import { DoDontTextModule } from 'src/app/shared/do-dont-text/do-dont-text.modul
     ThemeTokenIllustrationComponent,
     IllustrationBaseDirective,
     ColorListBaseDirective,
+    ColorPickerExhibitComponent,
   ],
 })
 export class ColorModule {}
