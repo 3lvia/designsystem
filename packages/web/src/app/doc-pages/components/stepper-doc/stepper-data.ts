2bd9e3e0607d046ef33ddcedf97d744b9138ed3a
majabnils <mani@computas.com>
2023-07-18 12:03:18 +0200

    docs(stepper): update imports

--- packages/web/src/app/doc-pages/components/stepper-doc/stepper-data.ts
+++ packages/web/src/app/doc-pages/components/stepper-doc/stepper-data.ts
@@ -1,4 +1,4 @@
-import changelogJson from 'src/assets/changelogs/elvis-stepper/CHANGELOG.json';
+import changelogJson from '@elvia/elvis-stepper/CHANGELOG.json';
 import ComponentData from '../component-data.interface';
 
 export const stepperData: ComponentData = {
