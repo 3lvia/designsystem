c77a81f53ae83426fc5496f1751891430e2a05dd
Trygve Scheline Urdahl <trur@computas.com>
2023-07-19 08:24:23 +0200

    test: check out signals in web

--- packages/web/src/app/doc-pages/other-pages/color/color-token-table/color-token-subtable/color-token-subtable.component.ts
+++ packages/web/src/app/doc-pages/other-pages/color/color-token-table/color-token-subtable/color-token-subtable.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, computed, signal, effect } from '@angular/core';
 import { TableColorArray } from '../colors';
 
 type Colors = {
@@ -16,58 +16,70 @@ export class ColorTokenSubtableComponent implements OnInit {
   @Input({ required: true }) colors: Colors;
   @Input({ required: true }) title: string;
   @Input({ required: true }) subtitle: string;
+
+  private _searchValue = signal('');
   @Input({ required: true }) set searchValue(value: string) {
-    this.updateFilter(value);
+    this._searchValue.set(value);
   }
 
-  isVisible = true;
-  isOpen = false;
-  hasExampleColumn = false;
-  hasRoleColumn = false;
-  visibleColors: Colors;
+  readonly isVisible = signal(true);
+  readonly isOpen = signal(false);
+  readonly hasExampleColumn = signal(false);
+  readonly hasRoleColumn = signal(false);
+
+  readonly visibleColors = computed(() => this.updateFilter(this._searchValue()));
+
+  constructor() {
+    effect(
+      () => {
+        if (this._searchValue()) {
+          if (this.visibleColors().some((entry) => entry.colors.length)) {
+            this.isVisible.set(true);
+            this.isOpen.set(true);
+          } else {
+            this.isVisible.set(false);
+            this.isOpen.set(false);
+          }
+        } else {
+          this.isVisible.set(true);
+          this.isOpen.set(false);
+        }
+      },
+      { allowSignalWrites: true },
+    );
+  }
 
   ngOnInit(): void {
     this.colors.forEach((category) => {
       category.colors.forEach((color) => {
         if (typeof color.example !== 'undefined') {
-          this.hasExampleColumn = true;
+          this.hasExampleColumn.set(true);
         }
         if (typeof color.role !== 'undefined') {
-          this.hasRoleColumn = true;
+          this.hasRoleColumn.set(true);
         }
       });
     });
-    this.visibleColors = this.colors;
   }
 
   private updateFilter(value: string) {
     const parsedValue = value.trim().toLowerCase();
-    if (value) {
-      this.visibleColors = this.colors.map((entry) => {
-        if (entry.title?.toLowerCase().includes(parsedValue)) {
-          return entry;
-        } else {
-          const newColors = entry.colors.filter((color) =>
-            JSON.stringify(color).toLowerCase().includes(parsedValue),
-          );
-          return {
-            title: newColors.length > 0 ? entry.title : undefined,
-            description: newColors.length > 0 ? entry.description : undefined,
-            colors: newColors,
-          };
-        }
-      });
-      if (this.visibleColors.some((entry) => entry.colors.length)) {
-        this.isOpen = true;
-        this.isVisible = true;
-      } else {
-        this.isOpen = false;
-        this.isVisible = false;
-      }
-    } else {
-      this.visibleColors = this.colors;
-      this.isVisible = true;
-      this.isOpen = false;
+    if (!parsedValue) {
+      return this.colors;
     }
+    return this.colors.map((entry) => {
+      if (entry.title?.toLowerCase().includes(parsedValue)) {
+        return entry;
+      } else {
+        const newColors = entry.colors.filter((color) =>
+          JSON.stringify(color).toLowerCase().includes(parsedValue),
+        );
+        return {
+          title: newColors.length > 0 ? entry.title : undefined,
+          description: newColors.length > 0 ? entry.description : undefined,
+          colors: newColors,
+        };
+      }
+    });
   }
 }
