f9c7ed4546a4b1ab729e057237e13121e42701bc
Trygve Scheline Urdahl <trur@computas.com>
2023-07-20 12:34:15 +0200

    fix(web): what's new, fix getdocPagesNotFromCMS

--- packages/web/src/app/doc-pages/about/changelog/changelog.component.ts
+++ packages/web/src/app/doc-pages/about/changelog/changelog.component.ts
@@ -9,9 +9,9 @@ import changelogJson from '@elvia/elvis/CHANGELOG.json';
   styleUrls: ['./changelog.component.scss'],
 })
 export class ChangelogComponent {
-  description = getDocPagesNotFromCMS('changelog')?.description;
-  title = getDocPagesNotFromCMS('changelog')?.title;
-  externalUrl = getDocPagesNotFromCMS('changelog')?.externalUrl;
+  description = getDocPagesNotFromCMS('whats-new')?.description;
+  title = getDocPagesNotFromCMS('whats-new')?.title;
+  externalUrl = getDocPagesNotFromCMS('whats-new')?.externalUrl;
   changelog = changelogJson.content;
 
   constructor(private titleService: Title) {
