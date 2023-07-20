7769e3f08cbfb7a12da19d1315d52c6396ec269d
majabnils <mani@computas.com>
2023-07-20 09:35:39 +0200

    update modules

--- packages/web/src/app/shared/component-documentation/front-page-changelog/front-page-changelog.module.ts
+++ packages/web/src/app/shared/component-documentation/front-page-changelog/front-page-changelog.module.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { FrontPageChangelogComponent } from './front-page-changelog.component';
-import { ChangelogTypePipe } from './front-page-changelog-pipe';
-import { ChangelogIdPipe } from './front-page-changelog-id-pipe';
 import { ComponentDocumentationDatePipe } from '../component-documentation-date-pipe';
 import '@elvia/elvis-accordion';
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule, ComponentDocumentationDatePipe],
-  declarations: [FrontPageChangelogComponent, ChangelogTypePipe, ChangelogIdPipe],
-  exports: [FrontPageChangelogComponent, ChangelogTypePipe, ChangelogIdPipe],
+  declarations: [FrontPageChangelogComponent],
+  exports: [FrontPageChangelogComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FrontPageChangelogModule {}
