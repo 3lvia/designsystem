d00dd212b5ec92be2cd0dd10d0abb16f3e169cab
majabnils <mani@computas.com>
2023-07-24 14:36:43 +0200

    remove unused code

--- packages/web/src/app/shared/component-documentation/front-page-changelog/front-page-changelog.module.ts
+++ packages/web/src/app/shared/component-documentation/front-page-changelog/front-page-changelog.module.ts
@@ -1,16 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { FrontPageChangelogComponent } from './front-page-changelog.component';
-import { ComponentDocumentationDatePipe } from '../component-documentation-date-pipe';
-import '@elvia/elvis-accordion';
 import { FrontPageChangelogDatePipe } from './front-page-changelog-date-pipe';
 import { FrontPageChangelogNamePipe } from './front-page-changelog-name-pipe';
 import { FrontPageChangelogUrlPipe } from './front-page-changelog-url-pipe';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule, ComponentDocumentationDatePipe],
+  imports: [CommonModule, RouterModule],
   declarations: [
     FrontPageChangelogComponent,
     FrontPageChangelogDatePipe,
