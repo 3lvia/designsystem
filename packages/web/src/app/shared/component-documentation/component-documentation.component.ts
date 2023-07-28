881aa607fa65e909de1923c13e7e850282fb08c9
majabnils <mani@computas.com>
2023-07-28 14:45:07 +0200

    remove props and refactor

--- packages/web/src/app/shared/component-documentation/component-documentation.component.ts
+++ packages/web/src/app/shared/component-documentation/component-documentation.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input, booleanAttribute } from '@angular/core';
+import { Component, Input, OnInit, booleanAttribute } from '@angular/core';
 import ComponentData from 'src/app/doc-pages/components/component-data.interface';
 import data from '@elvia/elvis/.internal/classlist.json';
+import { getComponent } from 'src/app/shared/doc-pages';
 
 @Component({
   selector: 'app-component-documentation',
   templateUrl: './component-documentation.component.html',
   styleUrls: ['./component-documentation.component.scss'],
 })
-export class ComponentDocumentationComponent {
-  @Input() title: string;
-  @Input() description: string;
-  @Input() figmaUrl: string;
+export class ComponentDocumentationComponent implements OnInit {
   @Input({ transform: booleanAttribute }) isElvis = false;
   @Input() componentData: ComponentData | undefined;
-  @Input() elvisTitle: keyof typeof data.block | undefined;
+  @Input() elvisTitle: string | undefined;
+
+  title: string | undefined;
+  description: string | undefined;
+  figmaUrl: string | undefined;
+  elvisClassName: keyof typeof data.block | undefined;
+
+  ngOnInit() {
+    if (this.isElvis && this.elvisTitle) {
+      this.figmaUrl = getComponent(this.elvisTitle)?.figmaUrl;
+      this.description = getComponent(this.elvisTitle)?.description;
+      this.title = getComponent(this.elvisTitle)?.title;
+      this.elvisClassName = ('e-' + this.elvisTitle) as keyof typeof data.block;
+    } else if (!this.isElvis && this.componentData) {
+      console.log(this.componentData.name);
+      this.figmaUrl = getComponent(this.componentData.name.toLowerCase())?.figmaUrl;
+      this.description = getComponent(this.componentData.name.toLowerCase())?.description;
+      this.title = getComponent(this.componentData.name.toLowerCase())?.title;
+    }
+  }
 }
