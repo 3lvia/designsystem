48b14a43c53ab56ffb681eafa78f5fafda1e06c7
majabnils <mani@computas.com>
2023-08-01 15:50:21 +0200

    add type to getcomponent input

--- packages/web/src/app/shared/component-documentation/component-documentation.component.ts
+++ packages/web/src/app/shared/component-documentation/component-documentation.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, booleanAttribute } from '@angular/core';
 import ComponentData from 'src/app/doc-pages/components/component-data.interface';
 import data from '@elvia/elvis/.internal/classlist.json';
 import { getComponent } from 'src/app/shared/doc-pages';
+import { DocPageName } from '../shared.enum';
 
 @Component({
   selector: 'app-component-documentation',
@@ -10,7 +11,7 @@ import { getComponent } from 'src/app/shared/doc-pages';
 export class ComponentDocumentationComponent implements OnInit {
   @Input({ transform: booleanAttribute }) isElvis = false;
   @Input() componentData: ComponentData | undefined;
-  @Input() elvisTitle: string | undefined;
+  @Input() elvisTitle: DocPageName | undefined;
 
   title: string | undefined;
   description: string | undefined;
@@ -24,9 +25,9 @@ export class ComponentDocumentationComponent implements OnInit {
       this.title = getComponent(this.elvisTitle)?.title;
       this.elvisClassName = ('e-' + this.elvisTitle) as keyof typeof data.block;
     } else if (!this.isElvis && this.componentData) {
-      this.figmaUrl = getComponent(this.componentData.name.toLowerCase())?.figmaUrl;
-      this.description = getComponent(this.componentData.name.toLowerCase())?.description;
-      this.title = getComponent(this.componentData.name.toLowerCase())?.title;
+      this.figmaUrl = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.figmaUrl;
+      this.description = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.description;
+      this.title = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.title;
     }
   }
 }
