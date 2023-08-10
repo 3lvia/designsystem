c5a708c51ead4481ce17decbc35ab60720f9063f
Erik Tallang <et@computas.com>
2023-08-10 10:57:41 +0200

    refactor(app): move breadcrumbs on mobile from component-header component to page-with-sidenav component

--- packages/web/src/app/shell/page-with-sidenav/page-with-sidenav.component.ts
+++ packages/web/src/app/shell/page-with-sidenav/page-with-sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { RouterService } from '../../core/services/router.service';
 
 @Component({
   selector: 'app-page-with-sidenav',
@@ -7,4 +10,26 @@ import { Component, Input } from '@angular/core';
 })
 export class PageWithSidenavComponent {
   @Input() isLandingPage = false;
+  backBtn = '';
+  currentRoute = '';
+
+  constructor(urlService: RouterService) {
+    /**
+     * We need this initial setter, because the url is resolved
+     * before this component is mounted, thus the urlPathChange()
+     * will not trigger on initial render (when pressing F5).
+     */
+    this.setCurrentRoute(urlService.getCurrentUrlPath());
+
+    urlService
+      .urlPathChange()
+      .pipe(takeUntilDestroyed())
+      .subscribe((e) => this.setCurrentRoute(e));
+  }
+
+  private setCurrentRoute(url: string): void {
+    this.currentRoute = url.split('/')[1];
+    this.backBtn = this.currentRoute.replace('-', ' ');
+    console.log(this.currentRoute);
+  }
 }
--- packages/web/src/app/shell/page-with-sidenav/page-with-sidenav.module.ts
+++ packages/web/src/app/shell/page-with-sidenav/page-with-sidenav.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PageWithSidenavComponent } from './page-with-sidenav.component';
 import { FeedbackModule } from '../../shared/feedback/feedback.module';
 import { NavbarModule } from '../navbar/navbar.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PageWithSidenavComponent],
-  imports: [CommonModule, FeedbackModule, NavbarModule],
+  imports: [CommonModule, RouterModule, FeedbackModule, NavbarModule],
   exports: [PageWithSidenavComponent],
 })
 export class PageWithSidenavModule {}
