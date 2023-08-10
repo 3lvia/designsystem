929e6650d5e6217b2c6c1c240c2245d9e4e8f4bf
Erik Tallang <et@computas.com>
2023-08-10 10:35:21 +0200

    refactor(feedback): use service to scroll feedback-block into view

--- packages/web/src/app/shared/feedback/feedback-link/feedback-link.component.ts
+++ packages/web/src/app/shared/feedback/feedback-link/feedback-link.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { ScrollService } from '../../../core/services/scroll.service';
+import { ScrollNotifierService } from '../scroll-notifier.service';
 
 @Component({
   selector: 'app-feedback-link',
   templateUrl: './feedback-link.component.html',
-  styleUrls: ['./feedback-link.component.scss'],
 })
 export class FeedbackLinkComponent {
-  constructor(private scrollService: ScrollService) {}
+  constructor(private scrollService: ScrollNotifierService) {}
+
   scrollToFeedback(): void {
-    const offsetTop = document.body.scrollHeight;
-    this.scrollService.scrollToElement(offsetTop);
+    this.scrollService.scrollToFeedback();
   }
 }
