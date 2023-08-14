18d7654af00395302568f22be8e2d79cb31149cf
majabnils <mani@computas.com>
2023-08-14 14:34:08 +0200

    undo changes to box and link

--- packages/web/src/app/doc-pages/components/box-doc/box-doc.component.ts
+++ packages/web/src/app/doc-pages/components/box-doc/box-doc.component.ts
@@ -9,6 +9,8 @@ import { Title } from '@angular/platform-browser';
 })
 export class BoxDocComponent {
   componentData = boxData;
+  figmaUrl = getComponent('box')?.figmaUrl;
+  description = getComponent('box')?.description;
   title = getComponent('box')?.title;
   does = ['Grouping content', 'To separate information from the rest of the page'];
 
