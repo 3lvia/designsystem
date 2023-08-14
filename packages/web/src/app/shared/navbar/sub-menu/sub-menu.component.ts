8879d0705f41fc352e7c78c8121acd3d72e10b36
Erik Tallang <et@computas.com>
2023-08-14 16:43:04 +0200

    fix(sub-menu): menu items not loading on slow pages (from cms)

--- packages/web/src/app/shared/navbar/sub-menu/sub-menu.component.ts
+++ packages/web/src/app/shared/navbar/sub-menu/sub-menu.component.ts
@@ -1,10 +1,12 @@
 import { Component, NgZone } from '@angular/core';
-import { fromEvent, take } from 'rxjs';
+import { fromEvent, merge, switchMap, take } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Location } from '@angular/common';
 import { AnchorService } from './anchor.service';
 import { NavbarAnchor } from '../types';
 import { trigger, transition, stagger, animate, style, query } from '@angular/animations';
+import { LocalizationService } from 'src/app/core/services/localization.service';
+import { CMSService } from '../../../core/services/cms/cms.service';
 
 @Component({
   selector: 'app-sub-menu',
@@ -32,12 +34,28 @@ export class SubMenuComponent {
   currentPage = '';
   activeAnchorPos = 0;
 
-  constructor(private anchorService: AnchorService, zone: NgZone, private location: Location) {
-    zone.onStable.pipe(take(1)).subscribe(() => {
-      // We reverse the list to find active anchor, searching last to first
-      this.anchors = this.anchorService.getVisibleAnchors().slice().reverse() || [];
-      this.setActiveAnchorFromUrl(location.path(true));
-    });
+  constructor(
+    private location: Location,
+    anchorService: AnchorService,
+    localization: LocalizationService,
+    cmsService: CMSService,
+    zone: NgZone,
+  ) {
+    /**
+     * Fetch sub items from the DOM both when localization has changed,
+     * and when content has been fetched from the CMS.
+     **/
+    merge(localization.listenLocalization(), cmsService.listenContentLoadedFromCMS())
+      .pipe(
+        takeUntilDestroyed(),
+        switchMap(() => zone.onStable.pipe(take(1))),
+      )
+      .subscribe(() => {
+        // We reverse the list to find active anchor, searching last to first
+        this.anchors = anchorService.getVisibleAnchors().slice().reverse() || [];
+        console.log(this.anchors);
+        this.setActiveAnchorFromUrl(location.path(true));
+      });
     this.currentPage = location.path();
     this.setActiveAnchor();
   }
