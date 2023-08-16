7b435b7f3b5ea3ec3b1e636c16d8e86dfdad61ed
majabnils <mani@computas.com>
2023-08-16 10:02:27 +0200

    make function to get the last updated date for both elvis and v2 components

--- packages/web/src/app/shared/component-documentation/component-documentation.component.ts
+++ packages/web/src/app/shared/component-documentation/component-documentation.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input, OnInit, booleanAttribute } from '@angular/core';
-import ComponentData from 'src/app/doc-pages/components/component-data.interface';
+import ComponentData, { ComponentChangelog } from 'src/app/doc-pages/components/component-data.interface';
 import data from '@elvia/elvis/.internal/classlist.json';
 import { getComponent } from 'src/app/shared/doc-pages';
 import { DocPageName } from '../shared.enum';
+import elvisChangelogJson from '@elvia/elvis/CHANGELOG.json';
+
+type ChangelogLinks = { displayName: string; url: string }[];
 
 @Component({
   selector: 'app-component-documentation',
@@ -28,6 +31,37 @@ export class ComponentDocumentationComponent implements OnInit {
       this.figmaUrl = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.figmaUrl;
       this.description = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.description;
       this.title = getComponent(this.componentData.name.toLowerCase() as DocPageName)?.title;
+    }
+  }
+
+  getLastUpdatedDate(): string {
+    if (this.isElvis) {
+      const filteredElvisChangelog: ComponentChangelog[] = [];
+      elvisChangelogJson.content.forEach((elvisChangelogEntry) => {
+        elvisChangelogEntry.changelog.forEach((version: (typeof elvisChangelogEntry.changelog)[number]) => {
+          let allEntries: ChangelogLinks = [];
+          if ('components' in version) {
+            allEntries = allEntries.concat(version['components'] as ChangelogLinks);
+          }
+          if ('pages' in version) {
+            allEntries = allEntries.concat(version['pages'] as ChangelogLinks);
+          }
+          if (allEntries.length === 0) return;
+          allEntries.some(({ displayName }) => {
+            if (
+              displayName.toLowerCase() === this.title?.toLowerCase() &&
+              !filteredElvisChangelog.includes(elvisChangelogEntry)
+            ) {
+              filteredElvisChangelog.push(elvisChangelogEntry);
+            }
+          });
+        });
+      });
+      return filteredElvisChangelog[0].date;
+    } else if (this.componentData && this.componentData.changelog) {
+      return this.componentData.changelog[0].date;
+    } else {
+      return '';
     }
   }
 }
