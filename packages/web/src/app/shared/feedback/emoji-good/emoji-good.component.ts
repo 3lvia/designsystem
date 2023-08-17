07a71d8bc980207d17f7a2e58d65e5d6c49da2d5
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-08-17 09:36:43 +0200

    refactor(feedback): add dark theme support for feedback emoji

---
+++ packages/web/src/app/shared/feedback/emoji-good/emoji-good.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emoji-good',
+  templateUrl: './emoji-good.component.html',
+})
+export class EmojiGoodComponent {}
--- packages/web/src/app/shared/feedback/feedback.module.ts
+++ packages/web/src/app/shared/feedback/feedback.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedbackComponent } from './feedback.component';
 import { FeedbackLinkComponent } from './feedback-link/feedback-link.component';
+import { EmojiBadComponent } from './emoji-bad/emoji-bad.component';
+import { EmojiNeutralComponent } from './emoji-neutral/emoji-neutral.component';
+import { EmojiGoodComponent } from './emoji-good/emoji-good.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FeedbackComponent, FeedbackLinkComponent],
+  declarations: [
+    FeedbackComponent,
+    FeedbackLinkComponent,
+    EmojiBadComponent,
+    EmojiNeutralComponent,
+    EmojiGoodComponent,
+  ],
   exports: [FeedbackComponent, FeedbackLinkComponent],
 })
 export class FeedbackModule {}
