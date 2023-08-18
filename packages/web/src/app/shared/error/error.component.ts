f93f4d6c1be3786be8db760328ae3b04694c8db9
frideska <skarseth.fride@gmail.com>
2023-08-18 13:51:44 +0200

    not found page fix

--- packages/web/src/app/shared/error/error.component.ts
+++ packages/web/src/app/shared/error/error.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Title } from '@angular/platform-browser';
+import { ThemeName } from '@elvia/elvis-colors';
+import { ThemeService } from 'src/app/core/services/theme.service';
 
 @Component({
   selector: 'app-error',
@@ -7,7 +10,15 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./error.component.scss'],
 })
 export class ErrorComponent {
-  constructor(private titleService: Title) {
+  currentTheme: ThemeName = 'light';
+
+  constructor(private titleService: Title, private themeService: ThemeService) {
+    this.themeService
+      .listenTheme()
+      .pipe(takeUntilDestroyed())
+      .subscribe((theme) => {
+        this.currentTheme = theme;
+      });
     this.titleService.setTitle('404: Not Found | Elvia design system');
   }
 }
