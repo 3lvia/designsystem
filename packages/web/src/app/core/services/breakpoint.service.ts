cbe98e69227ab832445fb903439a04c4c91da509
Erik Tallang <et@computas.com>
2023-08-21 22:06:04 +0200

    refactor(app): move breakpoint observables into a reusable service

---
+++ packages/web/src/app/core/services/breakpoint.service.ts
@@ -0,0 +1,23 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+
+export type ScreenSize = keyof typeof config;
+
+const config = {
+  sm: '(max-width: 767px)',
+  md: '(min-width: 768px) and (max-width: 1023px)',
+  lg: '(min-width: 1024px) and (max-width: 1439px)',
+  xl: '(min-width: 1440px)',
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreakpointService {
+  constructor(private observer: BreakpointObserver) {}
+
+  matches(sizes: ScreenSize[]): Observable<boolean> {
+    return this.observer.observe(sizes.map((value) => config[value])).pipe(map((state) => state.matches));
+  }
+}
