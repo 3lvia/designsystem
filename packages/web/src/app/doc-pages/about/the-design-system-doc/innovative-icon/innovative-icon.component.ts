24830fbf112a0f5e3285cedb8657543c452888e1
Fride Skarseth <fs@computas.com>
2023-08-24 09:10:45 +0200

    docs(the design system): Added theme support for icons (#1758)

---
+++ packages/web/src/app/doc-pages/about/the-design-system-doc/innovative-icon/innovative-icon.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-innovative-icon',
+  templateUrl: './innovative-icon.component.html',
+})
+export class InnovativeIconComponent {}
