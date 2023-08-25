118282adc0fa7dfa3e5c51c40a672ab9ba05fb9b
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-08-25 10:02:55 +0200

    test(datepicker-range) add errormessage test

--- packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.test.tsx
+++ packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DatepickerRange from './elvia-datepicker-range';
 import { axe } from 'jest-axe';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Elvis DatepickerRange', () => {
   describe('Default', () => {
@@ -32,12 +33,51 @@ describe('Elvis DatepickerRange', () => {
   });
 
   describe('with time picker', () => {
+    const minDate = new Date(2023, 0, 1, 12, 0, 0);
+    const maxDate = new Date(2024, 0, 1, 12, 0, 0);
+
     beforeEach(() => {
-      render(<DatepickerRange hasTimepickers></DatepickerRange>);
+      render(<DatepickerRange hasTimepickers minDate={minDate} maxDate={maxDate} />);
     });
 
     it('should display a start- and end-time picker', () => {
       expect(screen.getAllByPlaceholderText('tt:mm').length).toBe(2);
+    });
+
+    it('should show an error message including the time if the user sets the value out of bounds (min date)', async () => {
+      const user = userEvent.setup();
+      const datepicker = screen.getAllByRole('textbox')[0];
+      const timerpicker = screen.getAllByRole('textbox')[1];
+
+      //before min date
+      await user.type(timerpicker, '06:00');
+      await user.tab();
+
+      expect(datepicker).toHaveAttribute('aria-invalid', 'true');
+      expect(datepicker).toHaveValue('01.01.2023');
+      expect(timerpicker).toHaveAttribute('aria-invalid', 'true');
+      expect(timerpicker).toHaveValue('06:00');
+
+      const errorMessage = screen.queryByText(/kan ikke være før \d{2}\.\d{2}\.\d{4} kl\. \d{2}:\d{2}/i);
+      expect(errorMessage).toBeInTheDocument();
+    });
+
+    it('should show an error message including the time if the user sets the value out of bounds (max date)', async () => {
+      const user = userEvent.setup();
+      const datepicker = screen.getAllByRole('textbox')[2];
+      const timerpicker = screen.getAllByRole('textbox')[3];
+
+      //after max date
+      await user.type(timerpicker, '16:00');
+      await user.tab();
+
+      expect(datepicker).toHaveAttribute('aria-invalid', 'true');
+      expect(datepicker).toHaveValue('01.01.2024');
+      expect(timerpicker).toHaveAttribute('aria-invalid', 'true');
+      expect(timerpicker).toHaveValue('16:00');
+
+      const errorMessage = screen.queryByText(/kan ikke være etter \d{2}\.\d{2}\.\d{4} kl\. \d{2}:\d{2}/i);
+      expect(errorMessage).toBeInTheDocument();
     });
   });
 
