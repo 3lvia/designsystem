484c462104ac0c82ebfc1b8a1d5a0d8a8790f529
Erik Tallang <et@computas.com>
2023-09-15 08:39:35 +0200

    refactor(footer): standardize image names

--- packages/web/src/app/shell/footer/footer.component.ts
+++ packages/web/src/app/shell/footer/footer.component.ts
@@ -20,20 +20,20 @@ export class FooterComponent {
   links: IconLink[] = [
     {
       url: 'https://app.contentful.com/spaces/zez3t3t1iiwd/home',
-      defaultImgSrc: 'assets/icons/Contentful.svg',
-      invertedImgSrc: 'assets/icons/Contentful-inverted.svg',
+      defaultImgSrc: 'assets/icons/contentful-logo.svg',
+      invertedImgSrc: 'assets/icons/contentful-logo-inverted.svg',
       alt: 'Contentful icon',
     },
     {
       url: 'https://www.figma.com/files/880078299274452916/project/5995782/Designsystem-(Elvis)',
-      defaultImgSrc: 'assets/icons/figma-header.svg',
-      invertedImgSrc: 'assets/icons/figma-header_inverted.svg',
+      defaultImgSrc: 'assets/icons/figma-logo.svg',
+      invertedImgSrc: 'assets/icons/figma-logo-inverted.svg',
       alt: 'Figma icon',
     },
     {
       url: 'https://github.com/3lvia/designsystem',
-      defaultImgSrc: 'assets/icons/github-header.svg',
-      invertedImgSrc: 'assets/icons/github-header_inverted.svg',
+      defaultImgSrc: 'assets/icons/github-logo.svg',
+      invertedImgSrc: 'assets/icons/github-logo-inverted.svg',
       alt: 'GitHub icon',
     },
   ];
