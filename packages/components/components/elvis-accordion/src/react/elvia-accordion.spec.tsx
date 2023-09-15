bab7f45342199245b578281e4c91b2e1b9b77d5b
Trygve Scheline Urdahl <trur@computas.com>
2023-09-15 12:29:12 +0200

    chore(test): update accordion test

--- packages/components/components/elvis-accordion/src/react/elvia-accordion.spec.tsx
+++ packages/components/components/elvis-accordion/src/react/elvia-accordion.spec.tsx
@@ -2,20 +2,20 @@ import { test, expect } from '@playwright/experimental-ct-react';
 import { Accordion } from '../../react';
 import React from 'react';
 
-test.use({ viewport: { width: 500, height: 500 } });
+test.use({ viewport: { width: 500, height: 150 } });
 
 test('should render', async ({ mount }) => {
   const component = await mount(<Accordion openLabel="Open" closeLabel="Close" content={'test'} />);
   await expect(component).toBeAttached();
 });
 
-test('should look closed', async ({ mount }) => {
-  const component = await mount(<Accordion openLabel="Open" closeLabel="Close" content={'test'} />);
-  await expect(component).toHaveScreenshot();
+test('should look closed', async ({ mount, page }) => {
+  await mount(<Accordion openLabel="Open" closeLabel="Close" content={'test'} />);
+  await expect(page).toHaveScreenshot();
 });
 
-test('should look opened', async ({ mount }) => {
+test('should look opened', async ({ mount, page }) => {
   const component = await mount(<Accordion openLabel="Open" closeLabel="Close" content={'test'} />);
   await component.click();
-  await expect(component).toHaveScreenshot();
+  await expect(page).toHaveScreenshot();
 });
