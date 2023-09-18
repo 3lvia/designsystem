7f1d3d003087a2035a77a2a70f4743fca709a060
frideska <skarseth.fride@gmail.com>
2023-09-18 11:13:26 +0200

    patch(bradcrumb, card): Removed deprecations

--- packages/components/components/elvis-breadcrumb/src/react/config.ts
+++ packages/components/components/elvis-breadcrumb/src/react/config.ts
@@ -6,16 +6,5 @@ export const config: ComponentConfig = {
     { name: 'items', type: 'object' },
     { name: 'className', type: 'string' },
     { name: 'inlineStyle', type: 'object' },
-    // Deprecated attributes
-    {
-      name: 'breadcrumbs',
-      type: 'object',
-      deprecatedDetails: { version: '2.0.0', newProp: 'items', isDirectReplacement: true },
-    },
-    {
-      name: 'breadcrumbsOnChange',
-      type: 'event',
-      deprecatedDetails: { version: '2.0.0', newProp: 'onLinkClick', isDirectReplacement: true },
-    },
   ],
 };
--- packages/components/components/elvis-card/src/react/config.ts
+++ packages/components/components/elvis-card/src/react/config.ts
@@ -20,33 +20,5 @@ export const config: ComponentConfig = {
     { name: 'cornerIcon', type: 'string' },
     { name: 'className', type: 'string' },
     { name: 'inlineStyle', type: 'object' },
-    // Deprecated attributes
-    {
-      name: 'hasBorder',
-      type: 'boolean',
-      deprecatedDetails: {
-        version: '3.2.0',
-        explanation: 'Card now has the same border on all backgrounds. No replacement needed.',
-      },
-    },
-    {
-      name: 'header',
-      type: 'string',
-      deprecatedDetails: { version: '2.0.0', newProp: 'heading', isDirectReplacement: true },
-    },
-    {
-      name: 'shape',
-      type: 'string',
-      deprecatedDetails: {
-        version: '2.0.0',
-        isDirectReplacement: false,
-        explanation: 'The shape property has been removed as circle card is no longer supported.',
-      },
-    },
-    {
-      name: 'label',
-      type: 'string',
-      deprecatedDetails: { version: '2.0.0', newProp: 'tag', isDirectReplacement: true },
-    },
   ],
 };
