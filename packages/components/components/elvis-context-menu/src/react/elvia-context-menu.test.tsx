0fd0b939a4b7b5ed1f38ae74ea42da9c2c935d30
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-09-21 08:17:00 +0200

    test(context-menu): fix context-menu test indentation

--- packages/components/components/elvis-context-menu/src/react/elvia-context-menu.test.tsx
+++ packages/components/components/elvis-context-menu/src/react/elvia-context-menu.test.tsx
@@ -5,159 +5,11 @@ import { axe } from 'jest-axe';
 import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 
 describe('Elvis Context Menu', () => {
-  beforeEach(() => {
-    render(
-      <ContextMenu
-        trigger={<button>Trigger</button>}
-        content={
-          <div>
-            <div className="ewc-popover__list-group">
-              <button>
-                <span>Be om tilgang</span>
-              </button>
-              <button>
-                <span>Legg til bruker</span>
-              </button>
-            </div>
-            <div className="ewc-popover__list-group">
-              <a href="#">
-                <span>Endre passord</span>
-              </a>
-            </div>
-          </div>
-        }
-      />,
-    );
-  });
-
-  it('should render a trigger', () => {
-    const trigger = screen.queryByRole('button', { name: /trigger/i });
-    expect(trigger).toBeInTheDocument();
-  });
-
-  describe('when the trigger is clicked', () => {
-    beforeEach(async () => {
-      const user = userEvent.setup();
-      const trigger = screen.getByRole('button', { name: /trigger/i });
-      await user.click(trigger);
-    });
-
-    it('a context menu is visible', () => {
-      const menuItem = screen.queryByText('Be om tilgang');
-      expect(menuItem).toBeVisible();
-    });
-
-    it('the context menu closes when an item is selected', async () => {
-      const user = userEvent.setup();
-
-      await user.click(screen.getByText('Be om tilgang'));
-
-      await waitForElementToBeRemoved(() => screen.queryByText('Be om tilgang'));
-    });
-
-    it('the first option receives focus', () => {
-      const menu = screen.getByRole('menu');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
-      expect(buttons.length).toBe(3);
-      expect(buttons.item(0).tabIndex).toBe(0);
-      expect(buttons.item(1).tabIndex).toBe(-1);
-      expect(buttons.item(2).tabIndex).toBe(-1);
-    });
-
-    it('arrow down will focus the next item in the list', async () => {
-      const user = userEvent.setup();
-      await user.keyboard('{arrowdown}');
-
-      const menu = screen.getByRole('menu');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
-      expect(buttons.item(0).tabIndex).toBe(-1);
-      expect(buttons.item(1).tabIndex).toBe(0);
-      expect(buttons.item(2).tabIndex).toBe(-1);
-    });
-
-    it('arrow up will focus the last item in the list', async () => {
-      const user = userEvent.setup();
-      await user.keyboard('{arrowup}');
-      const menu = screen.getByRole('menu');
-      const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
-      expect(buttons.item(0).tabIndex).toBe(-1);
-      expect(buttons.item(1).tabIndex).toBe(-1);
-      expect(buttons.item(2).tabIndex).toBe(0);
-    });
-
-    it('clicking the trigger again will close the list', async () => {
-      const user = userEvent.setup();
-      const trigger = screen.getByRole('button', { name: /trigger/i });
-      await user.click(trigger);
-
-      const menuItem = screen.queryByText('Be om tilgang');
-      await waitFor(() => expect(menuItem).not.toBeInTheDocument());
-    });
-  });
-});
-
-describe('Events', () => {
-  let onOpenEvent: jest.Mock;
-  let onCloseEvent: jest.Mock;
-
-  beforeEach(() => {
-    onOpenEvent = jest.fn();
-    onCloseEvent = jest.fn();
-    render(
-      <ContextMenu
-        onOpen={onOpenEvent}
-        onClose={onCloseEvent}
-        trigger={<button>Trigger</button>}
-        content={
-          <div>
-            <div className="ewc-popover__list-group">
-              <button>
-                <span>Be om tilgang</span>
-              </button>
-              <button>
-                <span>Legg til bruker</span>
-              </button>
-            </div>
-            <div className="ewc-popover__list-group">
-              <a href="#">
-                <span>Endre passord</span>
-              </a>
-            </div>
-          </div>
-        }
-      />,
-    );
-  });
-
-  it('should not emit events when idle', async () => {
-    await waitFor(() => expect(onOpenEvent).not.toHaveBeenCalled());
-    await waitFor(() => expect(onCloseEvent).not.toHaveBeenCalled());
-  });
-
-  it('onOpenEvent: should emit onOpen when the trigger is clicked', async () => {
-    const user = userEvent.setup();
-    const trigger = screen.getByRole('button', { name: /trigger/i });
-    await user.click(trigger);
-
-    await waitFor(() => expect(onOpenEvent).toHaveBeenCalled());
-  });
-
-  it('onCloseEvent: should emit onClose when the trigger is clicked', async () => {
-    const user = userEvent.setup();
-    const trigger = screen.getByRole('button', { name: /trigger/i });
-    await user.click(trigger);
-    await user.tab();
-
-    await waitFor(() => expect(onCloseEvent).toHaveBeenCalled());
-  });
-});
-
-describe('the accessibility', () => {
-  it('should have no axe violations', async () => {
-    render(
-      <div data-testid="context-menu-wrapper">
+  describe('default', () => {
+    beforeEach(() => {
+      render(
         <ContextMenu
-          trigger={<button data-testid="trigger">Trigger</button>}
+          trigger={<button>Trigger</button>}
           content={
             <div>
               <div className="ewc-popover__list-group">
@@ -175,9 +27,88 @@ describe('the accessibility', () => {
               </div>
             </div>
           }
-        />
+        />,
+      );
+    });
+
+    it('should render a trigger', () => {
+      const trigger = screen.queryByRole('button', { name: /trigger/i });
+      expect(trigger).toBeInTheDocument();
+    });
+
+    describe('when the trigger is clicked', () => {
+      beforeEach(async () => {
+        const user = userEvent.setup();
+        const trigger = screen.getByRole('button', { name: /trigger/i });
+        await user.click(trigger);
+      });
+
+      it('a context menu is visible', () => {
+        const menuItem = screen.queryByText('Be om tilgang');
+        expect(menuItem).toBeVisible();
+      });
+
+      it('the context menu closes when an item is selected', async () => {
+        const user = userEvent.setup();
+
+        await user.click(screen.getByText('Be om tilgang'));
+
+        await waitForElementToBeRemoved(() => screen.queryByText('Be om tilgang'));
+      });
+
+      it('the first option receives focus', () => {
+        const menu = screen.getByRole('menu');
+        const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
+        expect(buttons.length).toBe(3);
+        expect(buttons.item(0).tabIndex).toBe(0);
+        expect(buttons.item(1).tabIndex).toBe(-1);
+        expect(buttons.item(2).tabIndex).toBe(-1);
+      });
+
+      it('arrow down will focus the next item in the list', async () => {
+        const user = userEvent.setup();
+        await user.keyboard('{arrowdown}');
+
+        const menu = screen.getByRole('menu');
+        const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
+        expect(buttons.item(0).tabIndex).toBe(-1);
+        expect(buttons.item(1).tabIndex).toBe(0);
+        expect(buttons.item(2).tabIndex).toBe(-1);
+      });
+
+      it('arrow up will focus the last item in the list', async () => {
+        const user = userEvent.setup();
+        await user.keyboard('{arrowup}');
+        const menu = screen.getByRole('menu');
+        const buttons: NodeListOf<HTMLButtonElement | HTMLAnchorElement> = menu.querySelectorAll('button, a');
+        expect(buttons.item(0).tabIndex).toBe(-1);
+        expect(buttons.item(1).tabIndex).toBe(-1);
+        expect(buttons.item(2).tabIndex).toBe(0);
+      });
+
+      it('clicking the trigger again will close the list', async () => {
+        const user = userEvent.setup();
+        const trigger = screen.getByRole('button', { name: /trigger/i });
+        await user.click(trigger);
+
+        const menuItem = screen.queryByText('Be om tilgang');
+        await waitFor(() => expect(menuItem).not.toBeInTheDocument());
+      });
+    });
+  });
+
+  describe('Events', () => {
+    let onOpenEvent: jest.Mock;
+    let onCloseEvent: jest.Mock;
+
+    beforeEach(() => {
+      onOpenEvent = jest.fn();
+      onCloseEvent = jest.fn();
+      render(
         <ContextMenu
-          trigger={<button data-testid="trigger">Trigger</button>}
+          onOpen={onOpenEvent}
+          onClose={onCloseEvent}
+          trigger={<button>Trigger</button>}
           content={
             <div>
               <div className="ewc-popover__list-group">
@@ -195,13 +126,84 @@ describe('the accessibility', () => {
               </div>
             </div>
           }
-        />
-      </div>,
-    );
+        />,
+      );
+    });
 
-    const contextMenu = screen.getByTestId('context-menu-wrapper');
-    const results = await axe(contextMenu);
+    it('should not emit events when idle', async () => {
+      await waitFor(() => expect(onOpenEvent).not.toHaveBeenCalled());
+      await waitFor(() => expect(onCloseEvent).not.toHaveBeenCalled());
+    });
 
-    expect(results).toHaveNoViolations();
+    it('onOpenEvent: should emit onOpen when the trigger is clicked', async () => {
+      const user = userEvent.setup();
+      const trigger = screen.getByRole('button', { name: /trigger/i });
+      await user.click(trigger);
+
+      await waitFor(() => expect(onOpenEvent).toHaveBeenCalled());
+    });
+
+    it('onCloseEvent: should emit onClose when the trigger is clicked', async () => {
+      const user = userEvent.setup();
+      const trigger = screen.getByRole('button', { name: /trigger/i });
+      await user.click(trigger);
+      await user.tab();
+
+      await waitFor(() => expect(onCloseEvent).toHaveBeenCalled());
+    });
+  });
+
+  describe('the accessibility', () => {
+    it('should have no axe violations', async () => {
+      render(
+        <div data-testid="context-menu-wrapper">
+          <ContextMenu
+            trigger={<button data-testid="trigger">Trigger</button>}
+            content={
+              <div>
+                <div className="ewc-popover__list-group">
+                  <button>
+                    <span>Be om tilgang</span>
+                  </button>
+                  <button>
+                    <span>Legg til bruker</span>
+                  </button>
+                </div>
+                <div className="ewc-popover__list-group">
+                  <a href="#">
+                    <span>Endre passord</span>
+                  </a>
+                </div>
+              </div>
+            }
+          />
+          <ContextMenu
+            trigger={<button data-testid="trigger">Trigger</button>}
+            content={
+              <div>
+                <div className="ewc-popover__list-group">
+                  <button>
+                    <span>Be om tilgang</span>
+                  </button>
+                  <button>
+                    <span>Legg til bruker</span>
+                  </button>
+                </div>
+                <div className="ewc-popover__list-group">
+                  <a href="#">
+                    <span>Endre passord</span>
+                  </a>
+                </div>
+              </div>
+            }
+          />
+        </div>,
+      );
+
+      const contextMenu = screen.getByTestId('context-menu-wrapper');
+      const results = await axe(contextMenu);
+
+      expect(results).toHaveNoViolations();
+    });
   });
 });
