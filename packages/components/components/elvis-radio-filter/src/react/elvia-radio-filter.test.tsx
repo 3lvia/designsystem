6b562a9d199d5d42a8c5646b7dc71d13e7edad1b
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-09-25 10:15:40 +0200

    LEGO-2983: event tester komponenter O–U (#1859)
    * test(pagination): add Event tests
    
    * test(popover): add events test
    
    * test(radio-filter): add Event test
    
    * test(segmented-control): add event tests
    
    * test(slider): add Events tests
    
    * test(Tabs): add event tests
    
    * test(time picker): add more event tests

--- packages/components/components/elvis-radio-filter/src/react/elvia-radio-filter.test.tsx
+++ packages/components/components/elvis-radio-filter/src/react/elvia-radio-filter.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import RadioFilter, { Option } from './elvia-radio-filter';
 import { axe } from 'jest-axe';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('Elvis RadioFilter', () => {
   const items: Option[] = [
@@ -56,6 +57,33 @@ describe('Elvis RadioFilter', () => {
       const radioFilterGroup = screen.getByTestId('radio-filter-group');
       expect(radioFilterGroup).toHaveStyle('margin: 24px');
       expect(radioFilterGroup).toHaveClass('test-class');
+    });
+  });
+
+  describe('events', () => {
+    //valueOnChange
+    let valueOnChangeEvent: jest.Mock;
+
+    beforeEach(() => {
+      valueOnChangeEvent = jest.fn();
+
+      render(
+        <RadioFilter items={items} name="radio-filter" value="all" valueOnChange={valueOnChangeEvent} />,
+      );
+    });
+
+    it('should not emit any event on init', async () => {
+      await waitFor(() => expect(valueOnChangeEvent).not.toHaveBeenCalled());
+    });
+
+    it('valueOnChange: should emit event when radio button is clicked', async () => {
+      const user = userEvent.setup();
+      const radioFilterButton = screen.getAllByRole('radio');
+
+      await user.click(radioFilterButton[1]);
+      screen.debug();
+
+      await waitFor(() => expect(valueOnChangeEvent).toHaveBeenCalled());
     });
   });
 
