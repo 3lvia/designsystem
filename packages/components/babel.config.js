397ed5e7505a811b7209e5e5e7063c9ae97e06c9
Erik Tallang <et@computas.com>
2023-09-25 16:27:54 +0200

    build(components): use esbuild to build public apis

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: ['@babel/plugin-transform-runtime'],
+  plugins: [
+    '@babel/plugin-transform-runtime',
+    'babel-plugin-styled-components',
+    {
+      ssr: true,
+    },
+  ],
 };
---
+++ packages/components/esbuild.config.mjs
@@ -0,0 +1,64 @@
+import esbuild from 'esbuild';
+import tinyGlob from 'tiny-glob';
+import fs from 'fs';
+import crypto from 'crypto';
+import { dtsPlugin } from 'esbuild-plugin-d.ts';
+
+const tmpOutputFolder = 'tmp-public-api-output';
+
+const getMd5 = (fileName) => {
+  if (!fs.existsSync(fileName) || fs.statSync(fileName).isDirectory()) {
+    return null;
+  }
+
+  const hash = crypto.createHash('md5');
+  const data = hash.update(fs.readFileSync(fileName), 'utf-8');
+  return data.digest('hex');
+};
+
+const movePublicApiFiles = {
+  name: 'move-public-api-files',
+  setup(build) {
+    build.onEnd(async () => {
+      const paths = await tinyGlob(`${tmpOutputFolder}/**/*.public.{d.ts,js}`);
+
+      paths.forEach((filePath) => {
+        const componentFolder = filePath.split('/')[1];
+        const isDTs = filePath.endsWith('.d.ts');
+        const folderPath = `components/${componentFolder}/dist/public-api`;
+        const newPath = `${folderPath}/public-api.${isDTs ? 'd.ts' : 'js'}`;
+
+        if (getMd5(filePath) !== getMd5(newPath)) {
+          fs.mkdirSync(folderPath, { recursive: true });
+          fs.renameSync(filePath, newPath);
+        }
+      });
+
+      fs.rmSync(tmpOutputFolder, { force: true, recursive: true });
+    });
+  },
+};
+
+(async () => {
+  const watchMode = process.argv.includes('--watch');
+
+  const paths = await tinyGlob('components/elvis-*/src/react/*.public.ts');
+
+  if (watchMode) {
+    const esBuildContext = await esbuild.context({
+      entryPoints: paths,
+      outdir: tmpOutputFolder,
+      sourcemap: true,
+      logLevel: 'info',
+      plugins: [dtsPlugin(), movePublicApiFiles],
+    });
+    esBuildContext.watch();
+  } else {
+    esbuild.build({
+      entryPoints: paths,
+      outdir: tmpOutputFolder,
+      logLevel: 'info',
+      plugins: [dtsPlugin(), movePublicApiFiles],
+    });
+  }
+})();
