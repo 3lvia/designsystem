4d106d93708fb012bc7181451d4014bb4cd29f53
Erik Tallang <et@computas.com>
2023-09-26 12:05:08 +0200

    build(components): export prop types for all components

--- packages/components/components/elvis-dropdown/react.d.ts
+++ packages/components/components/elvis-dropdown/react.d.ts
@@ -1,1 +1,3 @@
 export * from './dist/react/types';
+export * from './dist/public-api/types';
+export * from './dist/public-api/public-api';
--- packages/components/components/elvis-stepper/react.d.ts
+++ packages/components/components/elvis-stepper/react.d.ts
@@ -1,1 +1,2 @@
 export * from './dist/react/types';
+export * from './dist/public-api/types';
