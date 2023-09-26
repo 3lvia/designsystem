c65684747161b5c3a0931be74f48412f786a4524
Trygve Scheline Urdahl <trur@computas.com>
2023-09-26 10:13:58 +0200

    chore: remove dead code

--- packages/components/components/elvis-chip/src/react/elvia-chip.spec.tsx
+++ packages/components/components/elvis-chip/src/react/elvia-chip.spec.tsx
@@ -1,11 +1,8 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 import { Chip } from '../../react';
 import React from 'react';
-import { ToHaveScreenshotConfig } from '../../../../playwright-ct.config';
 
 test.use({ viewport: { width: 130, height: 70 } });
-
-const config: ToHaveScreenshotConfig = undefined; // { maxDiffPixelRatio: 0 };
 
 test('should render', async ({ mount }) => {
   const component = await mount(<Chip value="Chip" />);
@@ -14,42 +11,42 @@ test('should render', async ({ mount }) => {
 
 test('should be type choice', async ({ mount, page }) => {
   await mount(<Chip value="Chip" type="choice" isSelected />);
-  await expect(page).toHaveScreenshot(config);
+  await expect(page).toHaveScreenshot();
 });
 
 test.describe('should have color', () => {
   test('green', async ({ mount, page }) => {
     await mount(<Chip color="green" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
   test('blue', async ({ mount, page }) => {
     await mount(<Chip color="blue" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
   test('orange', async ({ mount, page }) => {
     await mount(<Chip color="orange" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
   test('purple', async ({ mount, page }) => {
     await mount(<Chip color="purple" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
   test('red', async ({ mount, page }) => {
     await mount(<Chip color="red" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
   test('violet', async ({ mount, page }) => {
     await mount(<Chip color="violet" value="Chip" type="legend" isSelected />);
-    await expect(page).toHaveScreenshot(config);
+    await expect(page).toHaveScreenshot();
   });
 });
 
 test('should be type legend', async ({ mount, page }) => {
   await mount(<Chip value="Chip" type="legend" isSelected />);
-  await expect(page).toHaveScreenshot(config);
+  await expect(page).toHaveScreenshot();
 });
 
 test('should be type removable', async ({ mount, page }) => {
   await mount(<Chip value="Chip" type="removable" />);
-  await expect(page).toHaveScreenshot(config);
+  await expect(page).toHaveScreenshot();
 });
