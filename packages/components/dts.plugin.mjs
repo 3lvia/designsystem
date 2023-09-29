2598d19a2c052422975d5cd76451085a24173fd6
Erik Tallang <et@computas.com>
2023-09-29 23:45:06 +0200

    build(components): create transpilation of d.ts files

--- packages/components/dts.plugin.mjs
+++ packages/components/dts.plugin.mjs
@@ -1,39 +1,58 @@
 /* eslint-disable no-console */
 import ts from 'typescript';
 import chalk from 'chalk';
+import path from 'path';
 import fs from 'fs';
 
-export const dtsPlugin = () => ({
+export const dtsPlugin = ({ destinationDir, paths }) => ({
   name: 'dts-plugin',
   async setup(build) {
     const tsConfig = JSON.parse(fs.readFileSync('./tsconfig.esbuild.json', 'utf-8'));
-    const compilerOpts = ts.convertCompilerOptionsFromJson(tsConfig.compilerOptions);
-    //   copts.tsBuildInfoFile = resolve(
-    //     tmpdir,
-    //     require(pjloc).name ?? "unnamed",
-    //     ".esbuild",
-    //     ".tsbuildinfo",
-    // );
-    const host = ts.createIncrementalCompilerHost(compilerOpts);
+    const compilerOpts = ts.convertCompilerOptionsFromJson(tsConfig.compilerOptions).options;
+    compilerOpts.declarationDir = destinationDir;
+    compilerOpts.listEmittedFiles = true;
+    const host = ts.createCompilerHost(compilerOpts);
     const files = [];
+    const f = paths.map((p) => p.in);
 
     // get all ts files
-    build.onLoad({ filter: /(\.tsx|\.ts)$/ }, async (args) => {
+    build.onResolve({ filter: /(\.tsx|\.ts)$/ }, async (args) => {
       files.push(args.path);
 
-      host.getSourceFile(args.path, ts.ScriptTarget.ES2015, (m) => console.log(m), true);
+      host.getSourceFile(args.path, compilerOpts.target, (m) => console.log(m), true);
 
       return {};
     });
 
     build.onEnd(() => {
-      const program = ts.createIncrementalProgram({
+      const program = ts.createProgram({
         options: compilerOpts,
         host: host,
         rootNames: files,
       });
       const start = Date.now();
-      const emit = program.emit();
+      const emit = program.emit(
+        undefined,
+        (fileName, text) => {
+          const componentName = fileName.split('/')[1];
+
+          const isPublic = path.basename(fileName).includes('.public');
+
+          if (isPublic) {
+            fs.writeFileSync(
+              path.join(destinationDir, componentName, 'dist', 'public-api', 'public-api.d.ts'),
+              text,
+            );
+          } else {
+            fs.writeFileSync(
+              path.join(destinationDir, componentName, 'dist', 'react', path.basename(fileName)),
+              text,
+            );
+          }
+        },
+        undefined,
+        true,
+      );
       console.log(
         chalk.green(
           `⚡️ Built ${emit.emittedFiles ? emit.emittedFiles.length : 0} typings in ${Date.now() - start}ms`,
--- packages/components/esbuild.config.mjs
+++ packages/components/esbuild.config.mjs
@@ -3,6 +3,7 @@ import { dtsPlugin } from './dts.plugin.mjs';
 import styledComponentsPlugin from 'esbuild-plugin-styled-components';
 import tinyGlob from 'tiny-glob';
 import fs from 'fs';
+import path from 'path';
 import crypto from 'crypto';
 
 const tmpOutputFolder = 'tmp-public-api-output';
@@ -19,7 +20,7 @@ const getMd5 = (fileName) => {
   return data.digest('hex');
 };
 
-const getPackageJsons = async () => {
+const getComponentData = async () => {
   const paths = await tinyGlob('components/elvis-*/package.json');
   return paths.map((path) => {
     const file = fs.readFileSync(path, 'utf-8');
@@ -28,7 +29,7 @@ const getPackageJsons = async () => {
 };
 
 const getEntryPoint = (componentData) => {
-  return `components/${componentData.component}/${componentData.content.source}`;
+  return path.join('components', componentData.component, componentData.content.source);
 };
 
 const getAllDependencies = (componentDataList) => {
@@ -36,86 +37,48 @@ const getAllDependencies = (componentDataList) => {
   return Array.from(new Set(dependencies));
 };
 
-const moveTypingsToComponentFolders = {
-  name: 'move-public-api-files',
-  setup(build) {
-    build.onEnd(async () => {
-      const paths = await tinyGlob(`${tmpOutputFolder}/**/*.{public,types}.{d.ts,js}`);
+const toInOutTuple = (filePath) => {
+  const fileName = path.basename(filePath);
+  const componentFolder = filePath.split('/')[1];
+  const subFolder = fileName.includes('.public') ? 'public-api' : 'react';
+  const newFileName = fileName.includes('.types')
+    ? 'types'
+    : fileName.includes('.public')
+    ? 'public-api'
+    : 'react';
 
-      paths.forEach((filePath) => {
-        const componentFolder = filePath.split('/')[1];
-        const isReactTypes = filePath.includes('.types');
-        const isDTs = filePath.endsWith('.d.ts');
-
-        const newDir = `components/${componentFolder}/dist/${isReactTypes ? 'react' : 'public-api'}`;
-        const newFileName = `${isReactTypes ? 'types' : 'public-api'}.${isDTs ? 'd.ts' : 'js'}`;
-
-        const newPath = `${newDir}/${newFileName}`;
-
-        if (getMd5(filePath) !== getMd5(newPath)) {
-          fs.mkdirSync(newDir, { recursive: true });
-          fs.renameSync(filePath, newPath);
-        }
-      });
-
-      fs.rmSync(tmpOutputFolder, { force: true, recursive: true });
-    });
-  },
-};
-
-const changePublicApiImportPath = {
-  name: 'move-public-api-files',
-  setup(build) {
-    build.onEnd(async () => {
-      const paths = await tinyGlob(`${tmpOutputFolder}/**/*.types.{d.ts,js}`);
-
-      paths.forEach((filePath) => {
-        let file = fs.readFileSync(filePath, 'utf-8');
-        file = file.replace(/\.\/publicApi.public/g, '../public-api/public-api');
-        fs.writeFileSync(filePath, file, 'utf-8');
-      });
-    });
-  },
-};
-
-const toInOutTuple = (path) => {
-  const componentFolder = path.split('/')[1];
-  const subFolder = path.includes('.public.') ? 'public-api' : 'react';
-  const fileName = path.includes('.types.') ? 'types' : path.includes('.public.') ? 'public-api' : 'react';
-
-  return { in: path, out: `${componentFolder}/dist/${subFolder}/${fileName}` };
+  return { in: filePath, out: path.join(componentFolder, 'dist', subFolder, newFileName) };
 };
 
 (async () => {
   const watchMode = process.argv.includes('--watch');
 
   const typePaths = await tinyGlob('components/elvis-*/src/react/*.{public,types}.ts*');
-  const pckJsons = await getPackageJsons();
-  const paths = typePaths.concat(pckJsons.map(getEntryPoint)).map(toInOutTuple);
-  const dependencies = getAllDependencies(pckJsons);
+  const componentDataList = await getComponentData();
+  const paths = typePaths.concat(componentDataList.map(getEntryPoint)).map(toInOutTuple);
+  const dependencies = getAllDependencies(componentDataList);
+
+  const baseConfig = {
+    entryPoints: paths,
+    outdir: tmpOutputFolder,
+    logLevel: 'info',
+    bundle: true,
+    format: 'esm',
+    external: dependencies,
+    plugins: [
+      dtsPlugin({ destinationDir: tmpOutputFolder, paths }),
+      styledComponentsPlugin({ ssr: true, displayName: true }),
+    ],
+  };
 
   if (watchMode) {
-    const esBuildContext = await esbuild.context({
-      entryPoints: paths,
-      outdir: tmpOutputFolder,
-      sourcemap: true,
-      logLevel: 'info',
-      bundle: true,
-      format: 'esm',
-      external: dependencies,
-      plugins: [styledComponentsPlugin({ ssr: true }), dtsPlugin()],
-    });
+    const esBuildContext = await esbuild.context(
+      Object.assign(baseConfig, {
+        sourcemap: true,
+      }),
+    );
     esBuildContext.watch();
   } else {
-    esbuild.build({
-      entryPoints: paths,
-      outdir: tmpOutputFolder,
-      minify: false,
-      logLevel: 'info',
-      bundle: true,
-      format: 'esm',
-      external: dependencies,
-      plugins: [styledComponentsPlugin({ ssr: true }), dtsPlugin()],
-    });
+    await esbuild.build(Object.assign(baseConfig, { minify: false }));
   }
 })();
