db457c8273a60b8f1280af0f6c132672344c5358
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-10-02 07:39:53 +0200

    feat(component-data.interface): add optional example key

--- packages/web/src/app/doc-pages/components/component-data.interface.ts
+++ packages/web/src/app/doc-pages/components/component-data.interface.ts
@@ -19,6 +19,11 @@ interface PropBase {
    */
   type: string;
   default?: string | number | boolean;
+  /** A example of how the prop can be used. Typed as a string, but should be a valid ts code example.
+   * @example
+   * "myObject = {key: 'value'}"
+   */
+  example?: string;
 }
 
 /**
