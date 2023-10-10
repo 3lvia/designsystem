3a56da6c8dc7583e4c8a3d80b4db987daff7ee99
Erik Tallang <et@computas.com>
2023-10-10 23:32:33 +0200

    chore: revert unintended changes

--- packages/components/babel.config.js
+++ packages/components/babel.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-  plugins: [
-    '@babel/plugin-transform-runtime',
-    'babel-plugin-styled-components',
-    {
-      ssr: true,
-    },
-  ],
+  plugins: ['@babel/plugin-transform-runtime'],
 };
