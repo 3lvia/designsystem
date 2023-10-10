74fd29ba99f2ca28345583848d9fcf3fd7c07089
Maja B. Nilsen <111271903+majabnils@users.noreply.github.com>
2023-10-10 12:14:55 +0200

    Lego 2667 stotte for objekter i tabellen nesting (#1854)
    * add colspan 2 to mobile version of property name
    
    * add recursive method to get the nested objects
    
    * style the property table for nesting
    
    * small style changes
    
    * remove datepicker change
    
    * remove unused code
    
    * small style changes
    
    * docs(datepicker): change datepicker data file
    
    * fix firefox css bug
    
    * refactor and fix styling bugs with nesting
    
    * docs(datepicker): Change errorOptions text description
    
    * refactor: remove recursion in template
    
    * fix bugs with flat list solution
    
    * refactor and small fixes
    
    * refactor styling and remove excess code
    
    * change comment
    
    * refactor isEvent logic
    
    * refactor
    
    * refactor functions in desktop property table
    
    * remove comment
    
    * refactor
    
    * small changes
    
    * fix sonar code smell
    
    ---------
    
    Co-authored-by: majabnils <mani@computas.com>

--- packages/web/src/app/shared/component-documentation/component-properties-table/component-properties-table.component.ts
+++ packages/web/src/app/shared/component-documentation/component-properties-table/component-properties-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, booleanAttribute } from '@angular/core';
-import ComponentData from 'src/app/doc-pages/components/component-data.interface';
+import ComponentData, { NestedProp } from 'src/app/doc-pages/components/component-data.interface';
 import { ComponentProp } from './types';
 import { SearchResult, Searcher } from '../../searcher';
 
@@ -16,7 +16,7 @@ export class ComponentPropertiesTableComponent implements OnInit {
   private searcher: Searcher<ComponentProp>;
 
   ngOnInit(): void {
-    this.searcher = new Searcher(this.getPropArray(), {
+    this.searcher = new Searcher(this.getRootPropArray(), {
       threshold: 0.4,
       includeMatches: true,
       keys: [
@@ -33,27 +33,83 @@ export class ComponentPropertiesTableComponent implements OnInit {
     if (searchTerm !== '') {
       this.filteredComponentProps = this.searcher.search(searchTerm);
     } else {
-      this.filteredComponentProps = this.getPropArray().map((prop) => ({ item: prop }));
+      this.filteredComponentProps = this.getRootPropArray().map((prop) => ({ item: prop }));
     }
+    this.filteredComponentProps = this.flattenProps(this.filteredComponentProps);
   }
 
-  private getPropArray(): ComponentProp[] {
-    const componentProps: ComponentProp[] = [];
-    Object.keys(this.componentData.attributes).forEach((prop) => {
-      const propData = this.componentData.attributes[prop];
+  private flattenProps(componentProps: SearchResult<ComponentProp>[]): SearchResult<ComponentProp>[] {
+    let flatComponentProps: SearchResult<ComponentProp>[] = [];
+
+    for (const prop of componentProps) {
+      flatComponentProps.push(prop);
+      const propData = this.componentData.attributes[prop.item.attribute];
+      if (propData && 'children' in propData) {
+        flatComponentProps = this.flattenPropsRecursive(propData, 1, flatComponentProps);
+      }
+    }
+    return flatComponentProps;
+  }
+
+  private flattenPropsRecursive(
+    nestedComponentProp: NestedProp<any>,
+    level: number,
+    componentProps: SearchResult<ComponentProp>[],
+  ): SearchResult<ComponentProp>[] {
+    for (const prop in nestedComponentProp.children) {
+      const propData = nestedComponentProp.children[prop];
       const componentProp: ComponentProp = {
         attribute: prop,
         ...propData,
-        description: propData.description ?? '',
+        description: propData.description || '',
+        level,
       };
-      componentProps.push(componentProp);
+      componentProps.push({ item: componentProp });
+      if ('children' in propData) {
+        componentProps = this.flattenPropsRecursive(propData, level + 1, componentProps);
+      }
+    }
+    return componentProps;
+  }
+
+  private getRootPropArray(): ComponentProp[] {
+    const componentProps: ComponentProp[] = Object.keys(this.componentData.attributes).map((prop) => {
+      const propData = this.componentData.attributes[prop];
+      return {
+        attribute: prop,
+        ...propData,
+        description: propData.description || '',
+        level: 0,
+      };
     });
+
+    componentProps.sort(this.sortProps);
 
     if (!this.ignoreDefaultProps) {
       componentProps.push(...this.getCommonProps());
     }
-
     return componentProps;
+  }
+
+  private sortProps(a: ComponentProp, b: ComponentProp): number {
+    const lastProps = ['className', 'inlineStyle'];
+    let lastPropComparison = 0;
+    if (lastProps.includes(a.attribute) && !lastProps.includes(b.attribute)) {
+      lastPropComparison = 1;
+    } else if (!lastProps.includes(a.attribute) && lastProps.includes(b.attribute)) {
+      lastPropComparison = -1;
+    }
+
+    let requiredComparison = 0;
+    if (a.isRequired && !b.isRequired) {
+      requiredComparison = -1;
+    } else if (!a.isRequired && b.isRequired) {
+      requiredComparison = 1;
+    }
+
+    const alphabeticalComparison = a.attribute.localeCompare(b.attribute);
+
+    return lastPropComparison || requiredComparison || alphabeticalComparison;
   }
 
   private getCommonProps(): ComponentProp[] {
@@ -62,11 +118,13 @@ export class ComponentPropertiesTableComponent implements OnInit {
         attribute: 'className',
         description: 'Custom CSS classes that can be added to the component.',
         type: 'string',
+        level: 0,
       },
       {
         attribute: 'inlineStyle',
         description: 'Custom CSS style object that can be added to the component.',
         type: '{[cssProperty: string]: string}',
+        level: 0,
         example: /* ts */ `inlineStyle = { marginTop: "8px" }`,
       },
     ];
