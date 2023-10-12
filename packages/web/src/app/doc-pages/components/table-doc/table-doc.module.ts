3314f3f553e4e0a2305a0693bcfeda0b04dfbb34
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-10-12 18:17:28 +0200

    docs(table): add able-extra-information-tooltip-ceg

--- packages/web/src/app/doc-pages/components/table-doc/table-doc.module.ts
+++ packages/web/src/app/doc-pages/components/table-doc/table-doc.module.ts
@@ -1,59 +1,58 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { TableDocComponent } from './table-doc.component';
 import { TableCegComponent } from './table-ceg/table-ceg.component';
-import { TableSizeMediumDesktopCegComponent } from './table-size-medium-desktop-ceg/table-size-medium-desktop-ceg.component';
-import { TableSizeMediumMobileCegComponent } from './table-size-medium-mobile-ceg/table-size-medium-mobile-ceg.component';
-import { TableSizeSmallDesktopCegComponent } from './table-size-small-desktop-ceg/table-size-small-desktop-ceg.component';
-import { TableSizeSmallMobileCegComponent } from './table-size-small-mobile-ceg/table-size-small-mobile-ceg.component';
+import { SharedDocumentationModule } from 'src/app/shared/component-documentation/shared-component-documentation.module';
 import { TableAlignmentCegComponent } from './table-alignment-ceg/table-alignment-ceg.component';
 import { TableButtonDesktopCegComponent } from './table-button-desktop-ceg/table-button-desktop-ceg.component';
 import { TableButtonMobileCegComponent } from './table-button-mobile-ceg/table-button-mobile-ceg.component';
 import { TableCheckboxDesktopCegComponent } from './table-checkbox-desktop-ceg/table-checkbox-desktop-ceg.component';
 import { TableCheckboxMobileCegComponent } from './table-checkbox-mobile-ceg/table-checkbox-mobile-ceg.component';
+import { TableExtraInformationTooltipCegComponent } from './table-extra-information-tooltip-ceg/table-extra-information-tooltip-ceg.component';
 import { TableFootnoteCegComponent } from './table-footnote-ceg/table-footnote-ceg.component';
 import { TableInputDesktopCegComponent } from './table-input-desktop-ceg/table-input-desktop-ceg.component';
 import { TableInputMobileCegComponent } from './table-input-mobile-ceg/table-input-mobile-ceg.component';
 import { TableLongColumnCegComponent } from './table-long-column-ceg/table-long-column-ceg.component';
-import { TableStickyRowHeaderDesktopCegComponent } from './table-sticky-row-header-desktop-ceg/table-sticky-row-header-desktop-ceg.component';
-import { TableStickyColumnHeaderDesktopCegComponent } from './table-sticky-column-header-desktop-ceg/table-sticky-column-header-desktop-ceg.component';
-import { TableStickyColumnHeaderMobileCegComponent } from './table-sticky-column-header-mobile-ceg/table-sticky-column-header-mobile-ceg.component';
 import { TableNoStripesCegComponent } from './table-no-stripes-ceg/table-no-stripes-ceg.component';
 import { TableNumbersCegComponent } from './table-numbers-ceg/table-numbers-ceg.component';
 import { TableSelectDesktopCegComponent } from './table-select-desktop-ceg/table-select-desktop-ceg.component';
 import { TableSelectMobileCegComponent } from './table-select-mobile-ceg/table-select-mobile-ceg.component';
-import { TableValidationRowCegComponent } from './table-validation-row-ceg/table-validation-row-ceg.component';
+import { TableSizeMediumDesktopCegComponent } from './table-size-medium-desktop-ceg/table-size-medium-desktop-ceg.component';
+import { TableSizeMediumMobileCegComponent } from './table-size-medium-mobile-ceg/table-size-medium-mobile-ceg.component';
+import { TableSizeSmallDesktopCegComponent } from './table-size-small-desktop-ceg/table-size-small-desktop-ceg.component';
+import { TableSizeSmallMobileCegComponent } from './table-size-small-mobile-ceg/table-size-small-mobile-ceg.component';
+import { TableStickyColumnHeaderDesktopCegComponent } from './table-sticky-column-header-desktop-ceg/table-sticky-column-header-desktop-ceg.component';
+import { TableStickyColumnHeaderMobileCegComponent } from './table-sticky-column-header-mobile-ceg/table-sticky-column-header-mobile-ceg.component';
+import { TableStickyRowHeaderDesktopCegComponent } from './table-sticky-row-header-desktop-ceg/table-sticky-row-header-desktop-ceg.component';
 import { TableValidationCellCegComponent } from './table-validation-cell-ceg/table-validation-cell-ceg.component';
-import '@elvia/elvis-tooltip';
-import '@elvia/elvis-accordion';
-import '@elvia/elvis-divider';
-import { SharedDocumentationModule } from 'src/app/shared/component-documentation/shared-component-documentation.module';
+import { TableValidationRowCegComponent } from './table-validation-row-ceg/table-validation-row-ceg.component';
 
 @NgModule({
   declarations: [
     TableDocComponent,
     TableCegComponent,
-    TableSizeMediumDesktopCegComponent,
-    TableSizeMediumMobileCegComponent,
-    TableSizeSmallDesktopCegComponent,
-    TableSizeSmallMobileCegComponent,
     TableAlignmentCegComponent,
     TableButtonDesktopCegComponent,
     TableButtonMobileCegComponent,
     TableCheckboxDesktopCegComponent,
     TableCheckboxMobileCegComponent,
+    TableExtraInformationTooltipCegComponent,
     TableFootnoteCegComponent,
     TableInputDesktopCegComponent,
     TableInputMobileCegComponent,
     TableLongColumnCegComponent,
-    TableStickyRowHeaderDesktopCegComponent,
-    TableStickyColumnHeaderDesktopCegComponent,
-    TableStickyColumnHeaderMobileCegComponent,
     TableNoStripesCegComponent,
     TableNumbersCegComponent,
     TableSelectDesktopCegComponent,
     TableSelectMobileCegComponent,
-    TableValidationRowCegComponent,
+    TableSizeMediumDesktopCegComponent,
+    TableSizeMediumMobileCegComponent,
+    TableSizeSmallDesktopCegComponent,
+    TableSizeSmallMobileCegComponent,
+    TableStickyColumnHeaderDesktopCegComponent,
+    TableStickyColumnHeaderMobileCegComponent,
+    TableStickyRowHeaderDesktopCegComponent,
     TableValidationCellCegComponent,
+    TableValidationRowCegComponent,
   ],
   imports: [SharedDocumentationModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
---
+++ packages/web/src/app/doc-pages/components/table-doc/table-extra-information-tooltip-ceg/table-extra-information-tooltip-ceg.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { StaticComponentExample } from 'src/app/shared/component-documentation/ceg';
+
+import * as template from 'html-loader!./table-extra-information-tooltip-ceg.component.html';
+
+@Component({
+  selector: 'app-table-extra-information-tooltip-ceg',
+  templateUrl: './table-extra-information-tooltip-ceg.component.html',
+  providers: [{ provide: StaticComponentExample, useExisting: TableExtraInformationTooltipCegComponent }],
+})
+export class TableExtraInformationTooltipCegComponent implements StaticComponentExample {
+  html = template.default;
+}
