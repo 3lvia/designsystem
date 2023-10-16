6b9eb4cd7d405ee34394683662b1875aecd57377
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-10-16 14:03:05 +0200

    * docs(divider): translate CEG to Norwegian

--- packages/web/src/app/doc-pages/components/divider-doc/divider-ceg/divider-ceg.component.ts
+++ packages/web/src/app/doc-pages/components/divider-doc/divider-ceg/divider-ceg.component.ts
@@ -40,7 +40,7 @@ export class DividerCegComponent implements ComponentExample {
       },
       groupOrder: ['Typography'],
       staticProps: {
-        heading: 'Heading',
+        heading: 'Overskrift',
       },
     },
     {
