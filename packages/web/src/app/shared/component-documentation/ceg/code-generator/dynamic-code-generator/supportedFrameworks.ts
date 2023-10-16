5329c4fafb59b418e01c3da31a17d1bd7eea522e
Erik Tallang <e.tallang@gmail.com>
2023-10-16 13:20:54 +0200

    docs(ceg): cast angular events to any

---
+++ packages/web/src/app/shared/component-documentation/ceg/code-generator/dynamic-code-generator/supportedFrameworks.ts
@@ -0,0 +1,22 @@
+type FrameworkName = 'angular' | 'vue';
+export interface FrameworkSpec {
+  attributePrefix?: string;
+  attributePostfix?: string;
+  eventPrefix?: string;
+  eventPostfix?: string;
+  castEventDataAsAny?: boolean;
+}
+
+export const frameworks: Record<FrameworkName, FrameworkSpec> = {
+  angular: {
+    attributePrefix: '[',
+    attributePostfix: ']',
+    eventPrefix: '(',
+    eventPostfix: ')',
+    castEventDataAsAny: true,
+  },
+  vue: {
+    attributePrefix: ':',
+    eventPrefix: '@',
+  },
+};
