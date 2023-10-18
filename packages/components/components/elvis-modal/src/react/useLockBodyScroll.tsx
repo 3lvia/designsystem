a4126daaa5fcd2bb5300e275157075934169d1d9
Erik Tallang <e.tallang@gmail.com>
2023-10-18 15:39:34 +0200

    fix(modal): prevent sticky elements from having wrong position in firefox

--- packages/components/components/elvis-modal/src/react/useLockBodyScroll.tsx
+++ packages/components/components/elvis-modal/src/react/useLockBodyScroll.tsx
@@ -1,36 +1,16 @@
 import { useRef } from 'react';
 
 export function useLockBodyScroll() {
-  const previousBodyWidth = useRef('');
-  const previousBodyPosition = useRef('');
-  const previousBodyTop = useRef(0);
-  const previousBodyLeft = useRef(0);
+  const previousOverflow = useRef('');
+  const root = document.documentElement ?? document.body;
 
   const lockBodyScroll = () => {
-    const top = window.scrollY;
-    const left = window.scrollX;
-
-    previousBodyPosition.current = document.body.style.position;
-    document.body.style.position = 'fixed';
-
-    previousBodyWidth.current = document.body.style.width;
-    document.body.style.width = '100%';
-
-    previousBodyTop.current = top;
-    document.body.style.top = `-${top}px`;
-
-    previousBodyLeft.current = left;
-    document.body.style.left = `-${left}px`;
+    previousOverflow.current = root.style.overflow;
+    root.style.overflow = 'hidden';
   };
 
   const releaseBodyScroll = () => {
-    document.body.style.position = previousBodyPosition.current;
-    document.body.style.width = previousBodyWidth.current;
-    window.scroll({
-      top: previousBodyTop.current,
-      left: previousBodyLeft.current,
-      behavior: 'instant',
-    });
+    root.style.overflow = previousOverflow.current;
   };
 
   return { lockBodyScroll, releaseBodyScroll };
