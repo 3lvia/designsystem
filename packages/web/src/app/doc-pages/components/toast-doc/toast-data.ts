c9a95c604f461d4c94384143ae5338d85416cf91
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2023-10-19 08:29:43 +0200

    docs(toast): move configuration props to ComponentData

--- packages/web/src/app/doc-pages/components/toast-doc/toast-data.ts
+++ packages/web/src/app/doc-pages/components/toast-doc/toast-data.ts
@@ -4,5 +4,46 @@ import ComponentData from '../component-data.interface';
 export const toastData: ComponentData = {
   changelog: changelogJson.content,
   name: 'Toast',
-  attributes: {},
+  attributes: {
+    title: {
+      description: 'The title showed in the toast.',
+      isArgument: true,
+      isRequired: false,
+      type: 'string',
+    },
+    body: {
+      description: 'The body showed in the toast.',
+      isArgument: true,
+      isRequired: false,
+      type: 'string',
+    },
+    duration: {
+      default: 7000,
+      description: 'How long the toast is visible, in milliseconds.',
+      isArgument: true,
+      isRequired: false,
+      type: 'number',
+    },
+    closable: {
+      default: false,
+      description: 'Whether the toast has a close button.',
+      isArgument: true,
+      isRequired: false,
+      type: 'boolean',
+    },
+    status: {
+      default: '"positive"',
+      description: 'The toast status, which changes its appearance',
+      isArgument: true,
+      isRequired: false,
+      type: '"positive" | "informative"',
+    },
+    customIcon: {
+      description: 'Allows for writing inner HTML to replace the default icon.',
+      example: /* ts */ `customIcon = '<i class="e-icon e-icon--crane"></i>'`,
+      isArgument: true,
+      isRequired: false,
+      type: 'string',
+    },
+  },
 };
