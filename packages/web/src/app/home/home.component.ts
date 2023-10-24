b47cdd90e5f3776a5052d1caaa5a07ab2f4c3a89
Erik Tallang <e.tallang@gmail.com>
2023-10-24 10:28:13 +0200

    chore(home): refactor home component (#2072)

--- packages/web/src/app/home/home.component.ts
+++ packages/web/src/app/home/home.component.ts
@@ -10,27 +10,49 @@ import { BreakpointService } from '../core/services/breakpoint.service';
 import { ThemeService } from '../core/services/theme.service';
 import { ThemeName } from '@elvia/elvis-colors';
 
+type Holiday = 'Birthday' | 'Christmas' | 'Halloween' | 'Pride' | 'ConstitutionDay' | 'None';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  overviewTitle = 'Elvia design system';
   pages = homeMenu;
   fontLoaded = false;
-  isNonHoliday = true;
-  isChristmas = false;
-  isHalloween = false;
-  isBirthday = false;
-  isPride = false;
-  isConstitutionDay = false;
-  private currentDate = new Date();
-  currentYear = this.currentDate.getFullYear();
+  holiday: Holiday = 'None';
   locale: LOCALE_CODE;
   currentTheme: ThemeName = 'light';
   changelog = changelogJson.content;
   isMobileScreenWidth: Observable<boolean>;
+
+  get topImagePath(): string {
+    if (this.holiday === 'Birthday') {
+      return `assets/holidayIllustrations/Birthday${new Date().getFullYear()}.png`;
+    } else if (this.holiday === 'Christmas') {
+      return 'assets/holidayIllustrations/ChristmasIllustration.png';
+    } else if (this.holiday === 'Halloween') {
+      return 'assets/holidayIllustrations/Halloween.png';
+    } else if (this.holiday === 'Pride') {
+      return 'assets/holidayIllustrations/Pride.png';
+    } else if (this.holiday === 'ConstitutionDay') {
+      return 'assets/holidayIllustrations/ConstitutionDay.png';
+    } else {
+      return 'assets/HomeIllustration.png';
+    }
+  }
+
+  get pageTitle(): string {
+    if (this.holiday === 'Birthday') {
+      return this.locale === 'en-GB' ? 'Happy Birthday' : 'Gratulerer med dagen';
+    } else if (this.holiday === 'Christmas') {
+      return this.locale === 'en-GB' ? 'Happy Holidays' : 'God jul';
+    } else if (this.holiday === 'ConstitutionDay') {
+      return 'Hipp Hipp Hurra';
+    } else {
+      return this.locale === 'en-GB' ? 'Elvia design system' : 'Elvia designsystem';
+    }
+  }
 
   constructor(
     localizationService: LocalizationService,
@@ -45,10 +67,8 @@ export class HomeComponent implements OnInit {
       .pipe(takeUntilDestroyed())
       .subscribe((locale) => {
         if (locale === Locale['en-GB']) {
-          this.overviewTitle = 'Elvia design system';
           this.locale = 'en-GB';
         } else {
-          this.overviewTitle = 'Elvia designsystem';
           this.locale = 'nb-NO';
         }
       });
@@ -68,28 +88,24 @@ export class HomeComponent implements OnInit {
     this.titleService.setTitle('Elvia design system');
   }
 
-  setHoliday = (): void => {
-    const startDateBirthday = new Date(this.currentYear, 1, 14);
-    const endDateBirthday = new Date(this.currentYear, 1, 20);
-    const startDateConstitutionDate = new Date(this.currentYear, 4, 9);
-    const endDateConstitutionDate = new Date(this.currentYear, 4, 17);
+  private setHoliday = (): void => {
+    const currentDate = new Date();
+    const currentYear = new Date().getFullYear();
+    const startDateBirthday = new Date(currentYear, 1, 14);
+    const endDateBirthday = new Date(currentYear, 1, 20);
+    const startDateConstitutionDate = new Date(currentYear, 4, 9);
+    const endDateConstitutionDate = new Date(currentYear, 4, 17);
 
-    this.isNonHoliday = false;
-    if (this.currentDate.getMonth() === 9 && this.currentDate.getDate() >= 25) {
-      this.isHalloween = true;
-    } else if (this.currentDate.getMonth() === 11) {
-      this.isChristmas = true;
-      this.overviewTitle = 'Happy Holidays';
-    } else if (this.currentDate >= startDateBirthday && this.currentDate <= endDateBirthday) {
-      this.isBirthday = true;
-      this.overviewTitle = 'Happy Birthday';
-    } else if (this.currentDate.getMonth() === 5) {
-      this.isPride = true;
-    } else if (this.currentDate >= startDateConstitutionDate && this.currentDate <= endDateConstitutionDate) {
-      this.isConstitutionDay = true;
-      this.overviewTitle = 'Hipp Hipp Hurra';
-    } else {
-      this.isNonHoliday = true;
+    if (currentDate.getMonth() === 9 && currentDate.getDate() >= 25) {
+      this.holiday = 'Halloween';
+    } else if (currentDate.getMonth() === 11) {
+      this.holiday = 'Christmas';
+    } else if (currentDate >= startDateBirthday && currentDate <= endDateBirthday) {
+      this.holiday = 'Birthday';
+    } else if (currentDate.getMonth() === 5) {
+      this.holiday = 'Pride';
+    } else if (currentDate >= startDateConstitutionDate && currentDate <= endDateConstitutionDate) {
+      this.holiday = 'ConstitutionDay';
     }
   };
 }
