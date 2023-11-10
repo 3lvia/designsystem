ed76644c2eff4b146cfd47062bc0ab4e67b0bead
Trygve Scheline Urdahl <trur@computas.com>
2023-11-10 07:20:17 +0100

    refactor(web): use CDK focus trap

--- packages/web/src/app/shell/header/header.module.ts
+++ packages/web/src/app/shell/header/header.module.ts
@@ -10,17 +10,11 @@ import '@elvia/elvis-modal';
 import '@elvia/elvis-popover';
 import '@elvia/elvis-divider';
 import { SearchHighlighterPipe } from '../../shared/search-highlighter.pipe';
-import { TrapFocusDirective } from './search-menu/trap-focus.directive';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule, SearchHighlighterPipe],
-  declarations: [
-    HeaderComponent,
-    SearchMenuComponent,
-    MobileMenuComponent,
-    ThemeSwitchComponent,
-    TrapFocusDirective,
-  ],
+  imports: [CommonModule, RouterModule, FormsModule, SearchHighlighterPipe, A11yModule],
+  declarations: [HeaderComponent, SearchMenuComponent, MobileMenuComponent, ThemeSwitchComponent],
   exports: [HeaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
