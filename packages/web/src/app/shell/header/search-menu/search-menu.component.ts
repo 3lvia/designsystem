f65955c5f766214d808fbe8b1d0cd6060e78471a
Trygve Scheline Urdahl <trur@computas.com>
2023-11-10 08:54:44 +0100

    fix(search-menu): focus trap inside only search for desktop

--- packages/web/src/app/shell/header/search-menu/search-menu.component.ts
+++ packages/web/src/app/shell/header/search-menu/search-menu.component.ts
@@ -18,6 +18,7 @@ import { SearchStatus, SearchItem } from './search-menu.interface';
 import { Router } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { SearchResult, Searcher } from 'src/app/shared/searcher';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-search-menu',
@@ -40,6 +41,7 @@ export class SearchMenuComponent implements OnInit, AfterViewInit {
     private cmsService: CMSService,
     private localizationService: LocalizationService,
     private router: Router,
+    public media: MediaMatcher,
   ) {
     this.localizationService
       .listenLocalization()
