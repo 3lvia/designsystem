0474144b9a4c1cfab48c0e6c91aa148bcfab1bbe
Trygve Scheline Urdahl <trur@computas.com>
2023-11-23 13:39:53 +0100

    fix(typography-doc): style fixes, make class copyable

--- packages/web/src/app/doc-pages/brand/typography-doc/typography-example-exhibit/typography-example-exhibit-list/typography-example-exhibit-list.component.ts
+++ packages/web/src/app/doc-pages/brand/typography-doc/typography-example-exhibit/typography-example-exhibit-list/typography-example-exhibit-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Modifier, Typographies } from '../typographies';
+import { CopyModule } from 'src/app/shared/copy/copy.module';
 
 @Component({
   standalone: true,
+  imports: [CopyModule],
   selector: 'app-typography-example-exhibit-list',
   templateUrl: './typography-example-exhibit-list.component.html',
   styleUrls: ['./typography-example-exhibit-list.component.scss'],
