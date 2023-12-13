d082ef706eeab5be00bb5a1b7041c3816668c4e7
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2023-12-13 04:02:59 -0800

    Lego 3287: stepper events kommer aldri ut nar triggret inni content sloten (#2288)
    * fix(stepper): event listeners on step slot
    
    * chore(stepper): changelog and bump
    
    * test: add stepper to react test project
    
    * docs(stepper): update types

--- packages/web/src/app/doc-pages/components/stepper-doc/stepper-data.ts
+++ packages/web/src/app/doc-pages/components/stepper-doc/stepper-data.ts
@@ -59,8 +59,9 @@ export const stepperData: ComponentData<BaseStepperProps & ContentMock> = {
     },
     content: {
       isRequired: true,
-      type: 'HTMLElement | JSX.Element',
-      description: 'Text, images, tables or any other content (slot in web component).',
+      type: 'HTMLElement | JSX.Element[]',
+      description:
+        'Text, images, tables or any other content. For React, an array of elements. For web component, a slot with element children.',
     },
     value: {
       type: 'number',
