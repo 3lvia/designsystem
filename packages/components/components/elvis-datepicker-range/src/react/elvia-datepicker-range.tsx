f5d3029b9f4a9e058476262921b6e5d5752b603a
Trygve Scheline Urdahl <trur@computas.com>
2023-12-15 10:49:28 +0100

    fix(timepicker): don't select by default minDate/maxDate time if not minDate/maxDate is selected

--- packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.tsx
+++ packages/components/components/elvis-datepicker-range/src/react/elvia-datepicker-range.tsx
@@ -90,7 +90,7 @@ export const DatepickerRange: FC<DatepickerRangeProps> = ({
   const setTime = (date: Date | number, when: 'startOfDay' | 'endOfDay'): Date => {
     const dateCopy = new Date(date);
     if (when === 'startOfDay') {
-      if (minDate) {
+      if (minDate && isSameDate(dateCopy, minDate)) {
         dateCopy.setHours(
           minDate.getHours(),
           minDate.getMinutes(),
@@ -101,7 +101,7 @@ export const DatepickerRange: FC<DatepickerRangeProps> = ({
         dateCopy.setHours(0, 0, 0, 0);
       }
     } else {
-      if (maxDate) {
+      if (maxDate && isSameDate(dateCopy, maxDate)) {
         dateCopy.setHours(
           maxDate.getHours(),
           maxDate.getMinutes(),
