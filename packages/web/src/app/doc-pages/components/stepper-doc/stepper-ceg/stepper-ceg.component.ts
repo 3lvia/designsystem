2f52f0d9c781c132059083c6f1da6c238a794a12
majabnils <mani@computas.com>
2024-01-10 15:48:45 +0100

    docs(stepper): change back example in ceg

--- packages/web/src/app/doc-pages/components/stepper-doc/stepper-ceg/stepper-ceg.component.ts
+++ packages/web/src/app/doc-pages/components/stepper-doc/stepper-ceg/stepper-ceg.component.ts
@@ -2,27 +2,19 @@ import { Component } from '@angular/core';
 import { BaseStepperProps } from '@elvia/elvis-stepper/react';
 import { CegControlManager, ComponentExample } from 'src/app/shared/component-documentation/ceg';
 
-interface StepState {
-  title: string;
-  isError: boolean;
-  isCompleted: boolean;
-}
-interface StepStates {
-  [step: number]: Partial<StepState>;
-}
-
 @Component({
   selector: 'app-stepper-ceg',
   templateUrl: './stepper-ceg.component.html',
+  styleUrls: ['./stepper-ceg.component.scss'],
   providers: [{ provide: ComponentExample, useExisting: StepperCegComponent }],
 })
 export class StepperCegComponent implements ComponentExample {
   elementName = 'stepper';
   stepperStates: BaseStepperProps['steps'] = {
-    '1': { heading: 'Kontakt' },
-    '2': { heading: 'Kontakt' },
-    '3': { heading: 'Kontakt' },
-    '4': { heading: 'Kontakt', nextButtonText: 'Lagre' },
+    '1': { heading: 'Tittel 1' },
+    '2': { heading: 'Tittel 2' },
+    '3': { heading: 'Tittel 3' },
+    '4': { heading: 'Tittel 4', nextButtonText: 'Lagre' },
   };
   cegContent = new CegControlManager<BaseStepperProps>([
     {
