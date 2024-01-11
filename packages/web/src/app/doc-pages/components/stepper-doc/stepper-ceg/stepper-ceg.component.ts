0398ea450f344c545dbc7311c73e24370142faec
Maja B. Nilsen <111271903+majabnils@users.noreply.github.com>
2024-01-11 09:07:05 +0100

    Lego 3286 eksempel tekster oppdatere eksempel tekstene vare (#2337)
    * docs(accordion): change example in ceg
    
    * docs(alert): change example in ceg
    
    * docs(box): change example in ceg
    
    * docs(button): change example in ceg
    
    * docs(card): change example in ceg
    
    * docs(checkbox): change example in ceg
    
    * docs(divider): change example in ceg
    
    * docs(link): change example in ceg
    
    * docs(list): change example in ceg
    
    * docs(modal): change example in ceg
    
    * docs(radio-button): change example in ceg
    
    * docs(table): change example in ceg
    
    * docs(tag): change example in ceg
    
    * docs(input): change example in ceg
    
    * docs(toggle): change example in ceg
    
    * docs(tooltip): change example in ceg
    
    * docs(toast): change button in example in ceg
    
    * docs(spotlight): change button in example in ceg
    
    * docs(header): change button in example in ceg
    
    * docs(box): update example in ceg
    
    * docs(chip): change example in ceg
    
    * docs(stepper): change example in ceg
    
    * docs(autocomplete): change example in ceg
    
    * fix: small fixes to examples in components
    
    * fix: small fixes to examples in components
    
    * docs(box): change back example in ceg
    
    * docs(modal): change example in ceg
    
    * docs(input): change example in ceg
    
    * docs(stepper): change back example in ceg
    
    * docs(modal): change example in ceg
    
    ---------
    
    Co-authored-by: majabnils <mani@computas.com>

--- packages/web/src/app/doc-pages/components/stepper-doc/stepper-ceg/stepper-ceg.component.ts
+++ packages/web/src/app/doc-pages/components/stepper-doc/stepper-ceg/stepper-ceg.component.ts
@@ -2,15 +2,6 @@ import { Component } from '@angular/core';
 import { BaseStepperProps } from '@elvia/elvis-stepper/react';
 import { CegControlManager, ComponentExample } from 'src/app/shared/component-documentation/ceg';
 
-interface StepState {
-  title: string;
-  isError: boolean;
-  isCompleted: boolean;
-}
-interface StepStates {
-  [step: number]: Partial<StepState>;
-}
-
 @Component({
   selector: 'app-stepper-ceg',
   templateUrl: './stepper-ceg.component.html',
@@ -20,8 +11,8 @@ interface StepStates {
 export class StepperCegComponent implements ComponentExample {
   elementName = 'stepper';
   stepperStates: BaseStepperProps['steps'] = {
-    '1': { heading: 'Tittel 1', isCompleted: true },
-    '2': { heading: 'Tittel 2', isCompleted: true },
+    '1': { heading: 'Tittel 1' },
+    '2': { heading: 'Tittel 2' },
     '3': { heading: 'Tittel 3' },
     '4': { heading: 'Tittel 4', nextButtonText: 'Lagre' },
   };
