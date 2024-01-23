661ff9b3ae93b619bf0cf27a136a7b1f56bdfca5
Trygve Scheline Urdahl <trur@computas.com>
2024-01-23 10:17:56 +0100

    chore: remove calls to warnDeprecatedProps

--- packages/components/components/elvis-box/src/react/elvia-box.tsx
+++ packages/components/components/elvis-box/src/react/elvia-box.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
 import { BoxArea, BoxColoredLine, BoxHeading, BoxContent } from './styledComponents';
-import { useSlot, warnDeprecatedProps } from '@elvia/elvis-toolbox';
+import { useSlot } from '@elvia/elvis-toolbox';
 import { BoxProps } from './elvia-box.types';
-import { config } from './config';
 
 export const Box: FC<BoxProps> = function ({
   heading,
@@ -13,16 +12,11 @@ export const Box: FC<BoxProps> = function ({
   webcomponent,
   ...rest
 }) {
-  warnDeprecatedProps(config, arguments[0]);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { title, ...restWithoutTitle } = rest;
-
   const { ref: boxContent } = useSlot<HTMLDivElement>('content', webcomponent);
   const { ref: boxHeading } = useSlot<HTMLDivElement>('heading', webcomponent);
 
   return (
-    <BoxArea className={className} style={inlineStyle} {...restWithoutTitle} data-testid="box-area">
+    <BoxArea className={className} style={inlineStyle} {...rest} data-testid="box-area">
       <BoxHeading ref={boxHeading}>{heading}</BoxHeading>
       <BoxContent data-testid="box-content">
         {isColored && <BoxColoredLine data-testid="box-colored-line"></BoxColoredLine>}
