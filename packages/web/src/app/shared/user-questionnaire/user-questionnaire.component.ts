45d4a0fa8af3e22b8cbd8acadf27c91ee4e7d718
Trygve Scheline Urdahl <trur@computas.com>
2024-01-30 11:05:57 +0100

    feat(questionnaire): store in local storage when completed

--- packages/web/src/app/shared/user-questionnaire/user-questionnaire.component.ts
+++ packages/web/src/app/shared/user-questionnaire/user-questionnaire.component.ts
@@ -2,8 +2,16 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { type DropdownItem } from '@elvia/elvis-dropdown';
 import { UserQuestionnaireIllustrationComponent } from './user-questionnaire-illustration/user-questionnaire-illustration.component';
 import { UserQuestionnaireService } from './user-questionnaire.service';
+import '@elvia/elvis-modal';
+import '@elvia/elvis-dropdown';
 
 type FormName = 'role' | 'feedback';
+
+const USER_QUESTIONNAIRE_STORAGE_KEY = 'hasCompletedUserQuestionnaire-30.01.2024';
+
+/**
+ * Remember to delete the empty <form>-elements from index.html when this component is removed!
+ */
 
 @Component({
   selector: 'app-user-questionnaire',
@@ -14,7 +22,6 @@ type FormName = 'role' | 'feedback';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class UserQuestionnaireComponent {
-  isOpen = true;
   roller: DropdownItem[] = [
     { label: 'UX / UI designer', value: 'uxui-desiger' },
     { label: 'Tjenestedesigner', value: 'tjenestedesigner' },
@@ -25,29 +32,43 @@ export class UserQuestionnaireComponent {
   rolle: string | undefined;
   step: FormName = 'role';
 
+  private hasBeenCompleted = localStorage.getItem(USER_QUESTIONNAIRE_STORAGE_KEY) === 'true';
+
+  // Only open modal on mount if the questionnaire hasn't been completed before
+  isOpen = !this.hasBeenCompleted;
+
   constructor(private userQuestionnaireService: UserQuestionnaireService) {}
 
   onSubmit = (event: SubmitEvent) => {
     event.preventDefault();
 
     const formData = new FormData(event.target as HTMLFormElement);
-    const formObject = Object.fromEntries(formData as any);
-    console.log(formObject);
+    const formState = Object.fromEntries(formData as any);
 
-    this.userQuestionnaireService.submitForm(formObject).subscribe(() => {});
-
-    // Debugging, make submit toggle between the two views
-    if (event.target && 'name' in event.target && typeof event.target.name === 'string') {
-      switch (event.target.name as FormName) {
-        case 'role': {
-          this.step = 'feedback';
-          break;
-        }
-        case 'feedback': {
-          this.step = 'role';
-          break;
-        }
+    this.userQuestionnaireService.submitForm(formState).subscribe(() => {
+      if (this.step === 'role') {
+        this.step = 'feedback';
+        this.setHasCompletedOrClosedQuestionnaire();
+      } else if (this.step === 'feedback') {
+        this.isOpen = false;
       }
-    }
+    });
+
+    // Debugging, make submit send to next form even when submit fails
+    // if (this.step === 'role') {
+    //   this.step = 'feedback';
+    //   this.setHasCompletedOrClosedQuestionnaire();
+    // } else if (this.step === 'feedback') {
+    //   this.isOpen = false;
+    // }
+  };
+
+  onModalClose = () => {
+    this.isOpen = !this.isOpen;
+    this.setHasCompletedOrClosedQuestionnaire();
+  };
+
+  private setHasCompletedOrClosedQuestionnaire = () => {
+    localStorage.setItem(USER_QUESTIONNAIRE_STORAGE_KEY, 'true');
   };
 }
