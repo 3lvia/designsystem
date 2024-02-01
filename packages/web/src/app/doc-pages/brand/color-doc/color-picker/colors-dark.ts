73d72ce82cfc1112db868aff145996bb740f4d0d
majabnils <mani@computas.com>
2024-02-01 09:06:09 +0100

    fix(colors): change base token category name from data to tertiary

--- packages/web/src/app/doc-pages/brand/color-doc/color-picker/colors-dark.ts
+++ packages/web/src/app/doc-pages/brand/color-doc/color-picker/colors-dark.ts
@@ -12,9 +12,9 @@ type SignalColors = DarkThemeColors['signal-colors'];
 type SignalColor = keyof SignalColors;
 const signalColors = Object.keys(darkThemeColors['signal-colors']) as SignalColor[];
 
-type DataColors = DarkThemeColors['data-colors'];
-type DataColor = keyof DataColors;
-const dataColors = Object.keys(darkThemeColors['data-colors']) as DataColor[];
+type TertiaryColors = DarkThemeColors['data-colors'];
+type TertiaryColor = keyof TertiaryColors;
+const tertiaryColors = Object.keys(darkThemeColors['data-colors']) as TertiaryColor[];
 
 type GreyColors = DarkThemeColors['grey-colors'];
 type GreyColor = keyof GreyColors;
@@ -23,6 +23,6 @@ const greyColors = Object.keys(darkThemeColors['grey-colors']) as GreyColor[];
 export const darkColors: ColorsObject = {
   primary: primaryColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['primary'],
   secondary: signalColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['secondary'],
-  data: dataColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['data'],
+  tertiary: tertiaryColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['tertiary'],
   grey: greyColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['grey'],
 };
