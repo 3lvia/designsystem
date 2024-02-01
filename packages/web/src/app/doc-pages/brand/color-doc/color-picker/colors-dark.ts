4c911e22da181b551b22bad69550aa0884ec547a
majabnils <mani@computas.com>
2024-02-01 12:05:09 +0100

    fix(colors): small change for consistency

--- packages/web/src/app/doc-pages/brand/color-doc/color-picker/colors-dark.ts
+++ packages/web/src/app/doc-pages/brand/color-doc/color-picker/colors-dark.ts
@@ -8,9 +8,9 @@ type PrimaryColors = DarkThemeColors['primary-colors'];
 type PrimaryColor = keyof PrimaryColors;
 const primaryColors = Object.keys(darkThemeColors['primary-colors']) as PrimaryColor[];
 
-type SignalColors = DarkThemeColors['signal-colors'];
-type SignalColor = keyof SignalColors;
-const signalColors = Object.keys(darkThemeColors['signal-colors']) as SignalColor[];
+type SecondaryColors = DarkThemeColors['signal-colors'];
+type SecondaryColor = keyof SecondaryColors;
+const secondaryColors = Object.keys(darkThemeColors['signal-colors']) as SecondaryColor[];
 
 type TertiaryColors = DarkThemeColors['data-colors'];
 type TertiaryColor = keyof TertiaryColors;
@@ -22,7 +22,9 @@ const greyColors = Object.keys(darkThemeColors['grey-colors']) as GreyColor[];
 
 export const darkColors: ColorsObject = {
   primary: primaryColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['primary'],
-  secondary: signalColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['secondary'],
+  secondary: secondaryColors.map((colorName) =>
+    getColorElement(colorName, 'dark'),
+  ) as ColorsObject['secondary'],
   tertiary: tertiaryColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['tertiary'],
   grey: greyColors.map((colorName) => getColorElement(colorName, 'dark')) as ColorsObject['grey'],
 };
