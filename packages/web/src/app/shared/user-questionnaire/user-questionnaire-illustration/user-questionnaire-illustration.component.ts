d1f6470e362511c7b08bb518c5a576722d1235f7
Trygve Scheline Urdahl <trur@computas.com>
2024-02-01 08:51:20 +0100

    chore(user-questionnaire-illustration): make template svg

--- packages/web/src/app/shared/user-questionnaire/user-questionnaire-illustration/user-questionnaire-illustration.component.ts
+++ packages/web/src/app/shared/user-questionnaire/user-questionnaire-illustration/user-questionnaire-illustration.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-questionnaire-illustration',
   standalone: true,
-  templateUrl: './user-questionnaire-illustration.component.html',
+  templateUrl: './user-questionnaire-illustration.component.svg',
 })
 export class UserQuestionnaireIllustrationComponent {}
