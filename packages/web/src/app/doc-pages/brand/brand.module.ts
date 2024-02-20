00ad501058035c5334c928d03c0866ec8e443428
Trygve Scheline Urdahl <trur@computas.com>
2024-02-20 09:34:18 +0100

    refactor(web): bootstrap app with standalone API

--- packages/web/src/app/doc-pages/brand/brand.module.ts
+++ packages/web/src/app/doc-pages/brand/brand.module.ts
@@ -3,8 +3,6 @@ import { ColorModule } from './color-doc/color.module';
 import { IconDocModule } from './icon-doc/icon-doc.module';
 import { LayoutDocModule } from './layout-doc/layout-doc.module';
 
-
-
 @NgModule({
   imports: [ColorModule, IconDocModule, LayoutDocModule],
 })
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,13 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule, AppRoutingModule, HttpClientModule), provideAnimations()],
+}).catch((err) => console.error(err));
