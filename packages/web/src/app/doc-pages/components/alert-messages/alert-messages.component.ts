f3bcd44dedca4a42725ccb640f7e145cba5546d3
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-02-21 08:38:27 +0100

    Update CHANGELOG.json and remove unused alert components

--- packages/web/src/app/doc-pages/components/alert-messages/alert-messages.component.ts
+++ packages/web/src/app/doc-pages/components/alert-messages/alert-messages.component.ts
@@ -12,7 +12,6 @@ import { AlertGlobalActionsCegComponent } from './alert-global-actions-ceg/alert
 import { AlertGlobalCegComponent } from './alert-global-ceg/alert-global-ceg.component';
 import { AlertGlobalClosableCegComponent } from './alert-global-closable-ceg/alert-global-closable-ceg.component';
 import { AlertGlobalExpandableCegComponent } from './alert-global-expandable-ceg/alert-global-expandable-ceg.component';
-import { AlertGlobalNoTitleCegComponent } from './alert-global-no-title-pandable-ceg/alert-global-no-title-ceg.component';
 import { AlertLocalCegComponent } from './alert-local-ceg/alert-local-ceg.component';
 import { AlertLocalClosableCegComponent } from './alert-local-closable-ceg/alert-local-closable-ceg.component';
 import { AlertRoleAlertCegComponent } from './alert-role-alert-ceg/alert-role-alert-ceg.component';
@@ -36,7 +35,6 @@ import { AlertRoleStatusCegComponent } from './alert-role-status-ceg/alert-role-
     AlertGlobalClosableCegComponent,
     AlertGlobalExpandableCegComponent,
     AlertGlobalActionsCegComponent,
-    AlertGlobalNoTitleCegComponent,
     AlertLocalCegComponent,
     AlertLocalClosableCegComponent,
     CopyComponent,
@@ -60,7 +58,7 @@ export class AlertMessagesComponent {
   doesGlobalDefault = ['Give a general message to all users across the pages'];
   dontsGlobalDefault = ['After an user action'];
 
-  //Accsesability
+  //Accessability
   doesAriaRoleAlertDefault = [
     'Alerts are used occasionally',
     'Situations where immediate attention is required',
