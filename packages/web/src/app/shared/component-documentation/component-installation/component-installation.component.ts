da9d033c62ac800c94462ff0f577a62f7c370bcd
majabnils <mani@computas.com>
2024-03-05 10:54:48 +0100

    feat(web): choose framework in installation guide

--- packages/web/src/app/shared/component-documentation/component-installation/component-installation.component.ts
+++ packages/web/src/app/shared/component-documentation/component-installation/component-installation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, Input, OnInit } from '@angular/core';
 
 import { CopyComponent } from '../../copy/copy.component';
 import { getPackageName } from './getPackageName';
@@ -9,14 +9,30 @@ import ComponentData from 'src/app/doc-pages/components/component-data.interface
   templateUrl: './component-installation.component.html',
   standalone: true,
   imports: [CopyComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ComponentInstallationComponent implements OnInit {
   @Input() componentData: ComponentData;
   reactElementName: string;
   packageName: string;
+  frameworkName: 'angular' | 'react' | 'vue' = 'angular';
 
   ngOnInit() {
     this.reactElementName = this.componentData.name;
     this.packageName = getPackageName(this.componentData.name);
+  }
+
+  setFramework(activeTabIndex: number): void {
+    switch (activeTabIndex) {
+      case 1:
+        this.frameworkName = 'react';
+        break;
+      case 2:
+        this.frameworkName = 'vue';
+        break;
+      default:
+        this.frameworkName = 'angular';
+        break;
+    }
   }
 }
