dc0cc8bbd72e310a286f5ef9a4ad318c32a2f072
Trygve Scheline Urdahl <trur@computas.com>
2024-03-15 12:30:14 +0100

    chore(web): move all illustration imports from playground to app.component

--- packages/web/src/app/dev/v2-playground/v2-playground-illustrations/v2-playground-illustrations.component.ts
+++ packages/web/src/app/dev/v2-playground/v2-playground-illustrations/v2-playground-illustrations.component.ts
@@ -1,30 +1,4 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import '@elvia/elvis-illustrations/broken';
-import '@elvia/elvis-illustrations/clock';
-import '@elvia/elvis-illustrations/confirmation';
-import '@elvia/elvis-illustrations/connected';
-import '@elvia/elvis-illustrations/consumption';
-import '@elvia/elvis-illustrations/explain';
-import '@elvia/elvis-illustrations/heating';
-import '@elvia/elvis-illustrations/hello';
-import '@elvia/elvis-illustrations/installation';
-import '@elvia/elvis-illustrations/meter-reading';
-import '@elvia/elvis-illustrations/new';
-import '@elvia/elvis-illustrations/no-connection';
-import '@elvia/elvis-illustrations/no-notifications';
-import '@elvia/elvis-illustrations/no-position';
-import '@elvia/elvis-illustrations/no-results';
-import '@elvia/elvis-illustrations/nothing-new';
-import '@elvia/elvis-illustrations/office';
-import '@elvia/elvis-illustrations/photo';
-import '@elvia/elvis-illustrations/power-line';
-import '@elvia/elvis-illustrations/price';
-import '@elvia/elvis-illustrations/secret';
-import '@elvia/elvis-illustrations/sharing';
-import '@elvia/elvis-illustrations/statistics';
-import '@elvia/elvis-illustrations/support';
-import '@elvia/elvis-illustrations/unplugged';
-import '@elvia/elvis-illustrations/user';
 
 @Component({
   selector: 'app-v2-playground-illustrations',
