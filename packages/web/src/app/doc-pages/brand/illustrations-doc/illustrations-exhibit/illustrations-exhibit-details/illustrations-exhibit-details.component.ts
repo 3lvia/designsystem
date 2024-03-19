e827d2f929ab12e9b9331c036f1b266f6e57fd6f
Trygve Scheline Urdahl <trur@computas.com>
2024-03-19 09:39:03 +0100

    feat(illustration-exhibit-details): create svg and png download blob from illustration component

--- packages/web/src/app/doc-pages/brand/illustrations-doc/illustrations-exhibit/illustrations-exhibit-details/illustrations-exhibit-details.component.ts
+++ packages/web/src/app/doc-pages/brand/illustrations-doc/illustrations-exhibit/illustrations-exhibit-details/illustrations-exhibit-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, effect, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 import { IllustrationsExhibitService } from '../illustrations-exhibit.service';
@@ -14,24 +14,26 @@ import { CopyComponent } from 'src/app/shared/copy/copy.component';
   styleUrl: './illustrations-exhibit-details.component.scss',
 })
 export class IllustrationsExhibitDetailsComponent {
+  private elementRef = inject(ElementRef);
+
   private illustrationExhibitService = inject(IllustrationsExhibitService);
-  protected selectedIllustration = toSignal(this.illustrationExhibitService.selectedIllustration);
-  protected colorValue = toSignal(this.illustrationExhibitService.colorValue);
-  protected locale = toSignal(inject(LocalizationService).listenLocalization());
+  selectedIllustration = toSignal(this.illustrationExhibitService.selectedIllustration);
+  colorValue = toSignal(this.illustrationExhibitService.colorValue);
+  locale = toSignal(inject(LocalizationService).listenLocalization());
+
+  svgBlobUrl = '';
+  pngBlobUrl = '';
 
   get importString() {
     return `@elvia/illustrations/${this.selectedIllustration()}`;
   }
-
   get importStatement() {
     return `import '@elvia/illustrations/${this.selectedIllustration()}';`;
   }
-
   get colorString() {
     const color = this.colorValue();
     return !color || color === 'grey' ? '' : ` color="${color}"`;
   }
-
   get usageString() {
     return `<elvia-illustrations-${this.selectedIllustration()}${this.colorString} />`;
   }
@@ -42,4 +44,66 @@ export class IllustrationsExhibitDetailsComponent {
   clearSelection() {
     this.illustrationExhibitService.setSelectedIllustration(null);
   }
+
+  constructor() {
+    effect(() => {
+      this.svgBlobUrl = this.createSvgBlobFromElement();
+      this.createPngBlobFromSvg();
+    });
+  }
+
+  imageFileName = (format: 'svg' | 'png') => {
+    if (this.colorValue() === 'grey' || !this.colorValue()) {
+      return `${this.selectedIllustration()}.${format}`;
+    }
+    return `${this.selectedIllustration()}_${this.colorValue()}.${format}`;
+  };
+
+  private createSvgBlobFromElement = () => {
+    const selectedIllustration = this.selectedIllustration();
+    if (!selectedIllustration) {
+      return '';
+    }
+
+    const illustrationElement = (this.elementRef.nativeElement as HTMLElement).querySelector(
+      `elvia-illustrations-${selectedIllustration}`,
+    );
+    const svgElement = illustrationElement?.shadowRoot?.querySelector('svg');
+    const styleElement = illustrationElement?.shadowRoot?.querySelector('style');
+    if (!styleElement || !svgElement) {
+      console.error('No SVG or style element found');
+      return '';
+    }
+
+    const newSvgElement = svgElement.cloneNode(true) as SVGElement;
+    newSvgElement.insertBefore(styleElement?.cloneNode(true) as Node, newSvgElement.firstChild);
+    newSvgElement.classList.add(this.colorValue() || '');
+
+    const svgString = new XMLSerializer().serializeToString(newSvgElement);
+    const blob = new Blob([svgString], { type: 'image/svg+xml' });
+    return URL.createObjectURL(blob);
+  };
+
+  private createPngBlobFromSvg = () => {
+    const img = new Image();
+    img.src = this.svgBlobUrl;
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      if (!ctx) {
+        console.error('Could not get 2d context');
+        return;
+      }
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      canvas.toBlob((blob) => {
+        if (!blob) {
+          console.error('Could not create blob');
+          return;
+        }
+        this.pngBlobUrl = URL.createObjectURL(blob);
+      });
+    };
+  };
 }
