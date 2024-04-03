2cc616d003ffd8bc41e5c4a36f13267a8eaf335f
Trygve Scheline Urdahl <trur@computas.com>
2024-04-03 11:38:43 +0200

    feat(empty-states-doc): add skeleton

--- packages/web/src/app/doc-pages/patterns/empty-states-doc/empty-states-doc.component.ts
+++ packages/web/src/app/doc-pages/patterns/empty-states-doc/empty-states-doc.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { EmptyStatesTopExampleComponent } from './empty-states-top-example/empty-states-top-example.component';
+import { ComponentHeaderComponent } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.component';
+import { ComponentSectionComponent } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.component';
+import { ComponentSubsectionComponent } from 'src/app/shared/component-documentation/component-structure/component-subsection/component-subsection.component';
+import { getDocPagesNotFromCMS } from 'src/app/shared/doc-pages';
+
+const emptyStatesDocPage = getDocPagesNotFromCMS('empty-states');
 
 @Component({
   selector: 'app-empty-states-doc',
   standalone: true,
-  imports: [],
+  imports: [
+    ComponentSectionComponent,
+    ComponentSubsectionComponent,
+    ComponentHeaderComponent,
+    RouterLink,
+    EmptyStatesTopExampleComponent,
+  ],
   templateUrl: './empty-states-doc.component.html',
   styleUrl: './empty-states-doc.component.scss',
 })
-export class EmptyStatesDocComponent {}
+export class EmptyStatesDocComponent {
+  title = emptyStatesDocPage?.title;
+  description = emptyStatesDocPage?.description;
+  figmaUrl = emptyStatesDocPage?.figmaUrl;
+}
---
+++ packages/web/src/app/doc-pages/patterns/empty-states-doc/empty-states-top-example/empty-states-top-example.component.ts
@@ -0,0 +1,11 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-states-top-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './empty-states-top-example.component.html',
+  styleUrl: './empty-states-top-example.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class EmptyStatesTopExampleComponent {}
