c854f3d1b70a92880c049b57c26c49d76c48d71d
Erik Tallang <e.tallang@gmail.com>
2024-04-10 15:14:01 +0200

    test(chips): concatinate chip tests

--- packages/components/components/elvis-chip/src/react/elvia-chip.spec.tsx
+++ packages/components/components/elvis-chip/src/react/elvia-chip.spec.tsx
@@ -11,44 +11,19 @@ test('should render', async ({ mount }) => {
   await expect(component).toBeAttached();
 });
 
-test('should be type choice', async ({ mount, page }) => {
-  await mount(<Chip value="Chip" type="choice" isSelected />);
+test('should pass visual diff check', async ({ mount, page }) => {
+  await mount(
+    <div style={{ display: 'grid', gap: '32px' }}>
+      <Chip value="Chip" type="choice" isSelected />
+      <Chip color="green" value="Chip" type="legend" isSelected />
+      <Chip color="blue" value="Chip" type="legend" isSelected />
+      <Chip color="orange" value="Chip" type="legend" isSelected />
+      <Chip color="purple" value="Chip" type="legend" isSelected />
+      <Chip color="red" value="Chip" type="legend" isSelected />
+      <Chip color="violet" value="Chip" type="legend" isSelected />
+      <Chip value="Chip" type="legend" isSelected />
+      <Chip value="Chip" type="removable" />
+    </div>,
+  );
   await percySnapshot(page, 'Choice chip');
-});
-
-test.describe('should have color', () => {
-  test('green', async ({ mount, page }) => {
-    await mount(<Chip color="green" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Green chip');
-  });
-  test('blue', async ({ mount, page }) => {
-    await mount(<Chip color="blue" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Blue chip');
-  });
-  test('orange', async ({ mount, page }) => {
-    await mount(<Chip color="orange" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Orange chip');
-  });
-  test('purple', async ({ mount, page }) => {
-    await mount(<Chip color="purple" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Purple chip');
-  });
-  test('red', async ({ mount, page }) => {
-    await mount(<Chip color="red" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Red chip');
-  });
-  test('violet', async ({ mount, page }) => {
-    await mount(<Chip color="violet" value="Chip" type="legend" isSelected />);
-    await percySnapshot(page, 'Violet chip');
-  });
-});
-
-test('should be type legend', async ({ mount, page }) => {
-  await mount(<Chip value="Chip" type="legend" isSelected />);
-  await percySnapshot(page, 'Legend chip');
-});
-
-test('should be type removable', async ({ mount, page }) => {
-  await mount(<Chip value="Chip" type="removable" />);
-  await percySnapshot(page, 'Removable chip');
 });
