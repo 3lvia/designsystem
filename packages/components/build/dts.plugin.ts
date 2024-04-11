2f88d61e4d846ebf723b4ee5c55674caf00c766c
Erik Tallang <e.tallang@gmail.com>
2024-04-11 14:47:40 +0200

    fix: sonar warning

--- packages/components/build/dts.plugin.ts
+++ packages/components/build/dts.plugin.ts
@@ -13,9 +13,9 @@ interface Props {
 // This and writeTypingsToDisc can be removed if we figure out how to define a more granular
 // outPath for our types.
 const resolveImportsOfPublicApi = (text: string): string => {
-  // [\\\/] matches for both forwards (UNIX) - and backwards (Windows)-slash
+  // [\\/] matches for both forwards (UNIX) - and backwards (Windows)-slash
   return text.replace(
-    /\.[\\\/]([\w-]+.public)/g,
+    /\.[\\/]([\w-]+.public)/g,
     (_match, fileName) => `..${path.sep}public-api${path.sep}${fileName}`,
   );
 };
