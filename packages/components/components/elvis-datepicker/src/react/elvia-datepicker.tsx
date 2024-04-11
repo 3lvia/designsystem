3324d764c1ba29d882c8354075982375cbbb00ca
Erik Tallang <e.tallang@gmail.com>
2024-04-11 12:11:39 +0200

    revert(datepicker): break datepicker

--- packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
+++ packages/components/components/elvis-datepicker/src/react/elvia-datepicker.tsx
@@ -1,4 +1,4 @@
-import call from '@elvia/elvis-assets-icons/dist/icons/call';
+import calendar from '@elvia/elvis-assets-icons/dist/icons/calendar';
 import {
   ErrorOptions,
   FormFieldContainer,
@@ -349,7 +349,7 @@ export const Datepicker: React.FC<DatepickerProps> = ({
             aria-haspopup="dialog"
           >
             <IconWrapper
-              icon={call}
+              icon={calendar}
               color={isDisabled ? 'text-disabled-1' : undefined}
               size={size === 'small' ? 'xs' : 'sm'}
             />
