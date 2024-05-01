4f5e3f1b822eb3b58f0adef97568d3190cd2dfbd
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-05-01 21:27:48 +0200

    Update elvia-dropdown.spec.tsx with new test cases

--- packages/components/components/elvis-dropdown/src/react/elvia-dropdown.spec.tsx
+++ packages/components/components/elvis-dropdown/src/react/elvia-dropdown.spec.tsx
@@ -4,51 +4,59 @@ import React from 'react';
 
 import { Dropdown } from '../../react';
 
-test.use({ viewport: { width: 500, height: 500 } });
+test.use({ viewport: { width: 1080, height: 300 } });
+
+const items = [
+  { value: 'Norge', label: 'Norge' },
+  { value: 'Sverige', label: 'Sverige' },
+  { value: 'Danmark', label: 'Danmark' },
+];
+
+const nestedItems = [
+  {
+    value: 'norge',
+    label: 'Norge',
+    children: [
+      {
+        label: 'Bergen',
+        value: 'bergen',
+        children: [
+          { label: 'Arna', value: 'arna' },
+          { label: 'Bergenhus', value: 'bergenhus' },
+        ],
+      },
+      { label: 'Trondheim', value: 'trondheim' },
+    ],
+  },
+  { value: 'spania', label: 'Spania' },
+  { value: 'tyskland', label: 'Tyskland' },
+];
 
 test('should render', async ({ mount }) => {
-  const component = await mount(
-    <Dropdown
-      value={'Norge'}
-      label={'Land'}
-      items={[
-        { value: 'Norge', label: 'Norge' },
-        { value: 'Sverige', label: 'Sverige' },
-        { value: 'Danmark', label: 'Danmark' },
-      ]}
-    />,
-  );
+  const component = await mount(<Dropdown value={'Norge'} label={'Land'} items={items} />);
   await expect(component).toBeAttached();
 });
 
 test('should look closed', async ({ mount, page }) => {
-  await mount(
-    <Dropdown
-      value={'Norge'}
-      label={'Land'}
-      items={[
-        { value: 'Norge', label: 'Norge' },
-        { value: 'Sverige', label: 'Sverige' },
-        { value: 'Danmark', label: 'Danmark' },
-      ]}
-    />,
-  );
+  await mount(<Dropdown value={'Norge'} label={'Land'} items={items} />);
   await percySnapshot(page, 'Closed Dropdown');
 });
 
 test('should look opened', async ({ mount, page }) => {
-  const component = await mount(
-    <Dropdown
-      value={'Norge'}
-      label={'Land'}
-      items={[
-        { value: 'Norge', label: 'Norge' },
-        { value: 'Sverige', label: 'Sverige' },
-        { value: 'Danmark', label: 'Danmark' },
-      ]}
-    />,
-  );
+  const component = await mount(<Dropdown value={'Norge'} label={'Land'} items={items} />);
 
   await component.getByRole('combobox').click();
   await percySnapshot(page, 'Open Dropdown');
+});
+
+test('should look opened with nested items', async ({ mount, page }) => {
+  await mount(<Dropdown label={'Sted'} items={nestedItems} />);
+
+  await page.getByRole('combobox').click();
+
+  await page.getByRole('option', { name: /norge/i }).hover();
+  await page.getByRole('option', { name: /bergen/i }).hover();
+  await page.waitForTimeout(300);
+
+  await percySnapshot(page, 'Open Dropdown with nested items');
 });
