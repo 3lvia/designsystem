a7b7889aca5cfdad2a516a0997f1ce393c5a344c
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-05-02 09:22:22 +0200

    test(dropdown): clean tests

--- packages/components/components/elvis-dropdown/src/react/elvia-dropdown.spec.tsx
+++ packages/components/components/elvis-dropdown/src/react/elvia-dropdown.spec.tsx
@@ -67,42 +67,7 @@ test('should look as expected', async ({ mount, page }) => {
   const dropdowns = await page.getByRole('combobox').all();
 
   await dropdowns[dropdowns.length - 1].click();
-  await percySnapshot(page, 'Dropdowns', { widths: [1200], enableLayout: true });
-});
-
-test('should look as expected 2', async ({ mount, page }) => {
-  await mount(
-    <div className="e-flex e-gap-8 e-flex-direction-column">
-      <Dropdown value={'Norge'} size={'small'} label={'Small'} items={items} />
-      <Dropdown value={'Norge'} size={'medium'} label={'Medium'} items={items} />
-      <Dropdown value={'Norge'} isDisabled={true} label={'Disabled'} items={items} />
-      <Dropdown value={'Norge'} isFullWidth label={'Full Width'} items={items} />
-      <Dropdown
-        value={'Norge'}
-        label={'Error State'}
-        items={items}
-        errorOptions={{
-          hasErrorPlaceholder: false,
-          isErrorState: true,
-        }}
-      />
-      <Dropdown
-        value={'Norge'}
-        label={'Error Message'}
-        items={items}
-        errorOptions={{
-          text: 'Error message',
-          isErrorState: true,
-        }}
-      />
-      <Dropdown value={'Norge'} label={'Land'} items={items} />
-    </div>,
-  );
-
-  const dropdowns = await page.getByRole('combobox').all();
-
-  await dropdowns[dropdowns.length - 1].click();
-  await percySnapshot(page, 'Dropdowns 2', { widths: [300], enableLayout: true });
+  await percySnapshot(page, 'Dropdowns');
 });
 
 test('should look opened with nested items', async ({ mount, page }) => {
@@ -114,5 +79,5 @@ test('should look opened with nested items', async ({ mount, page }) => {
   await page.getByRole('option', { name: /bergen/i }).click();
   await page.waitForTimeout(300);
 
-  await percySnapshot(page, 'Open Dropdown with nested items', { widths: [600, 1200] });
+  await percySnapshot(page, 'Open Dropdown with nested items');
 });
