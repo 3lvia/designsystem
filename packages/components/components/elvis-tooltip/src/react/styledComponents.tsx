01874bc85ae0d7a83f5c386d2b00ad106db351da
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2024-05-13 22:53:25 -0700

    refactor(tooltip): use @floating-ui (#2825)
    * refactor(tooltip): use @floating-ui

--- packages/components/components/elvis-tooltip/src/react/styledComponents.tsx
+++ packages/components/components/elvis-tooltip/src/react/styledComponents.tsx
@@ -3,3 +3,7 @@ import styled from 'styled-components';
 export const TriggerContainer = styled.span`
   display: inline-block;
 `;
+
+export const TooltipContent = styled.div`
+  display: contents;
+`;
