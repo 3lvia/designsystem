7340a61906893062f2620e6a195f77bcf6a0deb6
Erik Tallang <e.tallang@gmail.com>
2024-05-15 14:22:05 +0200

    fix(overlay): use proper pointer-events value

--- packages/components/components/elvis-toolbox/src/components/overlay/overlayStyles.ts
+++ packages/components/components/elvis-toolbox/src/components/overlay/overlayStyles.ts
@@ -50,17 +50,14 @@ export const OverlayContainer = styled.div<{ fadeOut: boolean; noAnimation: bool
     css`
       position: fixed;
       max-width: unset;
-      top: 50%;
-      left: 50%;
-      translate: -50% -50%;
-      /* display: grid;
+      display: grid;
       place-content: center;
       inset: 0;
       pointer-events: none; // Allow clicks to pass through, to the backdrop
 
       & > * {
-        pointer-events: all;
-      } */
+        pointer-events: auto;
+      }
     `}
 `;
 
