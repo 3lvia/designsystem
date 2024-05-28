20fe1714703b9de078498987de9a6fe162dadc5d
Maja B. Nilsen <111271903+majabnils@users.noreply.github.com>
2024-05-28 13:50:37 +0200

    fix(accordion): remove spacing below closed accordion (#2879)

--- packages/components/components/elvis-accordion/src/react/styledComponents.tsx
+++ packages/components/components/elvis-accordion/src/react/styledComponents.tsx
@@ -11,7 +11,6 @@ import {
 } from './elvia-accordion.types';
 
 interface AccordionAreaProps {
-  normalSpacing?: AccordionSpacingContent;
   overflowSpacing?: AccordionSpacingContent;
   isOverflow: boolean;
 }
@@ -19,11 +18,11 @@ interface AccordionAreaProps {
 export const AccordionArea = styled.div<AccordionAreaProps>`
   display: flex;
   flex-direction: ${({ isOverflow }) => (isOverflow ? 'column-reverse' : 'column')};
-  gap: ${({ normalSpacing, overflowSpacing, isOverflow }) => {
+  gap: ${({ overflowSpacing, isOverflow }) => {
     if (isOverflow) {
       return overflowSpacing ?? '8px';
     } else {
-      return normalSpacing ?? '8px';
+      return '0';
     }
   }};
   width: 100%;
@@ -97,7 +96,8 @@ const decideTypography = (size: AccordionSize) => {
     case 'small':
       return css`
         ${getTypographyCss('text-interactive-sm')}
-        line-height: 16px;
+        line-height: 100%;
+        height: 16px;
       `;
     case 'large':
       return css`
@@ -133,7 +133,10 @@ const decideDetailTextSize = (size: AccordionSize): string => {
   }
 };
 
-export const AccordionDetailText = styled.div<{ size: AccordionSize; openDetailText: string | undefined }>`
+export const AccordionDetailText = styled.div<{
+  size: AccordionSize;
+  openDetailText: string | undefined;
+}>`
   ${({ size }) => decideDetailTextSize(size)};
   display: flex;
   text-align: left;
@@ -141,8 +144,12 @@ export const AccordionDetailText = styled.div<{ size: AccordionSize; openDetailT
   margin-left: ${({ openDetailText }) => (openDetailText !== undefined ? '8px;' : '0;')};
 `;
 
-export const AccordionContent = styled.div`
+export const AccordionContent = styled.div<{
+  normalSpacing?: AccordionSpacingContent;
+  isOverflow: boolean;
+}>`
   overflow-x: auto;
+  padding-top: ${({ isOverflow, normalSpacing }) => (normalSpacing && !isOverflow ? normalSpacing : '0')};
 `;
 
 const decideContentTransitionDuration = (contentHeight: number): string => {
