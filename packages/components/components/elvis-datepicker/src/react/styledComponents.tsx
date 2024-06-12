cd5fedc81d1fd24a7399ea89bfa9150090116e0f
Erik Tallang <e.tallang@gmail.com>
2024-06-12 10:54:36 +0200

    fix(datepicker): form field modifiers not propagated correctly

--- packages/components/components/elvis-datepicker/src/react/styledComponents.tsx
+++
@@ -1,8 +0,0 @@
-import { FormFieldInput } from '@elvia/elvis-toolbox';
-import styled from '@emotion/styled';
-
-export const Input = styled(FormFieldInput, {
-  target: `${FormFieldInput}`.replace(/\./g, ''),
-})`
-  width: 5.4rem;
-`;
