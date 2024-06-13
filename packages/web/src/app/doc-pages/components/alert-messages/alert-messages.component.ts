6f0b6938380a10461d407c76b067c2c86e4bb5d0
Maja B. Nilsen <111271903+majabnils@users.noreply.github.com>
2024-06-13 15:49:02 +0200

    Lego 3506 global alert (#2922)
    * feat(alert): new design of global alert
    
    * fix(button): use transparent background on secondary button
    
    * docs(alert): update illustrations for alert
    
    * chore(elvis): update changelog and bump package
    
    * fix(alert): remove commented code
    
    * fix(alert): put back removed code
    
    * fix(alert): small fixes
    
    * docs(alert): remove change
    
    * fix(alert): use text-1 as color
    
    * fix(alert): change gap to 16px between action buttons on desktop
    
    * chore(alert): update changelog
    
    * fix(alert): update gulpfile
    
    * fix(alert): add padding
    
    * fix(alert): remove comments

--- packages/web/src/app/doc-pages/components/alert-messages/alert-messages.component.ts
+++ packages/web/src/app/doc-pages/components/alert-messages/alert-messages.component.ts
@@ -11,7 +11,6 @@ import { AlertCegComponent } from './alert-ceg/alert-ceg.component';
 import { AlertGlobalActionsCegComponent } from './alert-global-actions-ceg/alert-global-actions-ceg.component';
 import { AlertGlobalCegComponent } from './alert-global-ceg/alert-global-ceg.component';
 import { AlertGlobalClosableCegComponent } from './alert-global-closable-ceg/alert-global-closable-ceg.component';
-import { AlertGlobalExpandableCegComponent } from './alert-global-expandable-ceg/alert-global-expandable-ceg.component';
 import { AlertLocalCegComponent } from './alert-local-ceg/alert-local-ceg.component';
 import { AlertLocalClosableCegComponent } from './alert-local-closable-ceg/alert-local-closable-ceg.component';
 import { AlertRoleAlertCegComponent } from './alert-role-alert-ceg/alert-role-alert-ceg.component';
@@ -33,7 +32,6 @@ import { AlertRoleStatusCegComponent } from './alert-role-status-ceg/alert-role-
     WhenToUseComponent,
     ComponentSubsubsectionComponent,
     AlertGlobalClosableCegComponent,
-    AlertGlobalExpandableCegComponent,
     AlertGlobalActionsCegComponent,
     AlertLocalCegComponent,
     AlertLocalClosableCegComponent,
