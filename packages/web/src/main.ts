9e2b8065f994690a4fb901fbb84308c369e00964
Trygve Scheline Urdahl <trur@computas.com>
2024-06-25 09:05:36 +0200

    feat(icons): new component POC

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -3,12 +3,20 @@ import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
+import { bookmark, bookmarkFilled, han } from '@elvia/elvis-assets-icons';
+import { add } from '@elvia/elvis-icons';
 import '@elvia/elvis/checkDeprecations';
 import '@elvia/elvis/elvis.js';
 
 import { routes } from './app/app-routing';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+add({
+  bookmark: { svg: bookmark.getIcon() },
+  bookmarkFilled: { svg: bookmarkFilled.getIcon() },
+  han: { svg: han.getIcon() },
+});
 
 if (environment.production) {
   enableProdMode();
