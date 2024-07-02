d025772042c01424a9439ed308764ee8fd2486bd
Trygve Scheline Urdahl <trygve.urdahl@hotmail.com>
2024-07-02 02:21:11 -0700

    test(card, dropdown, timepicker): add percy tests (#2992)
    * test(card): add percy tests, remove bad jest tests
    
    * test(dropdown): add percy tests, remove bad jest tests
    
    * test(timepicker): add percy tests

---
+++ packages/components/components/elvis-card/src/react/elvia-card.spec.tsx
@@ -0,0 +1,34 @@
+import percySnapshot from '@percy/playwright';
+import { expect, test } from '@playwright/experimental-ct-react';
+import React from 'react';
+
+import { Card } from '../../react';
+
+test('should render', async ({ mount }) => {
+  const component = await mount(<Card />);
+  await expect(component).toBeAttached();
+});
+
+test('cards', async ({ mount, page }) => {
+  await mount(
+    <div style={{ display: 'flex', gap: '16px', flexDirection: 'row', flexWrap: 'wrap' }}>
+      <Card heading="Forbruk" description="Kort description" icon="Icon" />
+      <Card type="simple" borderColor="green" heading="Forbruk" description="Kort description" icon="Icon" />
+      <Card type="simple" borderColor="orange" heading="Forbruk" description="Kort description" icon="Icon" />
+      <Card type="simple" borderColor="red" heading="Forbruk" description="Kort description" icon="Icon" />
+      <Card
+        type="detail"
+        heading="Varsler ved lynnedslag"
+        description="Data om prognoser ved lyn og hvordan dette påvirker strømnettet."
+        tag="Sikkerhet"
+      />
+      <Card
+        type="detail"
+        heading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi perferendis, voluptate culpa quas earum fuga obcaecati voluptas officia cum."
+        description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam sequi assumenda itaque dignissimos porro, minima minus saepe earum totam! Blanditiis quisquam eligendi laborum nobis ullam reprehenderit quos id. Necessitatibus voluptatem quaerat, nihil aliquid sit quos fuga deleniti qui numquam ducimus laudantium voluptatibus molestiae architecto et soluta porro earum possimus natus cupiditate quas cumque illo alias eius. In pariatur laborum tenetur possimus corporis, dolorum incidunt qui ipsum quos harum. Nihil itaque aperiam perferendis praesentium, harum accusantium voluptate rem, non sequi earum officia quae molestias accusamus! Consectetur nostrum facere odit reiciendis pariatur, cumque illo ducimus accusamus commodi! Eos ea error beatae magni."
+        tag="Sikkerhet"
+      />
+    </div>,
+  );
+  await percySnapshot(page, 'Cards');
+});
--- packages/components/components/elvis-card/src/react/elvia-card.test.tsx
+++ packages/components/components/elvis-card/src/react/elvia-card.test.tsx
@@ -1,4 +1,3 @@
-import { getThemeColor } from '@elvia/elvis-colors';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { axe } from 'jest-axe';
@@ -27,11 +26,6 @@ describe('Elvis Card', () => {
       expect(description).toBeInTheDocument();
     });
 
-    it('should not have top border with color', () => {
-      const cardColoredLine = screen.queryByTestId('card-colored-line');
-      expect(cardColoredLine).toHaveStyle(`border-top: 4px solid transparent`);
-    });
-
     it('should not have corner icon', () => {
       const cornerIcon = screen.queryByTestId('card-corner-icon');
       expect(cornerIcon).not.toBeInTheDocument();
@@ -55,77 +49,6 @@ describe('Elvis Card', () => {
       await user.hover(screen.getByRole('article'));
       expect(cardIcon).toHaveTextContent('Icon');
     });
-
-    it('should have default width', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`width: 100%`);
-    });
-
-    it('should have default minWidth', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`min-width: 150px`);
-    });
-
-    it('should have default maxWidth', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`max-width: 250px`);
-    });
-  });
-
-  describe('Type = simple square with width and colored top', () => {
-    beforeEach(() => {
-      render(
-        <Card
-          icon={'Icon'}
-          heading={'Heading'}
-          description={'Description'}
-          borderColor={'red'}
-          tag={'Tag'}
-          width={'150px'}
-          minWidth={200}
-          maxWidth={220}
-        />,
-      );
-    });
-
-    it('should have top border with color', () => {
-      const coloredLine = screen.getByTestId('card-colored-line');
-      expect(coloredLine).toHaveStyle(`border-top: 4px solid ${getThemeColor('signal-danger')}`);
-    });
-
-    it('should have a set width', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`width: 150px`);
-    });
-
-    it('should have minWidth', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`min-width: 200px`);
-    });
-
-    it('should have maxWidth', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`max-width: 220px`);
-    });
-  });
-
-  describe.skip('Type = simple square, with hover icon', () => {
-    beforeEach(() => {
-      render(<Card icon={'Icon'} iconHover={'IconHover'} />);
-    });
-
-    it('should switch icon on hover', async () => {
-      const user = userEvent.setup();
-      const cardArea = screen.getByRole('article');
-      const cardIcon = screen.getByTestId('card-icon');
-      expect(cardIcon).toHaveTextContent('Icon');
-
-      await user.hover(cardArea);
-      expect(cardIcon).toHaveTextContent('IconHover');
-
-      await user.unhover(cardArea);
-      expect(cardIcon).toHaveTextContent('Icon');
-    });
   });
 
   describe('Type = detail', () => {
@@ -140,14 +63,9 @@ describe('Elvis Card', () => {
       expect(cardIcon).not.toBeInTheDocument();
     });
 
-    it('should have label', () => {
+    it('should have tag', () => {
       const tag = screen.getByText('Tag');
       expect(tag).toBeInTheDocument();
-    });
-
-    it('should have shape square', () => {
-      const cardArea = screen.getByRole('article');
-      expect(cardArea).toHaveStyle(`border-radius: 8px`);
     });
 
     it('should have corner icon', () => {
@@ -158,27 +76,6 @@ describe('Elvis Card', () => {
     it('should have hover arrow', () => {
       const hoverArrow = screen.getByTestId('card-detail-hover-arrow');
       expect(hoverArrow).toBeInTheDocument();
-    });
-
-    it('should have default lines of description text', () => {
-      const description = screen.getByText('Description');
-      expect(description).toHaveStyle(`line-clamp: 3;`);
-    });
-  });
-
-  describe('Type = detail, shorter description', () => {
-    beforeEach(() => {
-      render(<Card type={'detail'} tag={'Tag'} description={'Description'} maxDescriptionLines={4} />);
-    });
-
-    it('should have max 4 lines of description text', () => {
-      const description = screen.getByText('Description');
-      expect(description).toHaveStyle(`line-clamp: 4;`);
-    });
-
-    it('should have empty corner icon', () => {
-      const cornerIcon = screen.getByTestId('card-corner-icon');
-      expect(cornerIcon).toHaveTextContent('');
     });
   });
 
