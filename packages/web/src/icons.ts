36c246d139b239e4c1f199be8e6c1cf2d8318a1f
Trygve Scheline Urdahl <trur@computas.com>
2024-08-26 09:46:24 +0200

    refactor(web): automatically import all icons from assets package

--- packages/web/src/icons.ts
+++ packages/web/src/icons.ts
@@ -1,36 +1,32 @@
-import {
-  bookmark,
-  bookmarkFilled,
-  dashboard,
-  dashboardColor,
-  download,
-  fullBattery,
-  fullBatteryColor,
-  han,
-  informationCircle,
-  informationCircleFilledColor,
-  removeCircle,
-  search,
-  searchColor,
-  upload,
-  viewOn,
-} from '@elvia/elvis-assets-icons';
+import * as icons from '@elvia/elvis-assets-icons';
 import { addIcons } from '@elvia/elvis-icons';
 
-addIcons({
-  bookmark: { svg: bookmark.getIcon() },
-  bookmarkFilled: { svg: bookmarkFilled.getIcon() },
-  han: { svg: han.getIcon() },
-  download: { svg: download.getIcon() },
-  upload: { svg: upload.getIcon() },
-  removeCircle: { svg: removeCircle.getIcon() },
-  informationCircle: { svg: informationCircle.getIcon() },
-  informationCircleFilledColor: { svg: informationCircleFilledColor.getIcon() },
-  viewOn: { svg: viewOn.getIcon() },
-  search: { svg: search.getIcon() },
-  searchColor: { svg: searchColor.getIcon() },
-  fullBattery: { svg: fullBattery.getIcon() },
-  fullBatteryColor: { svg: fullBatteryColor.getIcon() },
-  dashboard: { svg: dashboard.getIcon() },
-  dashboardColor: { svg: dashboardColor.getIcon() },
-});
+// This adds all the icons from the icons package, with the names from the package
+const iconObj = Object.entries(icons).reduce(
+  (acc, [name, icon]) => {
+    acc[name] = { svg: icon.getIcon() };
+    return acc;
+  },
+  {} as Parameters<typeof addIcons>[0],
+);
+
+addIcons(iconObj);
+
+// This way to add single icons
+// addIcons({
+//   bookmark: { svg: bookmark.getIcon() },
+//   bookmarkFilled: { svg: bookmarkFilled.getIcon() },
+//   han: { svg: han.getIcon() },
+//   download: { svg: download.getIcon() },
+//   upload: { svg: upload.getIcon() },
+//   removeCircle: { svg: removeCircle.getIcon() },
+//   informationCircle: { svg: informationCircle.getIcon() },
+//   informationCircleFilledColor: { svg: informationCircleFilledColor.getIcon() },
+//   viewOn: { svg: viewOn.getIcon() },
+//   search: { svg: search.getIcon() },
+//   searchColor: { svg: searchColor.getIcon() },
+//   fullBattery: { svg: fullBattery.getIcon() },
+//   fullBatteryColor: { svg: fullBatteryColor.getIcon() },
+//   dashboard: { svg: dashboard.getIcon() },
+//   dashboardColor: { svg: dashboardColor.getIcon() },
+// });
