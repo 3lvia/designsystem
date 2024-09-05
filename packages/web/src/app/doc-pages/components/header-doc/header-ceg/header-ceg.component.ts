a2d5e1e17b7706bd26278c99bcb3ec76ba3b6576
Trygve Scheline Urdahl <trur@computas.com>
2024-09-05 09:34:11 +0200

    fix(header-ceg): react code example

--- packages/web/src/app/doc-pages/components/header-doc/header-ceg/header-ceg.component.ts
+++ packages/web/src/app/doc-pages/components/header-doc/header-ceg/header-ceg.component.ts
@@ -27,6 +27,32 @@ export class HeaderCegComponent implements ComponentExample {
     },
   ]);
 
+  reactSlotReplacement = {
+    navItems: `<div className="e-sidenav__container">
+    <a aria-label="Dashbord" className="e-sidenav__item e-sidenav__item--active">
+      <div className="e-sidenav__icon-container">
+        <Icon name="dashboard" size="sm"></Icon>
+        <Icon name="dashboardColor" size="sm"></Icon>
+      </div>
+      <div className="e-sidenav__item-text">Dashbord</div>
+    </a>
+    <a aria-label="Søk" className="e-sidenav__item">
+      <div className="e-sidenav__icon-container">
+        <Icon name="search" size="sm"></Icon>
+        <Icon name="searchColor" size="sm"></Icon>
+      </div>
+      <div className="e-sidenav__item-text">Søk</div>
+    </a>
+    <a aria-label="Analyse" className="e-sidenav__item">
+      <div className="e-sidenav__icon-container">
+        <Icon name="fullBattery" size="sm"></Icon>
+        <Icon name="fullBatteryColor" size="sm"></Icon>
+      </div>
+      <div className="e-sidenav__item-text">Analyse</div>
+    </a>
+  </div>`,
+  };
+
   toggleHeader() {
     this.headerIsVisible = !this.headerIsVisible;
     const externalHeader = document.getElementById('header');
