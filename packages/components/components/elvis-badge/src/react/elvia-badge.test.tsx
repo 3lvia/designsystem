13da5ccefdcff66479e6d6b5ac28a67dca93657c
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-09-18 10:24:02 +0200

    Update elvia-badge.test.tsx

--- packages/components/components/elvis-badge/src/react/elvia-badge.test.tsx
+++ packages/components/components/elvis-badge/src/react/elvia-badge.test.tsx
@@ -93,7 +93,7 @@ describe('Elvis Badge', () => {
     });
 
     it('should have a red contrast text color', () => {
-      const badgeCircle = screen.getByRole('status', { hidden: true });
+      const badgeCircle = screen.getByRole('status');
       expect(badgeCircle).toHaveClass('badge--red');
     });
   });
@@ -114,7 +114,7 @@ describe('Elvis Badge', () => {
     });
 
     it('should not be visible', () => {
-      const badgeCircle = screen.getByRole('status');
+      const badgeCircle = screen.getByRole('status', { hidden: true });
       expect(badgeCircle).toHaveClass('badge--hidden');
     });
   });
