faeda839ee0a7551f452ff6dc6d172383063f5a7
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-09-30 08:44:04 +0200

    Revert "Update elvia-slider.spec.tsx"
    This reverts commit e1f848fbab035ec0fbf61079b818a6bdbff122ae.

--- packages/components/components/elvis-slider/src/react/elvia-slider.spec.tsx
+++ packages/components/components/elvis-slider/src/react/elvia-slider.spec.tsx
@@ -15,52 +15,38 @@ const renderSliders = (type: SliderType, size: FormFieldSizes) => (
     <Slider type={type} size={size} label={'Label'} hasInputField={false} />
     <Slider type={type} size={size} label={'Label'} isDisabled />
     {type === 'range' && (
-      <>
-        <Slider
-          type={type}
-          size={size}
-          label={'Label'}
-          value={{
-            left: 20,
-            right: 80,
-          }}
-        />
-        <Slider
-          type={type}
-          size={size}
-          label={'Label'}
-          errorOptions={{
-            left: {
-              text: 'Left error text',
-              hideText: false,
-              isErrorState: true,
-              hasErrorPlaceholder: true,
-            },
-            right: {
-              text: 'Right error text',
-              hideText: false,
-              isErrorState: true,
-              hasErrorPlaceholder: true,
-            },
-          }}
-        />
-      </>
-    )}
-    {type !== 'range' && (
-      <>
-        <Slider type={type} size={size} label={'Label'} value={50} />
-        <Slider
-          type={type}
-          size={size}
-          label={'Label'}
-          errorOptions={{
-            text: 'Error message',
+      <Slider
+        type={type}
+        size={size}
+        label={'Label'}
+        errorOptions={{
+          left: {
+            text: 'Left error text',
             hideText: false,
             isErrorState: true,
             hasErrorPlaceholder: true,
-          }}
-        />
-      </>
+          },
+          right: {
+            text: 'Right error text',
+            hideText: false,
+            isErrorState: true,
+            hasErrorPlaceholder: true,
+          },
+        }}
+      />
+    )}
+    {type !== 'range' && (
+      <Slider
+        type={type}
+        size={size}
+        label={'Label'}
+        errorOptions={{
+          text: 'Error message',
+          hideText: false,
+          isErrorState: true,
+          hasErrorPlaceholder: true,
+        }}
+      />
     )}
   </div>
 );
@@ -74,26 +60,20 @@ test('should look as expected (simple, medium)', async ({ mount, page }) => {
   await mount(renderSliders('simple', 'medium'));
   setTimeout(async () => {
     await percySnapshot(page, 'Slider: simple (medium)');
-  }, 500); //timeout to wait for component to finish rendering
+  }, 500);
 });
 
 test('should look as expected (simple, small)', async ({ mount, page }) => {
   await mount(renderSliders('simple', 'small'));
-  setTimeout(async () => {
-    await percySnapshot(page, 'Slider: simple (small)');
-  }, 500);
+  await percySnapshot(page, 'Slider: simple (small)');
 });
 
 test('should look as expected (range, medium)', async ({ mount, page }) => {
   await mount(renderSliders('range', 'medium'));
-  setTimeout(async () => {
-    await percySnapshot(page, 'Slider: range (medium)');
-  }, 500);
+  await percySnapshot(page, 'Slider: range (medium)');
 });
 
 test('should look as expected (range, small)', async ({ mount, page }) => {
   await mount(renderSliders('range', 'small'));
-  setTimeout(async () => {
-    await percySnapshot(page, 'Slider: range (small)');
-  }, 500);
+  await percySnapshot(page, 'Slider: range (small)');
 });
