5fd0c28c076ad75c2fe38551d3f99c7a3fb0eb2a
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2024-12-10 14:08:39 +0100

    test

--- packages/components/components/elvis-context-menu/src/react/elvia-context-menu.spec.tsx
+++ packages/components/components/elvis-context-menu/src/react/elvia-context-menu.spec.tsx
@@ -14,6 +14,7 @@ test('should render', async ({ mount }) => {
 });
 
 test('should look as expected (default)', async ({ mount, page }) => {
+  await page.waitForTimeout(1000);
   const component = await mount(
     <ContextMenu
       trigger={<button className="e-btn">Open menu</button>}
@@ -31,8 +32,9 @@ test('should look as expected (default)', async ({ mount, page }) => {
       }
     />,
   );
-
+  await page.waitForTimeout(1000);
   await component.getByRole('button').click();
+  await page.waitForTimeout(1000);
 
   await percySnapshot(page, 'Context menu: open');
 });
