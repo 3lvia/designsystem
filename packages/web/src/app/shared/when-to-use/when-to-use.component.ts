3e778a1973477d2e83440ba31a5b09813fa2e5f0
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-01-13 22:48:19 +0100

    Update when-to-use.component.ts

--- packages/web/src/app/shared/when-to-use/when-to-use.component.ts
+++ packages/web/src/app/shared/when-to-use/when-to-use.component.ts
@@ -10,7 +10,7 @@ import { Locale } from 'src/app/core/services/localization.service';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class WhenToUseComponent {
-  whens = input<string[]>([]);
-  whenNots = input<string[]>([]);
-  locale = input<Locale>('en-GB');
+  readonly whens = input<string[]>([]);
+  readonly whenNots = input<string[]>([]);
+  readonly locale = input<Locale>('en-GB');
 }
