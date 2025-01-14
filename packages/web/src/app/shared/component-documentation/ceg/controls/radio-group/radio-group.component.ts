084c49d78c5d2c4c2fc93aa31d1d3860fe8632f6
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-01-14 08:43:40 +0100

    LEGO-3677 refactor(radio-group): migrate to signal input

--- packages/web/src/app/shared/component-documentation/ceg/controls/radio-group/radio-group.component.ts
+++ packages/web/src/app/shared/component-documentation/ceg/controls/radio-group/radio-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output, input } from '@angular/core';
 
 import { RadioGroup } from '../../controlType';
 
@@ -7,10 +7,10 @@ let CEG_RADIO_GROUP_ID = 0;
 @Component({
   selector: 'app-radio-group',
   templateUrl: './radio-group.component.html',
-  imports: [],
 })
 export class RadioGroupComponent {
-  @Input() radioGroup: RadioGroup;
+  readonly radioGroup = input.required<RadioGroup>();
+  // eslint-disable-next-line @angular-eslint/no-output-native
   @Output() toggle = new EventEmitter<RadioGroup['value']>();
   readonly radioGroupId = `ceg-checkbox-${CEG_RADIO_GROUP_ID++}`;
 
