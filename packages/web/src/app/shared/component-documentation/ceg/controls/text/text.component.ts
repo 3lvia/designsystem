16a5b9a0326eaf820922234be7971fb50c330652
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-01-14 08:36:55 +0100

    LEGO-3677 refactor(ceg text): migrate to signal input

--- packages/web/src/app/shared/component-documentation/ceg/controls/text/text.component.ts
+++ packages/web/src/app/shared/component-documentation/ceg/controls/text/text.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output, computed, input } from '@angular/core';
 
 import { Text } from '../../controlType';
 
@@ -12,14 +12,12 @@ let CEG_INPUT_ID = 0;
   imports: [NgClass],
 })
 export class TextComponent {
-  @Input() input: Text;
-  @Input() disabled: boolean;
+  readonly input = input.required<Text>();
+  readonly disabled = input<boolean>();
   @Output() valueChange = new EventEmitter<Text['value']>();
   readonly inputId = `ceg-input-${CEG_INPUT_ID++}`;
 
-  get textValue(): string | null {
-    return this.input.value ?? null;
-  }
+  textValue = computed<string | null>(() => this.input().value ?? null);
 
   onChange(newValue: Text['value']): void {
     this.valueChange.emit(newValue);
