ab28a8665726266c71bf03480578986519f958e7
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-03-21 21:24:38 +0100

    LEGO-3380 Create designsystem-slack-link.component.ts

---
+++ packages/web/src/app/shared/designsystem-slack-link/designsystem-slack-link.component.ts
@@ -0,0 +1,23 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designsystem-slack-link',
+  template: `<a
+    class="e-link e-link--inline e-link--new-tab"
+    target="_blank"
+    rel="noopener"
+    href="https://elvia-group.slack.com/archives/C01C1DU9X1Q"
+  >
+    <span class="e-link__title">#designsystemet</span>
+    <span class="e-link__icon">
+      <e-icon name="newTabBold"> </e-icon>
+    </span>
+  </a>`,
+  styles: `
+    :host {
+      display: contents;
+    }
+  `,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class DesignsystemSlackLinkComponent {}
