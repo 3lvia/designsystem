6f15899c835aa71d561ce29abb16499170dd27c4
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-03-21 11:16:11 +0100

    LEGO-3698 delete feedback component (#3555)

--- packages/web/src/app/shared/feedback/emoji-neutral/emoji-neutral.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-emoji-neutral',
-  templateUrl: './emoji-neutral.component.html',
-  standalone: true,
-})
-export class EmojiNeutralComponent {}
