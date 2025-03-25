3d26502032234be53ad90cac303d1c5c5c228ad1
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-03-25 20:20:17 +0100

    LEGO-3380 qa on pattern docs

--- packages/web/src/app/doc-pages/patterns/cookies-doc/cookies-doc.component.ts
+++ packages/web/src/app/doc-pages/patterns/cookies-doc/cookies-doc.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { ThemeService } from 'src/app/core/services/theme.service';
 import { ComponentHeaderComponent } from 'src/app/shared/component-documentation/component-structure/component-header/component-header.component';
 import { ComponentSectionComponent } from 'src/app/shared/component-documentation/component-structure/component-section/component-section.component';
 import { getDocPage } from 'src/app/shared/doc-pages';
@@ -12,6 +13,8 @@ const docPage = getDocPage('cookies');
   templateUrl: './cookies-doc.component.html',
 })
 export class CookiesDocComponent {
+  protected theme = inject(ThemeService).themeSignal;
+
   title = docPage.title;
   description = docPage.description;
   figmaUrl = docPage.figmaUrl;
