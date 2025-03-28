ef1199b1c5473bc3fadad1caa2d9b9138175cdb4
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-03-28 10:11:23 +0100

    fix(component-related) add missing `InlineSvgComponent` import

--- packages/web/src/app/shared/component-documentation/component-related/component-related.component.ts
+++ packages/web/src/app/shared/component-documentation/component-related/component-related.component.ts
@@ -2,13 +2,14 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { DocPagePipe } from '../../../doc-pages/components/docPage.pipe';
+import { InlineSvgComponent } from '../../inline-svg/inline-svg.component';
 import { DocPageName } from '../../shared.enum';
 
 @Component({
   selector: 'app-component-related',
   templateUrl: './component-related.component.html',
   styleUrls: ['./component-related.component.scss'],
-  imports: [RouterLink, DocPagePipe],
+  imports: [RouterLink, DocPagePipe, InlineSvgComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class ComponentRelatedComponent {
