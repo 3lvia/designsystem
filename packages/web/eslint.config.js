16453f95278367193e9080216a5c7cc151a83f40
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2025-05-27 11:54:39 +0200

    add `@angular-eslint/template/prefer-at-empty` (#3700)

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -97,9 +97,10 @@ module.exports = tseslint.config(
   {
     files: ['**/*.html'],
     rules: {
+      '@angular-eslint/template/prefer-at-empty': 'warn',
       '@angular-eslint/template/prefer-self-closing-tags': 'error',
-      '@angular-eslint/template/prefer-template-literal': 'error',
       '@angular-eslint/template/prefer-static-string-properties': 'error',
+      '@angular-eslint/template/prefer-template-literal': 'error',
     },
     ignores: ['**/*-ceg*.html'],
   },
