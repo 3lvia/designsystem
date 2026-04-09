a5bf6650fe0bc76875ad78be7a60417df7f04758
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2026-04-09 09:10:33 +0200

    LEGO-3761 fix(tutorial): incorrect file paths and step order

--- packages/web/src/app/doc-pages/about/tutorial/react-tutorial/react-tutorial.component.ts
+++ packages/web/src/app/doc-pages/about/tutorial/react-tutorial/react-tutorial.component.ts
@@ -10,13 +10,12 @@ import { CodeViewerComponent } from 'src/app/shared/component-documentation/ceg/
 })
 export class ReactTutorialComponent {
   codeStep1 = `npm install @elvia/elvis`;
-  codeStep2 = `import '@elvia/elvis/css/elvis.min.css';
-import '@elvia/elvis/elvis.js';`;
+  codeStep2 = `import '@elvia/elvis/css/elvis.min.css';`;
   codeStep3 = `<h1 className="e-title-lg">Elvia title</h1>
 <button className="e-btn">Elvia button</button>`;
-  codeStep4 = `import '@elvia/elvis/css-reset.min.css';`;
+  codeStep4 = `import '@elvia/elvis/css/css-reset.css';`;
   codeStepAdvanced1 = `npm install @elvia/elvis-datepicker`;
-  codeStepAdvanced2 = `import { Datepicker } from '@elvia/elvis-datepicker/react;'`;
+  codeStepAdvanced2 = `import { Datepicker } from '@elvia/elvis-datepicker/react';`;
   codeStepAdvanced3 = `<Datepicker></Datepicker>`;
   codeStepAdvanced4 = `<Datepicker isFullWidth={true} valueOnChange={newDate => handleOnChange(newDate)}
 ></Datepicker>`;
--- packages/web/src/app/doc-pages/about/tutorial/vue-tutorial/vue-tutorial.component.ts
+++ packages/web/src/app/doc-pages/about/tutorial/vue-tutorial/vue-tutorial.component.ts
@@ -10,16 +10,15 @@ import { CodeViewerComponent } from 'src/app/shared/component-documentation/ceg/
 })
 export class VueTutorialComponent {
   codeStep1 = `npm install @elvia/elvis`;
-  codeStep2 = `import '@elvia/elvis/css/elvis.min.css';
-import '@elvia/elvis/elvis.js';`;
+  codeStep2 = `import '@elvia/elvis/css/elvis.min.css';`;
   codeStep3 = `<h1 class="e-title-lg">Elvia title</h1>
 <button class="e-btn">Elvia button</button>`;
-  codeStep4 = `import '@elvia/elvis/css-reset.min.css';`;
-  codeStepAdvanced1 = ` npm install @elvia/elvis-datepicker`;
-  codeStepAdvanced2 = `import '@elvia/elvis-datepicker;'`;
+  codeStep4 = `import '@elvia/elvis/css/css-reset.css';`;
+  codeStepAdvanced1 = `npm install @elvia/elvis-datepicker`;
+  codeStepAdvanced2 = `import '@elvia/elvis-datepicker';`;
   codeStepAdvanced3 = `<elvia-datepicker></elvia-datepicker>`;
   codeStepAdvanced4 = `<elvia-datepicker 
-    [isFullWidth]="true"
-    (valueOnChange)="handleOnChange($event.detail.value)"
+    :isFullWidth="true"
+    @valueOnChange="handleOnChange($event.detail.value)"
   ></elvia-datepicker>`;
 }
